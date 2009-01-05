692f652ecf622c9a828a42f6b8a81a96a335a194
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-01-05 21:21:27 +0000

    added new framework to handle pde systems
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5180 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ life/lifesystem/systemexplicit.hpp
@@ -0,0 +1,116 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2009-01-04
+
+  Copyright (C) 2009 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file systemexplicit.hpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2009-01-04
+ */
+#ifndef __SystemExplicit_H
+#define __SystemExplicit_H 1
+
+#include <life/lifesystem/system.hpp>
+
+namespace Life
+{
+/**
+ * \class SystemExplicit
+ * \brief describes an explicit system
+ *
+ * @author Christophe Prud'homme
+ * @see
+ */
+template<typename SpaceType>
+class SystemExplicit : public System<SpaceType>
+{
+    typedef System<SpaceType> super;
+public:
+
+
+    /** @name Constants
+     */
+    //@{
+
+    static const uint16_type Dim = super::Dim;
+
+    //@}
+
+    /** @name Typedefs
+     */
+    //@{
+
+    typedef SystemExplicit<SpaceType> system_type;
+
+    typedef typename super::value_type value_type;
+    typedef typename super::functionspace_type functionspace_type;
+    typedef typename super::functionspace_type functionspace_ptrtype;
+    typedef typename super::element_type element_type;
+    //@}
+
+    /** @name Constructors, destructor
+     */
+    //@{
+
+    SystemExplicit( functionspace_ptrtype Xh, po::variables_map const& vm ) : super( Xh, vm ) {}
+    SystemExplicit( SystemExplicit const & se ) : super( se ) {}
+    ~SystemExplicit() {}
+
+    //@}
+
+    /** @name Operator overloads
+     */
+    //@{
+
+
+    //@}
+
+    /** @name Accessors
+     */
+    //@{
+
+
+    //@}
+
+    /** @name  Mutators
+     */
+    //@{
+
+
+    //@}
+
+    /** @name  Methods
+     */
+    //@{
+
+
+    //@}
+
+
+
+protected:
+
+private:
+
+};
+} // Life
+#endif /* __SystemExplicit_H */
