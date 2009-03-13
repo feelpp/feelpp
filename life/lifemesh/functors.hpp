0a05f6f19fc03330e0bf4f3060614197587d09b6
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-03-13 10:28:51 +0000

    don't override face marker if set to a non-zero value
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5290 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- life/lifemesh/functors.hpp
+++ life/lifemesh/functors.hpp
@@ -174,7 +174,8 @@ struct UpdateFaceConnection1
     {
         //element.setOnBoundary( false );
         element.setConnection1( M_conn );
-        if ( element.element0().marker() == element.element1().marker() )
+        if ( ( element.element0().marker() == element.element1().marker() ) &&
+             element.marker().value() == 0 )
             element.setMarker( element.element0().marker().value() );
     }
 private:
