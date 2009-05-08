cc35046fc4f1de9b731d9135f8ef6a83281573b4
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-05-08 14:03:00 +0000

    added lifematerial unit test
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5402 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ testsuite/lifematerial/test_material.cpp
@@ -0,0 +1,51 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2009-05-08
+
+  Copyright (C) 2009 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file test_material.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2009-05-08
+ */
+
+#define BOOST_TEST_MODULE material tests
+#include <boost/test/unit_test.hpp>
+using boost::unit_test::test_suite;
+
+
+#include <life/lifematerial/materiallib.hpp>
+using namespace Life;
+
+
+BOOST_AUTO_TEST_SUITE( material_testsuite )
+
+BOOST_AUTO_TEST_CASE( test_air )
+{
+    MaterialLib lib;
+    material_ptrtype m = MaterialLib::material( "Air" );
+    BOOST_CHECK_CLOSE( m->rho(), 1.2, 1e-15 );
+    BOOST_CHECK_CLOSE( m->nu(), 1.7*1e-5, 1e-15 );
+}
+
+BOOST_AUTO_TEST_SUITE_END() // material test suite
+
+
