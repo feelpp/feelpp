d3478f65da698f9a74b787bbd896b9c749238ad3
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-05-10 20:23:21 +0000

    forgot those ones
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5416 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ life/lifematerial/air.hpp
@@ -0,0 +1,130 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2008-05-25
+
+  Copyright (C) 2008 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file air.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2008-05-25
+ */
+#include <cmath>
+
+#include <boost/any.hpp>
+#include <map>
+#include <utility>
+
+#include <life/lifematerial/material.hpp>
+
+namespace Life
+{
+
+/**
+ * \class Air
+ *  \brief Air material
+ *
+ *  @author Christophe Prud'homme
+ * @see
+ */
+class Air : public Material
+{
+public:
+
+
+    /** @name Typedefs
+     */
+    //@{
+
+
+    //@}
+
+    /** @name Constructors, destructor
+     */
+    //@{
+
+    Air() : Material( "Air" ) {}
+    Air( Air const & m ): Material( m ) {}
+    ~Air() {}
+
+    //@}
+
+    /** @name Operator overloads
+     */
+    //@{
+
+
+    //@}
+
+    /** @name Accessors
+     */
+    //@{
+
+
+    //! thermal conductivity in \f$ W/(m*K) \f$
+    virtual double k() const
+    {
+        double T = 273; // K (default)
+        return pow(10,(0.8616*log10(abs(T))-3.7142));
+    }
+
+    //! density in \f$ kg/m^3 \f$
+    virtual double rho() const
+    {
+
+        return 1.2;
+    }
+
+    //! thermal capacity in \f$ J/(kg*K) \f$
+    virtual double C() const { double T=273; return 0.0769*T+1076.9; }
+
+    //! Poisson coefficient
+    virtual double nu() const { return 1.7*1e-5; }
+
+    //! Young modulus in \f$ Pa \f$
+    virtual double E() const { return -1; }
+
+
+    //@}
+
+    /** @name  Mutators
+     */
+    //@{
+
+
+    //@}
+
+    /** @name  Methods
+     */
+    //@{
+
+
+    //@}
+
+
+
+protected:
+
+private:
+
+};
+
+} // Life
+
