9ff89e73408c3397e89405e50296f2cf5f45d6cc
vielfaure <vielfaure@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-05-27 14:18:38 +0000

    add classes for non linear trilinos solver
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5441 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ life/lifealg/solvernonlineartrilinos.cpp
@@ -0,0 +1,94 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Florent Vielfaure <florent.vielfaure@gmail.com>
+       Date: 2009-05-25
+
+  Copyright (C) 2009 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file solvernonlineartrilinos.cpp
+   \author Florent Vielfaure <florent.vielfaure@gmail.com>
+   \date 2009-05-25
+ */
+
+#include <life/lifealg/glas.hpp>
+#include <life/lifealg/vectorpetsc.hpp>
+#include <life/lifealg/matrixpetsc.hpp>
+#include <life/lifealg/solvernonlineartrilinos.hpp>
+
+bool computeF( const Epetra_Vector & x, Epetra_Vector & f, NOX::Epetra::Interface::Required::FillType F ) {
+    return true;
+}
+bool computeJecobian( const Epetra_Vector & x, Epetra_Operator & Jac ) {
+    return true;
+}
+bool computePrecMatrix( const Epetra_Vector & x, Epetra_RowMatrix & M ) {
+    return true;
+}
+bool computePreconditioner( const Epetra_Vector & x, Epetra_Operator & O ) {
+    return true;
+}
+
+namespace Life
+{
+// SolverNonLinearTrilinos<> methods
+template <typename T>
+void SolverNonLinearTrilinos<T>::clear ()
+{
+
+}
+
+template <typename T>
+void SolverNonLinearTrilinos<T>::init ()
+{
+
+}
+
+template <typename T>
+std::pair<unsigned int, typename SolverNonLinearPetsc<T>::real_type>
+SolverNonLinearTrilinos<T>::solve ( sparse_matrix_ptrtype&  jac_in,  // System Jacobian Matrix
+                                    vector_ptrtype& x_in,    // Solution vector
+                                    vector_ptrtype& r_in,    // Residual vector
+                                    const double,              // Stopping tolerance
+                                    const unsigned int)
+{
+
+}
+
+template <typename T>
+std::pair<unsigned int, typename SolverNonLinearPetsc<T>::real_type>
+SolverNonLinearTrilinos<T>::solve ( dense_matrix_type&  jac_in,  // System Jacobian Matrix
+                                    dense_vector_type& x_in,    // Solution vector
+                                    dense_vector_type& r_in,    // Residual vector
+                                    const double,              // Stopping tolerance
+                                    const unsigned int)
+{
+
+}
+
+
+
+//------------------------------------------------------------------
+// Explicit instantiations
+template class SolverNonLinearTrilinos<double>;
+
+
+
+
+} // namespace Life
---
+++ life/lifealg/solvernonlineartrilinos.hpp
@@ -0,0 +1,180 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Florent Vielfaure <florent.vielfaure@gmail.com>
+       Date: 2009-05-25
+
+  Copyright (C) 2009 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file solvernonlineartrilinos.hpp
+   \author Florent Vielfaure <florent.vielfaure@gmail.com>
+   \date 2009-05-25
+ */
+
+#ifndef __SolverNonLinearTrilinos_H
+#define __SolverNonLinearTrilinos_H 1
+
+#include <life/lifecore/life.hpp>
+#include <life/lifealg/solvernonlinear.hpp>
+#include <life/lifealg/matrixepetra.hpp>
+#include <life/lifealg/vectorepetra.hpp>
+#include <life/lifealg/operatortrilinos.hpp>
+
+#include "NOX.H"
+#include "NOX_Epetra_Interface_Required.H"
+#include "NOX_Epetra_Interface_Jacobian.H"
+#include "NOX_Epetra_LinearSystem_AztecOO.H"
+#include "NOX_Epetra_Group.H"
+
+
+namespace Life
+{
+/**
+ * \class SolverNonLinearTrilinos
+ * \brief Trilinos non linear solvers interface
+ *
+ * This class provides an interface to Trilinos/NOX iterative solvers that is
+ * compatible with the \p SolverNonLinear<> base class
+ *
+ * @author Florent Vielfaure
+ */
+template<typename T>
+class SolverNonLinearTrilinos
+    :
+        public SolverNonLinear<T>,
+        public NOX::Epetra::Interface::Required,
+        public NOX::Epetra::Interface::Jacobian
+{
+    typedef SolverNonLinear<T> super;
+public:
+
+
+    /** @name Typedefs
+     */
+    //@{
+
+    typedef SolverNonLinearTrilinos<T> self_type;
+
+    typedef typename super::value_type value_type;
+    typedef typename super::real_type real_type;
+    typedef typename super::sparse_matrix_ptrtype sparse_matrix_ptrtype;
+    typedef typename super::vector_ptrtype vector_ptrtype;
+
+    typedef typename super::dense_matrix_type dense_matrix_type;
+    typedef typename super::dense_vector_type dense_vector_type;
+
+    //@}
+
+    /** @name Constructors, destructor
+     */
+    //@{
+
+      /**
+       *  Constructor. Initializes Trilinos data structures
+       */
+    SolverNonLinearTrilinos();
+    SolverNonLinearTrilinos( SolverNonLinearTrilinos const & );
+
+    /**
+     * Destructor.
+     */
+    ~SolverNonLinearTrilinos();
+
+    /**
+     * Initialize data structures if not done so already.
+     */
+    virtual void init ();
+
+    //@}
+
+    /** @name Operator overloads
+     */
+    //@{
+
+
+    //@}
+
+    /** @name Accessors
+     */
+    //@{
+
+    //@}
+
+    /** @name  Mutators
+     */
+    //@{
+
+    //@}
+
+    /** @name  Methods
+     */
+    //@{
+
+    /**
+     * Release all memory and clear data structures.
+     */
+    virtual void clear ();
+
+    /**
+     * Call the Trilinos solver.  It calls the method below, using the
+     * same matrix for the system and preconditioner matrices.
+     */
+    virtual std::pair<unsigned int, real_type> solve ( sparse_matrix_ptrtype&,    // System Jacobian Matrix
+                                                       vector_ptrtype&,          // Solution vector
+                                                       vector_ptrtype&,          // Residual vector
+                                                       const double,        // Stopping tolerance
+                                                       const unsigned int); // N. Iterations
+
+    virtual std::pair<unsigned int, real_type> solve ( dense_matrix_type&,    // System Jacobian Matrix
+                                                       dense_vector_type&,          // Solution vector
+                                                       dense_vector_type&,          // Residual vector
+                                                       const double,        // Stopping tolerance
+                                                       const unsigned int); // N. Iterations
+
+
+
+    //@}
+
+private:
+
+    bool computeF( const Epetra_Vector & x, Epetra_Vector & f, NOX::Epetra::Interface::Required::FillType F );
+    bool computeJecobian( const Epetra_Vector & x, Epetra_Operator & Jac );
+    bool computePrecMatrix( const Epetra_Vector & x, Epetra_RowMatrix & M );
+    bool computePreconditioner( const Epetra_Vector & x, Epetra_Operator & O );
+
+};
+
+template <typename T>
+inline
+SolverNonLinearPetsc<T>::SolverNonLinearTrilinos ()
+{
+}
+
+
+
+template <typename T>
+inline
+SolverNonLinearPetsc<T>::~SolverNonLinearTrilinos ()
+{
+  this->clear ();
+}
+
+} // Life
+
+#endif /* __SolverNonLinearPetsc_H */
