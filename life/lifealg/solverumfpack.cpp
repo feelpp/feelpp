adc4131d79f7591d6abc58a07fde27ffb860abb3
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-06-06 14:07:02 +0000

    update Life license from LGPL-2.1 to LGPL-3.0
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5468 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- life/lifealg/solverumfpack.cpp
+++ life/lifealg/solverumfpack.cpp
@@ -11,7 +11,7 @@
   This library is free software; you can redlstribute it and/or
   modlfy it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  version 3.0 of the License, or (at your option) any later version.
 
   This library is dlstributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
