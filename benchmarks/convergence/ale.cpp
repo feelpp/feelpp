2fcfb5c7c43e0aa69b78d6d392d49057e7cd26ed
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-07-08 14:58:50 +0000

    added ale tests
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5516 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ benchmarks/convergence/ale.cpp
@@ -0,0 +1,54 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2006-06-20
+
+  Copyright (C) 2006 EPFL
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file test_ale.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \author Goncalo Pena <goncalo.pena@epfl.ch>
+   \date 2006-06-20
+ */
+#include "test_ale.hpp"
+
+int
+main( int argc, char** argv )
+{
+  //MPI_Init(&argc, &argv);
+
+  Life::TestALE<1> mesh1( argc, argv, makeAbout(), makeOptions());
+  mesh1.run();
+  /*
+  Life::TestALE<2> mesh2( argc, argv, makeAbout(), makeOptions());
+  mesh2.run();
+  */
+  /*
+  Life::TestALE<3> mesh3( argc, argv, makeAbout(), makeOptions());
+  mesh3.run();
+
+  Life::TestALE<4> mesh4( argc, argv, makeAbout(), makeOptions());
+  mesh4.run();
+  */
+}
+
+
+
+
