05a566c01ecb77548ff22572503475435567a390
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-08-19 14:07:49 +0000

    added .dir-locals.el
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5547 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ .dir-locals.el
@@ -0,0 +1,25 @@
+;; -*- emacs-lisp -*-
+((nil . 
+  (
+    (tab-width . 4)
+    (indent-tabs-mode . nil)
+    (fill-column . 80)
+    (user-company . "Université de Grenoble")
+    (user-mail-address . "christophe.prudhomme@ujf-grenoble.fr")))
+(c++-mode . 
+  (
+    (indent-tabs-mode . nil)
+    (tab-width . 4)
+    (show-trailing-whitespace . t)
+    (indicate-empty-lines . t)
+    (c-basic-offset . 4)
+    (c-set-offset 'innamespace  '0)))
+(c-mode . 
+  (
+    (indent-tabs-mode . nil)
+    (tab-width . 4)
+    (show-trailing-whitespace . t)
+    (indicate-empty-lines . t)
+    (c-basic-offset . 4)
+    (c-set-offset 'innamespace  '0))))
+
