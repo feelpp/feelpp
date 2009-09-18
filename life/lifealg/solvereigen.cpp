1d99396696fad30f67ccf1a21cfe3904b0d2c0a4
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-09-18 08:50:23 +0000

    added prefix() method to differentiate between SolverEigen fix constructor
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5589 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- life/lifealg/solvereigen.cpp
+++ life/lifealg/solvereigen.cpp
@@ -88,9 +88,10 @@ SolverEigen<T>::SolverEigen( po::variables_map const& vm, std::string const& pre
 template <typename T>
 SolverEigen<T>::SolverEigen( po::variables_map const& vm, std::string const& prefix )
     :
-    M_eigen_solver_type    ((EigenSolverType)vm[prefix+"solvereigen-solver-type"].template as<int>() ),
-    M_eigen_problem_type   ((EigenProblemType)vm[prefix+"solvereigen-problem-type"].template as<int>() ),
-    M_position_of_spectrum ((PositionOfSpectrum)vm[prefix+"solvereigen-position"].template as<int>() ),
+    M_prefix( (!prefix.empty() && !boost::algorithm::ends_with( prefix, "-" ))? (prefix+"-"):prefix ),
+    M_eigen_solver_type    ((EigenSolverType)vm[M_prefix+"solvereigen-solver-type"].template as<int>() ),
+    M_eigen_problem_type   ((EigenProblemType)vm[M_prefix+"solvereigen-problem-type"].template as<int>() ),
+    M_position_of_spectrum ((PositionOfSpectrum)vm[M_prefix+"solvereigen-position"].template as<int>() ),
     M_is_initialized       (false)
 {
 }
