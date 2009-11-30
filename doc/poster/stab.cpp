f552cdf22994b748479a4af3ee43fa4951b531b2
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2009-11-30 10:41:39 +0000

    update  - support for boost 1.40  - added doc/poster  - added doc/figures (logos/backgrounds)  - updated ctest
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5685 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ doc/poster/stab.cpp
@@ -0,0 +1,9 @@
+AUTO(coeff, $\gamma_{\beta} + \module{\beta\SCAL n}$);
+form2(Xh, Xh, M) += 
+  integrate(
+            internalfaces(Xh->mesh()), IM,
+            coeff * $h^2/N^{3.5}$ * (
+                     trans(jumpt(gradt(u)))
+                     * jump(grad(v))
+                     )
+            );
