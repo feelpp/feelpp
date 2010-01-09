991d01c7081a340197008b06ad87bca848d4534b
gpena <gpena@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-01-09 18:28:36 +0000

    Fixed simplex and hypercube functions in 2D Added pointset generation in 1D case
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5791 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- applications/polyvis/polysetvis.py
+++ applications/polyvis/polysetvis.py
@@ -1,3 +1,38 @@
+#! /usr/bin/python
+
+# -*- mode: python -*-
+#
+#  This file is part of the Life library
+#
+#  Author(s): Goncalo Pena <gpena@mat.uc.pt>
+#        Date: 2010-01-09
+#
+#   Copyright (C) 2010
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#   University of Coimbra
+#
+# \file polysetvis.py
+# \author Goncalo Pena <gpena@mat.uc.pt>
+# \date 2010-01-09
+#
+
+# This file draws the equidistributed point set for the several elements and orders
+
+
 from pyx import *
 from scipy import *
 
@@ -6,8 +41,8 @@ import sys
 import string
 
 import math
-import square
-import triangle
+import hypercube
+import simplex
 
 
 from optparse import OptionParser
@@ -17,6 +52,7 @@ parser.add_option("--dim", dest="dim", type="int", help="topological dimension o
 parser.add_option("--element_type", dest="element", type="string", help="type of the reference element: simplex or hypercube", default="simplex")
 parser.add_option("--order", dest="order", type="int", help="pointset order", default=1)
 parser.add_option("--numbering", dest="numbers", type="int", help="adds numbering to points: 0 for no numbering, 1 to add the numbers", default=0)
+parser.add_option("--circle_radius", dest="circle_radius", type="float", help="radius of the circles that mark the nodes", default=0.07)
 (options, args) = parser.parse_args()
 
 
@@ -25,74 +61,92 @@ if (options.order < 0):
 	print "[validation] error : order must be nonnegative"
 	sys.exit(-1)
 
-if (options.numbers < 0):
+if not (options.numbers == 0 or options.numbers == 1):
 	print "[validation] error : argument must be 0 or 1"
 	sys.exit(-1)
 
-if (options.numbers > 1):
-	print "[validation] error : argument must be 0 or 1"
+if  (options.dim > 3 or options.dim < 1):
+	print "[validation] error : dimension must be 1, 2 or 3"
 	sys.exit(-1)
 
-if (options.dim < 1):
-	print "[validation] error : argument must be 1, 2 or 3"
+if not ( options.element == "simplex" or options.element == "hypercube"  ):
+	print "[validation] error : element must be simplex or hypercube"
 	sys.exit(-1)
 
-if (options.dim > 3):
-	print "[validation] error : argument must be 1, 2 or 3"
+if not (options.circle_radius > 0):
+	print "[validation] error : radius must be positive"
 	sys.exit(-1)
 
-print options.order
-print options.numbers
-print options.dim
-print options.element
+
+#print options.order
+#print options.numbers
+#print options.dim
+#print options.element
 
 
 c = canvas.canvas()
+
+line_size = 5
+
+if (options.dim == 1):
+        x0 = [0,0]
+        x1 = [line_size,0]
+
+        hypercube.line(c, x0, x1)
+
+        for i in range(2):
+                hypercube.Vertex(c, line_size, x0, i, options.circle_radius)
+                if ( options.numbers == 1 ):
+                        hypercube.numberVertex(c, line_size, x0, i, options.circle_radius, 0.1)
+
+        hypercube.Edge(c, line_size, x0, 0, options.order, options.circle_radius)
+
+        if ( options.numbers == 1 ):
+                hypercube.numberLine(c, line_size, x0, options.order, options.circle_radius, 0.1)
+
+
+
 
 
 if (options.dim == 2):
         x0 = [0,0]
 
-        line_size = 5
-
         if ( options.element == "hypercube" ):
-                square.border(c, x0, line_size)
+                hypercube.border(c, x0, line_size)
 
                 for i in range(4):
-                        square.Vertex(c, line_size, x0, i, 0.07)
-                        square.Edge(c, line_size, x0, i, options.order, 0.07)
+                        hypercube.Vertex(c, line_size, x0, i, options.circle_radius)
+                        hypercube.Edge(c, line_size, x0, i, options.order, options.circle_radius)
 
                         if ( options.numbers == 1 ):
-                                square.numberVertex(c, line_size, x0, i, 0.07, 0.1)
-                                square.numberEdge(c, line_size, x0, i, options.order, 0.07, 0.1)
+                                hypercube.numberVertex(c, line_size, x0, i, options.circle_radius, 0.1)
+                                hypercube.numberEdge(c, line_size, x0, i, options.order, options.circle_radius, 0.1)
 
-                                square.Face(c, line_size, x0, options.order, 0.07)
+                                hypercube.Face(c, line_size, x0, options.order, options.circle_radius)
 
                                 if ( options.numbers == 1 ):
-                                        square.numberFace(c, line_size, x0, options.order, 0.07, 0.1)
+                                        hypercube.numberFace(c, line_size, x0, options.order, options.circle_radius, 0.1)
 
 
         if ( options.element == "simplex" ):
-                triangle.border(c, x0, line_size)
+                simplex.border(c, x0, line_size)
 
                 for i in range(3):
-                        triangle.Vertex(c, line_size, x0, i, 0.07)
-                        triangle.Edge(c, line_size, x0, i, options.order, 0.07)
+                        simplex.Vertex(c, line_size, x0, i, options.circle_radius)
+                        simplex.Edge(c, line_size, x0, i, options.order, options.circle_radius)
 
                         if ( options.numbers == 1 ):
-                                triangle.numberVertex(c, line_size, x0, i, 0.07, 0.1)
-                                triangle.numberEdge(c, line_size, x0, i, options.order, 0.07, 0.1)
+                                simplex.numberVertex(c, line_size, x0, i, options.circle_radius, 0.1)
+                                simplex.numberEdge(c, line_size, x0, i, options.order, options.circle_radius, 0.1)
 
-                        triangle.Face(c, line_size, x0, options.order, 0.07)
+                        simplex.Face(c, line_size, x0, options.order, options.circle_radius)
 
                         if ( options.numbers == 1 ):
-                                triangle.numberFace(c, line_size, x0, options.order, 0.07, 0.1)
+                                simplex.numberFace(c, line_size, x0, options.order, options.circle_radius, 0.1)
 
 
 homename = os.path.expanduser("~/")
 dirname = homename + "life/pointsetvis/equidistributed/" + options.element + "/" + str(options.dim) + "D/P"+ str(options.order)
-
-print dirname
 
 if not os.path.isdir(dirname + "/"):
         os.makedirs(dirname + "/")
