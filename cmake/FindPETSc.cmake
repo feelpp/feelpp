ea280154c0585057f1941951fe5010aaf83dbaea
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-01-20 19:05:27 +0000

    in Debug use the -debug petsc flavor first otherwise use the -opt flavor first
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5834 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/FindPETSc.cmake
+++ cmake/FindPETSc.cmake
@@ -19,14 +19,20 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
- 
+
 find_path (PETSC_DIR include/petsc.h
   HINTS ENV PETSC_DIR
   PATHS
   /usr/lib/petscdir/3.0.0 /usr/lib/petscdir/2.3.3 /usr/lib/petscdir/2.3.2 # Debian
   $ENV{HOME}/petsc
   DOC "PETSc Directory")
- 
+
+IF ( ${CMAKE_BUILD_TYPE} STREQUAL "Debug" )
+  set( DEBIAN_FLAVORS "linux-gnu-c-debug linux-gnu-c-opt")
+ELSE()
+  set( DEBIAN_FLAVORS "linux-gnu-c-opt linux-gnu-c-debug")
+ENDIF()
+
 if (PETSC_DIR AND NOT PETSC_ARCH)
   set (_petsc_arches
     $ENV{PETSC_ARCH}                   # If set, use environment variable first
@@ -46,14 +52,16 @@ if (PETSC_DIR AND NOT PETSC_ARCH)
   endforeach (arch)
   set (petscconf "NOTFOUND" CACHE INTERNAL "Scratch variable" FORCE)
 endif (PETSC_DIR AND NOT PETSC_ARCH)
- 
+
+ message (STATUS "petsc_arch ${PETSC_ARCH}")
+
 set (petsc_slaves LIBRARIES_SYS LIBRARIES_VEC LIBRARIES_MAT LIBRARIES_DM LIBRARIES_KSP LIBRARIES_SNES LIBRARIES_TS
   INCLUDE_DIR INCLUDE_CONF)
 include (FindPackageMultipass)
 find_package_multipass (PETSc petsc_config_current
   STATES DIR ARCH
   DEPENDENTS INCLUDES LIBRARIES COMPILER MPIEXEC ${petsc_slaves})
- 
+
 # Determine whether the PETSc layout is old-style (through 2.3.3) or
 # new-style (3.0.0)
 if (EXISTS ${PETSC_DIR}/${PETSC_ARCH}/include/petscconf.h)   # > 2.3.3
@@ -65,13 +73,13 @@ elseif (EXISTS ${PETSC_DIR}/bmake/${PETSC_ARCH}/petscconf.h) # <= 2.3.3
 else (EXISTS ${PETSC_DIR}/bmake/${PETSC_ARCH}/petscconf.h)
   set (petsc_conf_base "NOTFOUND")
 endif (EXISTS ${PETSC_DIR}/${PETSC_ARCH}/include/petscconf.h)
- 
+
 if (petsc_conf_base AND NOT petsc_config_current)
   # Put variables into environment since they are needed to get
   # configuration (petscvariables) in the PETSc makefile
   set (ENV{PETSC_DIR} ${PETSC_DIR})
   set (ENV{PETSC_ARCH} ${PETSC_ARCH})
- 
+
   # A temporary makefile to probe the PETSc configuration
   set (petsc_config_makefile ${PROJECT_BINARY_DIR}/Makefile.petsc)
   file (WRITE ${petsc_config_makefile}
@@ -82,7 +90,7 @@ include ${petsc_conf_base}
 show :
 	-@echo -n \${\${VARIABLE}}
 ")
- 
+
   macro (PETSC_GET_VARIABLE name var)
     set (${var} "NOTFOUND" CACHE INTERNAL "Cleared" FORCE)
     execute_process (COMMAND ${CMAKE_MAKE_PROGRAM} -f ${petsc_config_makefile} show VARIABLE=${name}
@@ -97,13 +105,13 @@ show :
   #petsc_get_variable (MPIEXEC                  petsc_mpiexec)
   # We are done with the temporary Makefile, calling PETSC_GET_VARIABLE after this point is invalid!
   file (REMOVE ${petsc_config_makefile})
- 
+
   include (ResolveCompilerPaths)
   # Extract include paths and libraries from compile command line
   resolve_includes (petsc_includes_all "${petsc_cpp_line}")
- 
+
   message (STATUS "petsc_lib_dir ${petsc_lib_dir}")
- 
+
   macro (PETSC_FIND_LIBRARY suffix name)
     set (PETSC_LIBRARY_${suffix} "NOTFOUND" CACHE INTERNAL "Cleared" FORCE)
     find_library (PETSC_LIBRARY_${suffix} NAMES ${name} HINTS ${petsc_lib_dir})
@@ -117,7 +125,7 @@ show :
   petsc_find_library (KSP  petscksp)
   petsc_find_library (SNES petscsnes)
   petsc_find_library (TS   petscts)
- 
+
   include (CheckCSourceRuns)
   macro (PETSC_TEST_RUNS includes libraries runs)
     multipass_c_source_runs ("${includes}" "${libraries}" "
@@ -126,7 +134,7 @@ static const char help[] = \"PETSc test program.\";
 int main(int argc,char *argv[]) {
   PetscErrorCode ierr;
   TS ts;
- 
+
   ierr = PetscInitialize(&argc,&argv,0,help);CHKERRQ(ierr);
   ierr = TSCreate(PETSC_COMM_WORLD,&ts);CHKERRQ(ierr);
   ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
@@ -140,7 +148,7 @@ int main(int argc,char *argv[]) {
 	"Can the system successfully run a PETSc executable?  This variable can be manually set to \"YES\" to force CMake to accept a given PETSc configuration, but this will almost always result in a broken build.  If you change PETSC_DIR, PETSC_ARCH, or PETSC_CURRENT you would have to reset this variable." FORCE)
     endif (${${runs}})
   endmacro (PETSC_TEST_RUNS)
- 
+
   macro (PETSC_JOIN libs deps)
     list (APPEND PETSC_LIBRARIES_${libs} ${PETSC_LIBRARIES_${deps}})
   endmacro (PETSC_JOIN libs deps)
@@ -151,12 +159,12 @@ int main(int argc,char *argv[]) {
   petsc_join (SNES KSP)
   petsc_join (TS   SNES)
   petsc_join (ALL  TS)
- 
+
   find_path (PETSC_INCLUDE_DIR petscts.h HINTS "${PETSC_DIR}" PATH_SUFFIXES include NO_DEFAULT_PATH)
   find_path (PETSC_INCLUDE_CONF petscconf.h HINTS "${PETSC_DIR}" PATH_SUFFIXES "${PETSC_ARCH}/include" "bmake/${PETSC_ARCH}" NO_DEFAULT_PATH)
   mark_as_advanced (PETSC_INCLUDE_DIR PETSC_INCLUDE_CONF)
   set (petsc_includes_minimal ${PETSC_INCLUDE_CONF} ${PETSC_INCLUDE_DIR})
- 
+
   petsc_test_runs ("${petsc_includes_minimal}" "${PETSC_LIBRARIES_TS}" petsc_works_minimal)
   if (petsc_works_minimal)
     message (STATUS "Minimal PETSc includes and libraries work.  This probably means we are building with shared libs.")
@@ -187,7 +195,7 @@ int main(int argc,char *argv[]) {
       endif (petsc_works_alllibraries)
     endif (petsc_works_allincludes)
   endif (petsc_works_minimal)
- 
+
   # We do an out-of-source build so __FILE__ will be an absolute path, hence __SDIR__ is superfluous
   set (PETSC_DEFINITIONS "-D__SDIR__=\"\"" CACHE STRING "PETSc definitions" FORCE)
   # Sometimes this can be used to assist FindMPI.cmake
@@ -200,7 +208,7 @@ int main(int argc,char *argv[]) {
   # work.  It is likely that you will end up with a broken build.
   mark_as_advanced (PETSC_INCLUDES PETSC_LIBRARIES PETSC_COMPILER PETSC_DEFINITIONS PETSC_MPIEXEC PETSC_EXECUTABLE_RUNS)
 endif (petsc_conf_base AND NOT petsc_config_current)
- 
+
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (PETSc
   "PETSc could not be found.  Be sure to set PETSC_DIR and PETSC_ARCH."
