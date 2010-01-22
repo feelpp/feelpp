f3dc9d13387d388be4b610326245e6bbab4ae1e1
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-01-22 07:41:13 +0000

    added slepc cmake finder module
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5847 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ cmake/FindSLEPc.cmake
@@ -0,0 +1,41 @@
+# -*- mode: cmake -*-
+#
+#  This file is part of the Life library
+#
+#  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+#       Date: 2010-01-22
+#
+#  Copyright (C) 2010 Université Joseph Fourier
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+find_path (SLEPC_DIR include/slepc.h
+  HINTS ENV SLEPC_DIR
+  PATHS
+  /usr/lib/slepcdir/3.0.0 # Debian
+  $ENV{HOME}/slepc
+  DOC "SLEPc Directory")
+
+SET(SLEPC_INCLUDE_DIR "${SLEPC_DIR}/include/")
+CHECK_INCLUDE_FILE( ${SLEPC_INCLUDE_DIR}/slepc.h HAVE_SLEPC_H )
+FIND_LIBRARY(SLEPC_LIB_SLEPC     slepc )
+SET(SLEPC_LIBRARIES ${SLEPC_LIB_SLEPC} CACHE STRING "SLEPc libraries" FORCE)
+# message( "*** SLEPc directory : ${SLEPC_DIR}" )
+if (HAVE_SLEPC_H AND SLEPC_DIR AND SLEPC_LIBRARIES )
+  set(HAVE_SLEPC 1)
+  set(SLEPC_FOUND ON)
+endif()
+set(SLEPC_INCLUDES ${SLEPC_INCLUDE_DIR} CACHE STRING "SLEPc include path" FORCE)
+MARK_AS_ADVANCED( SLEPC_DIR SLEPC_LIB_SLEPC SLEPC_INCLUDES SLEPC_LIBRARIES )
