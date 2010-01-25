2620f4dbf1c4fb4cd9c08f221190c8fd6e624c79
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-01-25 06:08:04 +0000

    added stokes11
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5856 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ benchmarks/convergence/stokes11.cpp
@@ -0,0 +1,56 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2009-01-04
+
+  Copyright (C) 2010 Christophe Prud'homme
+  Copyright (C) 2010 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3.0 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file stokes11.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2010-01-24
+ */
+#include <stokes.hpp>
+
+int
+main( int argc, char** argv )
+{
+
+    using namespace Life;
+    /* assertions handling */
+    Life::Assert::setLog( "stokes.assert");
+
+    /* change parameters below */
+    const int nDim = 2;
+    const int OrderU = 1;
+    const int OrderP = 1;
+
+    typedef Life::Stokes<nDim, OrderU, OrderP, Simplex> stokes_type;
+
+
+    /* define and run application */
+    stokes_type stokes( argc, argv, makeAbout(), makeOptions() );
+    stokes.run();
+}
+
+
+
+
+
