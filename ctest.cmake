49f2f239fa647bb65ef6d06e7507050a9065fb71
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-02-15 12:07:49 +0000

    change the build name
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5904 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- ctest.cmake
+++ ctest.cmake
@@ -1,3 +1,29 @@
+cmake_minimum_required (VERSION 2.6)
+
+FUNCTION(_Life_COMPILER_DUMPVERSION _OUTPUT_VERSION)
+
+  EXEC_PROGRAM(gcc
+    ARGS  -dumpversion
+    OUTPUT_VARIABLE _life_COMPILER_VERSION
+  )
+  STRING(REGEX REPLACE "([0-9])\\.([0-9])(\\.[0-9])?" "\\1\\2"
+    _life_COMPILER_VERSION ${_life_COMPILER_VERSION})
+
+  SET(${_OUTPUT_VERSION} ${_life_COMPILER_VERSION} PARENT_SCOPE)
+ENDFUNCTION()
+
+
+_Life_COMPILER_DUMPVERSION(_life_COMPILER_VERSION)
+
+#MESSAGE(STATUS "life_COMPILER_VERSION: gcc${_life_COMPILER_VERSION}")
+# MESSAGE(STATUS "CMAKE_SYSTEM: ${CMAKE_SYSTEM}")
+#MESSAGE(STATUS "CMAKE_SYSTEM NAME: ${CMAKE_SYSTEM_NAME}")
+#MESSAGE(STATUS "CMAKE_SYSTEM_PROCESSOR: ${CMAKE_SYSTEM_PROCESSOR}")
+#MESSAGE(STATUS "CMAKE_SYSTEM_VERSION: ${CMAKE_SYSTEM_VERSION}")
+#set( build_name "${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}-gcc${_life_COMPILER_VERSION}")
+#MESSAGE(STATUS "build_name: ${build_name}")
+
+
 find_program(UNAME NAMES uname)
 macro(getuname name flag)
   exec_program("${UNAME}" ARGS "${flag}" OUTPUT_VARIABLE "${name}")
@@ -6,7 +32,7 @@ endmacro(getuname)
 getuname(osname -s)
 getuname(osrel  -r)
 getuname(cpu    -m)
-set(CTEST_BUILD_NAME        "${osname}-${cpu}")
+set(CTEST_BUILD_NAME        "${osname}-${cpu}-gcc${_life_COMPILER_VERSION}")
 
 
 SET(MODEL Nightly)
