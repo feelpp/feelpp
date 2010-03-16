72a238f6255c4754ca5c7cfe8269a22e4597b9d0
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-03-16 17:34:01 +0000

    add support for hermite polynomials visualisation
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@5939 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ applications/polyvis/hermite.cpp
@@ -0,0 +1,43 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2009-07-02
+
+  Copyright (C) 2009 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file hermite.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2009-07-02
+ */
+#include "hermite.hpp"
+
+namespace Life
+{
+namespace detail
+{
+# define ORDERS_HERMITE BOOST_PP_TUPLE_TO_LIST(1,(3))
+# define DIMS_HERMITE BOOST_PP_TUPLE_TO_LIST(2,(1, 2))
+
+BOOST_PP_LIST_FOR_EACH_PRODUCT(HERMITE_FACTORY_OP, 3, (DIMS, ORDERS, SIMPLEX))
+}
+}
+
+
+
---
+++ applications/polyvis/hermite.hpp
@@ -0,0 +1,64 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Life library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2009-07-02
+
+  Copyright (C) 2009 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file hermite.hpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2009-07-02
+ */
+#ifndef __phermite_H
+#define __phermite_H 1
+
+#include "basisgen.hpp"
+#include <life/lifepoly/hermite.hpp>
+
+namespace Life
+{
+namespace detail
+{
+template<typename Convex, uint16_type Order>
+PolyvisBase*
+createHermite()
+{
+    std::cout << "Creating Hermite(" << Convex::nDim << "," << Order << ")\n";
+    return new Polyvis<basis_type<Hermite<Order> >,convex_type<Convex> >;
+}
+# /* Generates code for all dim and order. */
+# define HERMITE_FACTORY_OP(_, GDO)           \
+    HERMITE_FACTORY GDO                        \
+    /**/
+#
+# define GEN_HERMITE(LDIM,LORDER,CONVEX)              \
+    "hermite(" #LDIM "," #LORDER "," #CONVEX ")"       \
+    /**/
+#
+#
+#define HERMITE_FACTORY(LDIM,LORDER,CONVEX)                     \
+    const bool BOOST_PP_CAT( BOOST_PP_CAT( BOOST_PP_CAT(lag, CONVEX), LDIM), LORDER ) = \
+        PolyvisBase::factory_type::instance()                           \
+        .registerProduct( GEN_HERMITE(LDIM,LORDER,CONVEX), &createHermite<CONVEX< LDIM >,LORDER> );
+
+} // detail
+} // Life
+#endif /* __hermite_H */
+
