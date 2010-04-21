47a88ad505db4896f32733e2cbf1836637a8affa
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-04-21 07:06:02 +0000

    removed
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6012 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- life/lifefilters/exporterimpl.hpp
+++ life/lifefilters/exporterimpl.hpp
@@ -159,37 +159,6 @@ Exporter<MeshType>::addPath( boost::format fmt )
     return this;
 }
 
-//
-// Exporter Options
-//
-/**
- * \return the command lines options for the exporter
- */
-po::options_description exporter_options( std::string const& prefix )
-{
-    std::string _prefix = prefix;
-    if ( !_prefix.empty() )
-        _prefix += "-";
-
-    po::options_description _options( "Exporter " + prefix + " options");
-    _options.add_options()
-        // do export
-        ((_prefix+"export").c_str(), Life::po::value<bool>()->default_value( true ), "true if export, false otherwise")
-
-        // exporter type
-        ((_prefix+"exporter").c_str(), Life::po::value<std::string>()->default_value( "ensight" ), "type of exporter")
-
-        // prefix options
-        ((_prefix+"exporter-prefix").c_str(), Life::po::value<std::string>()->default_value( prefix ), "prefix for exported files")
-
-        // frequency options
-        ((_prefix+"exporter-freq").c_str(), Life::po::value<int>()->default_value( 1 ), "frequency at which results are exported")
-
-        // file type options
-        ((_prefix+"exporter-file-type").c_str(), Life::po::value<int>()->default_value( ASCII ), "file type in which the results are exported ('ascii' = 0 or 'binary' = 1)")
-        ;
-    return _options;
-}
 
 }
 
