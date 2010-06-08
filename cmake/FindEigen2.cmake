6564ab608f808d8b2bebb20a08895c85357b928b
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-06-08 20:25:45 +0000

    added script for eigen2
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6129 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ cmake/FindEigen2.cmake
@@ -0,0 +1,45 @@
+# -*- mode: cmake -*-
+#
+#  This file is part of the Life library
+#
+#  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+#       Date: 2010-03-15
+#
+#  Copyright (C) 2010 Université Joseph Fourier
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+include (FindPackageHandleStandardArgs)
+INCLUDE(CheckIncludeFileCXX)
+
+FIND_PATH(Eigen2_INCLUDE_DIR
+  Eigen/Core
+  PATHS /usr/include/ /usr/include/eigen2
+  DOC "Directory where Eigen2 header files are stored" )
+
+
+CHECK_INCLUDE_FILE_CXX(Eigen/Core HAVE_EIGEN_CORE_H)
+
+# handle the QUIETLY and REQUIRED arguments and set Eigen2_FOUND to TRUE if
+# all listed variables are TRUE
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (Eigen2 DEFAULT_MSG
+  Eigen2_INCLUDE_DIR
+  )
+
+if ( Eigen2_FOUND )
+  MESSAGE( STATUS "Eigen2 found: ${Eigen2_INCLUDE_DIR}" )
+endif()
+
+mark_as_advanced( Eigen2_INCLUDE_DIR )
