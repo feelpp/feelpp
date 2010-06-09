95c23dda1b6de17d9b287a6e4263475b22867c5f
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-06-09 08:43:52 +0000

    openturns  wrapper
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6136 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/FindOpenTURNS.cmake
+++ cmake/FindOpenTURNS.cmake
@@ -185,13 +185,13 @@ endif ()
 # handle the QUIETLY and REQUIRED arguments and set OpenTURNS_FOUND to TRUE if
 # all listed variables are TRUE
 find_package_handle_standard_args (OpenTURNS DEFAULT_MSG
-  OpenTURNS_LIBRARY
-  OpenTURNS_INCLUDE_DIR
-  OpenTURNS_SWIG_INCLUDE_DIR
+#  OpenTURNS_LIBRARY
+#  OpenTURNS_INCLUDE_DIR
+#  OpenTURNS_SWIG_INCLUDE_DIR
   OpenTURNS_INCLUDE_DIRS
   OpenTURNS_LIBRARIES
   OpenTURNS_WRAPPER_DIR
-  OpenTURNS_MODULE_DIR
+#  OpenTURNS_MODULE_DIR
 )
 
 mark_as_advanced (
