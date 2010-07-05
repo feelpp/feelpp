3042a554bbec355d1a9e5e6b5f0d2196f6c64298
asamake <asamake@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-07-05 10:54:35 +0000

    modified test_ravth.cpp
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6212 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- testsuite/lifepoly/test_ravth.cpp
+++ testsuite/lifepoly/test_ravth.cpp
@@ -1,19 +1,20 @@
 #define BOOST_TEST_MODULE Raviar-Thomas polynomials test
 // Boost.Test
 #define USE_TEST 1
+#define TEST_2D
 #include <boost/test/unit_test.hpp>
 using boost::unit_test::test_suite;
 
 #include <life/lifepoly/raviartthomas.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
-#include <life/lifepoly/Polynomial_set.hpp>
+#include <life/lifepoly/polynomial_set.hpp>
 
 
 
 BOOST_AUTO_TEST_SUITE( ravth_testsuite )
 
-    
+
 BOOST_AUTO_TEST_CASE( ravth )
 {
     using namespace Life;
@@ -21,13 +22,13 @@ BOOST_AUTO_TEST_CASE( ravth )
 
     rt0_type rt0;
 
-  matrix_type m(6 ,3)   ;  
+  matrix_type m(6 ,3)   ;
 
   m(0 ,0)=m(0 ,1)=m(1 ,0)=m(1 ,2)=m(2 ,1)=m(3 ,0)=m(3 ,2)=m(4 ,0)
   =m(4 ,1)=m(5 ,2) = 1./4.;
    m(0 ,2)=m(1 ,1)=m(2 ,2)=m(3 ,1)=m(4 ,2)=m(5 ,1) = 0. ;
    m(2 ,0)=m(5 ,0)= -1./4. ;
-   
+
    Points_type p1(2); p1(0)=-1./3.; p1(1) =-1./3.;
    Points_type p2(2); p2(0)=-1.; p2(1) =-1.;
    Points_type p3(2); p3(0)= 1.; p3(1) =-1.;
@@ -38,18 +39,18 @@ BOOST_AUTO_TEST_CASE( ravth )
    Points_type p8(2); p8(0)= 0.; p8(1) =-1.;
    Points_type p9(2); p9(0)=-1./2.; p9(1) = 1./2.;
    Points_type p10(2); p10(0)= 1./2.; p10(1) =-1./2.;
-   vectors_type P(10 ) ; 
-  
+   vectors_type P(10 ) ;
+
    P(0)=p1;P(1)=p2;P(2)=p3;P(3)=p4;P(4)=p5;P(5)=p6;P(6)=p7;P(7)=p8;P(8)=p9;P(9)=p10 ;
 
    Polynomialset<10,6,3,10> Poly(m) ;
-  
+
    std::cout << "**************************************************Analytic method*********************************************" << std::endl ;
    std::cout<<"coefficients= " << m << std::endl ;
    std::cout <<"Points= " << P << std::endl ;
-   std::cout<<"evaluate_at_Points= " <<Poly.evaluate_Points ( P ) << std::endl ;
+   std::cout<<"evaluate_at_Points= " <<Poly.evaluates_Points ( P ) << std::endl ;
 
- 
+
 std::cout << "*****************************************Life method***********************************************************" << std::endl ;
 
        rt0_type::points_type pts(2,10);
@@ -63,14 +64,14 @@ std::cout << "*****************************************Life method**************
    pts(0 ,7)= 0.; pts(1 ,7) =-1.;
    pts(0 ,8)=-1./2.; pts(1 ,8) = 1./2.;
    pts(0 ,9)= 1./2.; pts(1 ,9) =-1./2.;
-   
+
    std::cout << "pts= " << pts << "\n" ;
    auto eval_at_pts = rt0.evaluate( pts );
    std::cout << "eval_at_pts= "<< eval_at_pts << "\n" ;
 
-   BOOST_CHECK (ublas::norm_inf(Poly.evaluate_Points( P )-eval_at_pts) < 0.001 )  ;
-      
-    
+   BOOST_CHECK (ublas::norm_inf(Poly.evaluates_Points( P )-eval_at_pts) < 0.001 )  ;
+
+
 }
 BOOST_AUTO_TEST_SUITE_END()
 
