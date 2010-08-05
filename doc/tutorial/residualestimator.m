93d04fd31460152337977f4221c5f77f2ae62455
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-05 12:12:16 +0000

    update
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6309 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- doc/tutorial/residualestimator.m
+++ doc/tutorial/residualestimator.m
@@ -10,15 +10,16 @@ L2=[];
 H1=[];
 eL2=[];
 eH1=[];
-#for h=[0.1 0.05 0.025 0.0125]
-for h=[0.1]
-  x(1)=h
-  y=residualestimator_1_1(x)
+h=[0.1 0.05 0.025 0.0125 0.005]
+for H=h
+  x(1)=H;
+  y=residualestimator_1_1(x);
   L2=[L2 y(1)];
   H1=[H1 y(2)];
   eL2=[eL2 y(3)];
   eH1=[eH1 y(4)];
 end
+
 loglog(h,L2,'-*;L_2;',h,h.*h,'-+;h^2;',
        h,H1,'-o;H_1;',h,h,'-x;h;',
        h,eL2,'-*;estimated L_2;',
