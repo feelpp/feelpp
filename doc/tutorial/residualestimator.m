9aa265aea64b5fff5ffb16365c7b9b53d7377c79
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-05 12:12:16 +0000

    cleanup
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6308 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- doc/tutorial/residualestimator.m
+++ doc/tutorial/residualestimator.m
@@ -1,24 +1,24 @@
 # -*- mode: octave -*-
 x=[0.1   # hsize
-   1     # dim
-   0     # shape
-   1     # function
+   0     # shape (0:simplex, 1:hypercube, 2:ellipsoid)
+   1     # function (0, 1)
    3     # parameter
-   1     # weakdir
-   50    # penaldir
-   ];
+   1     # weakdir (0: weak dirichlet condition, 1 strong)
+   50    # penaldir (value for weak dirichlet parameter)
+   ]
 L2=[];
 H1=[];
 eL2=[];
 eH1=[];
-for h=[0.1 0.05 0.025 0.0125]
-  x(1)=h;
-  y=residualestimator_1_1(x);
+#for h=[0.1 0.05 0.025 0.0125]
+for h=[0.1]
+  x(1)=h
+  y=residualestimator_1_1(x)
   L2=[L2 y(1)];
   H1=[H1 y(2)];
   eL2=[eL2 y(3)];
   eH1=[eH1 y(4)];
-endfor
+end
 loglog(h,L2,'-*;L_2;',h,h.*h,'-+;h^2;',
        h,H1,'-o;H_1;',h,h,'-x;h;',
        h,eL2,'-*;estimated L_2;',
