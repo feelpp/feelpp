9c01b9d210f9d49b3fa5ebf58d929fea5370f950
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-14 08:36:04 +0000

    update
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6417 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- life/lifepoly/Polynomial_set.hpp
+++ life/lifepoly/Polynomial_set.hpp
@@ -26,7 +26,7 @@
 #include <boost/numeric/ublas/triangular.hpp>
 #include <boost/numeric/ublas/lu.hpp>
 
- 
+
 namespace Life
 {
  typedef  boost::numeric::ublas::matrix<double> matrix_type ;
@@ -36,39 +36,39 @@ namespace Life
  typedef  boost::numeric::ublas::vector<double> Points_type ;
  typedef  boost::numeric::ublas::vector<Points_type> vectors_type ;
 
- 
+
 template< uint nDim ,uint nOrder , uint dim ,uint number >
 class Polynomialset
 {
-   
+
 public:
 
-    
-    
+
+
     Polynomialset() ;
     Polynomialset ( matrix_type  m , vector_type  b) ;
-    Polynomialset ( matrix_type  m ) ; 
+    Polynomialset ( matrix_type  m ) ;
     Polynomialset ( const Polynomialset<nDim,nOrder ,dim ,number> & P) ;
    ~Polynomialset() ;
     matrix_type evaluate_Points( vectors_type P ) ;
-  
+
 private :
 
     matrix_type M ;
-   
-    vector_type B ; 
 
- }; // Polynomial_set 
+    vector_type B ;
+
+ }; // Polynomial_set
 
 // Implementation
 
-template< uint nDim ,uint nOrder , uint dim ,uint number > 
+template< uint nDim ,uint nOrder , uint dim ,uint number >
 Polynomialset <nDim , nOrder ,dim ,number>::Polynomialset() :M( matrix_id(nOrder,dim)),B(vector_zero(nDim))
 {
 };
 
-template< uint nDim ,uint nOrder ,uint dim ,uint number > 
-Polynomialset <nDim , nOrder , dim ,number>::Polynomialset ( matrix_type  m , vector_type  b) 
+template< uint nDim ,uint nOrder ,uint dim ,uint number >
+Polynomialset <nDim , nOrder , dim ,number>::Polynomialset ( matrix_type  m , vector_type  b)
 {
     M =  matrix_type(nOrder,dim) ;
 
@@ -79,12 +79,12 @@ Polynomialset <nDim , nOrder , dim ,number>::Polynomialset ( matrix_type  m , ve
        {
            M(i , j) = m(i , j) ;
        }
-    } 
+    }
   for(uint k = 0 ;k< B.size() ; ++k)
     {
       B( k ) = b( k ) ;
     }
-  
+
 
 }
 
@@ -94,20 +94,20 @@ Polynomialset <nDim , nOrder , dim ,number>::Polynomialset ( matrix_type  m , ve
 template< uint nDim , uint nOrder ,uint dim ,uint number >
 Polynomialset <nDim , nOrder , dim ,number>::Polynomialset ( matrix_type  m )
 {
-     M =  matrix_type(nOrder,dim) ;
-     B =  vector_type(nDim) ;
+    M =  matrix_type(nOrder,dim) ;
+    B =  vector_type(nDim) ;
     for(uint i = 0 ;i < M.size1() ; ++i)
     {
-       for(uint j = 0 ;j< M.size2() ; ++j)
-       {
-           M(i , j) = m(i , j) ;
-       }
-    } 
-  for(uint k = 0 ;k< B.size() ; ++k)
-    {
-      B( k ) = 1. ;
+        for(uint j = 0 ;j< M.size2() ; ++j)
+        {
+            M(i , j) = m(i , j) ;
+        }
     }
-  
+    for(uint k = 0 ;k< B.size() ; ++k)
+    {
+        B( k ) = 1. ;
+    }
+
 }
 
 
@@ -116,40 +116,40 @@ Polynomialset <nDim , nOrder , dim ,number>::Polynomialset ( matrix_type  m )
 template< uint nDim , uint nOrder ,uint dim ,uint number >
 Polynomialset<nDim ,nOrder ,dim ,number >::Polynomialset ( const Polynomialset< nDim, nOrder ,dim ,number> & P)
 {
-     M =  matrix_type(nOrder,dim) ;
+    M =  matrix_type(nOrder,dim) ;
 
-     B =  vector_type(nDim) ;
+    B =  vector_type(nDim) ;
     for(uint i = 0 ;i < M.size1() ; ++i)
     {
-       for(uint j = 0 ;j< M.size2() ; ++j)
-       {
-           M(i , j) = P.M(i , j) ;
-       }
-    } 
-    
-    
-    for(uint k = 0 ;k< B.size() ; ++k)
-    {
-      B( k ) = P.B( k ) ;
+        for(uint j = 0 ;j< M.size2() ; ++j)
+        {
+            M(i , j) = P.M(i , j) ;
+        }
     }
 
-}  
+
+    for(uint k = 0 ;k< B.size() ; ++k)
+    {
+        B( k ) = P.B( k ) ;
+    }
+
+}
 
 
 
 template< uint nDim , uint nOrder ,uint dim ,uint number >
-Polynomialset<nDim ,nOrder ,dim ,number>::~Polynomialset() 
+Polynomialset<nDim ,nOrder ,dim ,number>::~Polynomialset()
 {
 }
 
 template< uint nDim , uint nOrder ,uint dim ,uint number >
-matrix_type Polynomialset<nDim ,nOrder ,dim ,number>::evaluate_Points( vectors_type  P) 
+matrix_type Polynomialset<nDim ,nOrder ,dim ,number>::evaluate_Points( vectors_type  P)
 {
   matrix_type matrice(nDim , number) ;
   matrix_type matrice_valeur(dim , number) ;
 
 
- 
+
     for(uint j = 0 ;j< matrice.size2() ;++j)
     {
        matrice(0 ,j) =  1. ;
@@ -158,12 +158,12 @@ matrix_type Polynomialset<nDim ,nOrder ,dim ,number>::evaluate_Points( vectors_t
        matrice(3 ,j) = B(3)*P(j)(0)*P(j)(1) ;
        matrice(4 ,j) = B(4)*pow(P(j)(0),2) ;
        matrice(5 ,j) = B(5)*pow(P(j)(1),2) ;
-       matrice(6 ,j) = B(6)*pow(P(j)(0),2)*P(j)(1) ; 
+       matrice(6 ,j) = B(6)*pow(P(j)(0),2)*P(j)(1) ;
        matrice(7 ,j) = B(7)*pow(P(j)(1),2)*P(j)(0) ;
        matrice(8 ,j) = B(8)*pow(P(j)(0),3) ;
        matrice(9 ,j) = B(9)*pow(P(j)(1),2) ;
     }
-    
+
     for(uint i = 0 ;i < matrice_valeur.size1() ;++i)
   {
     for(uint j = 0 ;j< matrice_valeur.size2() ;++j)
@@ -174,19 +174,19 @@ matrix_type Polynomialset<nDim ,nOrder ,dim ,number>::evaluate_Points( vectors_t
 
 
 
- return   boost::numeric::ublas::prod( M , matrice_valeur )  ; 
-   
-    
- 
+ return   boost::numeric::ublas::prod( M , matrice_valeur )  ;
+
+
+
 
 }
 
 
 
 
-}  //namespace Life 
- 
-#endif 
+}  //namespace Life
+
+#endif
 
 
 
