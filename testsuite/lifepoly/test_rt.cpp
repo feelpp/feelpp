c8eb0edac2ebb5f9141204aca8d8e2d8534f8715
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-14 08:36:05 +0000

    update
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6418 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- testsuite/lifepoly/test_rt.cpp
+++ testsuite/lifepoly/test_rt.cpp
@@ -1,25 +1,25 @@
 /* -*- mode: c++ -*-
 
-  This file is part of the Life library
+   This file is part of the Life library
 
-  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
-       Date: 2010-05-08
+   Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+        Date: 2010-05-08
 
-  Copyright (C) 2010 Université Joseph Fourier (Grenoble I)
+   Copyright (C) 2010 Université Joseph Fourier (Grenoble I)
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 /**
    \file test_rt.cpp
@@ -34,31 +34,29 @@ using boost::unit_test::test_suite;
 
 #include <life/lifepoly/raviartthomas.hpp>
 
-
 BOOST_AUTO_TEST_SUITE( rt_testsuite )
 
-#if 0
 BOOST_AUTO_TEST_CASE( rt0 )
 {
     using namespace Life;
     typedef RaviartThomas<0>::apply<2>::type rt0_type;
     rt0_type rt0;
 
-    rt0_type::points_type pts(2,1);
+    rt0_type::points_type pts(2,5);
     pts( 0, 0 ) = -1./3.; pts( 1, 0 ) = -1./3.;
-
+    pts( 0, 1 ) = -1.; pts( 1, 1 ) = -1.;
+    pts( 0, 2 ) =  1.; pts( 1, 2 ) = -1.;
+    pts( 0, 3 ) = -1.; pts( 1, 3 ) =  1.;
+    pts( 0, 4 ) = -1.; pts( 1, 4 ) = 0.;
     //pts = rt0.referenceConvex().barycenterFaces();
 
     std::cout << "pts= " << pts << "\n";
     auto eval_at_pts = rt0.evaluate( pts );
     std::cout << "eval at pts= " << eval_at_pts << "\n";
 }
-#endif
-
 #if 0
 BOOST_AUTO_TEST_CASE( rt0_2 )
 {
-    BOOST_TEST_MESSAGE( "creating RT0 on reference element :" );
     using namespace Life;
     typedef RaviartThomas<0>::apply<2>::type rt0_type;
     typedef boost::shared_ptr<rt0_type> rt0_ptrtype;
@@ -70,112 +68,40 @@ BOOST_AUTO_TEST_CASE( rt0_2 )
     std::cout << "pts= " << pts << "\n";
     auto eval_at_pts = rt0->evaluate( pts );
     std::cout << "eval at pts= " << eval_at_pts << "\n";
-    auto deriv_at_pts = rt0->derivate( pts );
-    std::cout << "deriv0 at pts= " << deriv_at_pts[0] << "\n";
-    std::cout << "deriv1 at pts= " << deriv_at_pts[1] << "\n";
-    //std::cout << "deriv2 at pts= " << deriv_at_pts[2] << "\n";
 
-    BOOST_TEST_MESSAGE( "creating another element :" );
-    typedef GeoND<2,Simplex<2, 1, 2> >::point_type point_type;
-    // interval
-    typedef GeoND<2,Simplex<2, 1, 2> > tria_type;
-    tria_type tria;
-    point_type V1; V1( 0 )=-1;V1( 1 )=-1;
-    point_type V2; V2( 0 )=1;V2( 1 )=-1;
-    point_type V3; V3( 0 )=-1;V3( 1 )=1;
-    tria.setPoint( 0, V1 );
-    tria.setPoint( 1, V2 );
-    tria.setPoint( 2, V3 );
-    ublas::vector<double> G1( 2 );
-    G1(0)=2;G1(1)=1./3.;
-    tria.update();
-    BOOST_TEST_MESSAGE( "evaluating the RT0 polynomialset on real element :" );
 
-    auto bpts = tria.gm()->referenceConvex().barycenterFaces();
-    auto gmpc = tria.gm()->preCompute( tria.gm(), bpts );
-    auto rtpc = rt0->preCompute( rt0, bpts );
 
-    auto geoctx = tria.gm()->context<vm::GRAD|vm::DIV|vm::KB|vm::POINT|vm::JACOBIAN>( tria, gmpc );
-#if 1
-    //auto rtctx = rt0->ctx<vm::GRAD,rt0_type, decltype(*tria.gm()),decltype(*rtpc), tria_type>( rt0, tria.gm(), rtpc );
-    //auto rtctx = rt0->ctx( rt0, tria.gm() );
-    //auto gmctx = tria.gm()->context<vm::GRAD>( tria, gmpc );
-    //auto rtctx = rt0->ctx<vm::GRAD,rt0_type,decltype(*geoctx),decltype(*rtpc),tria_type>( rt0, geoctx, rtpc, tria );
-    //auto rtctx = rt0->ctx();
-    //auto rtctx = rt0->context<vm::GRAD>( rt0, tria.gm(), rtpc );
 
-    typedef rt0_type::Context<vm::GRAD|vm::DIV|vm::KB|vm::POINT|vm::JACOBIAN,rt0_type,tria_type::gm_type,tria_type> rtctx_type;
-    boost::shared_ptr<rtctx_type> rtctx( new rtctx_type( rt0, geoctx, rtpc ) );
-    for( int i = 0;i < 3; ++i )
-        for(int j = 0;j < bpts.size2(); ++j )
-        {
-            std::cout << "rt(" << i << "," << j << ")=" << rtctx->div(i,0,0,j) << "\n";
-        }
-
-#endif
-
-}
-#endif // 0
-
-BOOST_AUTO_TEST_CASE( test_rt1 )
-{
-    BOOST_TEST_MESSAGE( "creating RT1 on reference element :" );
-    using namespace Life;
-    typedef RaviartThomas<1>::apply<2>::type rt1_type;
-    typedef boost::shared_ptr<rt1_type> rt1_ptrtype;
-    rt1_ptrtype rt1( new rt1_type ) ;
-    rt1_type::points_type pts = rt1->points();
-
-    std::cout << "pts= " << pts << "\n";
-    auto eval_at_pts = rt1->evaluate( pts );
-    std::cout << "eval at pts= " << eval_at_pts << "\n";
 #if 0
-    auto deriv_at_pts = rt0->derivate( pts );
-    std::cout << "deriv0 at pts= " << deriv_at_pts[0] << "\n";
-    std::cout << "deriv1 at pts= " << deriv_at_pts[1] << "\n";
-    //std::cout << "deriv2 at pts= " << deriv_at_pts[2] << "\n";
 
-    BOOST_TEST_MESSAGE( "creating another element :" );
+
     typedef GeoND<2,Simplex<2, 1, 2> >::point_type point_type;
     // interval
     typedef GeoND<2,Simplex<2, 1, 2> > tria_type;
     tria_type tria;
-    point_type V1; V1( 0 )=-1;V1( 1 )=-1;
-    point_type V2; V2( 0 )=1;V2( 1 )=-1;
-    point_type V3; V3( 0 )=-1;V3( 1 )=1;
+    point_type V1; V1( 0 )=1;V1( 1 )=0;
+    point_type V2; V2( 0 )=3;V2( 1 )=0;
+    point_type V3; V3( 0 )=2;V3( 1 )=1;
     tria.setPoint( 0, V1 );
     tria.setPoint( 1, V2 );
     tria.setPoint( 2, V3 );
     ublas::vector<double> G1( 2 );
     G1(0)=2;G1(1)=1./3.;
     tria.update();
-    BOOST_TEST_MESSAGE( "evaluating the RT0 polynomialset on real element :" );
 
     auto bpts = tria.gm()->referenceConvex().barycenterFaces();
     auto gmpc = tria.gm()->preCompute( tria.gm(), bpts );
     auto rtpc = rt0->preCompute( rt0, bpts );
-
-    auto geoctx = tria.gm()->context<vm::GRAD|vm::DIV|vm::KB|vm::POINT|vm::JACOBIAN>( tria, gmpc );
-#if 1
+    auto geoctx = tria.gm()->context<vm::JACOBIAN>( tria, gmpc );
     //auto rtctx = rt0->ctx<vm::GRAD,rt0_type, decltype(*tria.gm()),decltype(*rtpc), tria_type>( rt0, tria.gm(), rtpc );
     //auto rtctx = rt0->ctx( rt0, tria.gm() );
-    //auto gmctx = tria.gm()->context<vm::GRAD>( tria, gmpc );
-    //auto rtctx = rt0->ctx<vm::GRAD,rt0_type,decltype(*geoctx),decltype(*rtpc),tria_type>( rt0, geoctx, rtpc, tria );
+    auto rtctx = rt0->ctx<vm::GRAD>( rt0, tria.gm(), rtpc, tria );
     //auto rtctx = rt0->ctx();
     //auto rtctx = rt0->context<vm::GRAD>( rt0, tria.gm(), rtpc );
-
-    typedef rt0_type::Context<vm::GRAD|vm::DIV|vm::KB|vm::POINT|vm::JACOBIAN,rt0_type,tria_type::gm_type,tria_type> rtctx_type;
-    boost::shared_ptr<rtctx_type> rtctx( new rtctx_type( rt0, geoctx, rtpc ) );
-    for( int i = 0;i < 3; ++i )
-        for(int j = 0;j < bpts.size2(); ++j )
-        {
-            std::cout << "rt(" << i << "," << j << ")=" << rtctx->div(i,0,0,j) << "\n";
-        }
-
 #endif
-#endif
+
 }
-
+#endif
 BOOST_AUTO_TEST_SUITE_END()
 
 
