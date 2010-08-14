e57bff2d48be457d641425a8703db4f31b532ff9
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-14 08:35:57 +0000

    minor fixes
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6411 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- life/lifediscr/functionspace.hpp
+++ life/lifediscr/functionspace.hpp
@@ -6,7 +6,7 @@
    Date: 2004-11-22
 
    Copyright (C) 2004 EPFL
-   Copyright (C) 2007,2008 Université Joseph Fourier (Grenoble I)
+   Copyright (C) 2007-2010 Université Joseph Fourier (Grenoble I)
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
