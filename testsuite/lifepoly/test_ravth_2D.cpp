7eb3d00ef87a271e15c2ad89c00d2c2b3b6ae47b
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-15 13:49:22 +0000

    start cleanup
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6435 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- testsuite/lifepoly/test_ravth_2D.cpp
+++ testsuite/lifepoly/test_ravth_2D.cpp
@@ -8,7 +8,7 @@ using boost::unit_test::test_suite;
 #include <life/lifepoly/raviartthomas.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
-#include <life/lifepoly/polynomial_set.hpp>
+#include <polynomial_set.hpp>
 
 
 
