da0e048621f74e6c6081eb6134fe7e184ba47928
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-27 07:57:19 +0000

    moved
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6560 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ cmake/scripts/feelbuild.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export FEEL_DIR=$HOME/sources/feel/
+
+# go to source directory
+if [! -d $HOME/sources/ ]; then mkdir -p $HOME/sources/; fi
+cd $HOME/sources/
+
+#this is due to a bug, the process extracts information from the svn output which needs to be in english
+export LC_MESSAGES=en_GB
+
+#do the actual work
+if [ -f $HOME/sources/$1.log]; then  rm -f $HOME/sources/$1.log; fi
+/usr/bin/ctest -S $FEEL_DIR/cmake/scripts/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
