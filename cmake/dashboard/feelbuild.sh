d82f20f06f9d18199f1c2175fda9297efa9038a0
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-27 07:57:22 +0000

    ctest.cmake is in cmake/dashboard now
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6563 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/dashboard/feelbuild.sh
+++ cmake/dashboard/feelbuild.sh
@@ -11,4 +11,4 @@ export LC_MESSAGES=en_GB
 
 #do the actual work
 if [ -f $HOME/sources/$1.log]; then  rm -f $HOME/sources/$1.log; fi
-/usr/bin/ctest -S $FEEL_DIR/cmake/scripts/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
+/usr/bin/ctest -S $FEEL_DIR/cmake/dashboard/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
