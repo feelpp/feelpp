e5bf27101e88b05e5a7d748f1904fb65e8c0edb3
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-31 04:49:05 +0000

    add path for macports
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6572 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/dashboard/feelbuild.sh
+++ cmake/dashboard/feelbuild.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export PATH=/usr/bin:/opt/local/bin:$PATH
 export FEEL_DIR=$HOME/sources/feel/
 
 # go to source directory
@@ -11,4 +12,4 @@ export LC_MESSAGES=en_GB
 
 #do the actual work
 if [ -f $HOME/sources/$1.log]; then  rm -f $HOME/sources/$1.log; fi
-/usr/bin/ctest -S $FEEL_DIR/cmake/dashboard/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
+ctest -S $FEEL_DIR/cmake/dashboard/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
