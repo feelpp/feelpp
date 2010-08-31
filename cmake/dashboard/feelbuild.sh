1c8ca1e91fa491c9ab6725791f19823c597b0fb3
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-08-31 05:04:02 +0000

    better
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6573 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/dashboard/feelbuild.sh
+++ cmake/dashboard/feelbuild.sh
@@ -4,12 +4,20 @@ export PATH=/usr/bin:/opt/local/bin:$PATH
 export FEEL_DIR=$HOME/sources/feel/
 
 # go to source directory
-if [! -d $HOME/sources/ ]; then mkdir -p $HOME/sources/; fi
+if ! [ -d $HOME/sources/ ]; then mkdir -p $HOME/sources/; fi
 cd $HOME/sources/
+if ! [ -d $HOME/sources/feel ]; then 
+    svn checkout svn://scm.ljkforge.imag.fr/svnroot/life/life/trunk feel
+
+    echo "type 'crontab -e' and enter" 
+    echo " 0  1   *   *   * $HOME/sources/feel/cmake/dashboard/feelbuild.sh Nightly" 
+    exit
+fi
 
 #this is due to a bug, the process extracts information from the svn output which needs to be in english
 export LC_MESSAGES=en_GB
 
 #do the actual work
-if [ -f $HOME/sources/$1.log]; then  rm -f $HOME/sources/$1.log; fi
+if [ -f $HOME/sources/$1.log ]; then  rm -f $HOME/sources/$1.log; fi
 ctest -S $FEEL_DIR/cmake/dashboard/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
+
