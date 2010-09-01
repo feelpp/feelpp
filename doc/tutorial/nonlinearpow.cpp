b64c92ee43821c8de3a9a8e401a33bd2ba3c0d85
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-09-01 14:49:37 +0000

    fix create mesh
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6600 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- doc/tutorial/nonlinearpow.cpp
+++ doc/tutorial/nonlinearpow.cpp
@@ -210,7 +210,7 @@ NonLinearPow<Dim,Order,Entity>::NonLinearPow( int argc, char** argv, AboutData c
 
     mesh_ptrtype mesh = createGMSHMesh( _mesh=new mesh_type,
                                         _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES|MESH_RENUMBER,
-                                        _desc=domain( _name= (boost::format( "%1%-%2%-%3%" ) % "hypercube" % Dim % Order).str() ,
+                                        _desc=domain( _name= (boost::format( "%1%-%2%-%3%" ) % "hypercube" % Dim % 1).str() ,
                                                       _shape="hypercube",
                                                       _dim=Dim,
                                                       _order=1,
