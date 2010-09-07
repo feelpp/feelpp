e51dd4ae921bd616f3a2512f9dc68cc925d0c384
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-09-07 07:14:19 +0000

    specify the path for ctest
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6638 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/dashboard/feelbuild.sh
+++ cmake/dashboard/feelbuild.sh
@@ -6,11 +6,11 @@ export FEEL_DIR=$HOME/sources/feel/
 # go to source directory
 if ! [ -d $HOME/sources/ ]; then mkdir -p $HOME/sources/; fi
 cd $HOME/sources/
-if ! [ -d $HOME/sources/feel ]; then 
+if ! [ -d $HOME/sources/feel ]; then
     svn checkout svn://scm.ljkforge.imag.fr/svnroot/life/life/trunk feel
 
-    echo "type 'crontab -e' and enter" 
-    echo " 0  1   *   *   * $HOME/sources/feel/cmake/dashboard/feelbuild.sh Nightly" 
+    echo "type 'crontab -e' and enter"
+    echo " 0  1   *   *   * $HOME/sources/feel/cmake/dashboard/feelbuild.sh Nightly"
     exit
 fi
 
@@ -19,5 +19,11 @@ export LC_MESSAGES=en_GB
 
 #do the actual work
 if [ -f $HOME/sources/$1.log ]; then  rm -f $HOME/sources/$1.log; fi
-ctest -S $FEEL_DIR/cmake/dashboard/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
+if [ -x /usr/bin/ctest ]; then
+    /usr/bin/ctest -S $FEEL_DIR/cmake/dashboard/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
+elif [ -x /opt/local/bin/ctest ]; then
+    /opt/local/bin/ctest -S $FEEL_DIR/cmake/dashboard/ctest.cmake,$1 -V > $HOME/sources/$1.log 2>&1
+else
+    echo "CTest not found."
+fi
 
