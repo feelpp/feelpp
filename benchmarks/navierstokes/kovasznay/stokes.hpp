e238277742d8a971ff73cc3b06e7aebe810ccfa0
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-09-15 07:06:41 +0000

    fixes
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6690 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- benchmarks/navierstokes/kovasznay/stokes.hpp
+++ benchmarks/navierstokes/kovasznay/stokes.hpp
@@ -6,7 +6,7 @@
        Date: 2009-01-04
 
   Copyright (C) 2009 Christophe Prud'homme
-  Copyright (C) 2009 Université Joseph Fourier (Grenoble I)
+  Copyright (C) 2009-2010 Université Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
