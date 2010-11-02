19fd06b2917d98161605243250e246aea57cadd0
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-11-02 22:27:49 +0000

    fix encoding to utf-8
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6741 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- benchmarks/perf/bench1_main.cpp
+++ benchmarks/perf/bench1_main.cpp
@@ -1,11 +1,11 @@
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4 
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 
   This file is part of the Feel library
 
   Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
        Date: 2010-06-14
 
-  Copyright (C) 2010 Université Joseph Fourier (Grenoble I)
+  Copyright (C) 2010 UniversitÃ© Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -39,7 +39,7 @@ makeAbout()
                      "assembly performance",
                      AboutData::License_LGPL,
                      "Copyright (c) 2005,2006 EPFL"
-                     "Copyright (c) 2006-2010 Université Joseph Fourier (Grenoble 1)"
+                     "Copyright (c) 2006-2010 UniversitÃ© Joseph Fourier (Grenoble 1)"
         );
 
     about.addAuthor("Christophe Prud'homme", "developer", "christophe.prudhomme@ujf-grenoble.fr", "");
