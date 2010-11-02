b13cdfb31347263159406237116929569b8a59e9
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-11-02 22:28:08 +0000

    fix header
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6758 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- feel/feelcore/feel.hpp
+++ feel/feelcore/feel.hpp
@@ -66,7 +66,7 @@
 #include <tbb/tick_count.h>
 #include <tbb/blocked_range.h>
 #include <tbb/parallel_for.h>
-#include <tbb/parallel_reduced.h>
+#include <tbb/parallel_reduce.h>
 #include <tbb/task_scheduler_init.h>
 #endif // HAVE_TBB
 
