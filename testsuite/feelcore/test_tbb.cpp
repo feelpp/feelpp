15e40b253c71c39cdc56f900ec319b157d7c31a3
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-11-02 22:28:01 +0000

    added tbb test
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6751 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ testsuite/feelcore/test_tbb.cpp
@@ -0,0 +1,86 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-
+
+  This file is part of the Feel library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2010-10-26
+
+  Copyright (C) 2010 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file test_tbb.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2010-10-26
+ */
+#define USE_BOOST_TEST 1
+// Boost.Test
+
+// make sure that the init_unit_test function is defined by UTF
+#define BOOST_TEST_MAIN
+// give a name to the testsuite
+#define BOOST_TEST_MODULE TBB testsuite
+// disable the main function creation, use our own
+//#define BOOST_TEST_NO_MAIN
+
+
+#include <boost/test/unit_test.hpp>
+using boost::unit_test::test_suite;
+#include <boost/test/floating_point_comparison.hpp>
+
+#include "tbb/tick_count.h"
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_for.h>
+
+#include <feel/options.hpp>
+#include <feel/feelcore/environment.hpp>
+
+int N=100000000;
+class A
+{
+public:
+    A(  )  {}
+    ~A() {}
+    void operator() ( const tbb::blocked_range<std::vector<double>::iterator >& r ) const  {
+        for ( auto i = r.begin(); i != r.end(); ++i ) {
+            *i += std::cos(*i)+std::sin(*i);
+        }
+    }
+};
+
+BOOST_AUTO_TEST_CASE( test_tbb )
+{
+    BOOST_TEST_MESSAGE( "Test TBB" );
+    std::vector<double> vs(N,M_PI),v(N,M_PI);
+    tbb::blocked_range<std::vector<double>::iterator> r( v.begin(), v.end() );
+    BOOST_CHECK_EQUAL( r.size(), N );
+    tbb::tick_count serial_t0 = tbb::tick_count::now();
+    for( auto i = vs.begin();i != vs.end(); ++i )
+    {
+        *i += std::cos(*i)+std::sin(*i);
+    }
+    tbb::tick_count serial_t1 = tbb::tick_count::now();
+
+    tbb::tick_count parallel_t0 = tbb::tick_count::now();
+    tbb::parallel_for( r, A() );
+    tbb::tick_count parallel_t1 = tbb::tick_count::now();
+
+    BOOST_TEST_MESSAGE( "Serial version ran in " << (serial_t1 - serial_t0).seconds() << " seconds" << "\n"
+                        << "Parallel version ran in " <<  (parallel_t1 - parallel_t0).seconds() << " seconds" << "\n"
+                        << "Resulting in a speedup of " << (serial_t1 - serial_t0).seconds() / (parallel_t1 - parallel_t0).seconds() << "\n");
+    BOOST_TEST_MESSAGE( "Test TBB done" );
+}
+
