97fc7f137f4825948a1c06b38a9d9bf9262718c8
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2010-11-03 07:43:17 +0000

    fix encoding
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@6793 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- testsuite/feeldiscr/test_interpolation.cpp
+++ testsuite/feeldiscr/test_interpolation.cpp
@@ -1,11 +1,11 @@
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4 
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 
   This file is part of the Feel library
 
   Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
        Date: 2007-12-19
 
-  Copyright (C) 2007-2010 Université Joseph Fourier (Grenoble I)
+  Copyright (C) 2007-2010 UniversitÃ© Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -422,7 +422,7 @@ makeAbout()
                             "0.1",
                            "interpolation tests",
                            Feel::AboutData::License_GPL,
-                           "Copyright (C) 2007, 2008 Université Joseph Fourier (Grenoble I)");
+                           "Copyright (C) 2007, 2008 UniversitÃ© Joseph Fourier (Grenoble I)");
 
     about.addAuthor("Christophe Prud'homme", "developer", "christophe.prudhomme@ujf-grenoble.fr", "");
     return about;
