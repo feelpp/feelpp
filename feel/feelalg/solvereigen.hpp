1804b3a5518791c4dacb044999dd810c964e39dc
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-04-29 08:48:48 +0000

    fix encoding update header fix call to solve() passing now _maxit and _tolerance to ensure these tolerances are known to the underlying solver
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7020 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- feel/feelalg/solvereigen.hpp
+++ feel/feelalg/solvereigen.hpp
@@ -1,11 +1,11 @@
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4 
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 
   This file is part of the Feel library
 
   Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
        Date: 2007-07-04
 
-  Copyright (C) 2007 Université Joseph Fourier (Grenoble I)
+  Copyright (C) 2007-2011 UniversitÃ© Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -442,7 +442,10 @@ BOOST_PARAMETER_MEMBER_FUNCTION((typename SolverEigen<double>::eigenmodes_type),
     Log() << "eigenvalue tolerance = " << tolerance << "\n";
 
     unsigned int nconv, nits;
-    boost::tie( nconv, nits, boost::tuples::ignore)  = eigen->solve( matrixA, matrixB );
+    boost::tie( nconv, nits, boost::tuples::ignore)  = eigen->solve( _matrixA=matrixA,
+                                                                     _matrixB=matrixB,
+                                                                     _maxit=maxit,
+                                                                     _tolerance=tolerance );
 
     return eigen->eigenModes();
 }
