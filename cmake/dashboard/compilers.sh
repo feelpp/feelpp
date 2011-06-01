c3c0a63351b7d51327ca58f1aa72519dab95baeb
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-06-01 21:16:54 +0000

    more custom target for new testing framework
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7169 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/dashboard/compilers.sh
+++ cmake/dashboard/compilers.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 ARCH=`uname -m`
 SITE=`hostname`
-OS_VERSION=ubuntu-natty
-WORK_DIR=/home/prudhomm/sources/
-mkdir -p $WORK_DIR
+#OS_VERSION=ubuntu-natty
+OS_VERSION=macosx-leopard
+#WORK_DIR=/home/prudhomm/sources/
+WORK_DIR=$HOME/Devel/FEEL
+MAKE_ARGS="-j8"
+#mkdir -p $WORK_DIR
 # get the last version of the script
 #svn export svn://scm.forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk/cmake/dashboard/testsuite.cmake $WORK_DIR/testsuite.cmake
-COMMON="ctest -VV -S $WORK_DIR/testsuite.cmake,FEEL_WORK_DIR=$WORK_DIR,FEEL_SITE=$SITE,FEEL_MODE=$1,FEEL_BUILD_STRING=$OS_VERSION-$ARCH"
+COMMON="ctest -VV -S $WORK_DIR/feel/cmake/dashboard/testsuite.cmake,FEEL_WORK_DIR=$WORK_DIR,FEEL_MAKE_ARGS=$MAKE_ARGS,FEEL_SITE=$SITE,FEEL_MODE=$1,FEEL_BUILD_STRING=$OS_VERSION-$ARCH"
 #$COMMON-gcc-4.4.6,FEEL_CXX=g++-4.4,FEEL_EXPLICIT_VECTORIZATION=novec
 #$COMMON-gcc-4.4.6,FEEL_CXX=g++-4.4,FEEL_EXPLICIT_VECTORIZATION=SSE2
 #$COMMON-gcc-4.5.2,FEEL_CXX=g++-4.5,FEEL_EXPLICIT_VECTORIZATION=novec
