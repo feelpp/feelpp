f53a3ceeb914761b9d548bc03334c35322eff9ad
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-06-04 07:42:44 +0000

    update testsuite.cmake
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7182 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/dashboard/compilers.sh
+++ cmake/dashboard/compilers.sh
@@ -6,10 +6,11 @@ OS_VERSION=`uname -s`
 #WORK_DIR=/home/prudhomm/sources/
 WORK_DIR=$HOME/Devel/FEEL
 MAKE_ARGS="-j6"
+PARALLEL="6"
 #mkdir -p $WORK_DIR
 # get the last version of the script
 #svn export svn://scm.forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk/cmake/dashboard/testsuite.cmake $WORK_DIR/testsuite.cmake
-COMMON="ctest -VV -S $WORK_DIR/feel/cmake/dashboard/testsuite.cmake,FEEL_WORK_DIR=$WORK_DIR,FEEL_MAKE_ARGS=$MAKE_ARGS,FEEL_SITE=$SITE,FEEL_MODE=$1,FEEL_BUILD_STRING=$OS_VERSION-$ARCH"
+COMMON="ctest -VV -S $WORK_DIR/feel/cmake/dashboard/testsuite.cmake,FEEL_WORK_DIR=$WORK_DIR,FEEL_MAKE_ARGS=$MAKE_ARGS,CTEST_BUILD_FLAGS=-j$PARALLEL,CTEST_PARALLEL_LEVEL=$PARALLEL,FEEL_SITE=$SITE,FEEL_MODE=$1,FEEL_BUILD_STRING=$OS_VERSION-$ARCH"
 #$COMMON-gcc-4.4.6,FEEL_CXX=g++-4.4,FEEL_EXPLICIT_VECTORIZATION=novec
 #$COMMON-gcc-4.4.6,FEEL_CXX=g++-4.4,FEEL_EXPLICIT_VECTORIZATION=SSE2
 #$COMMON-gcc-4.5.2,FEEL_CXX=g++-4.5,FEEL_EXPLICIT_VECTORIZATION=novec
