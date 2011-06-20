768502b73c852c5d086e7ed174f7c6303652f6d4
morinba <morinba@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-06-20 13:38:43 +0000

    myfunctionspace tutorial
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7258 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- doc/manual/myfunctionspace.cpp
+++ doc/manual/myfunctionspace.cpp
@@ -91,12 +91,14 @@ public:
     typedef Mesh<convex_type> mesh_type;
     typedef boost::shared_ptr<mesh_type> mesh_ptrtype;
 
-    //# marker1 #
     //! function space that holds piecewise constant (\f$P_0\f$) functions (e.g. to store material properties or partitioning
-    typedef FunctionSpace<mesh_type, bases<Lagrange<0,Scalar, Discontinuous> > > p0_space_type;
+	//# marker1 #
+typedef FunctionSpace<mesh_type,bases<Lagrange<0,Scalar,Discontinuous> > > 
+			p0_space_type;
+	//# endmarker1 #
+	
     //! an element type of the \f$P_0\f$ discontinuous function space
     typedef typename p0_space_type::element_type p0_element_type;
-    //# endmarker1 #
 
     //# marker2 #
     //! the basis type of our approximation space
@@ -176,13 +178,14 @@ MyFunctionSpace<Dim, Order>::run( const double* X, unsigned long P, double* Y, u
 
     //# marker31 #
     //! create the mesh
-    mesh_ptrtype mesh = createGMSHMesh( _mesh=new mesh_type,
-                                        _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES|MESH_RENUMBER,
-                                        _desc=domain( _name= (boost::format( "%1%-%2%-%3%" ) % shape % Dim % Order).str() ,
-                                                      _shape=shape,
-                                                      _dim=Dim,
-                                                      _order=Order,
-                                                      _h=X[0] ) );
+    mesh_ptrtype mesh = 
+		createGMSHMesh( _mesh=new mesh_type,
+						_update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES|MESH_RENUMBER,
+                        _desc=domain( _name= (boost::format( "%1%-%2%-%3%" ) % shape % Dim % Order).str() ,
+                        _shape=shape,
+                        _dim=Dim,
+                        _order=Order,
+                        _h=X[0] ) );
 
     //# endmarker31 #
 
@@ -190,15 +193,18 @@ MyFunctionSpace<Dim, Order>::run( const double* X, unsigned long P, double* Y, u
      * The function space and some associated elements(functions) are then defined
      */
     /** \code */
-    //# marker32 #
     // function space \f$ X_h \f$
+    //# marker32 #
     space_ptrtype Xh = space_type::New( mesh );
-    // an element of the function space \f$ X_h \f$
+	//# endmarker32 #
+	
+	//# marker33 #
+    // an element of the function space X_h
     auto u = Xh->element( "u" );
-    // another element of the function space \f$ X_h \f$
+    // another element of the function space X_h
     element_type v( Xh, "v" );
     auto w = Xh->element( "w" );
-    //# endmarker32 #
+	//# endmarker33 #
     /** \endcode */
 
     value_type alpha = this->vm()["alpha"].template as<double>();
@@ -206,7 +212,8 @@ MyFunctionSpace<Dim, Order>::run( const double* X, unsigned long P, double* Y, u
 
     //# marker4 #
     auto g = sin(2*pi*Px())*cos(2*pi*Py())*cos(2*pi*Pz());
-    auto f = (1-Px()*Px())*(1-Py()*Py())*(1-Pz()*Pz())*pow(trans(vf::P())*vf::P(),(alpha/2.0));
+    auto f = 
+(1-Px()*Px())*(1-Py()*Py())*(1-Pz()*Pz())*pow(trans(vf::P())*vf::P(),(alpha/2.0));
     //# endmarker4 #
 
     //# marker5 #
