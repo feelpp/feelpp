376a8671bfdeaac9d71dc367cc3c385cb5a99c3e
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-06-21 12:57:53 +0000

    added test for matrix properties
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7265 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- testsuite/feelcore/test_context.cpp
+++ testsuite/feelcore/test_context.cpp
@@ -1,11 +1,11 @@
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4 
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 
   This file is part of the Feel library
 
   Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
        Date: 2008-04-07
 
-  Copyright (C) 2008 Université Joseph Fourier (Grenoble I)
+  Copyright (C) 2008 UniversitÃ© Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -25,34 +25,6 @@
    \file test_context.cpp
    \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
    \date 2008-04-07
- */
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4 
-
-  This file is part of the Feel library
-
-  Author(s): Christophe Prud'homme <prudhomm@mit.edu>
-       Date: 2004-09-10
-
-  Copyright (C) 2004 EPFL
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 3.0 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-/**
-   \file test_singleton.cpp
-   \author Christophe Prud'homme <prudhomm@mit.edu>
-   \date 2004-09-10
  */
 #define BOOST_TEST_MODULE context test
 #include <boost/test/unit_test.hpp>
@@ -79,6 +51,27 @@ BOOST_AUTO_TEST_CASE( test_context )
     BOOST_CHECK_EQUAL( ctx.test( ON_ELIMINATION ), true );
     BOOST_CHECK_EQUAL( ctx.test( ON_ELIMINATION_KEEP_DIAGONAL ), false );
     BOOST_CHECK_EQUAL( ctx.test( ON_ELIMINATION_SYMMETRIC ), true );
+    BOOST_CHECK_EQUAL( ctx.test( ON_ELIMINATION_SYMMETRIC | ON_ELIMINATION ), true );
+
+#if 0
+    detail::Flags<on_context_type> oct;
+
+    //FEEL_DECLARE_FLAGS( onct, on_context_type );
+
+
+    onct |= (ON_ELIMINATION|ON_ELIMINATION_SYMMETRIC);
+    BOOST_CHECK_EQUAL( onct.testFlag(ON_ELIMINATION), true );
+    BOOST_CHECK_EQUAL( onct.testFlag(ON_ELIMINATION_KEEP_DIAGONAL), false );
+    BOOST_CHECK_EQUAL( onct.testFlag(ON_ELIMINATION_SYMMETRIC), true );
+#endif
+}
+
+BOOST_AUTO_TEST_CASE( test_context_matrix_properties )
+{
+    using namespace Feel;
+    Context ctx( HERMITIAN | POSITIVE_DEFINITE );
+    BOOST_CHECK_EQUAL( ctx.test( HERMITIAN | POSITIVE_DEFINITE ), true );
+    BOOST_CHECK_EQUAL( ctx.test( SINGULAR ), true );
 
 #if 0
     detail::Flags<on_context_type> oct;
