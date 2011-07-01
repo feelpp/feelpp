bf724c8e0a7c8ad318586b9bb990757d5fa9c536
veys <veys@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-07-01 15:00:07 +0000

    up but still a problem at execution
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7340 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- examples/heat/fin_sink.cpp
+++ examples/heat/fin_sink.cpp
@@ -34,137 +34,125 @@
 #include <feel/feelfilters/gmsh.hpp>
 #include <feel/feeldiscr/mesh.hpp>
 
-//std::pair<std::string, std::string>
-mesh_ptrtype
+using namespace Feel;
+
+
+gmsh_ptrtype
 makefin( double hsize , double deep)
 {
     std::ostringstream ostr;
     if (!deep) { // 2D Mesh
-		//typedef Simplex<2> convex_type;
-		ostr << "Mesh.MshFileVersion = 1;\n"
-			<< "h=" << hsize << ";\n"
-			<< "Point (1) = {0, 0, 0, h};\n"
-			<< "Point (2) = {3.5, 0, 0, h};\n"
-			<< "Point (3) = {3.5, 3, 0, h};\n"
-			<< "Point (4) = {1, 3, 0, h};\n"
-			<< "Point (5) = {0, 3, 0, h};\n"
-			<< "Point (6) = {1, 17, 0, h};\n"
-			<< "Point (7) = {0, 17, 0, h};\n"
-			<< "Line (1) = {1, 2};\n"
-			<< "Line (2) = {2, 3};\n"
-			<< "Line (3) = {3, 4};\n"
-			<< "Line (4) = {4, 5};\n"
-			<< "Line (5) = {4, 6};\n"
-			<< "Line (6) = {6, 7};\n"
-			<< "Line (7) = {7, 5};\n"
-			<< "Line (8) = {5, 1};\n"
-			<< "Line Loop (9) = {1, 2, 3, 4, 8};\n"
-			<< "Line Loop (10) = {5, 6, 7, -4};\n"
-			<< "Plane Surface (9) = {9};\n"
-			<< "Plane Surface (10) = {10};\n"
-	
-			// physical entities
-			<< "Physical Surface (1) = {9};\n"
-			<< "Physical Surface (2) = {10};\n";
-	}
-	
-	else { //3D Mesh
-			//typedef Simplex<3> convex_type;
-			ostr << "Mesh.MshFileVersion = 1;\n"
-			<< "h=" << hsize << ";\n"
-			<< "d=" << deep << ";\n"
-			<< "Point (1) = {0, 0, 0, h};\n"
-			<< "Point (2) = {3.5, 0, 0, h};\n"
-			<< "Point (3) = {3.5, 3, 0, h};\n"
-			<< "Point (4) = {1, 3, 0, h};\n"
-			<< "Point (5) = {0, 3, 0, h};\n"
-			<< "Point (6) = {1, 17, 0, h};\n"
-			<< "Point (7) = {0, 17, 0, h};\n"
-			<< "Point (8) = {0, 0, d, h};\n"
-			<< "Point (9) = {3.5, 0, d, h};\n"
-			<< "Point (10) = {3.5, 3, d, h};\n"
-			<< "Point (11) = {1, 3, d, h};\n"
-			<< "Point (12) = {0, 3, d, h};\n"
-			<< "Point (13) = {1, 17, d, h};\n"
-			<< "Point (14) = {0, 17, d, h};\n"
-			<< "Line (1) = {1, 2};\n"
-			<< "Line (2) = {2, 3};\n"
-			<< "Line (3) = {3, 4};\n"
-			<< "Line (4) = {4, 5};\n"
-			<< "Line (5) = {4, 6};\n"
-			<< "Line (6) = {6, 7};\n"
-			<< "Line (7) = {7, 5};\n"
-			<< "Line (8) = {5, 1};\n"
-			<< "Line (9) = {2, 9};\n"
-			<< "Line (10) = {1, 8};\n"
-			<< "Line (11) = {8, 9};\n"
-			<< "Line (12) = {12, 8};\n"
-			<< "Line (13) = {9, 10};\n"
-			<< "Line (14) = {10, 11};\n"
-			<< "Line (15) = {5, 12};\n"
-			<< "Line (16) = {3, 10};\n"
-			<< "Line (17) = {4, 11};\n"
-			<< "Line (18) = {11, 13};\n"
-			<< "Line (19) = {14, 12};\n"
-			<< "Line (20) = {11, 12};\n"
-			<< "Line (21) = {13, 14};\n"
-			<< "Line (22) = {6, 13};\n"
-			<< "Line (23) = {7, 14};\n"
-			<< "Line Loop (24) = {1, 9, -11, -10};\n"
-			<< "Line Loop (25) = {-3, 16, 14, 20, -15, -4 };\n"
-			<< "Line Loop (26) = {1, 2, 3, 4, 8};\n"
-			<< "Line Loop (27) = {5, 6, 7, -4};\n"
-			<< "Line Loop (28) = {11, 13, 14, 20, 12};\n" 
-			<< "Line Loop (29) = {18, 21, 19, -20};\n"
-			<< "Line Loop (30) = {-6, 22, 21, -23};\n"
-			<< "Line Loop (31) = {5, 22, -18, -17};\n"
-			<< "Line Loop (32) = {15, -19, -23, 7};\n"
-			<< "Line Loop (33) = {-6, 22, 21, -23};\n"
-			<< "Line Loop (34) = {17, 20, -15, -4};\n"
-			<< "Line Loop (35) = {9, 13, -16, -2};\n"
-			<< "Line Loop (36) = {10, -12, -15, 8};\n"
-			<< "Line Loop (37) = {-3, 16, 14, -17};\n"
-			<< "Plane Surface (24) = {24};\n"
-			<< "Plane Surface (25) = {25};\n"
-			<< "Plane Surface (26) = {26};\n"
-			<< "Plane Surface (27) = {27};\n"
-			<< "Plane Surface (28) = {28};\n"
-			<< "Plane Surface (29) = {29};\n"
-			<< "Plane Surface (30) = {30};\n"
-			<< "Plane Surface (31) = {31};\n"
-			<< "Plane Surface (32) = {32};\n"
-			<< "Plane Surface (33) = {33};\n"
-			<< "Plane Surface (34) = {34};\n"
-			<< "Plane Surface (35) = {35};\n"
-			<< "Plane Surface (36) = {36};\n"
-			<< "Plane Surface (37) = {37};\n"
-			<< "Surface Loop (1) = {32, 31, 27, 29, 33, 34};\n"
-			<< "Surface Loop (2) = {35, 36, 34, 26, 24, 28, 37};\n"
-			<< "Volume (1) = {1};\n"
-            << "Volume (2) = {2};\n";
-	}
+        //typedef Simplex<2> convex_type;
+        ostr << "Mesh.MshFileVersion = 1;\n"
+             << "h=" << hsize << ";\n"
+             << "Point (1) = {0, 0, 0, h};\n"
+             << "Point (2) = {3.5, 0, 0, h};\n"
+             << "Point (3) = {3.5, 3, 0, h};\n"
+             << "Point (4) = {1, 3, 0, h};\n"
+             << "Point (5) = {0, 3, 0, h};\n"
+             << "Point (6) = {1, 17, 0, h};\n"
+             << "Point (7) = {0, 17, 0, h};\n"
+             << "Line (1) = {1, 2};\n"
+             << "Line (2) = {2, 3};\n"
+             << "Line (3) = {3, 4};\n"
+             << "Line (4) = {4, 5};\n"
+             << "Line (5) = {4, 6};\n"
+             << "Line (6) = {6, 7};\n"
+             << "Line (7) = {7, 5};\n"
+             << "Line (8) = {5, 1};\n"
+             << "Line Loop (9) = {1, 2, 3, 4, 8};\n"
+             << "Line Loop (10) = {5, 6, 7, -4};\n"
+             << "Plane Surface (9) = {9};\n"
+             << "Plane Surface (10) = {10};\n"
+            // physical entities
+             << "Physical Surface (1) = {9};\n"
+             << "Physical Surface (2) = {10};\n";
+    }
 
+    else { //3D Mesh
+        //typedef Simplex<3> convex_type;
+        ostr << "Mesh.MshFileVersion = 1;\n"
+             << "h=" << hsize << ";\n"
+             << "d=" << deep << ";\n"
+             << "Point (1) = {0, 0, 0, h};\n"
+             << "Point (2) = {3.5, 0, 0, h};\n"
+             << "Point (3) = {3.5, 3, 0, h};\n"
+             << "Point (4) = {1, 3, 0, h};\n"
+             << "Point (5) = {0, 3, 0, h};\n"
+             << "Point (6) = {1, 17, 0, h};\n"
+             << "Point (7) = {0, 17, 0, h};\n"
+             << "Point (8) = {0, 0, d, h};\n"
+             << "Point (9) = {3.5, 0, d, h};\n"
+             << "Point (10) = {3.5, 3, d, h};\n"
+             << "Point (11) = {1, 3, d, h};\n"
+             << "Point (12) = {0, 3, d, h};\n"
+             << "Point (13) = {1, 17, d, h};\n"
+             << "Point (14) = {0, 17, d, h};\n"
+             << "Line (1) = {1, 2};\n"
+             << "Line (2) = {2, 3};\n"
+             << "Line (3) = {3, 4};\n"
+             << "Line (4) = {4, 5};\n"
+             << "Line (5) = {4, 6};\n"
+             << "Line (6) = {6, 7};\n"
+             << "Line (7) = {7, 5};\n"
+             << "Line (8) = {5, 1};\n"
+             << "Line (9) = {2, 9};\n"
+             << "Line (10) = {1, 8};\n"
+             << "Line (11) = {8, 9};\n"
+             << "Line (12) = {12, 8};\n"
+             << "Line (13) = {9, 10};\n"
+             << "Line (14) = {10, 11};\n"
+             << "Line (15) = {5, 12};\n"
+             << "Line (16) = {3, 10};\n"
+             << "Line (17) = {4, 11};\n"
+             << "Line (18) = {11, 13};\n"
+             << "Line (19) = {14, 12};\n"
+             << "Line (20) = {11, 12};\n"
+             << "Line (21) = {13, 14};\n"
+             << "Line (22) = {6, 13};\n"
+             << "Line (23) = {7, 14};\n"
+             << "Line Loop (24) = {1, 9, -11, -10};\n"
+             << "Line Loop (25) = {-3, 16, 14, 20, -15, -4 };\n"
+             << "Line Loop (26) = {1, 2, 3, 4, 8};\n"
+             << "Line Loop (27) = {5, 6, 7, -4};\n"
+             << "Line Loop (28) = {11, 13, 14, 20, 12};\n"
+             << "Line Loop (29) = {18, 21, 19, -20};\n"
+             << "Line Loop (30) = {-6, 22, 21, -23};\n"
+             << "Line Loop (31) = {5, 22, -18, -17};\n"
+             << "Line Loop (32) = {15, -19, -23, 7};\n"
+             << "Line Loop (33) = {-6, 22, 21, -23};\n"
+             << "Line Loop (34) = {17, 20, -15, -4};\n"
+             << "Line Loop (35) = {9, 13, -16, -2};\n"
+             << "Line Loop (36) = {10, -12, -15, 8};\n"
+             << "Line Loop (37) = {-3, 16, 14, -17};\n"
+             << "Plane Surface (24) = {24};\n"
+             << "Plane Surface (25) = {25};\n"
+             << "Plane Surface (26) = {26};\n"
+             << "Plane Surface (27) = {27};\n"
+             << "Plane Surface (28) = {28};\n"
+             << "Plane Surface (29) = {29};\n"
+             << "Plane Surface (30) = {30};\n"
+             << "Plane Surface (31) = {31};\n"
+             << "Plane Surface (32) = {32};\n"
+             << "Plane Surface (33) = {33};\n"
+             << "Plane Surface (34) = {34};\n"
+             << "Plane Surface (35) = {35};\n"
+             << "Plane Surface (36) = {36};\n"
+             << "Plane Surface (37) = {37};\n"
+             << "Surface Loop (1) = {32, 31, 27, 29, 33, 34};\n"
+             << "Surface Loop (2) = {35, 36, 34, 26, 24, 28, 37};\n"
+             << "Volume (1) = {1};\n"
+             << "Volume (2) = {2};\n";
+    }
 
+    std::cout<<"start "<<std::endl;
     std::ostringstream nameStr;
     nameStr.precision( 3 );
     nameStr << "fin_sink";
+    gmsh_ptrtype gmshp( new Gmsh );
+    gmshp->setPrefix( nameStr.str() );
+    gmshp->setDescription( ostr.str() );
+    return gmshp;
 
-    std::string mesh_name, mesh_desc;
-	(mesh_name, mesh_desc) = std::make_pair( nameStr.str(), ostr.str() );
-	//return std::make_pair( nameStr.str(); ostr.str() );
-	
-	
-	// generate gmsh mesh
-	//std::string fname = gmsh.generate( mesh_name, mesh_desc );
-	
-	// mesh_ptrtype return type
-    //typedef Mesh<convex_type > mesh_type;
-    //typedef boost::shared_ptr<mesh_type> mesh_ptrtype;
-	mesh_ptrtype mesh;
-	mesh = (mesh_name, mesh_desc);
-	
-	return mesh;
-	
-	
 }
 
