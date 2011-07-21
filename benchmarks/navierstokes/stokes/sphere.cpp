1b2b02de40d63d1982aed1e8929b9a704a417ece
gpena <gpena@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-07-21 10:27:50 +0000

    New version
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7440 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- benchmarks/navierstokes/stokes/sphere.cpp
+++ benchmarks/navierstokes/stokes/sphere.cpp
@@ -116,11 +116,11 @@ int main(int argc, char** argv)
     auto uur = vf::project( _space=Ph, _expr=ur);
     auto uut = vf::project( _space=Ph, _expr=utheta);
 
-    auto u_1 = vf::project( _space=Ph, _expr=U*( (3*R*Px()*Px()/(4*r*r*r))*(R*R/(r*r) -1) -(R/4*r)*(3 + R*R/(r*r)) +1 ));
-    auto u_2 = vf::project( _space=Ph, _expr=U*( (3*R*Px()*Py()/(4*r*r*r))*(R*R/(r*r) -1) ) );
-    auto u_3 = vf::project( _space=Ph, _expr=U*( (3*R*Px()*Pz()/(4*r*r*r))*(R*R/(r*r) -1) ) );
+    auto u_1 = U*( (3*R*Px()*Px()/(4*r*r*r))*(R*R/(r*r) -1) -(R/4*r)*(3 + R*R/(r*r)) +1 );
+    auto u_2 = U*( (3*R*Px()*Py()/(4*r*r*r))*(R*R/(r*r) -1) );
+    auto u_3 = U*( (3*R*Px()*Pz()/(4*r*r*r))*(R*R/(r*r) -1) );
     auto p_goncalo = vf::project( _space=Ph, _expr=-3*mu*R*U*Px()/(2*r*r*r) );
-    //auto u_goncalo = vf::project( _space=Vh, _expr=(vec(u_1,u_2,u_3)) );
+    auto u_goncalo = vf::project( _space=Vh, _expr=vec(u_1,u_2,u_3));
 
     auto sigmaN=-idv(p)*N()+.5*mu*(gradv(u)+trans(gradv(u)))*N();
     auto volume = integrate( _range=elements(mesh), _expr=cst(1.0),_quad=_Q<6>(), _geomap=(GeomapStrategyType)vm["geomap"].as<int>()  ).evaluate();
@@ -155,9 +155,9 @@ int main(int argc, char** argv)
     exporter->step(0)->add( "uy", u.comp(Y) );
     exporter->step(0)->add( "uy", u.comp(Z) );
     exporter->step(0)->add( "p", p );
-    exporter->step(0)->add( "u_1x", u_1 );
-    exporter->step(0)->add( "u_2y", u_2 );
-    exporter->step(0)->add( "u_3y", u_3 );
+    exporter->step(0)->add( "u_1x", u_goncalo.comp(X) );
+    exporter->step(0)->add( "u_2y", u_goncalo.comp(Y) );
+    exporter->step(0)->add( "u_3y", u_goncalo.comp(Z) );
     exporter->step(0)->add( "p_goncalo", p_goncalo );
 
     exporter->save();
