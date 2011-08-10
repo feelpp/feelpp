3ec8717f6638498ce02c087e50072fdd427eae13
asamake <asamake@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-08-10 14:05:51 +0000

    fix operator lift
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7588 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- testsuite/feeldiscr/test_lift.cpp
+++ testsuite/feeldiscr/test_lift.cpp
@@ -3,7 +3,7 @@
   This file is part of the Feel library
 
   Author(s): Abdoulaye Samake <abdoulaye.samake@e.ujf-grenoble.fr>
-      Date: 2011-08-08
+       Date: 2011-08-10
 
   Copyright (C) 2008-2010 Universite Joseph Fourier (Grenoble I)
 
@@ -21,14 +21,12 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
-   \file test_lift.cpp
-   \author Abdoulaye Samake <abdoulaye.samake@e.ujf-grenoble.fr>
-   \date 2011-08-08
+   \file test.cpp
+   \author Abdoulaye Samake<abdoulaye.samake@e.ujf-grenoble.fr>
+   \date 2011-08-10
 */
 
 #include <feel/options.hpp>
-
-#include <feel/feelcore/application.hpp>
 
 #include <feel/feelalg/backend.hpp>
 
@@ -48,24 +46,25 @@
 
 #include <feel/feelvf/vf.hpp>
 
-
+/** use Feel namespace */
 using namespace Feel;
 using namespace Feel::vf;
+
 
 inline
 po::options_description
 makeOptions()
 {
-    po::options_description testLiftoptions("TestLift options");
-    testLiftoptions.add_options()
-        ("hsize", po::value<double>()->default_value( 0.5 ), "mesh size")
+    po::options_description testliftoptions("TestLift options");
+    testliftoptions.add_options()
+        ("hsize", po::value<double>()->default_value( 0.02 ), "mesh size")
         ("shape", Feel::po::value<std::string>()->default_value( "hypercube" ), "shape of the domain (either simplex or hypercube)")
         ("nu", po::value<double>()->default_value( 1 ), "grad.grad coefficient")
         ("weakdir", po::value<int>()->default_value( 1 ), "use weak Dirichlet condition" )
         ("penaldir", Feel::po::value<double>()->default_value( 10 ),
          "penalisation parameter for the weak boundary Dirichlet formulation")
         ;
-    return testLiftoptions.add( Feel::feel_options() );
+    return testliftoptions.add( Feel::feel_options() );
 }
 
 
@@ -73,8 +72,8 @@ inline
 AboutData
 makeAbout()
 {
-    AboutData about( "testlift" ,
-                     "testlift" ,
+    AboutData about( "test_lift" ,
+                     "test_lift" ,
                      "0.2",
                      "nD(n=1,2,3) TestLift on simplices or simplex products",
                      Feel::AboutData::License_GPL,
@@ -82,7 +81,9 @@ makeAbout()
 
     about.addAuthor("Christophe Prud'homme", "developer", "christophe.prudhomme@ujf-grenoble.fr", "");
     return about;
+
 }
+
 
 template<int Dim>
 class TestLift
@@ -100,32 +101,17 @@ public:
 
     typedef boost::shared_ptr<backend_type> backend_ptrtype;
 
-    typedef typename backend_type::sparse_matrix_type sparse_matrix_type;
-
-    typedef typename backend_type::sparse_matrix_ptrtype sparse_matrix_ptrtype;
-
-    typedef typename backend_type::vector_type vector_type;
-
-    typedef typename backend_type::vector_ptrtype vector_ptrtype;
-
     typedef Simplex<Dim> convex_type;
 
     typedef Mesh<convex_type> mesh_type;
 
-    typedef boost::shared_ptr<mesh_type> mesh_ptrtype;
-
-    //typedef bases<Lagrange<Order,Scalar> > basis_type;
-    typedef bases<Lagrange<Order,Vectorial> > basis_type;
+    typedef bases<Lagrange<Order,Scalar> > basis_type;
 
     typedef FunctionSpace<mesh_type, basis_type> space_type;
-
-    typedef boost::shared_ptr<space_type> space_ptrtype;
 
     typedef typename space_type::element_type element_type;
 
     typedef Exporter<mesh_type> export_type;
-
-    typedef boost::shared_ptr<export_type> export_ptrtype;
 
     /**
      * Constructor
@@ -144,7 +130,6 @@ public:
     void run( const double* X, unsigned long P, double* Y, unsigned long N );
 
 private:
-
 
     backend_ptrtype M_backend;
 
@@ -178,64 +163,117 @@ TestLift<Dim>::run( const double* X, unsigned long P, double* Y, unsigned long N
     if ( X[1] == 1 ) shape = "hypercube";
 
     if ( !this->vm().count( "nochdir" ) )
-        Environment::changeRepository( boost::format( "doc/tutorial/%1%/%2%-%3%/P%4%/h_%5%/" )
+
+        Environment::changeRepository( boost::format( "testsuite/feeldiscr/%1%/%2%-%3%/P%4%/h_%5%/" )
                                        % this->about().appName()
                                        % shape
                                        % Dim
                                        % Order
                                        % meshSize );
 
-
-    mesh_ptrtype mesh = createGMSHMesh( _mesh=new mesh_type,
+    auto mesh = createGMSHMesh( _mesh=new mesh_type,
                                         _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES|MESH_RENUMBER,
-                                        _desc=domain( _name=(boost::format( "%1%-%2%-%3%" ) % shape % Dim % 1 ).str() ,
-                                                      _usenames=false,
+                                        _desc=domain( _name=(boost::format( "%1%-%2%-%3%" ) % shape % Dim % 1).str() ,
+                                                      _usenames=true,
                                                       _shape=shape,
                                                       _dim=Dim,
                                                       _h=X[0],
-                                                       _xmin=-1.0,
-                                                      _xmax=1.0,
-                                                      _ymin=-1.0,
-                                                      _ymax=1.0 ) );
+                                                      _xmin=0.,
+                                                      _xmax=1.,
+                                                      _ymin=0.,
+                                                      _ymax=1.,
+                                                      _zmin=0.,
+                                                      _zmax=1.) );
 
 
 
-    space_ptrtype Xh = space_type::New( mesh );
-    element_type u( Xh, "u" );
-
-    using namespace Feel::vf;
+    auto Xh = space_type::New( mesh );
+    auto u = Xh->element();
+    auto v = Xh->element();
 
     value_type pi = M_PI;
 
-    // auto g = cos(pi*Py());
-    auto g = cos(pi*Py())*unitX() + cos(pi*Py())*unitY() ;
+    auto g = sin(pi*Px())*cos(pi*Py())*cos(pi*Pz());
+
+    auto f = cst(0.);
+
+    bool weakdir = this->vm()["weakdir"].template as<int>();
 
     WeakDirichlet dir_type = (WeakDirichlet)this->vm()["weakdir"].template as<int>();
 
+    value_type penaldir = this->vm()["penaldir"].template as<double>();
+
+    value_type nu = this->vm()["nu"].template as<double>();
+
+    using namespace Feel::vf;
+
+    auto F =  M_backend->newVector( Xh ) ;
+
+    form1( _test=Xh, _vector=F, _init=true ) =
+        integrate( elements(mesh), f*id(v) ) ;
+
+    if ( this->comm().size() != 1 || weakdir )
+    {
+
+        form1( _test=Xh, _vector=F ) +=
+            integrate( markedfaces(mesh,"Dirichlet"),
+                       g*(-grad(v)*vf::N()+penaldir*id(v)/hFace()) );
+
+    }
+
+    F->close();
+
+    auto D = M_backend->newMatrix( Xh, Xh ) ;
+
+    form2( Xh, Xh, D, _init=true ) =
+        integrate( elements(mesh), nu*gradt(u)*trans(grad(v)) );
+
+    if ( this->comm().size() != 1 || weakdir )
+    {
+
+        form2( Xh, Xh, D ) +=
+            integrate( markedfaces(mesh,"Dirichlet"),
+                       -(gradt(u)*vf::N())*id(v)
+                       -(grad(v)*vf::N())*idt(u)
+                       +penaldir*id(v)*idt(u)/hFace());
+
+        D->close();
+    }
+    else
+    {
+        D->close();
+
+        form2( Xh, Xh, D ) +=
+            on( markedfaces(mesh, "Dirichlet"), u, F, g );
+
+    }
+
+    backend_type::build()->solve( _matrix=D, _solution=u, _rhs=F );
+
     auto op_lift = operatorLift(Xh, M_backend, 20.0, dir_type);
 
-    auto glift1 = op_lift->lift( _range=markedfaces(mesh,1),_expr=trans(g));
+    auto glift = op_lift->lift( _range=markedfaces(mesh,"Dirichlet"),_expr=trans(g));
 
-    auto glift2 = (*op_lift)(markedfaces(mesh,1),trans(g));
+    auto glift2 = (*op_lift)( _range=markedfaces(mesh,"Dirichlet"),_expr= g);
 
-    auto ux = idv(glift1)(0,0);
+    auto gproj =  vf::project(_space=Xh, _range=elements(mesh), _expr=g );
 
-    auto uy = idv(glift1)(1,0);
-
-    auto tempx = trace(hessv(glift1)(0,0));
-
-    auto tempy = trace(hessv(glift1)(1,0));
-
-    auto temp = tempx*oneX() + tempy*oneY();
-
-    // auto verif1 = vf::project(Xh, elements(mesh), idv(temp));
-    //auto verif1 = vf::project(Xh, elements(mesh), trace(hessv(glift1)));
-    //auto verif2 = vf::project(Xh, elements(mesh), trace(hessv(glift2)));
-
-    auto gproj = vf::project(Xh, elements(mesh), g);
+    double L2error2 =integrate(elements(mesh),
+                               (idv(u)-idv(glift))*(idv(u)-idv(glift)) ).evaluate()(0,0);
 
 
-    export_ptrtype exporter( export_type::New( this->vm(),
+    double semi_H1error2 =integrate(elements(mesh),
+                                   ( gradv(u)-gradv(glift) )*trans( (gradv(u)-gradv(glift)) ) ).evaluate()(0,0);
+
+    double L2error =   math::sqrt( L2error2 );
+
+    double H1error = math::sqrt( L2error2 + semi_H1error2 );
+
+    Log() << "||error||_L2=" << L2error << "\n";
+
+    Log() << "||error||_H1=" << H1error << "\n";
+
+    auto exporter( export_type::New( this->vm(),
                                                (boost::format( "%1%-%2%-%3%" )
                                                 % this->about().appName()
                                                 % shape
@@ -246,17 +284,19 @@ TestLift<Dim>::run( const double* X, unsigned long P, double* Y, unsigned long N
 
         exporter->step(0)->setMesh( mesh );
 
-        exporter->step(0)->add( "glift1", glift1 );
-        exporter->step(0)->add( "glift2", glift2 );
+        exporter->step(0)->add( "u", u );
 
-        // exporter->step(0)->add( "verif1", verif1 );
-        // exporter->step(0)->add( "verif2", verif2 );
+        exporter->step(0)->add( "glift", glift );
+
         exporter->step(0)->add( "g", gproj );
 
         exporter->save();
+
         Log() << "exportResults done\n";
     }
+
 } // TestLift::run
+
 
 int
 main( int argc, char** argv )
@@ -270,13 +310,11 @@ main( int argc, char** argv )
         return 0;
     }
 
+    app.add( new TestLift<1>( app.vm(), app.about() ) );
     app.add( new TestLift<2>( app.vm(), app.about() ) );
+    // app.add( new TestLift<3>( app.vm(), app.about() ) );
 
     app.run();
 
 }
-
-
-
-
 
