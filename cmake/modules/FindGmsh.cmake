c56b37574d617d9fbaee2d42affaef962cf6fc58
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-09-24 20:46:39 +0000

    move Find_package gmsh to its own cmake module now check for Gmsh interface
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7713 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ cmake/modules/FindGmsh.cmake
@@ -0,0 +1,56 @@
+# -*- mode: cmake -*-
+#
+#  This file is part of the Feel library
+#
+#  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+#       Date: 2010-07-28
+#
+#  Copyright (C) 2010 Université de Grenoble 1 (Joseph Fourier)
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+include (FindPackageHandleStandardArgs)
+INCLUDE(CheckIncludeFileCXX)
+
+FIND_PATH(GMSH_INCLUDE_DIR
+  GMSH.h
+  PATHS /usr/include/ /usr/include/gmsh/ /usr/local/include/gmsh
+  DOC "Directory where GMSH header files are stored" )
+
+
+CHECK_INCLUDE_FILE_CXX(GMSH.h HAVE_GMSH_H)
+
+FIND_LIBRARY(GMSH_LIBRARY GMSH
+  /usr/lib
+  /usr/local/lib
+  )
+
+find_program( GMSH_EXECUTABLE gmsh DOC "GMSH mesh generator" )
+
+# handle the QUIETLY and REQUIRED arguments and set Madlib_FOUND to TRUE if
+# all listed variables are TRUE
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (GMSH DEFAULT_MSG
+  GMSH_INCLUDE_DIR GMSH_LIBRARY GMSH_EXECUTABLE
+  )
+
+if ( GMSH_FOUND )
+  MESSAGE( STATUS "GMSH found: header(${GMSH_INCLUDE_DIR}) lib(${GMSH_LIBRARY}) executable(${GMSH_EXECUTABLE})" )
+endif()
+
+mark_as_advanced( GMSH_INCLUDE_DIR )
+mark_as_advanced( GMSH_LIBRARY )
+mark_as_advanced( GMSH_EXECUTABLE )
+
+
