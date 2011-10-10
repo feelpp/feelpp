00f6736396049b03f5838bf388c602561ae10ce3
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-10-10 12:05:24 +0000

    header for openturns wrappers
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7780 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ feel/feelcore/opuswrapper.hpp
@@ -0,0 +1,54 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the Feel library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2010-03-19
+
+  Copyright (C) 2010,2011 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file opuswrapper.hpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2010-03-19
+ */
+#ifndef __OpusWrapper_H
+#define __OpusWrapper_H 1
+
+#define OPUS_FUNC_EXEC_BODY_IN_TEMPDIR( classname, dirname )            \
+{                                                                       \
+char* currentWorkingDirectory = getCurrentWorkingDirectory (0) ;        \
+char* temporaryDirectory=createTemporaryDirectory(#dirname,p_exchangedData,0); \
+                                                                        \
+int rc = 0;                                                             \
+try                                                                     \
+{                                                                       \
+    CAST(classname*,p_state)->run( INPOINT_ARRAY, INPOINT_SIZE, OUTPOINT_ARRAY, OUTPOINT_SIZE ); \
+}                                                                       \
+catch( ... )                                                            \
+{                                                                       \
+    rc = 1;                                                             \
+}                                                                       \
+if (rc) {                                                               \
+    PRINT( "Error in class "#classname );                               \
+    return WRAPPER_EXECUTION_ERROR;                                     \
+}                                                                       \
+deleteTemporaryDirectory ( temporaryDirectory , rc, 0 ) ;               \
+free ( currentWorkingDirectory ) ;                                      \
+}
+
+#endif /* __OpusWrapper_H */
