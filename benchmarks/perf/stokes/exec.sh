3f889449323a421a5bc4d5ccff7033b0d13eddf6
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-10-11 22:27:49 +0000

    added script to execute bench and retrieve data from bench
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7806 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ benchmarks/perf/stokes/exec.sh
@@ -0,0 +1,19 @@
+#!/bin/.sh
+
+for h in 0.2 0.1 0.05 0.025 0.0125 0.01
+do
+
+  ./feel_perf_stokes --hsize=$h --bctype=1 -ksp_constant_null_space true -pc_type fieldsplit -pc_fieldsplit_type schur -fieldsplit_0_pc_type lu -fieldsplit_0_ksp_type gmres -fieldsplit_1_pc_type none -fieldsplit_1_ksp_type gmres   -fieldsplit_1_ksp_max_it 1000 -fieldsplit_1_ksp_converged_use_initial_residual_norm true > log-$h 2>&1
+
+echo "=============================="
+echo "h=$h"
+cat $HOME/feel/perf/stokes/stokes/Simplex_2_1_2/P1/h_$h/toto-1.0 | grep "_error||_2"
+cat $HOME/feel/perf/stokes/stokes/Simplex_2_1_2/P1/h_$h/toto-1.0 | grep time
+cat $HOME/feel/perf/stokes/stokes/Simplex_2_1_2/P1/h_$h/toto-1.0 | grep elements
+cat $HOME/feel/perf/stokes/stokes/Simplex_2_1_2/P1/h_$h/toto-1.0 | grep dof
+
+
+
+
+
+done
