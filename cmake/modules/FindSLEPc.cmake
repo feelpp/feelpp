440e9212a6a4961a0141981e44a8a4efe361a7d6
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2011-11-12 22:55:51 +0000

    up slepc cmake script for 3.1 on Debian
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@7924 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/modules/FindSLEPc.cmake
+++ cmake/modules/FindSLEPc.cmake
@@ -21,16 +21,27 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
+foreach( debian_arches linux kfreebsd )
+  IF ( "${CMAKE_BUILD_TYPE}" STREQUAL "Debug" )
+    set( DEBIAN_FLAVORS ${debian_arches}-gnu-c-debug ${debian_arches}-gnu-c-opt ${DEBIAN_FLAVORS})
+  ELSE()
+    set( DEBIAN_FLAVORS ${debian_arches}-gnu-c-opt ${debian_arches}-gnu-c-debug ${DEBIAN_FLAVORS})
+  ENDIF()
+endforeach()
+
+
 find_path (SLEPC_DIR include/slepc.h
   HINTS ENV SLEPC_DIR
   PATHS
+  /usr/lib/slepcdir/3.1 # Debian
   /usr/lib/slepcdir/3.0.0 # Debian
   $ENV{HOME}/slepc
   DOC "SLEPc Directory")
 
+
 SET(SLEPC_INCLUDE_DIR "${SLEPC_DIR}/include/")
 CHECK_INCLUDE_FILE( ${SLEPC_INCLUDE_DIR}/slepc.h FEEL_HAVE_SLEPC_H )
-FIND_LIBRARY(SLEPC_LIB_SLEPC     slepc HINTS ${SLEPC_DIR}/lib )
+FIND_LIBRARY(SLEPC_LIB_SLEPC     slepc HINTS ${SLEPC_DIR}/linux-gnu-c-opt/lib )
 SET(SLEPC_LIBRARIES ${SLEPC_LIB_SLEPC} CACHE STRING "SLEPc libraries" FORCE)
 # handle the QUIETLY and REQUIRED arguments and set Madlib_FOUND to TRUE if
 # all listed variables are TRUE
