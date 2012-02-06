b82aa558b74631851a43b416210976ec8cd5908b
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-02-06 11:34:31 +0000

    added xml archive
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8248 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- feel/feelcore/application.hpp
+++ feel/feelcore/application.hpp
@@ -44,6 +44,8 @@
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
 #include <boost/serialization/string.hpp> // Needed to send/receive strings!
 
 #include <boost/mpi.hpp>
