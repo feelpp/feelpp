05afc0c388e1a59a6c3e1001badb0f5f07cb97fe
chakir <chakir@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-02-07 21:16:17 +0000

    new version of nirb.cpp
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8261 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- doc/manual/nirb/nirb.cpp
+++ doc/manual/nirb/nirb.cpp
@@ -33,6 +33,20 @@
 #include <feel/feelfilters/exporter.hpp>
 #include <feel/feelvf/vf.hpp>
 
+//#include <feel/feelalg/solvereigen.hpp>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include <Eigen/Core>
+#include <Eigen/LU>
+#include <Eigen/Dense> 
+#include <Eigen/Eigenvalues>
+
+#include <vector>
+
 /** use Feel namespace */
 using namespace Feel;
 using namespace Feel::vf;
@@ -58,7 +72,8 @@ makeOptions()
 	("NbSnapshot",po::value<int>()->default_value(100),"numbers of snapshot computed")
         ("sizeBR",po::value<int>()->default_value(15),"size of reduced basis")
 	
-	("mu", po::value<double>()->default_value( 0 ), "angle in [0,pi/2]")
+	("muMin", po::value<double>()->default_value( 0 ), "angle in [0,pi/2]")
+	("muMax", po::value<double>()->default_value( M_PI/2),"angle in [0,pi/2]")
         ;
     return laplacianoptions.add( Feel::feel_options() );
 }
@@ -138,14 +153,25 @@ public:
         CoarseMeshSize( this->vm()["hcoarsesize"].as<double>() ),
 	NbSnapshot( this->vm()["NbSnapshot"].as<int>() ),
 	sizeBR(this->vm()["sizeBR"].as<int>()),
-        mu( this->vm()["mu"].as<double>() )
+        muMin( this->vm()["muMin"].as<double>() ),
+        muMax( this->vm()["muMax"].as<double>() )
     {
     }
 
     void run();
 
     void run( const double* X, unsigned long P, double* Y, unsigned long N );
-    element_type blackbox( space_ptrtype Xh, double param );
+    element_type blackbox( space_ptrtype Xh, double param);
+    element_type BuildNirbSolution(space_ptrtype XhFine,space_ptrtype XhCoarse,double param);
+    void ConstructNIRB (space_ptrtype Xh); 
+    void CalculSnapshot(space_ptrtype Xh);
+    void ChooseRBFunction(space_ptrtype Xh);
+    void OrthogonalisationRBFunction(space_ptrtype Xh);
+    void OrthogonalisationRBFunctionL2GrammSchmidt(space_ptrtype Xh,Eigen::MatrixXd &M );
+
+    Eigen::MatrixXd  ConstructStiffMatrixSnapshot(space_ptrtype Xh);
+    Eigen::MatrixXd  ConstructStiffMatrixRB(space_ptrtype Xh);
+    Eigen::MatrixXd  ConstructMassMatrixRB(space_ptrtype Xh); 
     
 private:
 
@@ -158,7 +184,7 @@ private:
 
     // Reduced basis parameter
     int NbSnapshot,sizeBR;
-    double mu;
+    double muMin,muMax;
 }; // NIRBTEST
 const uint16_type NIRBTEST::Order;
 void
@@ -166,12 +192,13 @@ NIRBTEST::run()
 {
     std::cout << "------------------------------------------------------------\n";
     std::cout << "Execute NIRBTEST<" << 2 << ">\n";
-    std::vector<double> X(5);
+    std::vector<double> X(6);
     X[0] = FineMeshSize;
     X[1] = CoarseMeshSize;
     X[2] = NbSnapshot;
     X[3] = sizeBR;
-    X[4] = mu;
+    X[4] = muMin;
+    X[5] = muMax;
     std::vector<double> Y( 1 );
     run( X.data(), X.size(), Y.data(), Y.size() );
 }
@@ -214,36 +241,429 @@ NIRBTEST::run( const double* X, unsigned long P, double* Y, unsigned long N )
     //Selection of X[2] fonctions to build the "reduced basis" using a POD technique
     //Orthogonalisation in L2 and H1 norm of "reduced basis function"
     //Save this final functions
+    std:: cout << "STEP ONE  : Construction of the 'non intruisive' reduced basis (nirb) functions " << endl;
+    ConstructNIRB(XhFine) ;	
      
 
    //STEP TWO : Approximation of the solution using the "nirb" functions for a choosen mu 
     	
-
-
-
-    for( int i =0; i < sizeBR; ++i )
-    {
-        double p = i*M_PI/(2*9);
-        auto uFine = blackbox( XhFine, p );
-        auto uCoarse = blackbox( XhCoarse, p );
-
-        exporterFine->step(0)->add( (boost::format("uFine-%1%")%i).str(), uFine );
-        exporterCoarse->step(0)->add( (boost::format("uCoarse-%1%")%i).str(), uCoarse );
-    }
-
-    exporterFine->save();
-    exporterCoarse->save(); 
-    exporter2Grid->save();
-
+    std:: cout << "STEP TWO " << endl; 
+ 
+   
+    
+    double p = 5*M_PI/(2*9); 
+    auto uFine = XhFine->element();
+    auto uCoarse = XhCoarse->element();
+    auto uNirb = XhFine->element();
+    uFine = blackbox( XhFine, p );
+    uCoarse = blackbox( XhCoarse, p);
+    uNirb = BuildNirbSolution(XhFine,XhCoarse,p);
+    std :: cout << "After BuildNirbSolution " << endl;
+   
+    exporterFine->step(0)->add("uFine", uFine ); 
+    exporterCoarse->step(0)->add("uCoarse", uCoarse ); 
+    exporter2Grid->step(0)->add( "uNirb", uNirb ); 
+ 
+    exporterFine->save(); 
+    exporterCoarse->save();  
+    exporter2Grid->save(); 
 
 }
-NIRBTEST::element_type
-NIRBTEST::blackbox( space_ptrtype Xh, double param )
+//-----------------------------------------
+//-----------------------------------------
+void NIRBTEST::CalculSnapshot(space_ptrtype Xh){
+  
+	int XhNdof = Xh->nLocalDof(); 
+	double pi = 3.14159265;
+	char path [50], buf[3]; 
+	auto ui = Xh->element();
+        for (int i =0; i<NbSnapshot;i++){
+		double theta = i*(muMax-muMax)/(113.);
+		ui = blackbox(Xh,theta);
+		strcpy(path,"./Sol_");
+		sprintf(buf,"%d",i);
+		strcat(path,buf);
+		ui.save(_path=path);  
+	}
+}
+//-----------------------------------------
+//-----------------------------------------
+Eigen::MatrixXd NIRBTEST :: ConstructStiffMatrixSnapshot(space_ptrtype Xh){ 
+// 	std::cout << "Entering ConstructStiffMatrix routine "<<endl;
+	char path [50], buf[3]; 
+	auto ui = Xh->element();
+	auto uj = Xh->element();
+	auto D = M_backend->newMatrix( _test=Xh, _trial=Xh  );//Sparse F.E stiffness matrix D 
+ 	form2( _test=Xh, _trial=Xh, _matrix=D ) =
+        integrate( _range=elements(Xh->mesh()), _expr=gradt(ui)*trans(grad(uj)));
+	
+	Eigen::MatrixXd S (NbSnapshot,NbSnapshot);//Dense RB stiffness matrix S
+	 	 
+        for (int i = 0; i< NbSnapshot;i++){  
+		strcpy(path,"./Sol_");
+		sprintf(buf,"%d",i);
+		strcat(path,buf);
+		ui.load(_path=path);
+	 
+ 		for(int j=0; j< i;j++){   
+			strcpy(path,"./Sol_");
+			sprintf(buf,"%d",j);
+			strcat(path,buf);
+			uj.load(_path=path); 
+			double Sij = D->energy(ui,uj); 
+			S(i,j) = Sij;
+			S(j,i) = Sij;
+		}	
+ 		double Sii = D->energy(ui,ui); 
+		S(i,i) = Sii;	
+	}  
+	
+// 	std::cout << "Quitting ConstructStiffMatrix routine " << endl;
+	
+ 	return S;
+}
+ 
+//-----------------------------------------
+//-----------------------------------------
+Eigen::MatrixXd NIRBTEST :: ConstructStiffMatrixRB(space_ptrtype Xh){
+	char path [50], buf[3]; 
+	auto ui = Xh->element();
+	auto uj = Xh->element();
+	auto D = M_backend->newMatrix( _test=Xh, _trial=Xh  ); //Sparse FE stiffness matrix
+	form2( _test=Xh, _trial=Xh, _matrix=D ) =
+        integrate( _range=elements(Xh->mesh()), _expr=gradt(ui)*trans(grad(uj)));
+	
+	Eigen::MatrixXd S (sizeBR,sizeBR); //Dense RB stiffness matrix
+	
+	for (int i = 0; i< sizeBR;i++){  
+		strcpy(path,"./Sol_OR");
+		sprintf(buf,"%d",i);
+		strcat(path,buf);
+		ui.load(_path=path);
+	
+		for(int j=0; j< i;j++){   
+			strcpy(path,"./Sol_OR");
+			sprintf(buf,"%d",j);
+			strcat(path,buf);
+			uj.load(_path=path); 
+			double Sij = D->energy(ui,uj);
+			S(i,j) = Sij;
+			S(j,i) = Sij;
+		}	
+		double Sii = D->energy(ui,ui);
+		S(i,i) = Sii;	
+	} 
+	return S;
+}
+
+//-----------------------------------------
+//-----------------------------------------
+Eigen::MatrixXd NIRBTEST :: ConstructMassMatrixRB(space_ptrtype Xh){
+	char path [50], buf[3]; 
+	auto ui = Xh->element();
+	auto uj = Xh->element();
+	auto D = M_backend->newMatrix( _test=Xh, _trial=Xh  ); //Sparse FE mass matrix
+	form2( _test=Xh, _trial=Xh, _matrix=D ) =
+	integrate( _range=elements(Xh->mesh()), _expr=idt(ui)*id(uj));
+	
+	Eigen::MatrixXd S (sizeBR,sizeBR); //Dense RB mass matrix
+	
+	for (int i = 0; i< sizeBR;i++){  
+		strcpy(path,"./Sol_OR");
+		sprintf(buf,"%d",i);
+		strcat(path,buf);
+		ui.load(_path=path);
+	
+		for(int j=0; j< i;j++){   
+			strcpy(path,"./Sol_OR");
+			sprintf(buf,"%d",j);
+			strcat(path,buf);
+			uj.load(_path=path); 
+			double Sij = D->energy(ui,uj);
+			S(i,j) = Sij;
+			S(j,i) = Sij;
+		}	
+		double Sii = D->energy(ui,ui);
+		S(i,i) = Sii;	
+	} 
+	return S;
+} 
+//-----------------------------------------
+//-----------------------------------------
+void NIRBTEST ::ChooseRBFunction(space_ptrtype Xh){
+   
+	char path [50], buf[3];
+	
+	Eigen::MatrixXd S (NbSnapshot,NbSnapshot);//Dense Stiffness Matrix 
+	S = ConstructStiffMatrixSnapshot(Xh);
+	Eigen::EigenSolver <Eigen::MatrixXd> eigen_solver(S); 
+	//int nb_EigenValue = eigen_solver.eigenvalues().size();
+	//eigen_solver.eigenvectors().col(i)  =  eigenvector #i
+	std :: cout << "Computation of the eigenvalues of the stiffness matrix S (NbSnapshot,NbSnapshot) s: OK" << endl;
+	//Sorting N = "SizeBR" F.E solutions uh(mu_k) the more represented in the eigenvectors
+	//associated to the N largest eigenvalues
+	 
+ 	
+	
+	int *Lind = new int [NbSnapshot];
+	int *Uind = new int [NbSnapshot];
+	
+	for (int i =0;i<NbSnapshot;i++){
+	    Lind[i] = i;
+	    Uind[i] = -1; 
+	} 
+	double memory;
+	int compt,tempi;
+	bool marqueur;
+	
+	
+	
+	strcpy(path,"./IndBR"); 
+	// saving the index's number to identify the NIRB basis functions 
+	sprintf(buf,"%d",sizeBR);
+	strcat(path,buf);
+	std::ofstream find(path); 
+	if (!find){
+	  std :: cerr <<" 'ChooseRBFunction routine' - Error in opening file :" << path << endl;
+	} 
+	//Sorting eigenvector #i 
+	Eigen::VectorXd Vi(NbSnapshot); 
+	for (int i=0;i<sizeBR;i++){ 
+	  for(int ri = 0; ri < NbSnapshot;ri++){
+	     Vi[ri] =  real(eigen_solver.eigenvectors().col(i)[ri]);
+	     Lind[ri] = ri;
+	  }
+	 
+	  for (int j = 1; j< NbSnapshot;j++){  
+	    memory = Vi[j];
+	    tempi = Lind[j];
+	    compt = j-1;
+	    marqueur = std::abs (Vi[compt])>std::abs(memory);
+	    while(marqueur){
+	      if (std::abs(Vi[compt])>std::abs(memory)){
+		Vi[compt+1] = Vi[compt];
+		Lind[compt+1]= Lind[compt,1];
+		compt--;
+		marqueur = true;
+	      }
+	      else marqueur = false;
+	      if (compt<0)
+		marqueur = false;
+	    }
+	    Vi[compt+1] = memory;
+	    Lind[compt+1] = tempi;
+	  } 
+	  //Extracting reduced basis function #i + update of Uind
+	  bool pasoki = true;
+	  int rc = NbSnapshot-1;
+	  while (pasoki && rc >0){
+	    if (Uind [Lind[rc]] ==-1){
+	      pasoki= false; 
+	      Uind [Lind[rc]] = i;    
+	      find << Lind[rc]<<endl; 
+	    }
+	    else rc--; 
+	  }
+	  
+	}
+ 	find.close();  
+    delete [] Lind;
+    delete [] Uind;
+}
+
+//-----------------------------------------
+//-----------------------------------------
+void NIRBTEST :: OrthogonalisationRBFunctionL2GrammSchmidt
+	      (space_ptrtype Xh,Eigen::MatrixXd & M ){
+//      auto ui = Xh->element(); 
+//      auto uj = Xh->element();
+//      auto D = M_backend->newMatrix( _test=Xh, _trial=Xh  ); //Sparse FE mass matrix
+//      form2( _test=Xh, _trial=Xh, _matrix=D ) =
+// 	integrate( _range=elements(Xh->mesh()), _expr=idt(ui)*id(uj));
+   std::cout << " In OrthogonalisationRBFunctionL2GrammSchmidt :ATTENTION : sORTHOGONALISATION NOT DONE !" << endl;
+ 
+   
+}
+
+//-----------------------------------------
+//-----------------------------------------
+void NIRBTEST ::OrthogonalisationRBFunction(space_ptrtype Xh){
+     //First step : L2 -Pre-orthogonalization and normalization using a Gramm-schmidt method
+     
+    int Ndof = Xh->nLocalDof();
+    char path [50], buf[3];
+    int TindBR;
+    auto ui = Xh->element(); 
+    Eigen :: MatrixXd M(Ndof,sizeBR);
+    strcpy(path,"./IndBR"); 
+//     reading the index's number to identify the NIRB basis functions 
+    sprintf(buf,"%d",sizeBR);
+    strcat(path,buf);
+    std :: ifstream f_in(path);
+    if (!f_in){
+      std :: cerr << "In 'OrthogonalisationRBFunction' subroutine : Error in opening file -> " << path << endl;
+    }
+    for (int i=0;i<sizeBR;i++){
+      f_in >> TindBR; 
+      strcpy(path,"./Sol_");
+      sprintf(buf,"%d",TindBR);
+      strcat(path,buf);
+      ui.load(_path=path);
+      for (int j=0;j<Ndof;j++){
+	M(j,i) = ui(j);
+      }
+    } 
+     
+//      element_ptrtype Pu (new element_type(u.functionSpace()));
+//      Pu->zero();
+     double Dtemp;
+     OrthogonalisationRBFunctionL2GrammSchmidt(Xh,M);
+     
+     //save in a file the matrix A
+     for (int i=0;i<sizeBR;i++){
+	ui.zero();
+	for (int j =0;j<Ndof;j++){
+	  Dtemp = M(j,i);
+	  ui(j)=Dtemp;
+	}
+	strcpy(path,"./Sol_OR_");
+	sprintf(buf,"%d",i);
+	strcat(path,buf);
+	ui.save(_path=path);
+    }
+     
+     
+     
+     
+     //Second step : Solving a generalized eigenvalue problem
+     //A*Epsilon_i = \lambda_i*B*Epsilon_i
+     //A : Dense stiffness matrix in the pre-orthogonalized nirb basis
+     //B : Dense mass matrix in the pre-orthogonalized nirb basis  = Idendity matrix
+     
+     //Construction of the matrix A and B
+     
+    M.resize(sizeBR,sizeBR);  
+    Eigen :: MatrixXd A (sizeBR,sizeBR);
+    Eigen :: MatrixXd B (sizeBR,sizeBR); 
+    Eigen :: MatrixXd C (sizeBR,sizeBR);
+    A = ConstructStiffMatrixRB(Xh);
+    B = ConstructMassMatrixRB(Xh);
+    
+//     Eigen:: GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd> es(A,B);
+    M = B.inverse()*A;
+    Eigen:: EigenSolver<Eigen::MatrixXd> es (M);
+    M.resize(Ndof,sizeBR);//to download the NIRB function after the Gramm-schmidt orthogonalisation
+    
+    for (int i=0;i<sizeBR;i++){
+	ui.zero();
+	strcpy(path,"./Sol_OR_");
+	sprintf(buf,"%d",i);
+	strcat(path,buf);
+	ui.load(_path=path);
+	for (int j =0;j<Ndof;j++){
+	   M(j,i) = ui(j);;
+	}
+    }
+     
+    
+    A.resize(Ndof,sizeBR);//to upload the final NIRB function after H1-L2 orthogonalisation
+    A.setZero(Ndof,sizeBR);
+    for (int i = 0; i < sizeBR;i++){
+      for (int j =0;j< Ndof;j++){
+	for (int k =0;k< sizeBR;k++){
+	    A(j,i)=A(j,i)+ real(es.eigenvectors().col(i)[k])*M(j,k);
+	}
+      }
+    }
+    
+     OrthogonalisationRBFunctionL2GrammSchmidt(Xh,A);
+    
+    
+    //save in a file the final NIRB basis functions
+     for (int i=0;i<sizeBR;i++){
+	ui.zero();
+	for (int j =0;j<Ndof;j++){
+	  Dtemp = A(j,i);
+	  ui(j)=Dtemp;
+	}
+	strcpy(path,"./NIRB_BasisF_");
+	sprintf(buf,"%d",i);
+	strcat(path,buf);
+	ui.save(_path=path);
+    }
+     
+    
+
+    
+}
+//---------------------------------------------------
+//---------------------------------------------------
+NIRBTEST::element_type NIRBTEST ::BuildNirbSolution(space_ptrtype XhFine,space_ptrtype XhCoarse, double param){
+  
+  std :: cout << "In BuildNirbSolution" << endl;
+  char path [50], buf[3];
+  
+  auto uNirb = XhFine->element();
+  auto uCoarse = XhCoarse->element();
+  auto uCoarseInterpolate = XhFine->element();
+  auto ui = XhFine->element();
+ 
+  auto D = M_backend->newMatrix( _test=XhFine, _trial=XhFine  ); //Sparse FE mass matrix
+	form2( _test=XhFine, _trial=XhFine, _matrix=D ) =
+	integrate( _range=elements(XhFine->mesh()), _expr=idt(uNirb)*id(ui));
+   
+  uCoarse = blackbox(XhCoarse,param);//Computation of the coarse solution 
+  interpolate (XhFine,uCoarse,uCoarseInterpolate); 
+  //Interpolation of the coarse solution on the fine Mesh to compute the coefficiant BetaiH
+  
+  //Computation of the coefficiant \BetaiH = \int uCoarse*\Epsilon_i 
+  //with \Epsilon_i being the final Nirb basis functions
+   
+  double BetaiH;
+  uNirb.zero();
+  for (int i =0;i<sizeBR;i++){
+    ui.zero();
+     //Reading Nirb basis function #i and building the Nirb solution  
+    
+    strcpy(path,"./NIRB_BasisF_");
+    sprintf(buf,"%d",i);
+    strcat(path,buf);
+    ui.load(_path=path);
+    
+    BetaiH  = D->energy(uCoarseInterpolate,ui);
+    for (int j=0;j<XhFine->nLocalDof();j++){
+      BetaiH = ui(j)*BetaiH;
+      ui(j) = BetaiH;
+    }
+    uNirb = uNirb + ui; 
+  }
+  return uNirb;
+  
+  
+}
+
+//-----------------------------------------
+
+//-----------------------------------------
+//-----------------------------------------
+void NIRBTEST :: ConstructNIRB(space_ptrtype Xh){
+	
+	CalculSnapshot(Xh);
+	std :: cout << "Computation of the " << NbSnapshot << " snapshots : OK " << endl;
+	ChooseRBFunction(Xh); 
+	std:: cout << "Choice of " << sizeBR << " Reduced basis functions :OK " << endl; 
+	//Orthogonalisation de Gram-schmidt
+	OrthogonalisationRBFunction(Xh);	
+}
+
+
+//---------------------------------------------------
+NIRBTEST::element_type NIRBTEST::blackbox( space_ptrtype Xh, double param )
 {
 
     auto u = Xh->element();
     auto v = Xh->element();
-    std::cout << "Xh ndof=" << Xh->nLocalDof() << "\n";
+   // std::cout << "Xh ndof=" << Xh->nLocalDof() << "\n";
     value_type pi = M_PI;
 
     //! deduce from expression the type of g (thanks to keyword 'auto')
