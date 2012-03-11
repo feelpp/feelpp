6a363f5a531ebe43aa96b36ac007abc96aa8a814
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-03-11 18:34:30 +0000

    opus -> crb
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8559 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- applications/crb/heat1d/heat1dapp.cpp
+++ applications/crb/heat1d/heat1dapp.cpp
@@ -1,10 +1,10 @@
 /* this file is generated automatically */
 #include <heat1d.hpp>
-#include <feel/feelcrb/opusapp.hpp>
+#include <feel/feelcrb/crbapp.hpp>
 
 int main( int argc, char** argv )
 {
-    Feel::OpusApp<Feel::Heat1D> app( argc, argv,
+    Feel::CrbApp<Feel::Heat1D> app( argc, argv,
                                                       Feel::makeHeat1DAbout( "heat1d" ),
                                                       Feel::makeHeat1DOptions()  );
     app.run();
