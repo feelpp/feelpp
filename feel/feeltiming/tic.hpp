c758e4b19f2cc5de6961a845f518bdd7a6061208
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-03-20 07:51:35 +0000

    added timer framework
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8624 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ feel/feeltiming/tic.hpp
@@ -0,0 +1,69 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*-
+
+  This file is part of the Feel library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+       Date: 2012-03-20
+
+  Copyright (C) 2012 Université Joseph Fourier (Grenoble I)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file tic.hpp
+   \author Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+   \date 2012-03-20
+ */
+#if !defined(FEELPP_TIMING_TIC_HPP)
+#define FEELPP_TIMING_TIC_HPP 1
+
+#include <feel/feeltiming/timer.hpp>
+#include <feel/feeltiming/now.hpp>
+
+namespace Feel
+{
+namespace details
+{
+struct SecondBasedTimer
+{
+    static void print(const double& val);
+    static inline double  time() { return details::now(); }
+};
+
+counter<double,SecondBasedTimer> const sec_timer = {};
+}  // details
+} // Feel
+
+namespace Feel
+{
+namespace time
+{
+inline void tic() { details::sec_timer.tic(); }
+
+inline double  toc( bool display = true)
+{
+    return details::sec_timer.toc(display);
+}
+} // time
+} // Feel
+
+namespace Feel
+{
+  // Convenience namespace injection from time:: into Feel::
+  using time::tic;
+  using time::toc;
+}
+
+#endif /* FEELPP_TIMING_TIC_HPP */
