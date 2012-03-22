9cb56ce5b5f4897d72629c646b10f6945a4a13e2
calandreau <calandreau@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-03-22 16:30:32 +0000

    Finalcabineheat extension de cabineheat sur géométrie complète. Pas encore fonctionnel
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8649 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ applications/crb/Finalcabineheat/Finalcabineheatapp.cpp
@@ -0,0 +1,11 @@
+/* this file is generated automatically */
+#include <Finalcabineheat.hpp>
+#include <feel/feelcrb/opusapp.hpp>
+
+int main( int argc, char** argv )
+{
+    Feel::OpusApp<Feel::FinalCabineHeat> app( argc, argv,
+                                                      Feel::makeFinalCabineHeatAbout( "Finalcabineheat" ),
+                                                      Feel::makeFinalCabineHeatOptions()  );
+    app.run();
+}
