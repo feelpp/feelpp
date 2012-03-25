19d00e8295a1017b0bfb59e572b091b249f4c2dd
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-03-25 11:53:46 +0200

    --warnings (init of members in constructors)

--- doc/manual/dd/nonoverlapping.cpp
+++ doc/manual/dd/nonoverlapping.cpp
@@ -116,7 +116,6 @@ public:
         M_backend( backend_type::build( this->vm() ) ),
         meshSize( this->vm()["hsize"].template as<double>() ),
         shape( this->vm()["shape"].template as<std::string>() ),
-        timers(),
         M_firstExporter( export_type::New( this->vm(),
                                            (boost::format( "%1%-%2%-%3%" )
                                             % this->about().appName()
@@ -126,7 +125,9 @@ public:
                                             (boost::format( "%1%-%2%-%3%" )
                                              % this->about().appName()
                                              % Dim
-                                             % int(2)).str() ) )
+                                             % int(2)).str() ) ),
+        timers()
+
     {}
 
     template<typename DirichletExpr,
