c60371c9a0ff8c908f73be7d47b78daacd841ce1
chabanne <chabanne@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-04-03 18:36:10 +0000

    up test
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8717 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- testsuite/feeldiscr/test_operatorinterpolation.cpp
+++ testsuite/feeldiscr/test_operatorinterpolation.cpp
@@ -17,8 +17,6 @@ using boost::unit_test::test_suite;
 using namespace Feel;
 using namespace Feel::vf;
 
-
-
 namespace test_operatorinterpolation
 {
 
@@ -64,137 +62,71 @@ template <uint32_type OrderGeo>
 void
 test2dTo1d( Application_ptrtype test_app)
 {
-
-
     typedef Backend<double> backend_type;
-    typedef boost::shared_ptr<backend_type> backend_ptrtype;
 
     typedef Mesh<Simplex<1,OrderGeo,2> > mesh_1d_type;
     typedef Mesh<Simplex<2,OrderGeo,2> > mesh_2d_type;
-    typedef boost::shared_ptr< mesh_2d_type > mesh_ptrtype;
 
-    //typedef bases<Lagrange<2,Scalar,Continuous,PointSetFekete> > basis_2d_type;
-    typedef bases<Lagrange<2,Vectorial,Continuous,PointSetFekete> > basis_2d_type;
+    typedef bases<Lagrange<3,Vectorial,Continuous,PointSetFekete> > basis_2d_type;
     typedef FunctionSpace<mesh_2d_type, basis_2d_type> space_2d_type;
-    typedef boost::shared_ptr<space_2d_type> space_2d_ptrtype;
     typedef typename space_2d_type::element_type element_2d_type;
 
-    //typedef bases<Lagrange<3,Scalar,Continuous,PointSetFekete> > basis_1d_type;
-    typedef bases<Lagrange<1,Vectorial,Continuous,PointSetFekete> > basis_1d_type;
+    typedef bases<Lagrange<5,Vectorial,Continuous,PointSetFekete> > basis_1d_type;
     typedef FunctionSpace<mesh_1d_type, basis_1d_type> space_1d_type;
-    typedef boost::shared_ptr<space_1d_type> space_1d_ptrtype;
     typedef typename space_1d_type::element_type element_1d_type;
 
     //-----------------------------------------------------------//
 
-    double meshSize = test_app->vm()["hsize"].as<double>();
-#if 0
-    GeoTool::Node x1(0,0);
-    GeoTool::Node x2(0.5,0);
-    GeoTool::Circle C( meshSize,"OMEGA",x1,x2);
-
-    GeoTool::Node x3(-0.5,0);
-    GeoTool::Node x4(0.5,0);
-    GeoTool::Line L( meshSize, "Line",x3,x4);
-#else
+    auto meshSize = test_app->vm()["hsize"].as<double>();
     GeoTool::Node x1(0,0);
     GeoTool::Node x2(2,1);
     GeoTool::Rectangle C( meshSize,"OMEGA",x1,x2);
+    auto mesh2d = C.createMesh(_mesh=new mesh_2d_type,_name="domain");
 
     GeoTool::Node x3(0,0);
     GeoTool::Node x4(2,1);
     GeoTool::Line L( meshSize, "Line",x3,x4);
-#endif
-    GeoTool::Node x5(0.2,0.2);
-    GeoTool::Node x6(0.8,0.8);
-    GeoTool::Rectangle Rbis( meshSize,"koko",x5,x6);
-    auto mesh2dBis = Rbis.createMesh<mesh_2d_type>("domainBis");
-    auto Xh2dBis = space_2d_type::New(mesh2dBis);
+    auto mesh1d = L.createMesh(_mesh=new mesh_1d_type,_name="domain1d");
 
-    auto mesh2d = C.createMesh<mesh_2d_type>("domain");
-    auto mesh1d = L.createMesh<mesh_1d_type>("domain1d");
+    //-----------------------------------------------------------//
 
-    auto Xh1d = space_1d_type::New(mesh1d);
-    auto Xh2d = space_2d_type::New(mesh2d);
+    auto Xh1d = space_1d_type::New(_mesh=mesh1d);
+    auto Xh2d = space_2d_type::New(_mesh=mesh2d);
 
-    //auto u1d = Xh1d->element();
-    //auto u1d = vf::project(Xh1d,elements(mesh1d),vec( cos(M_PI*Px()),sin(M_PI*Py()) ) );
+    auto u1d = Xh1d->element();
+    auto u2d = vf::project(_space=Xh2d,
+                           _range=elements(mesh2d),
+                           _expr=vec( cos(M_PI*Px()),sin(M_PI*Py()) ) );
 
-    //auto u1d = vf::project(Xh1d,elements(mesh1d), cst(0.) );
-    //auto u2d = vf::project(Xh2d,elements(mesh2d), cos(M_PI*Px()) );
-    auto u1d = vf::project(Xh1d,elements(mesh1d),vec( cst(0.),cst(0.) ) );
-    auto u2d = vf::project(Xh2d,elements(mesh2d),vec( cos(M_PI*Px()),sin(M_PI*Py()) ) );
-
-
-
-
-    auto M_backend = backend_type::build( test_app->vm() );
-
+    //-----------------------------------------------------------//
 
     auto opI=opInterpolation( _domainSpace=Xh2d,
-                              //_imageSpace=Xh2dBis,
-                              _imageSpace=Xh1d,
-                              //_range=elements(mesh),
-                              _backend=M_backend );
-    //size check
-    std::cout << "\n size1 "<< opI->matPtr()->size1();
-    std::cout << "\n size2 "<< opI->matPtr()->size2();
-    std::cout << "\n size u2d "<< u2d.size();
-    std::cout << "\n size u1d "<< u1d.size();
-#if 1
+                              _imageSpace=Xh1d);
     opI->apply(u2d,u1d);
 
-    for (uint32_type i=0;i<u1d.size();++i)
-        std::cout << "\n i = "<< i << " val : " << u1d(i);
-
-#endif
-
-    double s = integrate(elements(mesh1d), trans(idv(u2d)-idv(u1d))*(idv(u2d)-idv(u1d))).evaluate()(0,0);
-
-    std::cout << "\ns = " << s ;
-#if 1
-
-    //BOOST_CHECK_SMALL( s1-s2,1e-8);
-#if 0
-    auto UNexporter2d = Exporter<mesh_2d_type>::New( test_app->vm(), "Export2d" );
-    UNexporter2d->step( 0 )->setMesh( mesh2d );
-    UNexporter2d->step( 0 )->add( "u2d", u2d );
-    UNexporter2d->save();
-#endif
-    u1d = vf::project(Xh1d,elements(mesh1d),vec( cos(M_PI*Px()),sin(M_PI*Py()) ) );
-    auto UNexporter1d = Exporter<mesh_1d_type>::New( test_app->vm(), "Export1d" );
-    UNexporter1d->step( 0 )->setMesh( mesh1d );
-    UNexporter1d->step( 0 )->add( "u1d", u1d );
-    //    UNexporter1d->step( 0 )->add( "u2dbis", u2d );
-    UNexporter1d->save();
-#endif
-
-
+    auto s = integrate(_range=elements(mesh1d),
+                         _expr=trans(idv(u2d)-idv(u1d))*(idv(u2d)-idv(u1d)) ).evaluate()(0,0);
+    BOOST_CHECK_SMALL( s,1e-8);
 }
 
 //---------------------------------------------------------------------------------------------//
-
-
+//---------------------------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------------------//
 
 template <uint32_type OrderGeo>
 void
-test2d( Application_ptrtype test_app)
+test2dTo2d( Application_ptrtype test_app)
 {
-
     typedef Backend<double> backend_type;
-    typedef boost::shared_ptr<backend_type> backend_ptrtype;
 
     typedef Mesh<Simplex<2,OrderGeo,2> > mesh_type;
-    typedef boost::shared_ptr<  mesh_type > mesh_ptrtype;
 
     typedef bases<Lagrange<3,Vectorial,Continuous,PointSetFekete> > basis_1_type;
     typedef FunctionSpace<mesh_type, basis_1_type> space_1_type;
-    typedef boost::shared_ptr<space_1_type> space_1_ptrtype;
     typedef typename space_1_type::element_type element_1_type;
 
     typedef bases<Lagrange<4,Vectorial,Continuous,PointSetFekete> > basis_2_type;
     typedef FunctionSpace<mesh_type, basis_2_type> space_2_type;
-    typedef boost::shared_ptr<space_2_type> space_2_ptrtype;
     typedef typename space_2_type::element_type element_2_type;
 
     //-------------------------------------------------------
@@ -218,10 +150,10 @@ test2d( Application_ptrtype test_app)
                      _range=elements(mesh),
                      _expr=vec( cos(M_PI*Px()),sin(M_PI*Py()) ) );
 
-    auto M_backend = backend_type::build( test_app->vm() );
+    auto mybackend = backend_type::build( test_app->vm() );
     auto opI=opInterpolation( _domainSpace=Xh1,
                               _imageSpace=Xh2,
-                              _backend=M_backend );
+                              _backend=mybackend );
     opI->apply(u1,u2);
 
     auto s1 = integrate(_range=elements(mesh),
@@ -233,7 +165,7 @@ test2d( Application_ptrtype test_app)
     auto opIa=opInterpolation( _domainSpace=Xh1,
                                _imageSpace=Xh2,
                                _range=boundaryfaces(mesh),
-                               _backend=M_backend );
+                               _backend=mybackend );
     opIa->apply(u1,u2a);
 
     auto s2a = integrate(_range=boundaryfaces(mesh),
@@ -253,7 +185,7 @@ test2d( Application_ptrtype test_app)
     auto opI2=opInterpolation( _domainSpace=Xh1,
                                _imageSpace=Xh2bis,
                                _range=elements(mesh2),
-                               _backend=M_backend );
+                               _backend=mybackend );
     opI2->apply(u1,u2bis);
 
     auto s3 = integrate(_range=elements(mesh2),
@@ -265,7 +197,7 @@ test2d( Application_ptrtype test_app)
     auto opI3=opInterpolation( _domainSpace=Xh1,
                                _imageSpace=Xh2bis,
                                _range=boundaryfaces(mesh2),
-                               _backend=M_backend );
+                               _backend=mybackend );
     opI3->apply(u1,u2bisbis);
 
     auto s7 = integrate(_range=boundaryfaces(mesh2),
@@ -281,27 +213,23 @@ BOOST_AUTO_TEST_SUITE( interp_operatorinterpolation )
 
 BOOST_AUTO_TEST_CASE( interp_operatorinterpolation )
 {
-
+    using namespace Feel::vf;
     using namespace test_operatorinterpolation;
 
-    using namespace Feel::vf;
-
-    auto test_app = Application_ptrtype( new Application_type(boost::unit_test::framework::master_test_suite().argc,
-                                                              boost::unit_test::framework::master_test_suite().argv,
-                                                              makeAbout(),
-                                                              makeOptions()
-                                                              ) );
+    Application_ptrtype test_app( new Application_type(boost::unit_test::framework::master_test_suite().argc,
+                                                       boost::unit_test::framework::master_test_suite().argv,
+                                                       test_operatorinterpolation::makeAbout(),
+                                                       test_operatorinterpolation::makeOptions()
+                                                       ) );
 
     test_app->changeRepository( boost::format( "/testsuite/feeldiscr/%1%/" )
-                                % test_app->about().appName()
-        );
-#if 1
-    test_operatorinterpolation::test2d<1>(test_app);
-    test_operatorinterpolation::test2d<2>(test_app);
-    test_operatorinterpolation::test2d<3>(test_app);
-#else
+                                % test_app->about().appName() );
+
+    test_operatorinterpolation::test2dTo2d<1>(test_app);
+    test_operatorinterpolation::test2dTo2d<2>(test_app);
+    //test_operatorinterpolation::test2dTo2d<3>(test_app);
     test_operatorinterpolation::test2dTo1d<1>(test_app);
-#endif
+    test_operatorinterpolation::test2dTo1d<2>(test_app);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
