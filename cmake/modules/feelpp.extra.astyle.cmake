21474633d65df8ffcfe17a0de11c93a6401068de
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-04-09 13:04:23 +0000

    configure astyle script
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8739 18f2cc81-8059-4896-b63e-f2d89ec8fd72

---
+++ cmake/modules/feelpp.extra.astyle.cmake
@@ -0,0 +1,29 @@
+# -*- mode: cmake -*-
+#
+#  This file is part of the Feel library
+#
+#  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+#       Date: 2012-04-09
+#
+#  Copyright (C) 2012 Université Joseph Fourier
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+find_program(ASTYLE astyle
+  HINTS
+  # for macosx mac port
+  /opt/local/bin )
+configure_file( scripts/astyle/feelpp.astyle.sh.in ${FEELPP_SOURCE_DIR}/scripts/astyle/feelpp.astyle.sh )
