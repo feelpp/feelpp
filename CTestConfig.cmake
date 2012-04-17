bc7559c87c9405da7f365d1fc9750c5ddfd7e88c
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-04-17 12:59:12 +0200

    update cdash scripts, use git now

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -12,7 +12,7 @@ set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Feel%2B%2B")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
-set(CTEST_UPDATE_TYPE "svn")
+set(CTEST_UPDATE_TYPE "git")
 
 set(CTEST_PROJECT_SUBPROJECTS
   feel++
@@ -20,5 +20,5 @@ set(CTEST_PROJECT_SUBPROJECTS
   testsuite
   examples
   benchmarks
-  opus
+  crb
   )
