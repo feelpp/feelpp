3f2aa0a1d4165cd20f087471c0aa792c59516500
chabanne <chabanne@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-04-26 14:48:38 +0000

    up WorldComm
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8884 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- feel/feelfilters/exportergmsh.hpp
+++ feel/feelfilters/exportergmsh.hpp
@@ -84,9 +84,9 @@ public:
      */
     //@{
 
-    ExporterGmsh( std::string const& __p = "default", int freq = 1 );
+    ExporterGmsh( std::string const& __p = "default", int freq = 1, WorldComm const& worldComm = WorldComm() );
 
-    ExporterGmsh( po::variables_map const& vm, std::string const& exp_prefix = "" );
+    ExporterGmsh( po::variables_map const& vm, std::string const& exp_prefix = "", WorldComm const& worldComm = WorldComm() );
 
     ExporterGmsh( ExporterGmsh const & __ex );
 
