42006537080e8ef81321a7b186569cf28a1f4f6e
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-05-10 11:57:41 +0000

    up
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@8974 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- cmake/dashboard/mac.sh
+++ cmake/dashboard/mac.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-#set -x
+#!/bin/sh
+set -x
 # $1 provides the path to the toplevel source Feel++ directory
 # e.g. $HOME/Devel/FEEL/feelpp.git
 COMMON="ctest -VV -S $1/cmake/dashboard/testsuite.cmake,FEELPP_CTEST_CONFIG=$1/cmake/dashboard/feelpp.site.`hostname -s`.cmake,FEELPP_MODE=$2"
