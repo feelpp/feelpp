f963063b49cb71674ba7bb1dd0dfb412ac4a94fc
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-05-15 10:10:06 +0200

    need the path for ctest, now crontab starts properly the nightly tests

--- cmake/dashboard/mac.sh
+++ cmake/dashboard/mac.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-set -x
 # $1 provides the path to the toplevel source Feel++ directory
 # e.g. $HOME/Devel/FEEL/feelpp.git
-COMMON="ctest -VV -S $1/cmake/dashboard/testsuite.cmake,FEELPP_CTEST_CONFIG=$1/cmake/dashboard/feelpp.site.`hostname -s`.cmake,FEELPP_MODE=$2"
-#$COMMON-gcc-4.4.6,FEELPP_CXX=g++-4.4,FEELPP_EXPLICIT_VECTORIZATION=novec
-#$COMMON-gcc-4.4.6,FEELPP_CXX=g++-4.4,FEELPP_EXPLICIT_VECTORIZATION=SSE2
-#$COMMON-gcc-4.5.2,FEELPP_CXX=g++-4.5,FEELPP_EXPLICIT_VECTORIZATION=novec
+# $2 provide the ctest mode: e.g. Nightly or Experimental
+
+COMMON="/opt/local/bin/ctest -VV -S $1/cmake/dashboard/testsuite.cmake,FEELPP_CTEST_CONFIG=$1/cmake/dashboard/feelpp.site.`hostname -s`.cmake,FEELPP_MODE=$2"
+
 if [ -x /opt/local/bin/g++-mp-4.6 ]; then
     $COMMON,FEELPP_CXXNAME=gcc-4.6.2,FEELPP_CXX=/opt/local/bin/g++-mp-4.6,FEELPP_EXPLICIT_VECTORIZATION=SSE2
     $COMMON,FEELPP_CXXNAME=gcc-4.6.2-mpi,FEELPP_CXX=/opt/local/bin/g++-mp-4.6,FEELPP_EXPLICIT_VECTORIZATION=SSE2,FEELPP_ENABLE_MPI_MODE=ON
