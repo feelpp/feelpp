d08d3b14977a23d3861c4359898f7fbcaac18ff7
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-05-22 23:47:33 +0200

    Update issue 18 added encode() and decode() in mesh data structure to compress it it is used to send/receive meshes

--- feel/feeldiscr/mesh.hpp
+++ feel/feeldiscr/mesh.hpp
@@ -6,7 +6,7 @@
        Date: 2005-07-05
 
   Copyright (C) 2005,2006 EPFL
-  Copyright (C) 2007,2008,2009,2010 Université Joseph Fourier (Grenoble I)
+  Copyright (C) 2006-2012 Université Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -337,20 +337,20 @@ public:
      */
     int markerName( std::string const& marker ) const
     {
-        return M_markername.find( marker )->second.template get<0>();
+        return M_markername.find( marker )->second[0];
     }
     /**
      * \return the topological dimension associated to the \p marker
      */
     int markerDim( std::string const& marker ) const
     {
-        return M_markername.find( marker )->second.template get<1>();
+        return M_markername.find( marker )->second[1];
     }
 
     /**
      * \return the marker names
      */
-    std::map<std::string, boost::tuple<int, int> > markerNames() const
+    std::map<std::string, std::vector<int> > markerNames() const
     {
         return M_markername;
     }
@@ -389,7 +389,7 @@ public:
     /**
      * add a new marker name
      */
-    void addMarkerName( std::pair<std::string, boost::tuple<int,int> > const& marker )
+    void addMarkerName( std::pair<std::string, std::vector<int> > const& marker )
     {
         M_markername.insert( marker );
     }
@@ -399,7 +399,9 @@ public:
      */
     void addMarkerName( std::string __name, int __id ,int __topoDim )
     {
-        auto data = boost::make_tuple( __id,__topoDim );
+        std::vector<int> data(2);
+        data[0]=__id;
+        data[1]=__topoDim;
         M_markername[__name]=data;
     }
 
@@ -540,6 +542,30 @@ public:
      */
     void checkAndFixPermutation();
 
+    /**
+     * send the mesh data structure to processor \p p with  \p tag
+     */
+    void send( int p, int tag );
+
+    /**
+     * receive the mesh data structure to processor \p p with  \p tag
+     */
+    void recv( int p, int tag );
+
+    /**
+     * encode the mesh data structure into a tighter data structure and to avoid
+     * pointers in order to serialize it for saving/loading and
+     * sending/receiving the mesh
+     */
+    void encode();
+
+    /**
+     * decode the mesh data structure from a tighter data structure and to avoid
+     * pointers in order to serialize it for saving/loading and
+     * sending/receiving the mesh
+     */
+    void decode();
+
     BOOST_PARAMETER_MEMBER_FUNCTION( ( void ),
                                      save,
                                      tag,
@@ -590,6 +616,7 @@ public:
           ( name,(std::string) )
           ( path,* ) )
         ( optional
+          ( update,(size_type), MESH_CHECK|MESH_UPDATE_EDGES|MESH_UPDATE_FACES )
           ( type,( std::string ),std::string( "binary" ) )
           ( suffix,( std::string ),std::string( "" ) )
           ( sep,( std::string ),std::string( "" ) )
@@ -641,6 +668,17 @@ public:
                 //boost::archive::xml_iarchive ia(ifs);
                 //ia >> *this;
             }
+            if ( update )
+            {
+                this->components().reset();
+                this->components().set( update );
+                this->updateForUse();
+            }
+
+            else
+            {
+                this->components().reset();
+            }
             return true;
         }
 
@@ -684,8 +722,35 @@ private:
     template<class Archive>
     void serialize( Archive & ar, const unsigned int version )
         {
-            Debug( 4015 ) << "(de)Serializing mesh ...\n";
-            ar & boost::serialization::base_object<super>( *this );
+            if ( Archive::is_saving::value )
+            {
+                Debug( 4015 ) << "Serializing mesh(saving) ...\n";
+                Debug( 4015 ) << "encoding...\n";
+                encode();
+                Debug( 4015 ) << "loading markers...\n";
+                ar & M_markername;
+                Debug( 4015 ) << "loading pts...\n";
+                ar & M_enc_pts;
+                Debug( 4015 ) << "loading faces...\n";
+                ar & M_enc_faces;
+                Debug( 4015 ) << "loading elts...\n";
+                ar & M_enc_elts;
+            }
+            if ( Archive::is_loading::value )
+            {
+                Debug( 4015 ) << "Serializing mesh(loading) ...\n";
+                Debug( 4015 ) << "loading markers...\n";
+                ar & M_markername;
+                Debug( 4015 ) << "loading pts...\n";
+                ar & M_enc_pts;
+                Debug( 4015 ) << "loading faces...\n";
+                ar & M_enc_faces;
+                Debug( 4015 ) << "loading elts...\n";
+                ar & M_enc_elts;
+                decode();
+
+            }
+
         }
 public:
     struct Inverse
@@ -1118,7 +1183,22 @@ private:
      * get<0>() provides the id
      * get<1>() provides the topological dimension
      */
-    std::map<std::string, boost::tuple<int, int> > M_markername;
+    std::map<std::string, std::vector<int> > M_markername;
+
+    /**
+     * to encode points coordinates
+     */
+    std::map<uint64_type, boost::tuple<bool, std::vector<int>, std::vector<double> > > M_enc_pts;
+
+    /**
+     * to encode elements
+     */
+    std::map<uint64_type, std::vector<int> > M_enc_faces;
+
+    /**
+     * to encode elements
+     */
+    std::map<uint64_type, std::vector<int> > M_enc_elts;
 
     /**
      * tool for localize point in the mesh
@@ -1155,8 +1235,8 @@ Mesh<Shape, T, Tag>::createSubmesh( self_type& new_mesh,
     BOOST_FOREACH( auto marker, new_mesh.M_markername )
     {
         std::cout << "marker name " << marker.first
-                  << " id: " << marker.second.template get<0>()
-                  << " geoe: " << marker.second.template get<1>() << "\n";
+                  << " id: " << marker.second[0]
+                  << " geoe: " << marker.second[1] << "\n";
 
     }
     // How the nodes on this mesh will be renumbered to nodes
@@ -1333,7 +1413,7 @@ Mesh<Shape, T, Tag>::createP1mesh() const
     // inherit the table of markersName
     BOOST_FOREACH( auto itMark, this->markerNames() )
     {
-        new_mesh->addMarkerName( itMark.first,itMark.second.template get<0>(),itMark.second.template get<1>() );
+        new_mesh->addMarkerName( itMark.first,itMark.second[0],itMark.second[1] );
     }
 
 
