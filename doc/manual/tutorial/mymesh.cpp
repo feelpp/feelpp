e4d88d98b0b3d60eb2ff2a61ce630bf150b781da
prudhomm <prudhomm@18f2cc81-8059-4896-b63e-f2d89ec8fd72>
2012-05-22 22:54:48 +0000

    test for mesh serialization
    git-svn-id: svn+ssh://forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/life/trunk/life/trunk@9012 18f2cc81-8059-4896-b63e-f2d89ec8fd72

--- doc/manual/tutorial/mymesh.cpp
+++ doc/manual/tutorial/mymesh.cpp
@@ -31,6 +31,7 @@
 #include <feel/feeldiscr/mesh.hpp>
 #include <feel/feeldiscr/functionspace.hpp>
 #include <feel/feeldiscr/region.hpp>
+#include <feel/feeltiming/tic.hpp>
 #include <feel/feelfilters/gmsh.hpp>
 
 #include <feel/feelfilters/exporter.hpp>
@@ -67,6 +68,18 @@ makeAbout()
                      "christophe.prudhomme@ujf-grenoble.fr", "" );
     return about;
 }
+
+class A { public:
+A():str(){}
+A(std::string s):str(s){}
+std::string str;
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize( Archive & ar, const unsigned int version )
+        {
+            ar & str;
+        }
+};
 
 template<int Dim>
 class MyMesh: public Feel::Simget
@@ -147,13 +160,34 @@ MyMesh<Dim>::run( const double* X, unsigned long P, double* Y, unsigned long N )
                                    % meshSize );
     //Environment::setLogs( this->about().appName() );
     //# marker4 #
-    auto mesh = createGMSHMesh( _mesh=new mesh_type,
-                                _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES|MESH_RENUMBER,
-                                _desc=domain( _name=( boost::format( "%1%-%2%" ) % shape % Dim ).str() ,
-                                        _shape=shape,
-                                        _dim=Dim,
-                                        _h=X[0] ),
-                                _partitions=this->comm().size() );
+    auto mesh = mesh_type::New();
+
+    tic();
+    auto is_mesh_loaded = mesh->load( _name="mymesh",_path=".",_type="text" );
+    toc( "load" );
+
+    if ( !is_mesh_loaded )
+    {
+        tic();
+        Log() << "Generating mesh using gmsh...\n";
+        mesh = createGMSHMesh( _mesh=new mesh_type,
+                               _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES|MESH_RENUMBER,
+                               _desc=domain( _name=( boost::format( "%1%-%2%" ) % shape % Dim ).str() ,
+                                             _shape=shape,
+                                             _dim=Dim,
+                                             _h=X[0] ),
+                               _partitions=this->comm().size() );
+        Log() << "Saving mesh...\n";
+        mesh->save( _name="mymesh",_path=".",_type="text" );
+        toc("generate+save");
+    }
+    else
+    {
+        tic();
+        mesh->save( _name="mymesh2",_path=".",_type="text" );
+        toc("save");
+    }
+
     //# endmarker4 #
 
     int ne = std::distance( mesh->beginElementWithProcessId( this->comm().rank() ),
@@ -167,10 +201,90 @@ MyMesh<Dim>::run( const double* X, unsigned long P, double* Y, unsigned long N )
     Log() << "Global number of elements: " << gne << "\n";
 
     //# marker62 #
-    exporter->step( 0 )->setMesh( mesh );
-    exporter->step( 0 )->addRegions();
-    exporter->save();
+    if ( exporter->doExport() )
+    {
+        Log() << "Exporting mesh\n";
+        exporter->step( 0 )->setMesh( mesh );
+        Log() << "Exporting regions\n";
+        exporter->step( 0 )->addRegions();
+        Log() << "Saving...\n";
+        exporter->save();
+    }
     //# endmarker62 #
+
+#if 1
+
+#if 1
+    mpi::communicator world;
+    Log() << "Sending/Receiving mesh...\n";
+    if ( world.rank() == 0 )
+    {
+        boost::shared_ptr<A> str( new A("Hello") );
+        world.send( 1, 10, mesh );
+        //world.send( 1, 10, str);
+        Log() << "done iSending\n";
+    }
+    else
+    {
+        Log() << "Receiving mesh from : " << 0 << "by " << world.rank() << "...\n";
+        boost::shared_ptr<A> str;
+        mesh_ptrtype mesh2;
+        Log() << "Receiving mesh from : " << 0 << "by " << world.rank() << "...\n";
+        Log() << "Receiving mesh from : " << 0 << "by " << world.rank() << "...\n";
+        Log() << "Receiving mesh from : " << 0 << "by " << world.rank() << "...\n";
+        //world.recv( 0, 10, str );
+        world.recv( 0, 10, mesh2 );
+        Log() << "Saving received mesh...\n";
+        Log() << str->str << "...\n";
+        //mesh2->save( _name="mymesh3", _type="text", _path="." );
+    }
+#else
+
+    mpi::communicator world;
+
+    if (world.rank() == 0) {
+        world.send(1, 0, std::string("Hello"));
+        std::string msg;
+        world.recv(1, 1, msg);
+        std::cout << msg << "!" << std::endl;
+    } else {
+        std::string msg;
+        world.recv(0, 0, msg);
+        std::cout << msg << ", ";
+        std::cout.flush();
+        world.send(0, 1, std::string("world"));
+    }
+
+#endif
+
+#else
+    if ( this->comm().size() > 1 )
+    {
+        Log() << "Sending meshes to proc 0\n";
+        // send all meshes to proc 0
+        if (  this->comm().rank() != 0 )
+        {
+            Log() << "gather mesh in 0\n";
+            mpi::gather( this->comm(), mesh, 0 );
+        }
+        else
+        {
+            Log() << "gather mesh in 0\n";
+            std::vector<mesh_ptrtype> mesh_vector;
+            mpi::gather( this->comm(), mesh, mesh_vector, 0 );
+            Log() << "mesh gathered in 0:" << mesh_vector.size() << "\n";
+#if 0
+            for( int i = 0; i < mesh_vector.size();++i )
+            {
+                exporter->step( i+1)->setMesh( mesh_vector[i] );
+                exporter->save();
+            }
+#endif
+        }
+
+    }
+#endif
+
 }
 
 //
