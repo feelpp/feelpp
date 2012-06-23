d4cbaa40f91c2bb1c5787130ba61279238ca2ce0
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-06-23 17:45:48 +0200

    added script to ease port on curie

--- ports/curie/gmsh.sh
+++ ports/curie/gmsh.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
 . ~/.bash_profile
+module unload c/intel/12.1.7.256  fortran/intel/12.1.7.256  c++/intel/12.1.7.256  
+module unload mkl
 module load cmake/2.8.3
-module load atlas/3.9.72 
+module load c++/gnu/4.5.1 
+
 cmake \
- -DBLAS_atlas_LIBRARY=/usr/local/atlas-3.9.72/lib/libatlas.a\
- -DBLAS_cblas_LIBRARY=/usr/local/atlas-3.9.72/lib/libcblas.a\
- -DBLAS_f77blas_LIBRARY=/usr/local/atlas-3.9.72/lib/libf77blas.a\
- -DBLAS_blas_LIBRARY=/usr/local/atlas-3.9.72/lib/libf77refblas.a\
- -DLAPACK_lapack_LIBRARY=/usr/local/atlas-3.9.72/lib/liblapack.a\
+ -DCMAKE_SHARED_LINKER_FLAGS="-L$WORKDIR/local-gcc45/lib;-L$MPI_ROOT/lib" \
+ -DCMAKE_Fortran_COMPILER="/usr/local/gcc-4.5.1/bin/gfortran" \
  -DCMAKE_INSTALL_PREFIX=$WORKDIR/local-gcc45 \
+ -DENABLE_PETSC=OFF \
  $SCRATCHDIR/Gmsh/gmsh-2.6.0-source/
+
+#make all lib shared
