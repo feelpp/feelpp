96d94a4903a918ed7ee83ffb494e867a3b00b0f7
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-07-04 15:24:32 +0200

    improve framework for benchmarks for the TGCC, now the code can be run first to prepare the meshes and partitions on the frontal, then the code is executed in parallel

--- benchmarks/perf/laplacian/bench.cpp
+++ benchmarks/perf/laplacian/bench.cpp
@@ -93,6 +93,9 @@ extern template class Laplacian<2, CrouzeixRaviart<1, Scalar>, Hypercube>;
 extern template class LaplacianV<2, CrouzeixRaviart<1, Vectorial>, Hypercube>;
 extern template class Laplacian<3, Lagrange<1, Scalar>, Hypercube>;
 extern template class Laplacian<3, Lagrange<2, Scalar>, Hypercube>;
+extern template class Laplacian<3, Lagrange<1, Scalar>, Simplex>;
+extern template class Laplacian<3, Lagrange<2, Scalar>, Simplex>;
+extern template class Laplacian<3, Lagrange<3, Scalar>, Simplex>;
 
 }
 
@@ -100,6 +103,10 @@ int main( int argc, char** argv )
 {
 
     using namespace Feel;
+    Environment env(argc,argv);
+    std::ofstream out;
+    if ( env.worldComm().rank() == 0 )
+        out.open( (boost::format("res-%1%.dat") % env.numberOfProcessors() ).str().c_str() );
     Application benchmark( argc, argv, makeAbout(), makeOptions() );
 
     if ( benchmark.vm().count( "help" ) )
@@ -109,6 +116,7 @@ int main( int argc, char** argv )
     }
 
 #if 1
+#if 0
     benchmark.add( new Laplacian<2, Lagrange<1, Scalar>, Hypercube>( "2D-P1-Hypercube", benchmark.vm(), benchmark.about() ) );
     benchmark.add( new Laplacian<2, Lagrange<2, Scalar>, Hypercube>( "2D-P2-Hypercube", benchmark.vm(), benchmark.about() ) );
     benchmark.add( new Laplacian<2, CrouzeixRaviart<1, Scalar>, Simplex>( "2D-CR1-Simplex", benchmark.vm(), benchmark.about() ) );
@@ -118,8 +126,15 @@ int main( int argc, char** argv )
     //benchmark.add( new LaplacianV<2, CrouzeixRaviart<1, Vectorial>, Hypercube>( "2D-CR1V-Hypercube", benchmark.vm(), benchmark.about() ) );
     //benchmark.add( new Laplacian<3, Lagrange<1, Scalar>, Hypercube>( "3D-P1-Hypercube", benchmark.vm(), benchmark.about() ) );
 #else
+    benchmark.add( new Laplacian<3, Lagrange<1, Scalar>, Simplex>( "3D-P1-Simplex", benchmark.vm(), benchmark.about() ) );
+    benchmark.add( new Laplacian<3, Lagrange<2, Scalar>, Simplex>( "3D-P2-Simplex", benchmark.vm(), benchmark.about() ) );
+    benchmark.add( new Laplacian<3, Lagrange<3, Scalar>, Simplex>( "3D-P3-Simplex", benchmark.vm(), benchmark.about() ) );
+#endif
+#else
     benchmark.add( new Laplacian<2, CrouzeixRaviart<1, Scalar>, Hypercube>( "2D-CR1-Hypercube", benchmark.vm(), benchmark.about() ) );
 #endif
+    benchmark.setStats( boost::assign::list_of( "e.l2" )( "e.h1" )( "n.space" )( "n.matrix" )( "t.init" )( "t.assembly.vector" )( "t.assembly.matrix" )( "t.solver" )( "d.solver" )( "t.integrate" )( "t.export" ) );
     benchmark.run();
-    benchmark.printStats( std::cout, boost::assign::list_of( "e.l2" )( "e.h1" )( "n.space" )( "n.matrix" )( "t.init" )( "t.assembly.vector" )( "t.assembly.matrix" )( "t.solver" )( "d.solver" )( "t.integrate" )( "t.export" ) );
+    benchmark.printStats( std::cout );
+    benchmark.printStats( out );
 }
