cfc4d5f5eca769dbed91b845ba8a7737923a566e
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-07-21 16:13:30 +0200

    fix

--- cmake/modules/UseCRB.cmake
+++ cmake/modules/UseCRB.cmake
@@ -163,7 +163,7 @@ macro(crb_add_model)
 int main( int argc, char** argv )
 {
     Feel::Environment env( argc, argv )\;
-    Feel::OpusApp<Feel::${CRB_MODEL_LONG_NAME}> app( argc, argv,
+    Feel::OpusApp<Feel::${CRB_MODEL_CLASS} > app( argc, argv,
                                                       Feel::make${CRB_MODEL_LONG_NAME}About( \"${CRB_MODEL_SHORT_NAME}\" ),
                                                       Feel::make${CRB_MODEL_LONG_NAME}Options()  )\;
     app.run()\;
