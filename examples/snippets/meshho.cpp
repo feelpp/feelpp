84b6a3b37d96f2697038527a454f2bf5400e28c3
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-07-26 21:46:29 +0200

    test for high order meshes straightening

--- examples/snippets/meshho.cpp
+++ examples/snippets/meshho.cpp
@@ -53,17 +53,16 @@ int main( int argc, char** argv )
     using namespace Feel;
     using namespace Feel::vf;
     Feel::Environment env( argc, argv );
-    typedef Mesh<Simplex<2,4> > mesh_type;
+    typedef Mesh<Simplex<3,2> > mesh_type;
 
     auto mesh = createGMSHMesh( _mesh=new mesh_type,
-                                _desc=domain( _name="ellipsoid-2",
+                                _desc=domain( _name="ellipsoid-3",
                                         _usenames=true,
                                         _shape="ellipsoid",
-                                        _dim=2,
-                                        _order=4,
                                         _h=hsize ),
-                                _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES );
-    //straightenMesh( _mesh=mesh );
+                                _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES,
+                                _straighten=0 );
+    straightenMesh( _mesh=mesh,_save=1 );
 
     //std::cout << "read mesh\n" << std::endl;
 
@@ -72,5 +71,34 @@ int main( int argc, char** argv )
               << std::setprecision( 16 ) << std::scientific << integrate( _range=elements( mesh ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_O1 ).evaluate() << " "
               << std::setprecision( 16 ) << std::scientific << integrate( _range=elements( mesh ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_OPT ).evaluate() << std::endl;
 
+
+    std::cout << "ho  p1  opt " << std::endl
+              << std::setprecision( 16 ) << std::scientific << integrate( _range=internalelements( mesh ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_HO ).evaluate() << " "
+              << std::setprecision( 16 ) << std::scientific << integrate( _range=internalelements( mesh ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_O1 ).evaluate() << " "
+              << std::setprecision( 16 ) << std::scientific << integrate( _range=internalelements( mesh ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_OPT ).evaluate() << std::endl;
+
+    std::cout << "ho  p1  opt " << std::endl
+              << std::setprecision( 16 ) << std::scientific << integrate( _range=boundaryelements( mesh ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_HO ).evaluate() << " "
+              << std::setprecision( 16 ) << std::scientific << integrate( _range=boundaryelements( mesh ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_O1 ).evaluate() << " "
+              << std::setprecision( 16 ) << std::scientific << integrate( _range=boundaryelements( mesh ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_OPT ).evaluate() << std::endl;
+
+
+    auto range = internalelements(mesh);
+    for( auto it = range.get<1>(), en = range.get<2>(); it != en; ++it )
+    {
+        double elt1 = integrate( _range=idedelements( mesh, it->id() ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_HO ).evaluate()( 0, 0 );
+        double elt2 = integrate( _range=idedelements( mesh, it->id() ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_OPT ).evaluate()( 0, 0 );
+        double elt3 = integrate( _range=idedelements( mesh, it->id() ), _quad=_Q<5>(), _expr=cst( 1. ), _geomap=GeomapStrategyType::GEOMAP_O1 ).evaluate()( 0, 0 );
+        if ( math::abs( elt1-elt2 ) > 1e-13 )
+        {
+            std::cout << "problem with element: " << it->id() << " elt1= " << elt1 << " elt2 = " << elt2 << " elt3 = " << elt3 << "\n";
+            auto meshe = createSubmesh( mesh, boost::make_tuple( mpl::int_<MESH_ELEMENTS>(), it, boost::next( it ) ) );
+            std::ostringstream os;
+            os << "elt-ho-" << it->id();
+            saveGMSHMesh( _mesh=meshe, _filename=os.str() );
+            //std::cout << "G=" << it->G() << "\n";
+            //std::cout << "V=" << it->vertices() << "\n";
+        }
+    }
 
 }
