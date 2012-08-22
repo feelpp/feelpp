d940de3c372152a10aa36e788eae7c5c01165b24
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2012-08-22 11:13:36 +0200

    Add libmatheval in contrib

--- cmake/modules/FindMATHEVAL.cmake
+++ cmake/modules/FindMATHEVAL.cmake
@@ -28,15 +28,45 @@
 
 INCLUDE(CheckIncludeFileCXX)
 
+# try to find libmatheval headers, if not found then install glog from contrib into
+# build directory and set MATHEVAL_INCLUDE_DIR and MATHEVAL_LIBRARIES
+FIND_PATH(MATHEVAL_INCLUDE_DIR matheval.h
+  ${CMAKE_BINARY_DIR}/contrib/libmatheval/include
+  /opt/local/include /opt/local/include/matheval
+  /usr/local/include /usr/local/include/matheval
+  /usr/include /usr/include/matheval
+  )
+message(STATUS "Libmatheval first pass: ${MATHEVAL_INCLUDE_DIR}")
 
-FIND_LIBRARY( MATHEVAL_LIB matheval PATHS /usr/lib /opt/local/lib  $ENV{MATHEVAL_DIR}/lib)
+if (NOT MATHEVAL_INCLUDE_DIR )
+  message(STATUS "Building libmatheval in ${CMAKE_BINARY_DIR}/contrib/libmatheval-compile...")
+  execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/contrib/libmatheval-compile)
+  execute_process(
+    COMMAND ${FEELPP_HOME_DIR}/contrib/libmatheval/configure --prefix=${CMAKE_BINARY_DIR}/contrib/libmatheval
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/contrib/libmatheval-compile
+    OUTPUT_QUIET
+    OUTPUT_FILE "libmatheval.log"
+    )
+  message(STATUS "Installing libmatheval in ${CMAKE_BINARY_DIR}/contrib/libmatheval...")
+  execute_process(
+    COMMAND make install
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/contrib/libmatheval-compile
+    OUTPUT_QUIET
+    )
+  set(MATHEVAL_INCLUDE_DIR ${CMAKE_BINARY_DIR}/contrib/libmatheval/include)
+
+endif ()
+
+
+FIND_LIBRARY( MATHEVAL_LIB matheval 
+  PATHS 
+  ${CMAKE_BINARY_DIR}/contrib/libmatheval/lib/
+  /usr/lib /opt/local/lib  $ENV{MATHEVAL_DIR}/lib)
 SET(MATHEVAL_LIBRARIES ${MATHEVAL_LIB} )
+message(STATUS "Libmatheval includes: ${MATHEVAL_INCLUDE_DIR} Libraries: ${MATHEVAL_LIBRARIES}" )
 
-FIND_PATH(MATHEVAL_INCLUDE_DIR
-  matheval.h
-  PATHS /usr/include/ /usr/include/matheval /opt/local/include/matheval /usr/local/include/matheval  $ENV{MATHEVAL_DIR}/include/ANN
-  DOC "Directory where MATHEVAL header files are stored" )
-
+# handle the QUIETLY and REQUIRED arguments and set GLOG_FOUND to TRUE if
+# all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MATHEVAL "Could not find MATHEVAL " MATHEVAL_INCLUDE_DIR MATHEVAL_LIBRARIES)
 MARK_AS_ADVANCED(MATHEVAL_INCLUDE_DIR MATHEVAL_LIBRARIES )
