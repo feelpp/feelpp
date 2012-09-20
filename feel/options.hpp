bc3fa0501bcab7cd114a754f2686724d8cca4c5d
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2012-09-20 20:39:35 +0200

    fix encoding

--- feel/options.hpp
+++ feel/options.hpp
@@ -5,7 +5,7 @@
   Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
        Date: 2007-07-21
 
-  Copyright (C) 2007 Université Joseph Fourier (Grenoble I)
+  Copyright (C) 2007 Universite Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
