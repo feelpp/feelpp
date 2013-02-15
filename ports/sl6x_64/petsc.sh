1387affa8323c6ec11858e270736181990b43ad5
V. Huber <vincent.huber@gmail.com>
2013-02-15 15:12:58 +0100

    Arborescence pour install selon compilateur

--- ports/sl6x_64/petsc.sh
+++ ports/sl6x_64/petsc.sh
@@ -1,10 +1,15 @@
 #PETSC
 export PETSC_ARCH="arch-linux2-c-opt"
-export PETSC_DIR="$workdir/petsc-3.3-p5"
-cd $workdir
+export PETSC_DIR="$petscDir/petsc-3.3-p5"
+mkdir $workdir/_petsc
+cd $workdir/_petsc
 wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.3-p5.tar.gz
 tar xzf petsc-3.3-p5.tar.gz
-cd $PETSC_DIR
-./configure --with-shared-libraries=1 --with-debugging=0 COPTFLAGS='-O3 -march=p4 -mtune=p4' FOPTFLAGS='-O3 -qarch=p4 -qtune=p4'
+cd petsc-3.3-p5
+./configure --with-shared-libraries=1 \
+	--with-debugging=0 \
+	COPTFLAGS='-O3 -march=p4 -mtune=p4' FOPTFLAGS='-O3 -qarch=p4 -qtune=p4' \
+	--prefix=$PETSC_DIR
 make all
 make test
+make install
