bfa5902c85a33bec9d99bc518235252d453e6d2b
vhuber <vhuber@cp-sl.u-strasbg.fr>
2013-02-15 11:49:26 +0100

    Disable documentation on sl6.x

--- ports/sl6x_64/feelpp.sh
+++ ports/sl6x_64/feelpp.sh
@@ -6,5 +6,6 @@ mkdir feelpp_build_dir
 cd feelpp_build_dir
 cmake $workdir/feelpp \
       -DCMAKE_BUILD_TYPE=release \
+      -DFEELPP_ENABLE_DOCUMENTATION=off \
       -DCMAKE_INSTALL_PREFIX=$workdir/feelppLib
 make -j4 install
