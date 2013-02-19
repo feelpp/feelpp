ce6b127ea7b3cd0efd225b2cca6b4c902f19fa80
Vincent H <vincent.huber@gmail.com>
2013-02-19 18:11:39 +0100

    Update install process for scientific linux

--- ports/sl6x_64/openmpi.sh
+++ ports/sl6x_64/openmpi.sh
@@ -1,10 +1,8 @@
 #installation d'openmPI
 mkdir $workdir/_openmpi
 cd $workdir/_openmpi
-wget http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.3.tar.bz2
+wget -c http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.3.tar.bz2
 tar xjf openmpi-1.6.3.tar.bz2
 cd openmpi-1.6.3
 ./configure CFLAGS=-m64 CXXFLAGS=-m64 FFLAGS=-m64 FCFLAGS=-m64 --prefix=$openmpiDir
-make -j all install
-export PATH=$openmpiDir/bin:$PATH
-export LD_LIBRARY_PATH=$openmpiDir/lib:$LD_LIBRARY_PATH
+make -j$nbProcs all install
