9a7c6c225e131bce30133933ae09c304fd511d2e
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-02-19 08:22:29 +0100

    fixed automatic page generation

--- scripts/doxygen/publish.sh
+++ scripts/doxygen/publish.sh
@@ -1,8 +1,8 @@
-#/bin/sh
+#! /opt/local/bin/bash
 
 function builddox
 {
-    branch = $1
+    branch=$1
     cd feelpp.git && git pull && git checkout $branch && cd ..
     [ -d doxygen-$branch ] && rm -rf doxygen-$branch
     mkdir doxygen-$branch
@@ -14,20 +14,19 @@ function builddox
     # now work in feelpp.docs to push the newly created doxygen files
     mkdir -p feelpp.docs/api/$branch
     cd feelpp.docs && git pull
-    # cleanup html directory
-    if [ -d api/$branch/html]; then
-        rm -rf api/$branch/html;
-    fi
-    cp -r ../doxygen-$branch/doc/api/html api/$branch/
-    git add api/$branch/html
+    
+    rsync -avz ../doxygen-$branch/doc/api/html/ api/$branch/html/
+    git add api/$branch/html/* 
+    git commit -m"update $branch doxygen documentation" -a
     git push
+    cd ..
 }
 
-if [! -d feelpp.docs ]; then git clone  https://code.google.com/p/feelpp.docs/ feelpp.docs; fi
+if [ ! -d feelpp.docs ]; then git clone  https://code.google.com/p/feelpp.docs/ feelpp.docs; fi
 cd feelpp.docs && git pull && cd ..
-if [! -d feelpp.git ]; then git clone  https://github.com/feelpp/feelpp.git feelpp.git; fi
+if [ ! -d feelpp.git ]; then git clone  https://github.com/feelpp/feelpp.git feelpp.git; fi
 cd feelpp.git && git pull && cd ..
 
 # checkout in master branch
-builddox master
+# builddox master
 builddox develop
