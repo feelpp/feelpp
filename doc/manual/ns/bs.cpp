d24e2643c6238add67617bd2e44d6ec247a97a54
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-02-21 17:02:52 +0100

    added

---
+++ doc/manual/ns/bs.cpp
@@ -0,0 +1,49 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-
+
+  This file is part of the Feel library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@math.unistra.fr>
+       Date: 2012-09-30
+
+  Copyright (C) 2012 Université de Strasbourg
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file bs.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@math.unistra.fr>
+   \date 2012-09-30
+ */
+#include <ns.hpp>
+
+int
+main( int argc, char** argv )
+{
+
+    using namespace Feel;
+
+    Environment env( _argc=argc, _argv=argv,
+                     _desc=makeOptions(),
+                     _about=about(_name="backwardstep",
+                                  _author="Christophe Prud'homme",
+                                  _email="christophe.prudhomme@feelpp.org") );
+
+    /* define and run application */
+    NavierStokes stokes;
+    stokes.run();
+}
+
+
+
