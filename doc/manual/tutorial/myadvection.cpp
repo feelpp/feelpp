2c9a0618422dad72c808d8fa7013ef9ee9c11772
dolleg <gdolle@math.unistra.fr>
2013-03-01 12:04:16 +0100

    Add (basic) advection tutorial
    * add advection code + documentation
    * fix stokes and laplacian exporter
      (_name is defined through environment)
    * up CMakelist to work with advection code

--- doc/manual/tutorial/myadvection.cpp
+++ doc/manual/tutorial/myadvection.cpp
@@ -5,7 +5,7 @@
   Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
 	     Guillaume Dollé <guillaume.dolle@math.unistra.fr>
  
-  Date 2013-02-18
+  Date 2013-02-25
 
   Copyright (C) 2013 Université de Strasbourg
 
@@ -23,60 +23,73 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
-   \file mylaplacian.cpp
+   \file myadvection.cpp
    \author Christophe Prud'homme <christophe.prudhomme@feelpp.org>,
                  Guillaume Dollé <guillaume.dolle@math.unistra.fr>
-   \date 2013-02-11
-   This program show how to solve the Laplacian with dirichlet homogene 
-   conditions.
+   \date 2013-02-25
+   This program show how to solve the advection problem.
  */
 #include <feel/feel.hpp>
 using namespace Feel;
 
-/*
- *  Entry point
+/**
+ * Entry point
  */
 //\code
 //# marker_main #
-int main(int argc, char**argv )
+int
+main( int argc, char** argv )
 {
-    // initialize feel++
-    using namespace Feel;
+   
+    // Initialize Feel++ Environment
     Environment env( _argc=argc, _argv=argv,
                      _desc=feel_options(),
-                     _about=about(_name="mylaplacian",
+                     _about=about(_name="myadvection",
                                   _author="Feel++ Consortium",
-                                  _email="feelpp-devel@feelpp.org"));
+                                  _email="feelpp-devel@feelpp.org") );
     // create mesh
     auto mesh = unitSquare();
 
-    // function space 
-    auto Vh = Pch<1>( mesh );
-    auto u = Vh->element();
-    auto v = Vh->element();
+    // function space
+    auto Xh = Pch<1>( mesh );
+    auto u = Xh->element( "u" );
+    auto v = Xh->element( "v" );
+
+    // diffusion coeff.
+    double epsilon = 1;
+    // reaction coeff.
+    double mu = 1;    
+    auto beta = vec( cst(1.),
+                     cst(1.) );
+    auto f = cst(1.);
 
     // left hand side
-    auto a = form2( _trial=Vh, _test=Vh );
-    a = integrate(_range=elements(mesh),
-                  _expr=gradt(u)*trans(grad(v)) );
+    auto a = form2( _test=Xh, _trial=Xh );
+    a += integrate( _range=elements( mesh ),
+                    _expr=( epsilon*gradt( u )*trans( grad( v ) )
+                         + ( gradt( u )*beta )*id(v)
+                         + mu*idt( u )*id( v ) ) );
 
     // right hand side
-    auto l = form1( _test=Vh );
-    l = integrate(_range=elements(mesh),
-                  _expr=id(v));
+    auto l = form1( _test=Xh );
+    l+= integrate( _range=elements( mesh ), _expr=f*id( v ) );
 
-    // apply the boundary condition
-    a+=on(_range=boundaryfaces(mesh), _rhs=l, _element=u,
-          _expr=constant(0.) );
+    // boundary condition
+    a += on( _range=boundaryfaces( mesh ), _rhs=l, _element=u,
+             _expr=cst(0.) );
 
-    // solve the equation a(u,v) = l(v)
-    a.solve(_rhs=l,_solution=u);
+    // solve the system
+    a.solve( _rhs=l, _solution=u );
 
     // export results
     auto e = exporter( _mesh=mesh );
-    e->add( "u", u );
+    e->add("u",u);
     e->save();
-}
+} // end main
 //# endmarker_main #
 //\endcode
+
+
+
+
 
