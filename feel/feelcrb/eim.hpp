ba49dc805af07fb97e50a3ff9bcb575297c3e1be
Stephane Veys <stephane.veys@gmail.com>
2013-03-15 16:56:58 +0100

    add timer in beta() function

--- feel/feelcrb/eim.hpp
+++ feel/feelcrb/eim.hpp
@@ -514,6 +514,8 @@ template<typename ModelType>
 typename EIM<ModelType>::vector_type
 EIM<ModelType>::beta( parameter_type const& mu, size_type __M ) const
 {
+    boost::mpi::timer ti;
+
     // beta=B_M\g(Od(indx),mut(i))'
     vector_type __beta( __M );
     for ( size_type __m = 0;__m < __M;++__m )
@@ -521,12 +523,18 @@ EIM<ModelType>::beta( parameter_type const& mu, size_type __M ) const
         __beta[__m] = M_model->operator()( this->M_t[__m], mu );
     }
     this->M_B.block(0,0,__M,__M).template triangularView<Eigen::UnitLower>().solveInPlace(__beta);
+
+    double time_beta = ti.elapsed();
+    LOG(INFO)<<"[develop-beta(mu,__M)] for mu = "<<mu<<" and M ="<< __M<<" , beta coefficients are computed in "<<time_beta<<" s\n";
+
     return __beta;
 }
 template<typename ModelType>
 typename EIM<ModelType>::vector_type
 EIM<ModelType>::beta( parameter_type const& mu, solution_type const& T, size_type __M ) const
 {
+    boost::mpi::timer ti;
+
     // beta=B_M\g(Od(indx),mut(i))'
     vector_type __beta( __M );
     for ( size_type __m = 0;__m < __M;++__m )
@@ -534,6 +542,10 @@ EIM<ModelType>::beta( parameter_type const& mu, solution_type const& T, size_typ
         __beta[__m] = M_model->operator()( T, this->M_t[__m], mu );
     }
     this->M_B.block(0,0,__M,__M).template triangularView<Eigen::UnitLower>().solveInPlace(__beta);
+
+    double time_beta = ti.elapsed();
+    LOG(INFO)<<"[develop-beta(mu,__M,T)] for mu = "<<mu<<" and M ="<< __M<<" , beta coefficients are computed in "<<time_beta<<" s\n";
+
     return __beta;
 }
 
