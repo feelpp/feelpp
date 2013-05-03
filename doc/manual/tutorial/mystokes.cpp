a301d7b61316741676f6c088b37eedabdf95cfb9
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-05-03 06:24:45 +0200

    - added input doc/manual  - added bibliography (does not show up correctly yet)  - moved Stokes Tutorial examples back into the cpp files  - added stokes curl-curl formulation example

--- doc/manual/tutorial/mystokes.cpp
+++ doc/manual/tutorial/mystokes.cpp
@@ -33,8 +33,111 @@
 #include <feel/feel.hpp>
 using namespace Feel;
 
-/*
- * Entry point
+/**
+ \page StokesTutorial Stokes Tutorial
+\author Vincent Huber
+\author Christophe Prud'homme
+\author Guillaume Dolle
+
+\tableofcontents
+<br>
+<hr>
+<br>
+
+\section Stokes_Math Mathematical Problem
+Let solve the stokes equation considering a Poiseuille flow profile. <br>
+We have the following system of equations,
+<br><center>\f$
+\left\{
+\begin{aligned}
+-\mu\Delta \bf u + \nabla p & =  \bf f & \text{on}\; \Omega \;, \\
+                \nabla\cdot\bf u & =  0 & \text{on}\; \Omega \;, \\
+                \bf u  & =  g & \text{on}\; \Gamma \;, \\
+\end{aligned}
+\right
+\f$</center><br>
+where \f$u\in [H_g^1(\Omega)]^d\f$ denotes the flow speed, \f$p\in [L_0^2(\Omega)]\f$ the fluid pressure, \f$\mu\f$ the fluid viscosity.<br>
+The last boundary condition expresses a null pressure fixed on the outlet.<br>
+The Poiseuille profile on the boundary is,
+<br><center>\f$
+g(x,y)=\left(
+\begin{aligned}
+ y(1-y) \\
+ 0      \\
+\end{aligned}
+\right)
+\f$</center><br>
+The method used to obtain the strong formulation is closed to the one used for the laplacian (see section \ref Laplacian ).<br>
+We multiply the first equation by a test function \f$v\in H^1(\Omega)\f$ and we integrate on the domain \f$\Omega\f$,
+<br><center>\f$
+\begin{aligned}
+ \left(
+\int_\Omega \mu \nabla \mathbf u : \nabla \mathbf v
+-\int_{\partial\Omega} \frac{\partial \mathbf u}{\partial \mathbf n} \cdot \mathbf v
+\right)
++\int_\Omega ( \nabla\cdot(p \mathbf v) - \mathbf v \nabla\cdot p )
+=\int_\Omega \mathbf f \cdot \mathbf v \;.
+\end{aligned}
+\right)
+\f$</center><br>
+where $n$ denotes a normal vector on the boundary.<br>
+The divergence theorem (or Gauss's theorem) gives,
+<br><center>\f$
+\begin{aligned}
+\int_\Omega \nabla\cdot(p \mathbf v) = \int_{\partial\Omega} p \mathbf v\cdot \mathbf n \;.
+\end{aligned}
+\right)
+\f$</center><br>
+We have to add a consistency terms to the equation to guaranty the symmetry of the bilinear form.<br>
+This term is provided by the second equation. We multiply this equation by a test function \f$q\in L_2(\Omega)\f$ and we integrate on the domain \f$\Omega\f$,
+<br><center>\f$
+\begin{aligned}
+\int_{\Omega} \nabla\cdot\mathbf u q = 0 \;,
+\end{aligned}
+\right)
+\f$</center><br>
+Finally, we deduce from the equations and after rearranging the integrals the variationnal formulation,
+<br><center>\f$
+\begin{aligned}
+\int_\Omega \mu \nabla \mathbf u :\nabla \mathbf v
++\int_\Omega \left( \nabla\cdot\mathbf u q - p \nabla\cdot\mathbf v \right)
++
+    \int_{\partial\Omega} \left( p \mathbf n -
+    \underbrace{\frac{\partial \mathbf u}{\partial \mathbf n}}_{=0} \right)
+     \cdot \mathbf v
+=\int_\Omega \mathbf f \cdot \mathbf v
+\end{aligned}
+\right)
+\f$</center><br>
+We have an equation which can be rewritten as find \f$(\mathbf u,p)\in [H_g^1(\Omega)]^d\times L_0^2(\Omega) \f$ such that for all \f$(\mathbf v,q) \in [H_0^1(\Omega)]^d \times L_0^2(\Omega)\f$
+<br><center>\f$
+\begin{aligned}
+  a((\mathbf u,p),(\mathbf v,q)) = l((\mathbf v,q))
+\end{aligned}
+\right)
+\f$</center><br>
+where \f$a\f$ is a bilinear form, continuous, coercive and where \f$l\f$ is a linear form.
+
+\section Stokes_Code Feel++ Code
+Let's see the \feel code corresponding to this mathematical statement (source \c "doc/manual/tutorial/mystokes.cpp").<br>
+We suppose for this example the viscosity \f$\mu=1\f$ and \f$\mathbf f = 0\f$.
+
+The procedure to create the mesh is very simple.<br>
+You have to provide to the command line (or via the cfg file) the gmsh.filename option.<br>
+You can provide a \c .geo or a \c .msh file (created via gmsh).
+
+As for the laplacian problem, the code is very closed to the mathematical formulation.<br>
+We define the product of function spaces for the flow speed and the flow pressur using \c THch<order>()(TH stands for Taylor-Hoods) function which is \c Pch<N+1> \f$\times\f$ \c Pch<N> for respectively flow speed and pressure spaces.<br>
+We take an element
+\f$U=\left(
+    \begin{array}{c}
+        u \\
+        p \\
+    \end{array}
+\right)
+\f$
+in this space. Then we define the integrals of the variationnal formulation for the left and the right hand side. Finally, we apply the Poiseuille profile on the boundary.<br>
+We call the solver to resolve the problem (\ref Solver).
  */
 //\code
 //# marker_main #
