a6e135d1bf9aab76507e183730e092021db4dd01
Vincent H <vincent.huber@math.unistra.fr>
2013-07-26 14:06:32 +0200

    add error, remove useless lines

--- testsuite/feeldiscr/test_on_inside.cpp
+++ testsuite/feeldiscr/test_on_inside.cpp
@@ -1,72 +1,69 @@
 /* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim: set syntax=cpp fenc=utf-8 ft=tcl et sw=4 ts=4 sts=4
 
-  This file is part of the Feel library
+   This file is part of the Feel library
 
-  Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
-       Date: 2008-02-07
+   Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
+Date: 2008-02-07
 
-  Copyright (C) 2008-2009 Université Joseph Fourier (Grenoble I)
+Copyright (C) 2008-2009 Université Joseph Fourier (Grenoble I)
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
-   \file generecrash.cpp
-   \author Vincent HUBER <vincent.huber@cemosis.fr>
-   \date 2013-08-22
- */
+  \file generecrash.cpp
+  \author Vincent HUBER <vincent.huber@cemosis.fr>
+  \date 2013-08-22
+  */
 #include <feel/feel.hpp>
 using namespace Feel;
 
-int
+  int
 main( int argc, char** argv )
 {
-    // Initialize Feel++ Environment
-    Environment env( _argc=argc, _argv=argv,
-                     _desc=feel_options(),
-                     _about=about( _name="test_on_inside" ,
-                                   _author="Feel++ Consortium",
-                                   _email="feelpp-devel@feelpp.org" ) );
+  // Initialize Feel++ Environment
+  Environment env( _argc=argc, _argv=argv,
+      _desc=feel_options(),
+      _about=about( _name="test_on_inside" ,
+        _author="Feel++ Consortium",
+        _email="feelpp-devel@feelpp.org" ) );
 
-    // create the mesh (specify the dimension of geometric entity)
-    typedef Mesh<Simplex<2>> mesh_type;
-    typedef FunctionSpace<mesh_type,bases<Lagrange<2,Scalar>>,double> space_type;
-    typedef boost::shared_ptr<space_type> space_ptrtype;
+  // create the mesh (specify the dimension of geometric entity)
+  typedef Mesh<Simplex<2>> mesh_type;
 
-    auto mesh = loadMesh(_mesh = new mesh_type );
-    space_ptrtype Xh;
-    Xh = space_type::New(mesh);
-    auto u = Xh->element( "u" );
-    auto v = Xh->element( "v" );
+  auto mesh = loadMesh(_mesh = new mesh_type );
 
-    CHECK( nelements( markedfaces(mesh,"toto") ) > 1 )
-        << "Invalid number of faces marked toto: "
-        << nelements( markedfaces(mesh,"toto") );
-    double length = integrate( _range = markedfaces(mesh,"toto"),_expr = cst(1.0) ).evaluate()( 0,0 );
-    CHECK( math::abs( length - math::sqrt(2*.8*.8) ) < 1e-10 )
-        << "wrong line interface length between (.1,.1) amd (.9,.9)"
-        << " length : " << length
-        << " expected value : " << math::sqrt(2*.8*.8);
-    LOG(INFO) << "length : " << length << " expected value : " << math::sqrt(2*.8*.8);
+  double nEle_Loc = nelements( markedfaces(mesh,"toto") );
+  double nEle_glob = nEle_Loc;
 
-    auto l = form1(_test=Xh);
-    auto a = form2(_test=Xh, _trial=Xh);
+  if (Environment::worldComm().size() > 1) {
+    mpi::all_reduce(Environment::worldComm().globalComm(),
+        nEle_Loc,
+        nEle_glob, 
+        std::plus<int>());
+  }
 
-    l += integrate(_range=elements( mesh ), _expr=cst(1.));
-    a += integrate(_range=elements( mesh ), _expr=idt(u)*id(v));
-    a += on(_range=markedfaces(mesh,"toto"),_expr=cst(1),_rhs=l,_element=u);
+  CHECK( nEle_glob > 1 )
+    << "Invalid number of faces marked toto: "
+    << nEle_glob ;
 
-    a += on(_range=boundaryfaces(mesh),_expr=cst(0.1),_rhs=l,_element=u);
-    a += on(_range=markedfaces(mesh,"toto"),_expr=cst(0.1),_rhs=l,_element=u);
+  double length = integrate( _range = markedfaces(mesh,"toto"),_expr = cst(1.0) ).evaluate()( 0,0 );
 
+  CHECK( math::abs( length - math::sqrt(2*.8*.8) ) < 1e-10 )
+    << "wrong line interface length between (.1,.1) amd (.9,.9)"
+    << " length : " << length
+    << " expected value : " << math::sqrt(2*.8*.8);
+
+  LOG(INFO) << "number of faces : " << nEle_glob ;
+  LOG(INFO) << "length : " << length << " expected value : " << math::sqrt(2*.8*.8) << " error " << std::fabs(length -  math::sqrt(2*.8*.8))/std::fabs(math::sqrt(2*.8*.8));
 }
