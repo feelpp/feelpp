50f17e6f70c8d7d771fc853ab1262565c3731c99
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-08-13 16:37:51 +0200

    update emacs dirvars

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,23 +5,28 @@
     (indent-tabs-mode . nil)
     (fill-column . 80)
     (user-company . "Université de Grenoble")
-    (user-mail-address . "christophe.prudhomme@ujf-grenoble.fr")
-    (debian-changelog-mailing-address . "christophe.prudhomme@ujf-grenoble.fr")
+    (user-mail-address . "christophe.prudhomme@feelpp.org")
+    (debian-changelog-mailing-address . "christophe.prudhomme@feelpp.org")
     ))
 (c++-mode .
   (
+    (subdirs . nil)
     (indent-tabs-mode . nil)
     (tab-width . 4)
     (show-trailing-whitespace . t)
     (indicate-empty-lines . t)
     (c-basic-offset . 4)
-    (c-set-offset 'innamespace  0)))
+    (c-set-offset 'innamespace  0)
+    (add-hook 'write-file-functions 'delete-trailing-whitespace)
+    ))
 (c-mode .
   (
+    (subdirs . nil)
     (indent-tabs-mode . nil)
     (tab-width . 4)
     (show-trailing-whitespace . t)
     (indicate-empty-lines . t)
     (c-basic-offset . 4)
-    (c-set-offset 'innamespace  0))))
-
+    (c-set-offset 'innamespace  0)
+    (add-hook 'write-file-functions 'delete-trailing-whitespace)
+    )))
