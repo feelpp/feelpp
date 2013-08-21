273a2ffccb8a945fecda06fb0ac3b9ff867ceb0d
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-08-22 01:27:09 +0200

    update issue #160 @vincentchabannes @asamake Now use the Dof data structure to store the global dof information and not a tuple (Dof derives from a tuple for backward compatibility) define also LocalDof and LocalDofSet for the new dof_Table data structure which allows to go back and forth between the local and global dof using boost::bimap

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,32 +1,34 @@
 ;; -*- emacs-lisp -*-
-((nil .
-  (
-    (tab-width . 4)
-    (indent-tabs-mode . nil)
-    (fill-column . 80)
-    (user-company . "Université de Grenoble")
-    (user-mail-address . "christophe.prudhomme@feelpp.org")
-    (debian-changelog-mailing-address . "christophe.prudhomme@feelpp.org")
-    ))
-(c++-mode .
-  (
-    (subdirs . nil)
-    (indent-tabs-mode . nil)
-    (tab-width . 4)
-    (show-trailing-whitespace . t)
-    (indicate-empty-lines . t)
-    (c-basic-offset . 4)
-    (c-set-offset 'innamespace  0)
-    (add-hook 'write-file-functions 'delete-trailing-whitespace)
-    ))
-(c-mode .
-  (
-    (subdirs . nil)
-    (indent-tabs-mode . nil)
-    (tab-width . 4)
-    (show-trailing-whitespace . t)
-    (indicate-empty-lines . t)
-    (c-basic-offset . 4)
-    (c-set-offset 'innamespace  0)
-    (add-hook 'write-file-functions 'delete-trailing-whitespace)
-    )))
+(
+  (nil .
+    (
+      (tab-width . 4)
+      (indent-tabs-mode . nil)
+      (fill-column . 80)
+      (user-company . "Feel++ Consortium")
+      (user-mail-address . "christophe.prudhomme@feelpp.org")
+      (debian-changelog-mailing-address . "christophe.prudhomme@feelpp.org")
+      ))
+  (c++-mode .
+    (
+      (subdirs . nil)
+      (indent-tabs-mode . nil)
+      (tab-width . 4)
+      (show-trailing-whitespace . t)
+      (indicate-empty-lines . t)
+      (c-basic-offset . 4)
+      (c-set-offset 'innamespace  0)
+      (add-hook 'write-file-functions 'delete-trailing-whitespace)
+      ))
+  (c-mode .
+    (
+      (subdirs . nil)
+      (indent-tabs-mode . nil)
+      (tab-width . 4)
+      (show-trailing-whitespace . t)
+      (indicate-empty-lines . t)
+      (c-basic-offset . 4)
+      (c-set-offset 'innamespace  0)
+      (add-hook 'write-file-functions 'delete-trailing-whitespace)
+      ))
+  )
