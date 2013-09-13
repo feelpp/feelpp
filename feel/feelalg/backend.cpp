6c91ac3a8cb88adf454e47418b8b716d77d3fdad
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-09-13 17:40:15 +0200

    Fix issue #7 #8 #133 #7 check for leaks in PETSc in test_solve_leak now clear properly solvers before rebuilding a backend, it ensures that not only the preconditioners but also the petsc solvers (lineat and non-linear) are destroyed

--- feel/feelalg/backend.cpp
+++ feel/feelalg/backend.cpp
@@ -150,6 +150,12 @@ Backend<T>::Backend( po::variables_map const& vm, std::string const& prefix, Wor
 template <typename T>
 Backend<T>::~Backend()
 {
+    this->clear();
+}
+template <typename T>
+void
+Backend<T>::clear()
+{
     if ( M_preconditioner )
         M_preconditioner->clear();
     LOG(INFO) << "Sending delete signal to all observers...\n";
@@ -205,19 +211,21 @@ template <typename T>
 typename Backend<T>::backend_ptrtype
 Backend<T>::build( po::variables_map const& vm, std::string const& prefix, WorldComm const& worldComm )
 {
-    LOG(INFO) << "Loading backend " << vm["backend"].template as<std::string>();
+
+    std::string n= option( _name="backend" ).template as<std::string>();
+    LOG(INFO) << "Loading backend " << n;
     BackendType bt;
 
-    if ( vm["backend"].template as<std::string>() == "eigen" )
+    if ( n == "eigen" )
         bt = BACKEND_EIGEN;
 
-    else if ( vm["backend"].template as<std::string>() == "eigen_dense" )
+    else if ( n == "eigen_dense" )
         bt = BACKEND_EIGEN_DENSE;
 
-    else if ( vm["backend"].template as<std::string>() == "petsc" )
+    else if ( n == "petsc" )
         bt = BACKEND_PETSC;
 
-    else if ( vm["backend"].template as<std::string>() == "trilinos" )
+    else if ( n == "trilinos" )
         bt = BACKEND_TRILINOS;
 
     else
@@ -228,7 +236,7 @@ Backend<T>::build( po::variables_map const& vm, std::string const& prefix, World
         LOG(INFO) << "Falling back to backend petsc\n";
         bt = BACKEND_PETSC;
 #else
-        LOG(FATAL) << "Backend " << vm["backend"].template as<std::string>() << " not available";
+        LOG(FATAL) << "Backend " << n << " not available";
 #endif
     }
 
@@ -237,12 +245,12 @@ Backend<T>::build( po::variables_map const& vm, std::string const& prefix, World
     {
     case BACKEND_EIGEN:
     {
-        return backend_ptrtype( new BackendEigen<value_type>( vm, prefix, worldComm ) );
+        return backend_ptrtype( new BackendEigen<value_type>( Environment::vm(), prefix, worldComm ) );
     }
     break;
     case BACKEND_EIGEN_DENSE:
     {
-        return backend_ptrtype( new BackendEigen<value_type,1>( vm, prefix, worldComm ) );
+        return backend_ptrtype( new BackendEigen<value_type,1>( Environment::vm(), prefix, worldComm ) );
     }
     break;
 
@@ -251,7 +259,7 @@ Backend<T>::build( po::variables_map const& vm, std::string const& prefix, World
     default:
     case BACKEND_PETSC:
     {
-        return backend_ptrtype( new BackendPetsc<value_type>( vm, prefix, worldComm ) );
+        return backend_ptrtype( new BackendPetsc<value_type>( Environment::vm(), prefix, worldComm ) );
     }
     break;
 #endif
@@ -260,7 +268,7 @@ Backend<T>::build( po::variables_map const& vm, std::string const& prefix, World
     case BACKEND_TRILINOS:
     {
 #if defined ( FEELPP_HAS_TRILINOS_EPETRA )
-        return backend_ptrtype( new BackendTrilinos( vm, prefix, worldComm ) );
+        return backend_ptrtype( new BackendTrilinos( Environment::vm(), prefix, worldComm ) );
 #else
         return backend_ptrtype();
 #endif
