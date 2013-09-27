c7b5698a7281241c3a4ebb652808b3776c42721f
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-09-27 15:44:48 +0200

    specific directories for logs and results, remove them afterwards

--- cmake/dashboard/linux.sh
+++ cmake/dashboard/linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #set -x
+
+
 # $1 provides the path to the toplevel source Feel++ directory
 # e.g. $HOME/Devel/FEEL/feelpp.git
 COMMON="ctest -VV -S $1/cmake/dashboard/testsuite.cmake,FEELPP_CTEST_CONFIG=$1/cmake/dashboard/feelpp.site.`hostname -s`.cmake,FEELPP_MODE=$2"
@@ -12,15 +14,30 @@ do_gcc47=`echo $compiler_list | grep gcc47`
 do_gcc48=`echo $compiler_list | grep gcc48`
 do_clang=`echo $compiler_list | grep clang`
 
+export FEELPP_SCRATCHDIR=/tmp/feel-logs/
+rm -rf $FEELPP_SCRATCHDIR
 if [ ! -z "$do_gcc46" -a -x /usr/bin/g++-4.6 ]; then
+    export FEELPP_WORKDIR=/tmp/feel-gcc46/
+    rm -rf /tmp/feel-gcc46
     $COMMON,FEELPP_CXXNAME=gcc-4.6,FEELPP_CXX=/usr/bin/g++-4.6 #,FEELPP_EXPLICIT_VECTORIZATION=SSE2
+    rm -rf /tmp/feel-gcc46
 fi
 if [ ! -z "$do_gcc47" -a -x /usr/bin/g++-4.7 ]; then
+    export FEELPP_WORKDIR=/tmp/feel-gcc47/
+    rm -rf /tmp/feel-gcc47
     $COMMON,FEELPP_CXXNAME=gcc-4.7,FEELPP_CXX=/usr/bin/g++-4.7 #,FEELPP_EXPLICIT_VECTORIZATION=SSE2
+    rm -rf /tmp/feel-gcc47
 fi
 if [ ! -z "$do_gcc48" -a -x /usr/bin/g++-4.8 ]; then
+    export FEELPP_WORKDIR=/tmp/feel-gcc48/
+    rm -rf /tmp/feel-gcc48
     $COMMON,FEELPP_CXXNAME=gcc-4.8,FEELPP_CXX=/usr/bin/g++-4.8 #,FEELPP_EXPLICIT_VECTORIZATION=SSE2
+    rm -rf /tmp/feel-gcc48
 fi
 if [ ! -z "$do_clang" -a -x /usr/bin/clang++ ]; then
+    export FEELPP_WORKDIR=/tmp/feel-clang
+    rm -rf /tmp/feel-clang
     $COMMON,FEELPP_CXXNAME=clang-3.3,FEELPP_CXX=/usr/bin/clang++ #,FEELPP_EXPLICIT_VECTORIZATION=SSE2
+    rm -rf /tmp/feel-clang
 fi
+rm -rf $FEELPP_SCRATCHDIR
