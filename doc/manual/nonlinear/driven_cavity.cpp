27bd8463268129cc1b922c2d7f5d37338d95b1a3
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-11-22 05:35:51 +0000

    minor changes in application @mszopos, @ranine:  - make it generic : 2D/3D  - reduced function sizes (cleanup)  - jacobian and residual are now member functions

--- doc/manual/nonlinear/driven_cavity.cpp
+++ doc/manual/nonlinear/driven_cavity.cpp
@@ -35,15 +35,11 @@ makeOptions()
 {
     Feel::po::options_description  drivencavityoptions( "Driven Cavity problem options" );
      drivencavityoptions.add_options()
-    ( "penal", Feel::po::value<double>()->default_value( 0.5 ), "penalisation parameter" )
-    ( "f", Feel::po::value<double>()->default_value( 0 ), "forcing term" )
-    ( "mu", Feel::po::value<double>()->default_value( 1.0 ), "reaction coefficient component" )
-    ( "hsize", Feel::po::value<double>()->default_value( 0.1 ), "first h value to start convergence" )
-    ( "bctype", Feel::po::value<int>()->default_value( 0 ), "0 = strong Dirichlet, 1 = weak Dirichlet" )
-    ( "bccoeff", Feel::po::value<double>()->default_value( 100.0 ), "coeff for weak Dirichlet conditions" )
-    ( "export-matlab", "export matrix and vectors in matlab" )
-    ;
-    return  drivencavityoptions.add( Feel::feel_options() );
+         ( "continuation", Feel::po::value<bool>()->default_value( 0 ), "use continuation algorithm: =0 (no) =1(yes)" )
+         ( "Re", Feel::po::value<double>()->default_value( 100 ), "Reynolds number" )
+         ( "bccoeff", Feel::po::value<double>()->default_value( 100.0 ), "coeff for weak Dirichlet conditions" )
+         ;
+     return  drivencavityoptions.add( Feel::feel_options() );
 }
 
 inline
@@ -57,7 +53,8 @@ makeAbout()
                            Feel::AboutData::License_GPL,
                            "Copyright (c) 2009-2012 Université de Grenoble 1 (Joseph Fourier)" );
 
-    about.addAuthor( "Christophe Prud'homme", "developer", "christophe.prudhomme@ujf-grenoble.fr", "" );
+    about.addAuthor( "Christophe Prud'homme", "developer", "christophe.prudhomme@feelpp.org", "" );
+    about.addAuthor( "Ranine Tarabay", "developer", "ranine.a.tarabay@gmail.com", "" );
     return about;
 
 }
@@ -65,17 +62,8 @@ makeAbout()
 
 namespace Feel
 {
-using namespace Feel::vf;
-
-/**
- * \class Stokes class
- * \brief solves the stokes equations
- *
- */
-
-class Driven_Cavity
-    :
-public Application
+template<int Dim>
+class DrivenCavity : public Application
 {
     typedef Application super;
 public:
@@ -83,14 +71,7 @@ public:
 
     typedef double value_type;
 
-    typedef Backend<value_type> backend_type;
-    typedef boost::shared_ptr<backend_type> backend_ptrtype;
-
-    /*matrix*/
-    typedef backend_type::sparse_matrix_ptrtype sparse_matrix_ptrtype;
-    typedef backend_type::vector_ptrtype vector_ptrtype;
-    typedef Simplex<2> convex_type;
-
+    typedef Simplex<Dim> convex_type;
     typedef Mesh<convex_type> mesh_type;
     typedef boost::shared_ptr<mesh_type> mesh_ptrtype;
 
@@ -124,76 +105,151 @@ public:
     /* export */
     typedef Exporter<mesh_type> export_type;
 
-    FEELPP_DONT_INLINE
-    Driven_Cavity( );
+    DrivenCavity( );
 
     // init mesh and space
-    FEELPP_DONT_INLINE
-    void init();
+    FEELPP_DONT_INLINE void init();
 
-    /**
-     * run the convergence test
-     */
-    FEELPP_DONT_INLINE
-    void run();
+    FEELPP_DONT_INLINE void run();
 
-private:
-    //FEELPP_DONT_INLINE
-    //void exportResults( element_type& u, element_type& v );
+    FEELPP_DONT_INLINE void exportResults( element_type const& U );
+
+    FEELPP_DONT_INLINE void Jacobian(const vector_ptrtype& X, sparse_matrix_ptrtype& J);
+    FEELPP_DONT_INLINE void Residual(const vector_ptrtype& X, vector_ptrtype& R);
 
 private:
 
-    backend_ptrtype M_backend;
     double meshSize;
 
-    double mu;
+    double Re;
     double penalbc;
 
     mesh_ptrtype mesh;
     space_ptrtype Vh;
-    space_ptrtype_U Uh;
-    sparse_matrix_ptrtype M,D;
-    vector_ptrtype F;
 
     boost::shared_ptr<export_type> exporter;
-}; // Driven_Cavity
+}; // DrivenCavity
 
-Driven_Cavity::Driven_Cavity( )
+template<int Dim>
+DrivenCavity<Dim>::DrivenCavity( )
     :
     super( ),
-    M_backend( backend_type::build( this->vm() ) ),
-    meshSize( this->vm()["hsize"].template as<double>() ),
-    mu( this->vm()["mu"].template as<value_type>() ),
+    Re( this->vm()["Re"].template as<value_type>() ),
     penalbc( this->vm()["bccoeff"].template as<value_type>() ),
     exporter( Exporter<mesh_type>::New( this->vm(), this->about().appName() ) )
 {
 
 }
 
-void Driven_Cavity::init()
+template<int Dim>
+void DrivenCavity<Dim>::init()
 {
     if ( this->vm().count( "help" ) )
     {
         std::cout << this->optionsDescription() << "\n";
         return;
     }
-     mesh = createGMSHMesh( _mesh=new mesh_type,
-                            _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES|MESH_RENUMBER,
-                            _desc=domain( _name= ( boost::format( "%1%-%2%-%3%" ) % "hypercube" % convex_type().dimension() % 1 ).str() ,
-                                          _shape="hypercube",
-                                          _dim=convex_type().dimension(),
-                                          _h=meshSize ) );
-     //mesh = unitSquare();
-    mesh->addMarkerName( "inlet", 1, 1 );
-    mesh->addMarkerName( "outlet", 3, 1 );
-    mesh->addMarkerName( "wall1", 2, 1 );
-    mesh->addMarkerName( "wall2", 4, 1 );
+    mesh = loadMesh(_mesh=new Mesh<Simplex<2>>);
     std::cout << "number of elements of 2D: " << mesh->numElements() << "\n";
 
     Vh = space_type::New( mesh );
 }
 
-void Driven_Cavity::run()
+template<int Dim>
+void
+DrivenCavity<Dim>::Jacobian(const vector_ptrtype& X, sparse_matrix_ptrtype& J)
+{
+    auto U = Vh->element( "(u,p)" );
+    auto V = Vh->element( "(v,q)" );
+    auto u = U.template element<0>( "u" );
+    auto v = V.template element<0>( "u" );
+    auto p = U.template element<1>( "p" );
+    auto q = V.template element<1>( "p" );
+    //#if defined( FEELPP_USE_LM )
+    auto lambda = U.template element<2>();
+    auto nu = V.template element<2>();
+
+    //#endif
+
+    if (!J) J = backend(_name="newtonns")->newMatrix( Vh, Vh );
+    auto a = form2( _test=Vh, _trial=Vh, _matrix=J );
+    a = integrate( elements( mesh ), inner(gradt( u ),grad( v ))/Re );
+    a += integrate( elements( mesh ), id(q)*divt(u) -idt(p)*div(v) );
+    // Convective terms
+    a += integrate( elements( mesh ), trans(id(v))*gradv(u)*idt(u));
+    a += integrate( elements( mesh ), trans(id(v))*gradt(u)*idv(u));
+
+    //#if defined( FEELPP_USE_LM )
+    a += integrate(elements(mesh), id(q)*idt(lambda)+idt(p)*id(nu));
+    //#elif
+    //a += integrate(elements(mesh), idt(p)*id(nu));
+
+    //Weak Dirichlet conditions
+    a += integrate( boundaryfaces( mesh ),-trans( -idt(p)*N()+gradt(u)*N()/Re )*id( v ));//
+    a += integrate( boundaryfaces( mesh ),-trans( -id(p)*N()+grad(u)*N()/Re )*idt( u ));//
+    a += integrate( boundaryfaces( mesh ), +penalbc*inner( idt( u ),id( v ) )/hFace() );
+
+
+}
+
+template<int Dim>
+void
+DrivenCavity<Dim>::Residual(const vector_ptrtype& X, vector_ptrtype& R)
+{
+    auto U = Vh->element( "(u,p)" );
+    auto V = Vh->element( "(v,q)" );
+    auto u = U.template element<0>( "u" );
+    //auto u_exact = U.template element<0>( "u_exact" );
+    auto v = V.template element<0>( "u" );
+    auto p = U.template element<1>( "p" );
+    auto q = V.template element<1>( "p" );
+    //#if defined( FEELPP_USE_LM )
+    auto lambda = U.template element<2>();
+    auto nu = V.template element<2>();
+    //#endif
+
+    auto uex=unitX();
+    auto u_exact=vf::project(Vh->template functionSpace<0>(), markedfaces(mesh, "wall2"), uex );
+
+
+    U=*X;
+    auto r = form1( _test=Vh, _vector=R );
+    //r += integrate( elements( mesh ),-inner( f,id( v ) ) );
+    r = integrate( elements( mesh ), trans(gradv( u )*idv(u))*id(v));//convective term
+    r += integrate( elements( mesh ), inner(gradv( u ),grad( v ))/Re );
+    r +=  integrate( elements( mesh ),-idv(p)*div(v) + id(q)*divv(u));
+    //#if defined( FEELPP_USE_LM )
+    r += integrate ( elements( mesh ), +id( q )*idv( lambda )+idv( p )*id( nu ) );
+    //#endif
+
+
+    //Weak Dirichlet
+    auto SigmaNv = ( -idv( p )*N() + gradv( u )*N()/Re );
+    auto SigmaN = ( -id( q )*N() + grad( v )*N()/Re );
+    r +=integrate ( boundaryfaces(mesh), - trans( SigmaNv )*id( v ) - trans( SigmaN )*( idv( u ) -idv(u_exact) ) + penalbc*trans( idv( u ) - idv(u_exact) )*id( v )/hFace() );
+
+
+}
+
+template<int Dim>
+void DrivenCavity<Dim>::exportResults( element_type const& U )
+{
+    auto uex=unitX();
+    auto u_exact=vf::project(Vh->template functionSpace<0>(), markedfaces(mesh, "wall2"), uex );
+
+    if ( exporter->doExport() )
+    {
+        exporter->step( 0 )->setMesh( U.functionSpace()->mesh() );
+        exporter->step( 0 )->addRegions();
+        exporter->step( 0 )->add( "u", U.template element<0>() );
+        exporter->step( 0 )->add( "p", U.template element<1>() );
+        exporter->step( 0 )->add( "uex", u_exact);
+        exporter->save();
+    }
+
+}
+template<int Dim>
+void DrivenCavity<Dim>::run()
 {
     this->init();
 
@@ -209,104 +265,37 @@ void Driven_Cavity::run()
     //#endif
 
 
-    auto Jacobian = [=](const vector_ptrtype& X, sparse_matrix_ptrtype& J)
-        {
-            auto U = Vh->element( "(u,p)" );
-            auto V = Vh->element( "(v,q)" );
-            auto u = U.template element<0>( "u" );
-            auto v = V.template element<0>( "u" );
-            auto p = U.template element<1>( "p" );
-            auto q = V.template element<1>( "p" );
-            //#if defined( FEELPP_USE_LM )
-            auto lambda = U.template element<2>();
-            auto nu = V.template element<2>();
-            auto mu=0.04;
-            //#endif
 
-            if (!J) J = backend()->newMatrix( Vh, Vh );
-            auto a = form2( _test=Vh, _trial=Vh, _matrix=J );
-            a = integrate( elements( mesh ), mu*inner(gradt( u ),grad( v )) );
-            a += integrate( elements( mesh ), id(q)*divt(u) -idt(p)*div(v) );
-            // Convective terms
-            a += integrate( elements( mesh ), trans(id(v))*gradv(u)*idt(u));
-            a += integrate( elements( mesh ), trans(id(v))*gradt(u)*idv(u));
-
-            //#if defined( FEELPP_USE_LM )
-            a += integrate(elements(mesh), id(q)*idt(lambda)+idt(p)*id(nu));
-            //#elif
-            //a += integrate(elements(mesh), idt(p)*id(nu));
-
-            //Weak Dirichlet conditions
-            a += integrate( boundaryfaces( mesh ),-trans( -idt(p)*N()+mu*gradt(u)*N() )*id( v ));//
-            a += integrate( boundaryfaces( mesh ),-trans( -id(p)*N()+mu*grad(u)*N() )*idt( u ));//
-            a += integrate( boundaryfaces( mesh ), +penalbc*inner( idt( u ),id( v ) )/hFace() );
-
-
-        };
-
-    auto Residual = [=](const vector_ptrtype& X, vector_ptrtype& R)
-        {
-            auto U = Vh->element( "(u,p)" );
-            auto V = Vh->element( "(v,q)" );
-            auto u = U.template element<0>( "u" );
-            //auto u_exact = U.template element<0>( "u_exact" );
-            auto v = V.template element<0>( "u" );
-            auto p = U.template element<1>( "p" );
-            auto q = V.template element<1>( "p" );
-            //#if defined( FEELPP_USE_LM )
-            auto lambda = U.template element<2>();
-            auto nu = V.template element<2>();
-            //#endif
-            auto mu=0.04;
-
-
-            auto uex=vec(cst(1.) , cst(0.) );
-            auto u_exact=vf::project(Vh->functionSpace<0>(), markedfaces(mesh, 4), uex );
-
-
-            U=*X;
-            auto r = form1( _test=Vh, _vector=R );
-            //r += integrate( elements( mesh ),-inner( f,id( v ) ) );
-            r = integrate( elements( mesh ), trans(gradv( u )*idv(u))*id(v));//convective term
-            r += integrate( elements( mesh ), inner(mu*gradv( u ),grad( v )) );
-            r +=  integrate( elements( mesh ),-idv(p)*div(v) + id(q)*divv(u));
-            //#if defined( FEELPP_USE_LM )
-            r += integrate ( elements( mesh ), +id( q )*idv( lambda )+idv( p )*id( nu ) );
-            //#endif
-
-
-            //Weak Dirichlet
-            auto SigmaNv = ( -idv( p )*N() + mu*gradv( u )*N() );
-            auto SigmaN = ( -id( q )*N() + mu*grad( v )*N() );
-            r +=integrate ( boundaryfaces(mesh), - trans( SigmaNv )*id( v ) - trans( SigmaN )*( idv( u ) -idv(u_exact) ) + penalbc*trans( idv( u ) - idv(u_exact) )*id( v )/hFace() );
-
-
-        };
-    u=vf::project(Vh->functionSpace<0>(), elements(mesh), zero<2,1>());
-    p=vf::project(Vh->functionSpace<1>(), elements(mesh), constant(0.0));
+    u=vf::project(Vh->template functionSpace<0>(), elements(mesh), zero<Dim,1>());
+    p=vf::project(Vh->template functionSpace<1>(), elements(mesh), constant(0.0));
 
     std::cout << "Initializing residual " << "\n";
-    backend()->nlSolver()->residual = Residual;
+    backend(_name="newtonns")->nlSolver()->residual = boost::bind( &DrivenCavity::Residual,
+                                                                   boost::ref( *this ), _1, _2 );
     std::cout << "Initializing the Jacobian matrix" << "\n";
-    backend()->nlSolver()->jacobian = Jacobian;
-    std::cout << "Start solving" << "\n";
-    backend()->nlSolve( _solution=U );
-    std::cout << "Done!" << "\n";
+    backend(_name="newtonns")->nlSolver()->jacobian = boost::bind( &DrivenCavity::Jacobian,
+                                                                   boost::ref( *this ), _1, _2 );
 
-    auto uex=vec(cst(1.) , cst(0.) );
-    auto u_exact=vf::project(Vh->functionSpace<0>(), markedfaces(mesh, 4), uex );
+    if ( option(_name="continuation" ).as<bool>() )
+    {
+        double ReTarget = Re;
+        int N = std::ceil( std::log( Re ) );
+        for( int i  = 0; i <= N; ++i )
+        {
+            Re = std::exp( std::log(1)+i*(std::log(ReTarget)-std::log(1))/double(N));
+            std::cout << "Start solving for Reynolds = " << Re << "\n";
+            backend(_name="newtonns")->nlSolve( _solution=U );
+            std::cout << "Done!" << "\n";
+        }
+    }
+    else
+    {
+        std::cout << "Start solving for Reynolds = " << Re << "\n";
+        backend(_name="newtonns")->nlSolve( _solution=U );
+        std::cout << "Done!" << "\n";
+    }
 
-        if ( exporter->doExport() )
-            {
-                exporter->step( 0 )->setMesh( U.functionSpace()->mesh() );
-                exporter->step( 0 )->addRegions();
-                auto v = U.functionSpace()->template functionSpace<0> ()->element();
-                v = U.template element<0>();
-                exporter->step( 0 )->add( "u", U.template element<0>() );
-                exporter->step( 0 )->add( "p", U.template element<1>() );
-                exporter->step( 0 )->add( "uex", u_exact);
-                exporter->save();
-            }
+    this->exportResults( U );
 };
 }
 
@@ -320,6 +309,6 @@ int main( int argc, char** argv )
                      _about=about(_name="driven_cavity",
                                   _author="Christophe Prud'homme",
                                   _email="christophe.prudhomme@feelpp.org") );
-    Feel::Driven_Cavity Driven_Cavity;
-    Driven_Cavity.run();
+    Feel::DrivenCavity<2> DrivenCavity;
+    DrivenCavity.run();
 }
