1777ff59c11061418b4682df379fe17d6ae169c7
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-11-22 06:47:02 +0000

    bigger changes @ranine @mszopos:  - split files  - make it 2D, 3D: instantiate separately 2D and 3D  - driven_cavity -> drivencavity  - 3d geo file to be done  - ...

---
+++ doc/manual/nonlinear/drivencavity_2d.cpp
@@ -0,0 +1,35 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-
+
+  This file is part of the Feel library
+
+  Author(s): Christophe Prud'homme <prudhomme@unistra.fr>
+       Date: 2013-11-22
+
+  Copyright (C) 2013 Université de Strasbourg
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file drivencavity_2d.cpp
+   \author Christophe Prud'homme <prudhomme@unistra.fr>
+   \date 2013-11-22
+ */
+#include "drivencavity_impl.hpp"
+
+namespace Feel
+{
+// instantiate 2D version
+template class DrivenCavity<2>;
+}
