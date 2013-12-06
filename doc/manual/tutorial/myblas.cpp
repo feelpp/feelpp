97e3954f814dd30836bc342e64663af078b3b266
Vincent Huber <vincent.huber@cemosis.fr>
2013-12-06 16:57:28 +0100

    try different matrix vector product

---
+++ doc/manual/tutorial/myblas.cpp
@@ -0,0 +1,63 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim: set fenc=utf-8 ft=tcl et sw=4 ts=4 sts=4 expandtab
+
+  This file is part of the Feel library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
+	     Guillaume Dollé <guillaume.dolle@math.unistra.fr>
+  Date: 2013-02-07
+
+  Copyright (C) 2008-2009 Université Joseph Fourier (Grenoble I)
+  Copyright (C) 2013 Feel++ Consortium
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3.0 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file myapp.cpp
+   \author Guillaume DOLLÉ <guillaume.dolle@math.unistra.fr>
+   \date 2013-02-07
+ */
+
+#include <feel/feel.hpp>
+using namespace Feel;
+
+int main( int argc, char* argv[] )
+{
+    // create custom command option
+    po::options_description app_options( "MyApp options" );
+    app_options.add( feel_options() );
+    app_options.add_options()
+	( "value",
+          po::value<double>() -> default_value(4.2),
+          "a 'double' with default value" )
+    ;
+
+    // initialize feel++ environment
+    Environment env( _argc=argc, _argv=argv,
+                     _desc=app_options,
+                     _about=about( _name="myapp",
+                                   _author="Feel++ Consortium",
+                                   _email="feelpp-devel@feelpp.org") );
+
+    auto m = mat<2,2>(cst(1),cst(1),cst(1),cst(1));
+    auto v1 = vec(cst(1),cst(1));
+    auto v2 = mat<2,1>(cst(1),cst(1));
+    auto v3 = trans(v2); 
+    auto mv1 = m*v1;
+    auto mv2 = m*v2;
+    auto mv3 = m*v3;
+
+
+} 
+
