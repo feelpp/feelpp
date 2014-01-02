affcb196cd601c841103c5a61ade4e33dace2f27
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-01-02 13:50:29 +0000

    update(simpler and use #226) and fix execution

--- doc/manual/heat/thermalfin.cpp
+++ doc/manual/heat/thermalfin.cpp
@@ -21,7 +21,15 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
-#include <feel/feel.hpp>
+#include <feel/feelcore/environment.hpp>
+#include <feel/feeldiscr/mesh.hpp>
+#include <feel/feeldiscr/pch.hpp>
+#include <feel/feelfilters/exporter.hpp>
+#include <feel/feelfilters/loadmesh.hpp>
+
+#include <feel/feelvf/form.hpp>
+#include <feel/feelvf/operators.hpp>
+#include <feel/feelvf/operations.hpp>
 
 inline
 Feel::po::options_description
@@ -86,7 +94,7 @@ ThermalFin::run()
     /*
      * First we create the mesh
      */
-    auto mesh = loadMesh( _mesh=new mesh_type );
+    auto mesh = loadMesh( _mesh=new Mesh<Simplex<2>> );
 
     /*
      * The function space and some associate elements are then defined
@@ -97,67 +105,59 @@ ThermalFin::run()
 
     // flux a bottom of fin
     auto f = form1( Xh );
-    f = integrate( markedfaces( mesh,1 ), id( v ) );
+    f = integrate( markedfaces( mesh, "Tflux" ), id( v ) );
 
-    /*
-     * Construction of the left hand side
-     */
-    Dcst = M_backend->newMatrix( Xh, Xh );
 
-    form2( Xh, Xh, Dcst ) = integrate( markedelements( mesh,1 ), ( gradt( u )*trans( grad( v ) ) ) );
+    double Bimin = option(_name="Bimin").as<double>();
+    double Bimax = option(_name="Bimax").as<double>();
+    int N = option(_name="N").as<int>();
 
-    Dcst->close();
-
-    double Bimin = this->vm()["Bimin"].as<double>();
-    double Bimax = this->vm()["Bimax"].as<double>();
-    int N = this->vm()["N"].as<int>();
-
+    auto e = exporter( _mesh=mesh );
     for ( int i = 0; i < N; ++i )
     {
-
         int Nb = 2;
         Eigen::MatrixXd k( Nb, Nb );
-        k( 0 , 0 ) = this->vm()["k0"].as<double>();
-        k( 1 , 0 ) = this->vm()["k1"].as<double>();
-        k( 0 , 1 ) = this->vm()["k2"].as<double>();
-        k( 1 , 1 ) = this->vm()["k3"].as<double>();
+        k( 0 , 0 ) = option(_name="k0").as<double>();
+        k( 1 , 0 ) = option(_name="k1").as<double>();
+        k( 0 , 1 ) = option(_name="k2").as<double>();
+        k( 1 , 1 ) = option(_name="k3").as<double>();
+        std::map<std::string,double> material{ {"Mat0", k(0,0)}, {"Mat1",k(1,0)}, {"Mat2",k(0,1)},  {"Mat3",k(1,1)} };
 
         auto a = form2( Xh, Xh );
-        for ( int r = 0; r < Nb; ++r )
-            for ( int c = 0; c < Nb; ++c )
-            {
-
-                a += integrate( markedelements( mesh,Nb*c+r+1 ),
-                                k( r,c )*gradt( u )*trans( grad( v ) ) );
-            }
+        for ( auto marker : material )
+        {
+            LOG(INFO) << "Material " << marker.first << " with conductivity " << marker.second;
+            a += integrate( markedelements( mesh, marker.first ),
+                            marker.second*gradt( u )*trans( grad( v ) ) );
+        }
 
         double Bi;
 
         if ( N == 1 )
             Bi = 0.1;
-
         else
-            Bi = math::exp( math::log( Bimin )+value_type( i )*( math::log( Bimax )-math::log( Bimin ) )/value_type( N-1 ) );
+            Bi = math::exp( math::log( Bimin )+double( i )*( math::log( Bimax )-math::log( Bimin ) )/double( N-1 ) );
 
         LOG(INFO) << "Bi = " << Bi << "\n";
 
-        a += integrate( markedfaces( mesh,2 ), Bi*idt( u )*id( v ) );
+        a += integrate( markedfaces( mesh, "Tfourier" ), Bi*idt( u )*id( v ) );
 
 
-        a.solve( _solution=u, _rhs=Fcst );
+        a.solve( _solution=u, _rhs=f );
 
-        double moy_u = ( integrate( markedfaces( mesh,1 ), idv( u ) ).evaluate()( 0,0 ) /
-                         integrate( markedfaces( mesh,1 ), constant( 1.0 ) ).evaluate()( 0,0 ) );
-        std::cout.precision( 5 );
-        std::cout << std::setw( 5 ) << k( 0,0 ) << " "
-                  << std::setw( 5 ) << k( 1,0 ) << " "
-                  << std::setw( 5 ) << k( 0,1 ) << " "
-                  << std::setw( 5 ) << k( 1,1 ) << " "
-                  << std::setw( 5 ) << Bi << " "
-                  << std::setw( 10 ) << moy_u << "\n";
-
-        e->step( i )->setMesh( U.functionSpace()->mesh() );
-        e->step( i )->add( "Temperature", U );
+        double moy_u = mean( markedfaces( mesh, "Tflux" ), idv( u ) )( 0,0 );
+        if (Environment::worldComm().isMasterRank())
+        {
+            std::cout.precision( 5 );
+            std::cout << std::setw( 5 ) << k( 0,0 ) << " "
+                      << std::setw( 5 ) << k( 1,0 ) << " "
+                      << std::setw( 5 ) << k( 0,1 ) << " "
+                      << std::setw( 5 ) << k( 1,1 ) << " "
+                      << std::setw( 5 ) << Bi << " "
+                      << std::setw( 10 ) << moy_u << "\n";
+        }
+        e->step( i )->setMesh( mesh );
+        e->step( i )->add( "Temperature", u );
         e->save();
 
 
