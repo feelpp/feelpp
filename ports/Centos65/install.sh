133a1e8aebf9efab2b56297eee569f09524d93f4
Vincent Huber <vincent.huber@cemosis.fr>
2014-01-24 14:38:11 +0100

    Up to devtoolset-2

--- ports/Centos65/install.sh
+++ ports/Centos65/install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-wget http://people.centos.org/tru/devtools-1.1/devtools-1.1.repo -O /etc/yum.repos.d/devtools-1.1.repo
-wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+sudo wget -c https://www.scientificlinux.org/documentation/gpg/RPM-GPG-KEY-cern -O RPM-GPG-KEY-cern.asc
+sudo rpm --import RPM-GPG-KEY-cern.asc
+wget -c http://people.centos.org/tru/devtools-1.1/devtools-1.1.repo -O /etc/yum.repos.d/devtools-1.1.repo
+wget -c http://linuxsoft.cern.ch/cern/devtoolset/slc6-devtoolset.repo -O /etc/yum.repos.d/devtools-2.repo
+wget -c http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+wget -c http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+wget -c ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/cathay4t:/misc-rhel6/CentOS_CentOS-6/x86_64/cmake-2.8.9-3.1.x86_64.rpm
+sudo rpm -U cmake-2.8.9-3.1.x86_64.rpm
 sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 sudo yum -y update
-sudo yum -y install devtoolset-1.1 cmake28 cmake28-gui openmpi openmpi-devel git bison bison-devel flex lapack lapack-devel automake autoconf
-sudo ln -s /usr/bin/cmake28 /usr/bin/cmake
+sudo yum -y install devtoolset-1.1 devtoolset-2 openmpi openmpi-devel git bison bison-devel flex lapack lapack-devel automake autoconf
 export PATH=/usr/lib64/openmpi/bin:$PATH
 export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib:$LD_LIBRARY_PATH
-scl enable devtoolset-1.1 ./coreInstall.sh
+scl enable devtoolset-2 ./coreInstall.sh
