5f894caef61e47193aef4c18f943ba9a491846da
Vincent Huber <vincent.huber@cemosis.fr>
2014-01-24 14:38:11 +0100

    Up to devtoolset-2

--- ports/Centos65/feelpp.sh
+++ ports/Centos65/feelpp.sh
@@ -4,4 +4,4 @@ git clone https://github.com/feelpp/feelpp.git
 mkdir -p $basedir/feelpp_build_dir 
 cd $basedir/feelpp_build_dir
 cmake $basedir/feelpp -DFEELPP_ENABLE_DOCUMENTATION=off 
-make -j$nbProc
+make -j4
