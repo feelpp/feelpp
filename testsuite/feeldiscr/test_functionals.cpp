b5422ff5121480d7ba0c801d1097fcef6996899d
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-01-30 21:01:06 +0100

    added functional tests. start with raviartthomas see #251

--- testsuite/feeldiscr/test_functionals.cpp
+++ testsuite/feeldiscr/test_functionals.cpp
@@ -38,8 +38,7 @@
 #include <testsuite/testsuite.hpp>
 
 #include <feel/feelalg/backend.hpp>
-#include <feel/feelfilters/creategmshmesh.hpp>
-#include <feel/feelfilters/domain.hpp>
+#include <feel/feelfilters/loadmesh.hpp>
 #include <feel/feelfilters/exporter.hpp>
 #include <feel/feelvf/form.hpp>
 #include <feel/feelvf/operators.hpp>
@@ -47,11 +46,41 @@
 #include <feel/feelvf/on.hpp>
 #include <feel/feelvf/vf.hpp>
 #include <feel/feeldiscr/dh.hpp>
+#include <feel/feeldiscr/odh.hpp>
 #include <feel/feeldiscr/pchv.hpp>
+
+using namespace Feel;
+
+inline
+po::options_description
+makeOptions()
+{
+    po::options_description testoptions( "test functionals options" );
+    testoptions.add_options()
+    ;
+    return testoptions.add( Feel::feel_options() );
+}
+
+inline
+AboutData
+makeAbout()
+{
+    AboutData about( "test_functionals" ,
+                     "test_functionals" ,
+                     "0.1",
+                     "Test for functionals",
+                     AboutData::License_GPL,
+                     "Copyright (c) 2014 Feel++ Consortium" );
+    about.addAuthor( "Cecile Daversin", "developer", "cecile.daversin@lncmi.cnrs.fr", "" );
+    about.addAuthor( "Christophe Prud'homme", "developer", "christophe.prudhomme@feelpp.org", "" );
+    return about;
+
+}
+
 
 #if defined( USE_BOOST_TEST )
 
-FEELPP_ENVIRONMENT_WITH_OPTIONS( Feel::makeAbout(), Feel::makeOptions() )
+FEELPP_ENVIRONMENT_WITH_OPTIONS( makeAbout(), makeOptions() )
 
 BOOST_AUTO_TEST_SUITE( functionals )
 
@@ -59,30 +88,44 @@ BOOST_AUTO_TEST_CASE( test_projection_hdiv_rt )
 {
     BOOST_TEST_MESSAGE( "test Directional component integration" );
 
+    using namespace Feel;
+
     // only one element
     auto mesh = loadMesh( _mesh=new Mesh<Simplex<2>>, _filename="ReferenceTriangle.geo" );
 
     // RTk
-    auto Rth = Dh<1>( mesh );
-    auto urt = Xh->element();
+    auto RTh = Dh<0>( mesh );
+    auto urt = RTh->element();
     urt.zero();
-
+    BOOST_TEST_MESSAGE( "Rth defined, dimension: " << RTh->nLocalDof() );
     // Lagrange
     auto Xh = Pchv<1>( mesh );
     auto ul = Xh->element();
     ul = vf::project( _space=Xh, _range=elements(mesh), _expr=P() );
-
+    ul.printMatlab( "ul.m" );
+    BOOST_TEST_MESSAGE( "Xh defined, dimension: " << Xh->nLocalDof() );
+    BOOST_TEST_MESSAGE( "n elements: " << nelements( elements(mesh)  ) );
     for( auto const& element : elements(mesh) )
     {
-        auto mesh_element = createSubmesh( mesh, idedelement(element.id()), Environment::worldCommSeq() );
-        auto mesh_face = createSubmesh( mesh_element, boundaryfaces( mesh ), Environment::worldCommSeq() );
-        auto P = Odh<0>( mesh_face );
-        auto p = P->element();
-        auto l = form1( _test=P );
-        l = integrate( _range=elements(mesh_face), _expr=trans(idv(u))*N()*id(p) );
-        urt.add( l.vector() );
+        LOG(INFO) << "element : " << element.id();
+        auto mesh_element = createSubmesh( mesh, idedelements(mesh,element.id()), Environment::worldCommSeq() );
+        BOOST_TEST_MESSAGE( "n elements of extracted element " << element.id() << " : " << nelements( elements(mesh_element)  ) );
+        auto mesh_face = createSubmesh( mesh_element, boundaryfaces( mesh_element ), Environment::worldCommSeq() );
+        BOOST_TEST_MESSAGE( "n boundary faces in extracted element " << element.id() << " : " << nelements( elements(mesh_face)  ) );
+        auto Ph = Odh<0>( mesh_face );
+        BOOST_TEST_MESSAGE( "dimension of Ph : " << Ph->nLocalDof() );
+        auto p = Ph->element();
+        auto l = form1( _test=Ph );
+        l = integrate( _range=boundaryfaces(mesh_element), _expr=trans(print(idv(ul),"ul"))*print(N(),"n")*print(id(p),"p" ) );
+        l.vector().printMatlab( "l.m" );
+        for( auto const& dof: RTh->dof()->localDof( element.id() ) )
+        {
+            LOG(INFO) << "dof[ " << element.id() << ", " << dof.first.localDof() << "]=" << dof.second.index();
+            urt[dof.second.index()] = l.vector()(dof.first.localDof());
+            LOG(INFO) << "value[dof.second.index()] = " << urt[dof.second.index()];
+        }
     }
-
+    urt.printMatlab( "urt.m" );
 
     BOOST_TEST_MESSAGE( "test Directional component integration done" );
 }
