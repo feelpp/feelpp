005152eea7b9deb4870cd3a05e79070370210716
vhuber <vincent.huber@cemosis.fr>
2014-02-11 17:01:29 +0100

    Update boost.sh

--- ports/Centos65/boost.sh
+++ ports/Centos65/boost.sh
@@ -2,7 +2,11 @@
 mkdir -p $boostDir/src
 cd $boostDir/src
 _ver=54
-wget -c http://kent.dl.sourceforge.net/project/boost/boost/1.$ver.0/boost_1_$ver_0.tar.bz2
+if [ $_ver -eq 54 ]
+  then wget -c http://netcologne.dl.sourceforge.net/project/boost/boost/1.54.0/boost_1_54_0.tar.bz2
+  else wget -c http://kent.dl.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2
+fi
+
 tar xjf boost_1_$ver_0.tar.bz2
 cd boost_1_$ver_0
 rm user-config.jam
