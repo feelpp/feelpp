cae3b45c428b8d13a6df3b15085d57d6565bb809
vhuber <vincent.huber@cemosis.fr>
2014-02-11 16:55:18 +0100

    set boost version to 1.54

--- ports/Centos65/boost.sh
+++ ports/Centos65/boost.sh
@@ -1,9 +1,10 @@
 #Installation de BOOST
 mkdir -p $boostDir/src
 cd $boostDir/src
-wget -c http://kent.dl.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2
-tar xjf boost_1_55_0.tar.bz2
-cd boost_1_55_0
+_ver=54
+wget -c http://kent.dl.sourceforge.net/project/boost/boost/1.$ver.0/boost_1_$ver_0.tar.bz2
+tar xjf boost_1_$ver_0.tar.bz2
+cd boost_1_$ver_0
 rm user-config.jam
 echo "using mpi ;" >> user-config.jam
 echo "" >> user-config.jam
