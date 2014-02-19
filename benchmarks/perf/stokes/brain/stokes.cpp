6ee347e77e730320fc5b5d414f401d037cb125ae
Ranine Tarabay <ranine.a.tarabay@gmail.com>
2014-02-19 15:36:54 +0100

    up code

--- benchmarks/perf/stokes/brain/stokes.cpp
+++ benchmarks/perf/stokes/brain/stokes.cpp
@@ -27,124 +27,6 @@
 #include <feel/feel.hpp>
 using namespace Feel;
 
-/**
- \page StokesTutorial Stokes Tutorial
-\author Feel++ Consortium
-\date 2013-02-19
-
-\tableofcontents
-<br>
-<hr>
-<br>
-
-\section Stokes_Theory Theory
-Let solve the stokes equation considering a Poiseuille flow profile. <br>
-We have the following system of equations,
-<br><center>\f$
-\left\{
-\begin{aligned}
--\mu\Delta \bf u + \nabla p & =  \bf f & \text{on}\; \Omega \;, \\
-                \nabla\cdot\bf u & =  0 & \text{on}\; \Omega \;, \\
-                \bf u  & =  g & \text{on}\; \Gamma \;, \\
-\end{aligned}
-\right
-\f$</center><br>
-where \f$u\in [H_g^1(\Omega)]^d\f$ denotes the flow speed, \f$p\in [L_0^2(\Omega)]\f$ the fluid pressure, \f$\mu\f$ the fluid viscosity.<br>
-The last boundary condition expresses a null pressure fixed on the outlet.<br>
-The Poiseuille profile on the boundary is,
-<br><center>\f$
-g(x,y)=\left(
-\begin{aligned}
- y(1-y) \\
- 0      \\
-\end{aligned}
-\right)
-\f$</center><br>
-The method used to obtain the strong formulation is closed to the one used for the laplacian (see section \ref Laplacian ).<br>
-We multiply the first equation by a test function \f$v\in H^1(\Omega)\f$ and we integrate on the domain \f$\Omega\f$,
-<br><center>\f$
-\begin{aligned}
- \left(
-\int_\Omega \mu \nabla \mathbf u : \nabla \mathbf v
--\int_{\partial\Omega} \frac{\partial \mathbf u}{\partial \mathbf n} \cdot \mathbf v
-\right)
-+\int_\Omega ( \nabla\cdot(p \mathbf v) - \mathbf p \nabla\cdot v )
-=\int_\Omega \mathbf f \cdot \mathbf v \;.
-\end{aligned}
-\right)
-\f$</center><br>
-where \f$n\f$ denotes a normal vector on the boundary.<br>
-The divergence theorem (or Gauss's theorem) gives,
-<br><center>\f$
-\begin{aligned}
-\int_\Omega \nabla\cdot(p \mathbf v) = \int_{\partial\Omega} p \mathbf v\cdot \mathbf n \;.
-\end{aligned}
-\right)
-\f$</center><br>
-We have to add a consistency terms to the equation to guaranty the symmetry of the bilinear form.<br>
-This term is provided by the second equation. We multiply this equation by a test function \f$q\in L_2(\Omega)\f$ and we integrate on the domain \f$\Omega\f$,
-<br><center>\f$
-\begin{aligned}
-\int_{\Omega} \nabla\cdot\mathbf u q = 0 \;,
-\end{aligned}
-\right)
-\f$</center><br>
-Finally, we deduce from the equations and after rearranging the integrals the variationnal formulation,
-<br><center>\f$
-\begin{aligned}
-\int_\Omega \mu \nabla \mathbf u :\nabla \mathbf v
-+\int_\Omega \left( \nabla\cdot\mathbf u q - p \nabla\cdot\mathbf v \right)
-+
-    \int_{\partial\Omega} \left( p \mathbf n -
-   \frac{\partial \mathbf u}{\partial \mathbf n} \right)
-     \cdot \mathbf v
-=\int_\Omega \mathbf f \cdot \mathbf v
-\end{aligned}
-\right)
-\f$</center><br>
-Let us assume now that \f$(\mathbf v,q) \in [H_0^1(\Omega)]^d \times L_0^2(\Omega)\f$, the variationnal formulation leads to:
-Find \f$(\mathbf u,p)\in [H_g^1(\Omega)]^d\times L_0^2(\Omega) \f$ such that for all \f$(\mathbf v,q) \in [H_0^1(\Omega)]^d \times L_0^2(\Omega)\f$
-<br><center>\f$
-\begin{aligned}
-\int_\Omega \mu \nabla \mathbf u :\nabla \mathbf v
-+\int_\Omega \left( \nabla\cdot\mathbf u q - p \nabla\cdot\mathbf v \right)
-=\int_\Omega \mathbf f \cdot \mathbf v
-\end{aligned}
-\right)
-\f$</center><br>
-Or equivalently:
-<br><center>\f$
-\begin{aligned}
-  a((\mathbf u,p),(\mathbf v,q)) = l((\mathbf v,q))
-\end{aligned}
-\right)
-\f$</center><br>
-where \f$a\f$ is a bilinear form, continuous, coercive and where \f$l\f$ is a linear form.
-
-\section Stokes_Implementation Implementation
-Let's see the \feel code corresponding to this mathematical statement (source \c "doc/manual/tutorial/mystokes.cpp").<br>
-We suppose for this example the viscosity \f$\mu=1\f$ and \f$\mathbf f = 0\f$.
-
-The procedure to create the mesh is very simple.<br>
-You have to provide to the command line (or via the cfg file) the gmsh.filename option.<br>
-You can provide a \c .geo or a \c .msh file (created via gmsh).
-
-As for the laplacian problem, the code is very closed to the mathematical formulation.<br>
-We define the product of function spaces for the flow speed and the flow pressur using \c THch<order>()(TH stands for Taylor-Hoods) function which is \c Pch<N+1> \f$\times\f$ \c Pch<N> for respectively flow speed and pressure spaces.<br>
-We take an element
-\f$U=\left(
-    \begin{array}{c}
-        u \\
-        p \\
-    \end{array}
-\right)
-\f$
-in this space. Then we define the integrals of the variationnal formulation for the left and the right hand side. Finally, we apply the Poiseuille profile on the boundary.<br>
-We call the solver to resolve the problem (\ref Solver).
-\snippet mystokes.cpp marker_main
-
-*/
-/// [marker_main]
 int main(int argc, char**argv )
 {
     Environment env( _argc=argc, _argv=argv,
@@ -153,24 +35,14 @@ int main(int argc, char**argv )
                                   _author="Feel++ Consortium",
                                   _email="feelpp-devel@feelpp.org"));
 
-    // create the mesh
-    //auto mesh = loadMesh(_mesh=new Mesh<Simplex< 2 > > );
+    auto mesh = loadMesh( _mesh=new Mesh<Simplex<3>>,
+                          _filename="brainVeines20.msh",
+                          //_update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES,
+                          //_physical_are_elementary_regions=true,
+                          //_partitions=Environment::worldComm().localSize(),
+                          //_worldcomm=Environment::worldComm()
+                         );
 
-
-    //++++++++++++
-    auto mesh = loadGMSHMesh( _mesh=new Mesh<Simplex<3>>,
-                              _filename="brainVeines.msh",
-                              //_h = meshSizeInit(),
-                              _update=MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES,
-                              _physical_are_elementary_regions=true,
-                              _partitions=Environment::worldComm().localSize(),
-                              _worldcomm=Environment::worldComm() );
-    //double meshSize = option(_name="gmsh.hsize").template as<double>();
-    //std::cout<<"Mesh size = "<<mesh->h()<< "\n";
-    /*mesh->addMarkerName( "inlet", 52, 2 );
-    mesh->addMarkerName( "outlet", 53, 2 );
-    mesh->addMarkerName( "wall", 54, 2 );*/
-    //++++++++++++
 
 
     // function space
@@ -183,7 +55,7 @@ int main(int argc, char**argv )
 
     int i;
     //+++++++++++++++++++++++++ Velocity at inlet +++++++++++++++++++++++++++
-    auto sumAreaIN = integrate(markedfaces(mesh,1),cst(1.)).evaluate()(0,0);
+    /*auto sumAreaIN = integrate(markedfaces(mesh,1),cst(1.)).evaluate()(0,0);
     std::cout << "Area of inlet 1 = "<< sumAreaIN << "\n";
     double Area;
     std::cout.precision(20);
@@ -195,25 +67,28 @@ int main(int argc, char**argv )
                    sumAreaIN += Area;
                    std::cout << "Area of inlet "<<i<< " = "<< Area << "\n";
                }
-        }
+               }*/
+    auto sumAreaIN = integrate(markedfaces(mesh,52),cst(1.)).evaluate()(0,0); //52 is a physical group assembling all the entries
     std::cout << "The total sum of the inlet area = "<<sumAreaIN << "\n";
     auto u_inlet = -6000./sumAreaIN;
     std::cout << "u_inlet = "<< u_inlet << "\n";
-
-    auto flowIn = integrate(markedfaces(mesh,1),inner(u_inlet*N(),N() ) ).evaluate()(0,0);
+    auto flowIn = integrate(markedfaces(mesh,52),inner(u_inlet*N(),N() ) ).evaluate()(0,0);
+    /*auto flowIn = integrate(markedfaces(mesh,1),inner(u_inlet*N(),N() ) ).evaluate()(0,0);
     for (i=2;i<=31;i++)
         {
             if( (i!=7) && (i!=24))
                 flowIn += integrate(markedfaces(mesh,i),inner(u_inlet*N(),N() ) ).evaluate()(0,0);
-        }
+                }*/
     std::cout<<"flow_inlet = "<<flowIn<<"\n";
 
     //+++++++++++++++++++++++++ Velocity at Outlet +++++++++++++++++++++++++++
-    auto sumAreaOUT = integrate(markedfaces(mesh,7),cst(1.)).evaluate()(0,0) + integrate(markedfaces(mesh,24),cst(1.)).evaluate()(0,0);
+    //auto sumAreaOUT = integrate(markedfaces(mesh,7),cst(1.)).evaluate()(0,0) + integrate(markedfaces(mesh,24),cst(1.)).evaluate()(0,0);
+    auto sumAreaOUT = integrate(markedfaces(mesh,51),cst(1.)).evaluate()(0,0);
     std::cout << "The total sum of the outlet area = "<<sumAreaOUT << "\n";
     auto u_outlet = 6000./sumAreaOUT;
     std::cout << "u_outlet = "<< u_outlet << "\n";
-    auto flowOut = integrate(markedfaces(mesh,7),inner(u_outlet*N(),N() ) ).evaluate()(0,0) + integrate(markedfaces(mesh,24),inner(u_outlet*N(),N() ) ).evaluate()(0,0);
+    //auto flowOut = integrate(markedfaces(mesh,7),inner(u_outlet*N(),N() ) ).evaluate()(0,0) + integrate(markedfaces(mesh,24),inner(u_outlet*N(),N() ) ).evaluate()(0,0);
+    auto flowOut = integrate(markedfaces(mesh,51),inner(u_outlet*N(),N() ) ).evaluate()(0,0) ;
     std::cout<<"flow_outlet = "<<flowOut<<"\n";
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -233,34 +108,35 @@ int main(int argc, char**argv )
 
 
     //++++++++ Boundary Conditions ++++++++++++++++
-    //++++ On the wall ++++
-    a+=on(_range=markedfaces(mesh,50),  _rhs=l, _element=u, _expr=vec(cst(0.), cst(0.), cst(0.)) );
-    std::cout << "Start solving the system \n";
-
     //++++ At the inlets ++++
-    for (i=1;i<=31;i++)
+    /*for (i=1;i<=31;i++)
         {
             if( (i!=7) && (i!=24))
                 a+=on(_range=markedfaces(mesh,i), _rhs=l, _element=u, _expr=vec(u_inlet*Nx(),u_inlet*Ny(),u_inlet*Nz()));
-        }
-    //++++ At the outlets ++++
+                }*/
+    a+=on(_range=markedfaces(mesh,52), _rhs=l, _element=u, _expr=vec(u_inlet*Nx(),u_inlet*Ny(),u_inlet*Nz()));
+    //++++ On the wall ++++
+    a+=on(_range=markedfaces(mesh,53),  _rhs=l, _element=u, _expr=vec(cst(0.), cst(0.), cst(0.)) );
+    std::cout << "Start solving the system \n";
+
+    /*//++++ At the outlets ++++
     a+=on(_range=markedfaces(mesh,7),  _rhs=l, _element=u, _expr=vec(u_outlet*Nx(),u_outlet*Ny(),u_outlet*Nz()) );
-    a+=on(_range=markedfaces(mesh,24), _rhs=l, _element=u, _expr=vec(u_outlet*Nx(),u_outlet*Ny(),u_outlet*Nz()) );
-    
+    a+=on(_range=markedfaces(mesh,24), _rhs=l, _element=u, _expr=vec(u_outlet*Nx(),u_outlet*Ny(),u_outlet*Nz()) );*/
 
     //+++++++++ Solve +++++++++
     a.solve(_rhs=l,_solution=U);
 
     //Flow at inlet
-    auto flowInCal = integrate(markedfaces(mesh,1),inner(idv(u),N() ) ).evaluate()(0,0);
+    /*auto flowInCal = integrate(markedfaces(mesh,1),inner(idv(u),N() ) ).evaluate()(0,0);
     for (i=2;i<=31;i++)
         {
             if( (i!=7) && (i!=24))
                 flowInCal += integrate(markedfaces(mesh,i),inner(idv(u),N() ) ).evaluate()(0,0);
-        }
- 
+                }*/
+    auto flowInCal = integrate(markedfaces(mesh,52),inner(idv(u),N() ) ).evaluate()(0,0);
     //Flow at the outlet
-    auto flowOutCal = integrate(markedfaces(mesh,7),inner(idv(u),N() ) ).evaluate()(0,0) + integrate(markedfaces(mesh,24),inner(idv(u),N() ) ).evaluate()(0,0);
+    //auto flowOutCal = integrate(markedfaces(mesh,7),inner(idv(u),N() ) ).evaluate()(0,0) + integrate(markedfaces(mesh,24),inner(idv(u),N() ) ).evaluate()(0,0);
+    auto flowOutCal = integrate(markedfaces(mesh,51),inner(idv(u),N() ) ).evaluate()(0,0) ;
     std::cout.precision(20);
     std::cout<<"flow_inlet calcule = "<<flowInCal<<"\n";
     std::cout<<"flow_outlet calcule = "<<flowOutCal<<"\n";
