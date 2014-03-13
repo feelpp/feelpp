9bc7156cab59c7e9c461109a688a8dbe706c5fb8
Alexandre Ancel <alexandre.ancel@cemosis.fr>
2014-03-13 16:58:27 +0000

    Onefeel: - Fixed path bug - made code more robust

--- scripts/onelab/syncData.py
+++ scripts/onelab/syncData.py
@@ -4,29 +4,39 @@
 # it will be executed, before the actual merges of data
 # Authors: Alexandre Ancel
 
+import os
 import sys
 import subprocess
 
 def main():
     # TODO argument check
+    if(len(sys.argv) != 2):
+        print "Invalid number of arguments"
+        return 1
 
-    # read first line of file with files to sync
-    f = open(sys.argv[1], 'r')
-    line = f.readline()
+    # check that the file exists
+    if(os.path.exists(sys.argv[1])):
+        # read first line of file with files to sync
+        f = open(sys.argv[1], 'r')
+        line = f.readline()
 
-    print line
+        print line
 
-    # delete comment character and remove blanks before and after
-    line = line.translate(None, '#').lstrip().rstrip()
+        # delete comment character and remove blanks before and after
+        line = line.translate(None, '#').lstrip().rstrip()
 
-    # get filenames
-    files = line.split()
+        # get filenames
+        files = line.split()
 
-    print "External call: Syncing data ..."
-    for f in files:
-        cmd = ["scp", f, "."]
-        retval = subprocess.call(cmd)
-        if(retval != 0):
-            exit(1)
+        print "External call: Syncing data ..."
+        for f in files:
+            cmd = ["scp", f, "."]
+            retval = subprocess.call(cmd)
+            if(retval != 0):
+                exit(1)
+    else:
+        print "The file named " + sys.argv[1] + " does not exist"
+
+    return 0
 
 main()
