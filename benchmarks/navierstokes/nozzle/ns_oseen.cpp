161f83dc83718f696170de05048782dcae9d612a
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-03-27 15:39:46 +0000

    update code  - use isMasterRank  - update cfg

--- benchmarks/navierstokes/nozzle/ns_oseen.cpp
+++ benchmarks/navierstokes/nozzle/ns_oseen.cpp
@@ -35,7 +35,7 @@ int main(int argc, char**argv )
                                   _author="Feel++ Consortium",
                                   _email="feelpp-devel@feelpp.org"));
 
-    
+
     auto mesh = loadMesh(_mesh=new Mesh<Simplex<3>>);
 
     auto Vh = THch<1>( mesh );
@@ -52,16 +52,24 @@ int main(int argc, char**argv )
     double rho = 1056;
     //double nu = option(_name="nu").as<double>();
 
-    auto intUz = integrate(_range=markedfaces(mesh,"inlet"), _expr=expr( option(_name="functions.g").as<std::string>() )).evaluate()(0,0) ;
-    std::cout<<"Integrale U = "<< intUz;
+    mpi::timer ti;
+    auto g = expr( option(_name="functions.g").as<std::string>(), "g" );
+
+    auto intUz = integrate(_range=markedfaces(mesh,"inlet"), _expr=g ).evaluate()(0,0) ;
     auto aireIn = integrate(_range=markedfaces(mesh,"inlet"),_expr=cst(1.)).evaluate()(0,0);
-    std::cout<<"   Aire Inlet = "<< aireIn;
     auto meanU = intUz/aireIn;
-    std::cout<<"   Mean U = "<< meanU;
-    auto flow = integrate(_range=markedfaces(mesh,"inlet"), _expr=inner(expr( option(_name="functions.g").as<std::string>() )*N(),N())).evaluate()(0,0) ;
-    std::cout<<"   Flow = "<< flow;
+    auto flow = integrate(_range=markedfaces(mesh,"inlet"), _expr=inner(g*N(),N())).evaluate()(0,0) ;
     auto reynolds = meanU*0.012/(0.0035/1056);
-    std::cout<<"   Reynolds = "<< reynolds <<"\n";
+    if ( Environment::isMasterRank() )
+    {
+        std::cout<<"  Integrale U = "<< intUz << "\n";
+        std::cout<<"   Aire Inlet = "<< aireIn << "\n";
+        std::cout<<"       Mean U = "<< meanU << "\n";
+        std::cout<<"         Flow = "<< flow << "\n";
+        std::cout<<"     Reynolds = "<< reynolds <<"\n";
+        std::cout << " time:  " << ti.elapsed() << "s\n";
+    }
+    ti.restart();
 
     auto mybdf = bdf( _space=Vh, _name="mybdf" );
 
@@ -70,45 +78,78 @@ int main(int argc, char**argv )
     auto a = form2( _trial=Vh, _test=Vh), at = form2( _trial=Vh, _test=Vh);
     a = integrate( _range=elements( mesh ), _expr=2*mu*inner( deft,def ) + mybdf->polyDerivCoefficient(0)*trans(rho*idt(u))*id(u) );
     a +=integrate( _range=elements( mesh ), _expr=-div( v )*idt( p ) - divt( u )*id( q ) );
-
+    if ( Environment::isMasterRank() )
+    {
+        std::cout << "assembly a time:  " << ti.elapsed() << "s\n";
+    }
     auto e = exporter( _mesh=mesh );
     if (Environment::worldComm().isMasterRank())
         std::cout<<"Nb elexments in throat surface" <<nelements(markedfaces(mesh,"throat"))<<"\n";
 
     for ( mybdf->start();  mybdf->isFinished() == false; mybdf->next(U) )
     {
+        ti.restart();
+        if (Environment::worldComm().isMasterRank())
+        {
+            std::cout << "============================================================\n";
+            std::cout << "Time : " << mybdf->time() << "s\n";
+        }
         auto bdf_poly = mybdf->polyDeriv();
         auto rhsu =  bdf_poly.element<0>();
         auto extrap = mybdf->poly();
         auto extrapu = extrap.element<0>();
         ft = integrate( _range=elements(mesh), _expr=(rho*trans(idv(rhsu))*id(u) ) );
-
+        if (Environment::worldComm().isMasterRank())
+        {
+            std::cout << "assembly rhs time:  " << ti.elapsed() << "s\n";
+        }
+        ti.restart();
 
         at = a;
         at += integrate( _range=elements( mesh ), _expr= trans(rho*gradt(u)*idv(extrapu))*id(v) );
+        if (Environment::worldComm().isMasterRank())
+        {
+            std::cout << "assembly convect time:  " << ti.elapsed() << "s\n";
+        }
+        ti.restart();
         at+=on(_range=markedfaces(mesh,"wall"), _rhs=ft, _element=u,
-              _expr=0*one() );
+               _expr=0*one() );
         at+=on(_range=markedfaces(mesh,"inlet"), _rhs=ft, _element=u,
-              _expr=-expr( option(_name="functions.g").as<std::string>() )*N() );
-
+               _expr=-g*N() );
+        if (Environment::worldComm().isMasterRank())
+        {
+            std::cout << "assembly on time:  " << ti.elapsed() << "s\n";
+        }
+        ti.restart();
         at.solve(_rhs=ft,_solution=U);
-
+        if (Environment::worldComm().isMasterRank())
+        {
+            std::cout << "solve time:  " << ti.elapsed() << "s\n";
+        }
+        ti.restart();
         auto intUzt= integrate(_range=markedfaces(mesh,"throat"),_expr=idv(u)).evaluate()(0,0);
         auto aireInThroat= integrate(_range = markedfaces(mesh,"throat"),_expr=cst(1.)).evaluate()(0,0);
         auto meanUThroat=intUzt/aireInThroat;
         auto reynoldsThroat = meanUThroat*0.004/(mu*rho);
-        std::cout<<"Integrale U throat = "<< intUzt;
-        std::cout<<"   Airea of the throat's inlet = "<< aireInThroat;
-        std::cout<<"   Mean U at throat's inlet  = "<< meanUThroat;
-        std::cout<<"   Reynolds at the throat inlet = "<<reynoldsThroat;
         auto flowT = integrate(_range=markedfaces(mesh,"throat"), _expr=inner(idv(u),N())).evaluate()(0,0) ;
-        std::cout<<"   Flow at the throat's inlet= "<< flowT<<"\n";
 
+        if (Environment::worldComm().isMasterRank())
+        {
+            std::cout<<"Integrale U throat = "<< intUzt;
+            std::cout<<"   Airea of the throat's inlet = "<< aireInThroat;
+            std::cout<<"   Mean U at throat's inlet  = "<< meanUThroat;
+            std::cout<<"   Reynolds at the throat inlet = "<<reynoldsThroat;
+            std::cout<<"   Flow at the throat's inlet= "<< flowT<<"\n";
+            std::cout << "postproc time:  " << ti.elapsed() << "s\n";
+        }
         e->step(mybdf->time())->add( "u", u );
         e->step(mybdf->time())->add( "p", p );
         e->save();
-
-
+        if (Environment::worldComm().isMasterRank())
+        {
+            std::cout << "export time:  " << ti.elapsed() << "s\n";
         }
+
+    }
     return 0;
 }
