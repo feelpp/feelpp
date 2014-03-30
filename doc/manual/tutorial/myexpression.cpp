5d97a363cf3618836ed734bfc5c71e50a80b3c95
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-03-30 22:25:39 +0200

    rework the tutorial and make it a step-by-step tutorial, see #300

--- doc/manual/tutorial/myexpression.cpp
+++ doc/manual/tutorial/myexpression.cpp
@@ -21,78 +21,30 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
    */
-
+//! [all]
 #include <feel/feel.hpp>
 using namespace Feel;
 
-/// [marker1]
-namespace Feel
-{
-  struct myFunctor
-  {
-    static const size_type context = vm::JACOBIAN|vm::POINT;
-    typedef double value_type;
-    typedef Feel::uint16_type uint16_type;
-    static const uint16_type rank = 0;
-    static const uint16_type imorder = 1;
-    static const bool imIsPoly = true;
-    double operator()( uint16_type, uint16_type, ublas::vector<double> const& x, ublas::vector<double> const& /*n*/ ) const
-    {
-      double a = x[0];
-      double b = x[1];
-      return _esp*std::min(a,b); 
-    }
-    void setEps(double _a=0.){ _esp = _a;}
-    double _esp;
-  };
-}
-/// [marker1]
-
 int main(int argc, char**argv )
 {
-/// [marker2]
-  po::options_description opts ( "myOptions ");
-  opts.add_options()
-    ( "_ex_", po::value<std::string>()->default_value( "cos(x)*sin(y)" ), "my Expression to project" );
+    Environment env( _argc=argc, _argv=argv,
+                     _about=about(_name="myexpression",
+                                  _author="Feel++ Consortium",
+                                  _email="feelpp-devel@feelpp.org"));
 
-  using namespace Feel;
+    //! [mesh]
+    auto mesh = loadMesh(_mesh=new Mesh<Simplex<2>>);
+    //! [mesh]
 
-  Environment env( _argc=argc, _argv=argv,
-      _desc=opts,
-      _about=about(_name="myexpression",
-        _author="Feel++ Consortium",
-        _email="feelpp-devel@feelpp.org"));
+    //Define symbols, ie "x", "y"...
+    auto g = soption(_name="functions.g");
+    auto f = soption(_name="functions.f");
 
-  // create mesh
-  const int Dim = 2;
-  auto mesh = unitSquare();
-  //Exporter
-  auto e = exporter( _mesh=mesh );
-
-  // function space
-  auto Vh = Pch<1>( mesh );
-  auto u = Vh->element();
-  auto v = Vh->element();
-
-  //Define symbols, ie "x", "y"...
-  std::vector<GiNaC::symbol> vars = symbols<Dim>();
-  std::string _u = option(_name="_ex_").as<std::string>();
-
-  //Create the expression from the string
-  GiNaC::ex expr_u = parse(_u,vars);
-  u =  vf::project( _space=Vh, _range=elements(mesh), _expr=expr(expr_u,vars) );
-/// [marker2]
-
-  // export results
-  e->add( "u", u );
-
-/// [marker3]
-  Feel::myFunctor _f;
- _f.setEps(0.1); 
-  v = vf::project( _space=Vh, _range=elements(mesh), _expr=idf(_f));
-/// [marker3]
-
-  // export results
-  e->add( "v", v );
-  e->save();
+    //! [export]
+    auto e = exporter(_mesh=mesh);
+    e->add( "g", g );
+    e->add( "f", f );
+    e->save();
+    //! [export]
 }
+//! [all]
