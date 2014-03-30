8626b2d89fd0e3435e61f0f1d58fac5a361ebf2e
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-03-30 22:25:39 +0200

    rework the tutorial and make it a step-by-step tutorial, see #300

--- doc/manual/tutorial/myintegrals.cpp
+++ doc/manual/tutorial/myintegrals.cpp
@@ -20,20 +20,14 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-/**
-   \file myintegrals.cpp
-   \author Christophe Prud'homme <christophe.prudhomme@feelpp.org>
-   \date 2008-02-07
-   Tutorial : how to use integrals
- */
-#include <feel/feel.hpp>
+//! [all]
+#include <feel/feelfilters/loadmesh.hpp>
+#include <feel/feelvf/integrate.hpp>
+#include <feel/feelfilters/exporter.hpp>
+
+
 using namespace Feel;
 
-/**
- * Entry point
- */
-//\code
-//# marker_main #
 int
 main( int argc, char** argv )
 {
@@ -43,27 +37,28 @@ main( int argc, char** argv )
                                    _author="Feel++ Consortium",
                                    _email="feelpp-devel@feelpp.org" ) );
 
+    //! [mesh]
     // create the mesh (specify the dimension of geometric entity)
     auto mesh = loadMesh( _mesh=new Mesh<Simplex<2>> );
+    //! [mesh]
 
+    //! [expression]
     // our function to integrate
     auto f = expr( option(_name="functions.g").as<std::string>(), Symbols{"x","y"});
+    //! [expression]
 
+    //! [integrals]
     // compute integral of f (global contribution)
-    double intf_1 = integrate( _range = elements( mesh ),
-                               _expr = f ).evaluate()( 0,0 );
-
-    // compute integral of f (local contribution)
-    double intf_2 = integrate( _range = elements( mesh ),
-                               _expr = f ).evaluate(false)( 0,0 );
+    auto intf_1 = integrate( _range = elements( mesh ),
+                                 _expr = f ).evaluate();
 
     // compute integral f on boundary
     double intf_3 = integrate( _range = boundaryfaces( mesh ),
-                               _expr = f ).evaluate()( 0,0 );
+                               _expr = f ).evaluate();
 
-    if ( Environment::rank() == 0 )
+    if ( Environment::isMasterRank() == 0 )
         std::cout << "int global ; local ; boundary" << std::endl
                   << intf_1 << ";" << intf_2 << ";" << intf_3 << std::endl;
+    //! [integrals]
 }
-//# endmarker_main #
-//\endcode
+//! [all]
