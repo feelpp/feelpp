df6feb44434ecf5def814b4e26365ce7afba9eaf
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-04-15 17:35:57 +0000

    update #338 add evaluate_type

--- feel/feelvf/expr.hpp
+++ feel/feelvf/expr.hpp
@@ -108,6 +108,7 @@ public:
 
     typedef ExprT expression_type;
     typedef typename expression_type::value_type value_type;
+    typedef value_type evaluate_type;
     typedef ComponentsExpr<ExprT> this_type;
 
     //@}
