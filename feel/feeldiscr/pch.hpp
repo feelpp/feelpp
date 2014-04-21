4f2220c3dfd1f8f087edcdbc91ba240fff57b6ba
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-04-21 16:39:44 +0200

    @asamake update Moch to support other PointSets than equidistributed one

--- feel/feeldiscr/pch.hpp
+++ feel/feeldiscr/pch.hpp
@@ -39,14 +39,24 @@ namespace Feel {
  * build a function space of continuous function which are piecewise polynomial
  * of degree (total or in each variable) less than k.
  */
-template<int Order,int Tag = 0, template<class, uint16_type, class> class Pts = PointSetEquiSpaced,typename MeshType>
+template<int Order,
+         int Tag = 0,
+         template<class, uint16_type, class> class Pts = PointSetEquiSpaced,
+         typename MeshType>
 inline
-boost::shared_ptr<FunctionSpace<MeshType,bases<Lagrange<Order,Scalar,Continuous,Pts,Tag>>,double,Periodicity <NoPeriodicity>, mortars<NoMortar>>>
+boost::shared_ptr<FunctionSpace<MeshType,
+                                bases<Lagrange<Order,Scalar,Continuous,Pts,Tag>>,
+                                double,
+                                Periodicity <NoPeriodicity>,
+                                mortars<NoMortar>>>
 Pch( boost::shared_ptr<MeshType> mesh, bool buildExtendedDofTable=false )
 {
-    return FunctionSpace<MeshType,bases<Lagrange<Order,Scalar,Continuous,Pts,Tag>>, double, Periodicity <NoPeriodicity>, mortars<NoMortar>>::New( _mesh=mesh,
-                                                                                                               _worldscomm=std::vector<WorldComm>( 1,mesh->worldComm() ),
-                                                                                                               _extended_doftable=std::vector<bool>( 1,buildExtendedDofTable ) );
+    return FunctionSpace<MeshType,
+                         bases<Lagrange<Order,Scalar,Continuous,Pts,Tag>>,
+                         double, Periodicity <NoPeriodicity>,
+                         mortars<NoMortar>>::New( _mesh=mesh,
+                                                  _worldscomm=std::vector<WorldComm>( 1,mesh->worldComm() ),
+                                                  _extended_doftable=std::vector<bool>( 1,buildExtendedDofTable ) );
 }
 
 
