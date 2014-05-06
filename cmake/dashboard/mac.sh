7141d9c6fa574c133afa4f43a9b690caef46758a
Christophe Prud'homme <prudhomm@debian.org>
2014-05-07 01:57:32 +0200

    clang -> clang++

--- cmake/dashboard/mac.sh
+++ cmake/dashboard/mac.sh
@@ -6,7 +6,7 @@
 COMMON="/usr/local/bin/ctest -VV -S $1/cmake/dashboard/testsuite.cmake,FEELPP_CTEST_CONFIG=$1/cmake/dashboard/feelpp.site.`hostname -s`.cmake,FEELPP_MODE=$2"
  
 if [ -x /usr/bin/clang ]; then
-    $COMMON,FEELPP_CXXNAME=clang-3.4,FEELPP_CXX=/usr/bin/clang,FEELPP_EXPLICIT_VECTORIZATION=SSE2
+    $COMMON,FEELPP_CXXNAME=clang++-3.4,FEELPP_CXX=/usr/bin/clang++,FEELPP_EXPLICIT_VECTORIZATION=SSE2
 fi
 
 if [ -x /usr/local/bin/g++-4.8 ]; then
