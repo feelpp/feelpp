cbb9eca69c3dd6a47e784c46ad4f49609a6d282d
Vincent Huber <vincent.huber@cemosis.fr>
2014-05-14 13:42:36 +0200

    close #352

--- scripts/doxygen/publish_to_www.feelpp.org.sh
+++ scripts/doxygen/publish_to_www.feelpp.org.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-base_dir=$HOME
+#the base dir
+base_dir=${1:-$HOME}
+#Where the sources are stored
+feelpp_source=$base_dir/${2:-feelpp.git}
+#Where the gh-pages copy is
+gh_pages=$base_dir/${3:-gh-pages}
 
 # First: generate the doc extracted from feelpp/feelpp
-./publish.sh
+# ./publish.sh $base_dir $feelpp_source $gh_pages master develop release/version-0.92 release/v0.95.0 release/v0.96.0
 
-if [ ! -d www.feelpp.org ]; 
+cd $base_dir
+
+if [ ! -d "$base_dir/www.feelpp.org" ]; 
 then
-  git clone http://github.com/feelpp/www.feelpp.org
+  git clone http://github.com/feelpp/www.feelpp.org $base_dir/www.feelpp.org
 else
-  cd www.feelpp.org
+  cd $base_dir/www.feelpp.org
   git pull
 fi
 cd www.feelpp.org
 
+git checkout gh-pages
+
 # Copy the generated doc
-rsync -avz $HOME/gh-pages docs
+rsync -avz $gh_pages/ docs
 
 # Generate the static web site
 jekyll build
 
 # the resulting static pages are in _site
-git checkout gh-pages
 rsync -avz _site ./
+
 git add -A
 git commit -m "deploy web site" -a
 git pull origin gh-pages
