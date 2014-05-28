c392a07fb0f956c7ac608ed44da226a20a6d272c
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-05-28 12:02:02 +0200

    proper boundary conditions on u \times n

--- doc/manual/magnetostatic/magnetostatic.cpp
+++ doc/manual/magnetostatic/magnetostatic.cpp
@@ -41,25 +41,44 @@ int main(int argc, char**argv )
 
     auto mesh = loadMesh( _mesh=new Mesh<Simplex<FEELPP_DIM>> );
 
+    auto Xh = Pchv<1>( mesh );
     auto Nh = Ned1h<0>( mesh );
     auto a = form2( _trial=Nh, _test=Nh );
     auto c = doption("parameters.c");
     auto f = expr<FEELPP_DIM,1>(soption("functions.f"));
     auto e = expr<FEELPP_DIM,1>(soption("functions.e"));
+    std::cout << "curl(curl(E)) + E = " << curl(curl(e)) << "\n";
     auto u = Nh->element();
     auto v = Nh->element();
-
+    auto w = Xh->element(e);
+    auto z = Xh->element();
+    double penaldir=30;
     a = integrate(_range=elements(mesh), _expr=c*trans(idt(u))*id(v)+curlxt(u)*curlx(v));
+    a += integrate(boundaryfaces(mesh), -curlxt(u)*(cross(N(),id(u)) )
+                   - curlx(u)*(cross(N(),idt(u)) )
+                   + penaldir*trans(cross(idt(u),N()))*cross(id(u),N())/hFace() );
+
     auto l = form1( _test=Nh );
     l = integrate( _range=elements(mesh), _expr=trans(f)*id(v));
+    l += integrate(boundaryfaces(mesh), - curlx(u)*(cross(N(),e) )
+                   + penaldir*trans(cross(e,N()))*cross(id(u),N())/hFace() );
+
     a.solve(_rhs=l,_solution=u);
     auto err = normL2( _range=elements(mesh), _expr=idv(u)-e );
     if ( Environment::isMasterRank() )
     {
         std::cout << "L2 error = " << err << "\n";
     }
+    auto b = form2( _test=Xh, _trial=Xh );
+    b = integrate(_range=elements(mesh), _expr=trans(idt(w))*id(w));
+    auto ll = form1( _test=Xh );
+    ll = integrate( _range=elements(mesh), _expr=trans(idv(u))*id(w));
+    b.solve( _solution=z, _rhs=ll, _rebuild=true );
+
     auto ex = exporter( _mesh=mesh );
     ex->add( "u", u );
+    ex->add( "e", w );
+    ex->add( "ul2", z );
     ex->save();
 
 }
