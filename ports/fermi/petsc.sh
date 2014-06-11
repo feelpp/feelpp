b2bf0e53184972aea2bdb8a110ef411be1edcbd9
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-06-11 16:37:49 +0200

    up #351  - update boost,gmsh,petsc scripts for fermi

--- ports/fermi/petsc.sh
+++ ports/fermi/petsc.sh
@@ -2,30 +2,35 @@
 
 set -x
 . ~/.bash_profile
-#module load bgq-gnu/4.4.6
+module load bgq-gnu/4.4.6
 #module load compilers/bgclang.feelpp
-module load fermi/bgqgcc472.feelpp
+#module load fermi/bgqgcc472.feelpp
 
-export CC=$WORK/local/bin/mpicc
-export CXX=$WORK/local/bin/mpic++
+#export CC=$WORK/local/bin/mpicc
+#export FC=$WORK/local/bin/mpif90
+#export CXX=$WORK/local/bin/mpic++
+export CC=/bgsys/drivers/V1R2M1/ppc64/comm/bin/gcc/mpicc
+export FC=/bgsys/drivers/V1R2M1/ppc64/comm/bin/gcc/mpif90
 prefix=$WORK/local/petsc/3.4.4-bgq
 export MPI_ROOT=/bgsys/drivers/ppcfloor/comm/
 export PETSC_DIR=`pwd`
 ./configure  --prefix=$prefix\
-    --with-shared-libraries=0 \
+    --with-shared-libraries=1 \
     --with-debugging=0 \
     --download-f-blas-lapack=1 \
+    --with-cxx=0 \
     --with-mpi=1 \
     --with-mpi-include=$MPI_ROOT/include \
     --with-mpi-compilers=1 \
-    --with-mpi-lib=[$MPI_ROOT/lib/libmpichcxx-gcc.a,$MPI_ROOT/lib/libmpich-gcc.a] \
+    --with-mpi-lib=[/bgsys/drivers/V1R2M1/ppc64/comm/lib/libmpich-gcc.so,/bgsys/drivers/V1R2M1/ppc64/comm/lib/libopa-gcc.so,/bgsys/drivers/V1R2M1/ppc64/comm/lib/libmpl-gcc.so,/bgsys/drivers/V1R2M1/ppc64/comm/lib/libpami-gcc.so,/bgsys/drivers/V1R2M1/ppc64/spi/lib/libSPI.a,/bgsys/drivers/V1R2M1/ppc64/spi/lib/libSPI_cnk.a] \
     --download-mumps=0 \
     --download-pastix=0 \
     --download-ptscotch=1 \
     --download-scalapack=1 \
-    --download-ml=1 \
-    --download-superlu=1 \
-    --download-hypre=1
+    --download-ml=0 \
+    --download-superlu=1 --CC=$CC --FC=$FC
+
+#    --download-hypre=1
 
 make PETSC_DIR=/gpfs/work/LI03s_DDFD/sources/petsc/petsc-3.4.4 PETSC_ARCH=arch-linux2-c-opt all
 make PETSC_DIR=/gpfs/work/LI03s_DDFD/sources/petsc/petsc-3.4.4 PETSC_ARCH=arch-linux2-c-opt install
