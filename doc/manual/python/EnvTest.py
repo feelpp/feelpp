9c3e584854a51a37f2129d2d9dfbd61fcf1fee99
Thomas Lantz <tlantz35@yahoo.com>
2014-07-25 10:54:15 +0200

    update Python script for the new library

--- doc/manual/python/EnvTest.py
+++ doc/manual/python/EnvTest.py
@@ -1,10 +1,9 @@
-# to place where the module are created
-
 #!/usr/bin/python
 
 from mpi4py import MPI
-import libEnvironment
-import libMesh
+#import libEnvironment
+#import libMesh
+import libPyFeelpp
 import sys
 
 #from libEnvironment import *
@@ -22,12 +21,40 @@ import sys
 
 #libEnvironment.Environment(len(sys.argv),sys.argv)
 
-x=libEnvironment.Environment(sys.argv)
-k=libMesh.Simplex()
-print k.dim()
+#x=libEnvironment.Environment(sys.argv)
+#k=libMesh.Simplex()
+#print k.dim()
 
-m=libMesh.Mesh();
+#m=libMesh.Mesh()
+#m=libMesh.Mesh.new()
 
-#k=libMesh.ConstruSimplex()
+#s=libMesh.loadMesh(m);
+#B=libMesh.loadMesh(libMesh.Mesh())
+
+#m.clear()
+
+z=libPyFeelpp.Environment(sys.argv)
+#g=libPyFeelpp.Environment.worldComm()
+s=libPyFeelpp.Simplex()
+m=libPyFeelpp.Mesh.new()
+l=libPyFeelpp.loadMesh(m)
+#g=libPyFeelpp.exporter(s)
+#g=libPyFeelpp.new()
+w=libPyFeelpp.Environment.worldComm()
+#e=libPyFeelpp.new();
+#e=libPyFeelpp.Exporter(w)
+#e.setMesh(s)
+
+#libPyFeelpp.setMesh1(e,s)
+
+#e.addRegions()
+#e.save()
+
+
+x=libPyFeelpp.export(l);
+
+
+#d=libPyFeelpp.export2(m,w)
+
+
 print "Thomas"
-#libEnvironment.Environment()
