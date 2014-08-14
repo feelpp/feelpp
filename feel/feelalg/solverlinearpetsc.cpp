ac6dcd71ccacc8a579f397606c8c055b637ebd74
Vincent Chabannes <vincent.chabannes@gmail.com>
2014-08-14 10:58:34 +0200

    fix reuse prec with last petsc version (up #415)

--- feel/feelalg/solverlinearpetsc.cpp
+++ feel/feelalg/solverlinearpetsc.cpp
@@ -481,6 +481,8 @@ SolverLinearPetsc<T>::solve ( MatrixSparse<T> const&  matrix_in,
     ierr = KSPSetOperators( M_ksp, matrix->mat(), precond->mat(),
                             PetscGetMatStructureEnum(this->precMatrixStructure()) );
 #else
+    ierr = KSPSetReusePreconditioner( M_ksp, (this->precMatrixStructure() == Feel::SAME_PRECONDITIONER)? PETSC_TRUE : PETSC_FALSE );
+    CHKERRABORT( this->worldComm().globalComm(),ierr );
     ierr = KSPSetOperators( M_ksp, matrix->mat(), precond->mat() );
 #endif
     CHKERRABORT( this->worldComm().globalComm(),ierr );
