9f96e29659aabbe5c4acfb14119ce1fc3d13e67a
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-08-19 11:40:35 +0200

    up #407: call autoreconf several times

--- cmake/modules/FindFeel++.cmake
+++ cmake/modules/FindFeel++.cmake
@@ -395,11 +395,8 @@ else()
 
     execute_process(
       COMMAND  touch  swig/nlopt.scm.in
-      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/contrib/nlopt
-      OUTPUT_FILE "nlopt-touch"
-      ERROR_FILE "nlopt-touch-errors")
-
-    execute_process(
+      COMMAND autoreconf --verbose --install --force
+      COMMAND autoreconf --verbose --install --force
       COMMAND autoreconf --verbose --install --force
       WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/contrib/nlopt
       #OUTPUT_FILE "nlopt-autoreconf"
