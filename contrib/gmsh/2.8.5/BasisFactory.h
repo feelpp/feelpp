1b0d127f3aaf3f37ab93fa956909b4ed539e6870
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-08-20 00:02:01 +0200

    moved to 2.8.5 and 2.8.4 version added

--- contrib/gmsh/2.8.5/BasisFactory.h
+++ contrib/gmsh/2.8.5/BasisFactory.h
@@ -10,14 +10,18 @@
 #include "MPyramid.h"
 #include "nodalBasis.h"
 #include "JacobianBasis.h"
+#include "MetricBasis.h"
 
 class BasisFactory
 {
   typedef std::map<std::pair<int, int>, bezierBasis*> Cont_bezierBasis;
+  typedef std::map<std::pair<int, int>, GradientBasis*> Cont_gradBasis;
 
  private:
   static std::map<int, nodalBasis*> fs;
   static std::map<int, JacobianBasis*> js;
+  static std::map<int, MetricBasis*> ms;
+  static Cont_gradBasis gs;
   static Cont_bezierBasis bs;
   // store bezier bases by parentType and order (no serendipity..)
 
@@ -25,6 +29,9 @@ class BasisFactory
   // Caution: the returned pointer can be NULL
   static const nodalBasis* getNodalBasis(int tag);
   static const JacobianBasis* getJacobianBasis(int tag);
+  static const MetricBasis* getMetricBasis(int tag);
+
+  static const GradientBasis* getGradientBasis(int tag, int order);
   static const bezierBasis* getBezierBasis(int parentTag, int order);
   static inline const bezierBasis* getBezierBasis(int tag) {
       return getBezierBasis(ElementType::ParentTypeFromTag(tag),
