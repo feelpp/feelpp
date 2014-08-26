8bda6e324af98be13e742ee10473471a70d4edc5
Thomas Lantz <tlantz35@yahoo.com>
2014-08-26 16:04:21 +0200

    - bug fix with Python script

--- doc/manual/python/mylaplacian.py
+++ doc/manual/python/mylaplacian.py
@@ -1,10 +1,8 @@
-## to move into the folders where Python libraries are created
-
 #!/usr/bin/python
 
 from mpi4py import MPI
 import libPyLapla
-from libFeelpp import *
+from libPyFeelpp import *
 import sys
 
 
