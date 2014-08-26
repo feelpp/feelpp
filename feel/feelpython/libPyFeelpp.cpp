e9e50b40c30ed60ce7abc66c36604d68df49ecf9
Thomas Lantz <tlantz35@yahoo.com>
2014-08-26 16:27:40 +0200

    - added credits

--- feel/feelpython/libPyFeelpp.cpp
+++ feel/feelpython/libPyFeelpp.cpp
@@ -1,3 +1,36 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim: set syntax=cpp fenc=utf-8 ft=tcl et sw=4 ts=4 sts=4
+
+  This file is part of the Feel library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
+             Thomas Lantz <lantz.thomas0@gmail.com>
+
+  Date: 2014-08-26
+
+  Copyright (C)  2014 Université de Strasborg
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+   \file libPyFeelpp.cpp
+   \author Thomas Lantz <lantz.thomas0@gmail.com>
+   \date 2014-08-26
+
+*/
+
+
+
 #include <feel/feel.hpp>
 #include <boost/python.hpp>
 #include <boost/python/stl_iterator.hpp>
