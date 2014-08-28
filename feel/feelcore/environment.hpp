65643d24a64f1ea62d9e78e42514871e78041c18
Alexandre Ancel <alexandre.ancel@cemosis.fr>
2014-08-28 16:33:00 +0200

    Fixed bad names in python wrapping implementation: WRAPPER instead of WRAPPING

--- feel/feelcore/environment.hpp
+++ feel/feelcore/environment.hpp
@@ -35,7 +35,7 @@
 #include <boost/signals2.hpp>
 #include <boost/format.hpp>
 
-#if defined(FEELPP_HAS_BOOST_PYTHON) && defined(FEELPP_ENABLE_PYTHON_WRAPPER)
+#if defined(FEELPP_HAS_BOOST_PYTHON) && defined(FEELPP_ENABLE_PYTHON_WRAPPING)
 #include <boost/python.hpp>
 #include <boost/python/stl_iterator.hpp>
 
@@ -155,7 +155,7 @@ public:
      */
     Environment( int& argc, char** &argv );
 
-#if defined(FEELPP_HAS_BOOST_PYTHON) && defined(FEELPP_ENABLE_PYTHON_WRAPPER)
+#if defined(FEELPP_HAS_BOOST_PYTHON) && defined(FEELPP_ENABLE_PYTHON_WRAPPING)
     Environment( boost::python::list arg );
 #endif
 
