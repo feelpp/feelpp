9910f3fdb3afc5df581ae229f8f4a3ce7c31fddd
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-09-18 13:21:52 +0000

    fix test

--- testsuite/feeldiscr/test_mesh_transfinite.cpp
+++ testsuite/feeldiscr/test_mesh_transfinite.cpp
@@ -1,31 +1,31 @@
 /* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-
 
-  This file is part of the Feel library
+   This file is part of the Feel library
 
-  Author(s): Christophe Prud'homme <prudhomme@unistra.fr>
-       Date: 2013-07-05
+   Author(s): Christophe Prud'homme <prudhomme@unistra.fr>
+   Date: 2013-07-05
 
-  Copyright (C) 2013 Université de Strasbourg
+   Copyright (C) 2013 Université de Strasbourg
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 /**
    \file test_integration_ginac.cpp
    \author Christophe Prud'homme <prudhomme@unistra.fr>
    \date 2013-07-05
- */
+*/
 #include <sstream>
 #include <boost/timer.hpp>
 #include <feel/feel.hpp>
@@ -33,8 +33,11 @@
 int main(int argc, char**argv )
 {
     using namespace Feel;
-    Environment env( Feel::_argc=argc,
-                     Feel::_argv=argv );
+    Environment env( _argc=argc,
+                     _argv=argv,
+                     _about=about( _name="test_mesh_transfinite" ,
+                                   _author="Feel++ Consortium",
+                                   _email="feelpp-devel@feelpp.org" )  );
 
     auto mesh = loadMesh( _mesh=new Mesh<Simplex<3>> );
     //auto Xh = Pch<3>( mesh );
