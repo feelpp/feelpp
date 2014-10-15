502a7a8458ceb296317e1ff6acb6544dba5dc2dd
Pierre Jolivet <jolivet@ann.jussieu.fr>
2014-10-15 08:29:46 +0200

    fix compilation with old versions of boost::mpi

--- feel/feeldiscr/mesh.hpp
+++ feel/feeldiscr/mesh.hpp
@@ -355,8 +355,14 @@ public:
 
         if ( this->worldComm().localSize() >1 )
         {
+#if BOOST_VERSION >= 105500
             std::vector<int> globals{ ne, nf, ned, np, (int)this->numVertices() };
             mpi::all_reduce( this->worldComm(), mpi::inplace(globals.data()), 5, std::plus<int>() );
+#else
+            std::vector<int> locals{ ne, nf, ned, np, (int)this->numVertices() };
+            std::vector<int> globals( 5, 0 );
+            mpi::all_reduce( this->worldComm(), locals.data(), 5, globals.data(), std::plus<int>() );
+#endif
             M_numGlobalElements = globals[0];
             M_numGlobalFaces = globals[1];
             M_numGlobalEdges = globals[2];
