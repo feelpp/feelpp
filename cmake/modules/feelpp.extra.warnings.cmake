aa0d9f4c2001fe36da61232bad0af54ae6c2ef41
Alexandre Ancel <alexandre.ancel@cemosis.fr>
2014-10-23 17:25:59 +0200

    Issue #466: - Update cmake files for new compiler id

--- cmake/modules/feelpp.extra.warnings.cmake
+++ cmake/modules/feelpp.extra.warnings.cmake
@@ -96,7 +96,7 @@ if(FEELPP_EXTRA_WARNINGS)
   endif()
 endif()
 
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
    set(FEELPP_FLAGS "${FEELPP_FLAGS} -fmacro-backtrace-limit=0" )
    set(FEELPP_FLAGS "${FEELPP_FLAGS} -ftemplate-backtrace-limit=0" )
 endif()
