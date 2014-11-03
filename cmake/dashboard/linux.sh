bbf8d0104671a1e966a22f7837615e79bb7ef666
Vincent Huber <vincent.huber@cemosis.fr>
2014-11-03 15:18:10 +0100

    Add gcc-4.9, specify clang to clang-3.5

--- cmake/dashboard/linux.sh
+++ cmake/dashboard/linux.sh
@@ -7,10 +7,11 @@
 COMMON="ctest -VV -S $1/cmake/dashboard/testsuite.cmake,FEELPP_CTEST_CONFIG=$1/cmake/dashboard/feelpp.site.`hostname -s`.cmake,FEELPP_MODE=$2"
 
 #To make available specific compilation instead of "whatever is available"
-compiler_list=${3:-"gcc47,gcc48,clang"}
+compiler_list=${3:-"gcc47,gcc48,gcc49,clang"}
 #do_gcc46=`echo $compiler_list | grep gcc46`
 do_gcc47=`echo $compiler_list | grep gcc47`
 do_gcc48=`echo $compiler_list | grep gcc48`
+do_gcc49=`echo $compiler_list | grep gcc49`
 do_clang=`echo $compiler_list | grep clang`
 
 export FEELPP_SCRATCHDIR=/tmp/feel-logs/
@@ -33,11 +34,17 @@ if [ ! -z "$do_gcc48" -a -x /usr/bin/g++-4.8 ]; then
     $COMMON,FEELPP_CXXNAME=gcc-4.8,FEELPP_CXX=/usr/bin/g++-4.8 
     rm -rf $FEELPP_WORKDIR 
 fi
-if [ ! -z "$do_clang" -a -x /usr/bin/clang++ ]; then
+if [ ! -z "$do_gcc49" -a -x /usr/bin/g++-4.9 ]; then
+    export FEELPP_WORKDIR=/tmp/feel-gcc49/
+    rm -rf $FEELPP_WORKDIR 
+    $COMMON,FEELPP_CXXNAME=gcc-4.9,FEELPP_CXX=/usr/bin/g++-4.9 
+    rm -rf $FEELPP_WORKDIR 
+fi
+if [ ! -z "$do_clang" -a -x /usr/bin/clang++-3.5 ]; then
     export FEELPP_WORKDIR=/tmp/feel-clang
     rm -rf $FEELPP_WORKDIR 
     clang_version=`echo | clang -dM -E - | grep clang_version | awk '{print $3}' | sed "s/\"//g"`
-    $COMMON,FEELPP_CXXNAME=clang-$clang_version,FEELPP_CXX=/usr/bin/clang++ 
+    $COMMON,FEELPP_CXXNAME=clang-$clang_version,FEELPP_CXX=/usr/bin/clang++-3.5
     rm -rf $FEELPP_WORKDIR 
 fi
 rm -rf $FEELPP_SCRATCHDIR
