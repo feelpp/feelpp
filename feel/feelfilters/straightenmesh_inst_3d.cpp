1245426d346725af88424fe7103f3ec0d30ab57e
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-12-18 15:14:14 +0100

    distribute instantiation over several cpp files depending on dimension if an instantiation is not available include directly ```c++ ```

---
+++ feel/feelfilters/straightenmesh_inst_3d.cpp
@@ -0,0 +1,39 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-
+ 
+ This file is part of the Feel++ library
+ 
+ Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
+ Date: 18 Dec 2014
+ 
+ Copyright (C) 2014 Feel++ Consortium
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#define FEELPP_INSTANTIATE_STRAIGHTENMESH 
+#include <feel/feelfilters/straightenmesh_impl.hpp>
+
+
+
+namespace Feel
+{
+template boost::shared_ptr<Mesh<Simplex<3,1>>>
+straightenMesh<Mesh<Simplex<3,1>>>( boost::shared_ptr<Mesh<Simplex<3,1>>>, 
+                                    WorldComm const& , bool, bool  );
+template boost::shared_ptr<Mesh<Simplex<3,2>>>
+straightenMesh<Mesh<Simplex<3,2>>>( boost::shared_ptr<Mesh<Simplex<3,2>>>, 
+                                    WorldComm const& , bool, bool  );
+
+
+}
