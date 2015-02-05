9af8690da92b6d91fee636a6d4d8808d217edf0c
Pierre Jolivet <jolivet@ann.jussieu.fr>
2015-02-05 14:13:31 +0100

    Fixes #516.

--- cmake/modules/FindPETSc.cmake
+++ cmake/modules/FindPETSc.cmake
@@ -122,8 +122,13 @@ find_package_multipass (PETSc petsc_config_current
 # Determine whether the PETSc layout is old-style (through 2.3.3) or
 # new-style (>= 3.0.0)
 if (EXISTS "${PETSC_DIR}/${PETSC_ARCH}/include/petscconf.h")   # > 2.3.3
-  set (petsc_conf_rules "${PETSC_DIR}/conf/rules")
-  set (petsc_conf_variables "${PETSC_DIR}/conf/variables")
+  if (EXISTS "${PETSC_DIR}/conf/rules")
+    set (petsc_conf_rules "${PETSC_DIR}/conf/rules")
+    set (petsc_conf_variables "${PETSC_DIR}/conf/variables")
+  else()
+    set (petsc_conf_rules "${PETSC_DIR}/lib/petsc-conf/rules")
+    set (petsc_conf_variables "${PETSC_DIR}/lib/petsc-conf/variables")
+  endif()
 elseif (EXISTS "${PETSC_DIR}/bmake/${PETSC_ARCH}/petscconf.h") # <= 2.3.3
   set (petsc_conf_rules "${PETSC_DIR}/bmake/common/rules")
   set (petsc_conf_variables "${PETSC_DIR}/bmake/common/variables")
