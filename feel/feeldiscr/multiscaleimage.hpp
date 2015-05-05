8f598bde2b884b3159a73230eb340a2b52d626ea
Thomas Lantz <tlantz35@yahoo.com>
2015-05-05 14:37:31 +0200

    - Add multiscale operator and testsuite - Not finish yet

--- feel/feeldiscr/multiscaleimage.hpp
+++ feel/feeldiscr/multiscaleimage.hpp
@@ -1,3 +1,33 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
+
+  This file is part of the Feel library
+
+  Author(s) : Thomas Lantz
+       Date: 2015-04-27
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3.0 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/**
+   \file multiscaleimage.hpp
+   \author Thomas Lantz 
+   \date 2015-04-27
+ */
+
+//#ifndef _MULTISCALEIMAGE_HPP_
+//#define _MULTISCALEIMAGE_HPP_
+
 namespace Feel
 {
 template <typename T = float>
@@ -5,32 +35,27 @@ using holo3_image = Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajo
 class MultiScaleImage
 {
     public :
-    int T(holo3_image<float> im, std::pair<double,double> c)
+    int operator()(holo3_image<float> im, std::pair<double,double> c)
+ 
     {
      double x = c.first;
      double y = c.second;
      
      int i = x/dx;
      int j = y/dy;
-     /*
-     auto Xhc = Pch<1>();
-     Hbf2Feelpp h2f(im.cols(),im.rows,Xhc);
-    */
-    return im(i,j);
+
+    return im(j,i);
     }
 
-    int T(holo3_image<float> im, std::pair<double,double> c, int L)
+    int operator()(holo3_image<float> im, std::pair<double,double> c, int L)
     {
      double x = c.first;
      double y = c.second;
      
      int i = x/dx;
      int j = y/dy;
-     /*
-     auto Xhc = Pch<1>();
-     Hbf2Feelpp h2f(im.cols(),im.rows,Xhc);
-    */
-    return im(L*i,L*j);
+
+    return im(L*j,L*i);
     }
      
     private :
