274e53e223b34023d8e57340782652c94d0823bc
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2015-05-16 21:38:44 +0200

    added helper function makeGeometricTransformation<Convex,T>() to generate a geometric transformation

--- feel/feelpoly/geomap.hpp
+++ feel/feelpoly/geomap.hpp
@@ -53,6 +53,7 @@
 #include <feel/feelpoly/lagrange.hpp>
 #include <feel/feelpoly/fekete.hpp>
 #include <feel/feelmesh/marker.hpp>
+#include <feel/feelmesh/traits.hpp>
 
 
 
@@ -2964,6 +2965,19 @@ struct GT_QK
             {}                                                          \
     };                                                                  \
     /**/
+
+template<typename Convex,typename T>
+typename mpl::if_<Feel::is_simplex<Convex>,
+                  mpl::identity<boost::shared_ptr<GT_Lagrange<Convex::nDim,1,Convex::nDim,Simplex,T>>>,
+                  mpl::identity<boost::shared_ptr<GT_Lagrange<Convex::nDim,1,Convex::nDim,Hypercube,T>>> >::type::type
+makeGeometricTransformation()
+{
+    using gmt = typename mpl::if_<is_simplex<Convex>,
+        mpl::identity<GT_Lagrange<Convex::nDim,1,Convex::nDim,Simplex,T>>,
+        mpl::identity<GT_Lagrange<Convex::nDim,1,Convex::nDim,Hypercube,T>> >::type::type;
+    return boost::make_shared<gmt>();
+}
+
 #if 0
 #define FEELPP_GT_FACTORY(GEOM,LDIM,LORDER,LREALDIM,ENTITY)               \
     FEELPP_GT_FACTORY_BIS(GEOM,LDIM,LORDER,LREALDIM,ENTITY)),             \
