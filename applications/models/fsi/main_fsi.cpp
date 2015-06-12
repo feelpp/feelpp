600b401b06b653f8c3cdd66b9db1c8095e153e29
Vincent Chabannes <vincent.chabannes@feelpp.org>
2015-06-12 16:45:37 +0200

    rename in include feelmodels2 into feelmodels

--- applications/models/fsi/main_fsi.cpp
+++ applications/models/fsi/main_fsi.cpp
@@ -1,6 +1,6 @@
 /* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4*/
 
-#include <feel/feelmodels2/fsi/fsi.hpp>
+#include <feel/feelmodels/fsi/fsi.hpp>
 
 int
 main( int argc, char** argv )
