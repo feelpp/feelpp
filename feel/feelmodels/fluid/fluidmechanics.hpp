9080bb7d03ec915509f2836a9cfc99f1f6a89a43
Vincent Chabannes <vincent.chabannes@feelpp.org>
2015-07-02 10:24:05 +0200

    up fluid : use PostProcess Fields in json for exporter

--- feel/feelmodels/fluid/fluidmechanics.hpp
+++ feel/feelmodels/fluid/fluidmechanics.hpp
@@ -61,6 +61,7 @@ public:
     //___________________________________________________________________________________//
     // load config files
     void loadConfigBCFile();
+    void loadConfigPostProcess();
     void loadConfigMeshFile( std::string const& geofilename );
     // update for use
     void init( bool buildModelAlgebraicFactory=true );
