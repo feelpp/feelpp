d7a5bf7edbe49e459ae5b4d639fa3b92e45d09be
Vincent Huber <vincent.huber@cemosis.fr>
2015-07-16 17:43:00 +0200

    up bench #600

--- testsuite/feelpde/test_prec.sh
+++ testsuite/feelpde/test_prec.sh
@@ -4,7 +4,7 @@
 
 function simu() {
   title=${2}D-h-${4}-mu-${3}-$5-${6}_$7-${8}_${9}-${10}-${13}
-  echo "mpirun -np $1 ./feelpp_test_precAFP${2}D --config-files precAFC${2}D_${13}.cfg backend.cfg --functions.m ${3} --gmsh.hsize ${4} --ms.pc-type=$5 --ms.ksp-type=$6 --ms.blockms.11.pc-type=$7 --ms.blockms.11.ksp-type=$8 --ms.blockms.22.pc-type=$9 --ms.blockms.22.ksp-type=${10} --title $title --generateMD true > ${title}_${11}"
+  echo "mpirun -np $1 ./feelpp_test_precAFP${2}D --config-files precAFP${2}D_${13}.cfg backend.cfg --functions.m ${3} --gmsh.hsize ${4} --ms.ksp-type=$5 --ms.pc-type=$6 --ms.blockms.11.ksp-type=$7 --ms.blockms.11.pc-type=$8 --ms.blockms.22.ksp-type=$9 --ms.blockms.22.pc-type=${10} --title $title --generateMD true > ${title}_${11}"
 }
 
 # $1 - nbProcs
@@ -20,24 +20,24 @@ function simuBatch() {
   rm $out; touch $out
   echo "#!/bin/bash" >>$out
   echo "# Lines with SBATCH starting with ## are comments and starting with # are actual commands for sbatch">>$out
-  echo "#SBATCH --job-name  ${2}D-h-${4}-mu-${3}-$5-${6}_$7-${8}_${9}-${10}">>$out
+  echo "#SBATCH --job-name  $title">>$out
   echo "source \$HOME/.zsh/my/atlas">>$out
   echo "unset LC_CTYPE">>$out
   echo "##SBATCH -p public">>$out
   echo "# number of cores">>$out
-  echo "##SBATCH -n $1">>$out
+  echo "#SBATCH -n $1">>$out
   echo "# min-max number of nodes">>$out
   echo "##SBATCH -N 1-4">>$out
   echo "# max time of exec (will be killed afterwards)">>$out
-  echo "##SBATCH -t 12:00:00">>$out
+  echo "#SBATCH -t 01:00:00">>$out
   echo "# number of tasks per node">>$out
-  echo "##SBATCH --tasks-per-node 1">>$out
+  echo "##SBATCH --tasks-per-node 4">>$out
   echo "# specify execution constraitns">>$out
   echo "##SBATCH --constraint \"intel\"">>$out
   echo "# min mem size">>$out
   echo "##SBATCH --mem=16684">>$out
   echo "# display info about cpu binding">>$out
-  echo "##SBATCH --cpu_bind=verbose">>$out
+  echo "#SBATCH --cpu_bind=verbose">>$out
   echo "# send a mail at the end of the exec">>$out
   echo "#SBATCH --mail-type=END">>$out
   echo "#SBATCH --mail-user=vincent.huber@cemosis.fr">>$out
@@ -48,14 +48,14 @@ function simuBatch() {
   echo "export FEELPP_WORKDIR=/scratch/job.\${SLURM_JOB_ID}/export">>$out
   echo "">>$out
   echo "#################### OPTIONAL: ">>$out
-  echo "module load gcc490.profile" >>$out
+  echo "module load feelpp.profile" >>$out
   echo "#################### OPTIONAL:">>$out
   echo "">>$out
   echo "# Finally launch the job">>$out
   echo "# mpirun of openmpi is natively interfaced with Slurm">>$out
   echo "# No need to precise the number of processors to use">>$out
   echo "cd ${12}">>$out
-  echo "mpirun --bind-to core -x LD_LIBRARY_PATH ./feelpp_test_precAFP${2}D --config-files precAFC${2}D_${13}.cfg backend.cfg --functions.m ${3} --gmsh.hsize ${4} --ms.pc-type=$5 --ms.ksp-type=$6 --ms.blockms.11.pc-type=$7 --ms.blockms.11.ksp-type=$8 --ms.blockms.22.pc-type=$9 --ms.blockms.22.ksp-type=${10} --title $title --generateMD true > ${title}_${11}">>$out
+  echo "mpirun --bind-to core -x LD_LIBRARY_PATH ./feelpp_test_precAFP${2}D --config-files precAFP${2}D_${13}.cfg backend.cfg --functions.m ${3} --gmsh.hsize ${4} --ms.ksp-type=$5 --ms.pc-type=$6 --ms.blockms.11.ksp-type=$7 --ms.blockms.11.pc-type=$8 --ms.blockms.22.ksp-type=$9 --ms.blockms.22.pc-type=${10} --title $title --generateMD true > ${title}_${11}">>$out
 
   echo "mkdir -p /data/`whoami`/prec_behavior/${title} ">>$out
   echo "cp -r /scratch/job.\${SLURM_JOB_ID}/* /data/`whoami`/prec_behavior/${title} ">>$out
--- testsuite/feelpde/test_prec_res.sh
+++ testsuite/feelpde/test_prec_res.sh
@@ -14,9 +14,37 @@ function res() {
   title=${2}D-h-*-mu-${3}-$5-${6}_$7-${8}_${9}-${10}-${13}_${11}
   # Saved file
   out=OUT_${2}D-mu-${3}-$5-${6}_$7-${8}_${9}-${10}-${13}_${11}
-  echo "*************"
-  grep RES $title > $out
-  echo "*************"
+  echo -e "RES\thSize\tnDof\tnNz\tmu\terr\terr_rel" > $out
+  grep -h RES $title 1>> $out 2>&-
+}
+
+function nbIter() {
+  # List of files we are looking in
+  title=${2}D-h-*-mu-${3}-$5-${6}_$7-${8}_${9}-${10}-${13}_${11}
+  # Saved file
+  out=ITER_${2}D-mu-${3}-$5-${6}_$7-${8}_${9}-${10}-${13}_${11}
+  echo -e "h\tTimer\tCount\tTotal\tMax\tMin\tMean\tStdDev" > $out
+  for i in $title; do
+    h=$(echo $i| sed "s/-/ /g" | awk '{print $3}')
+    r=$(grep -h -m 1 "Inverse" $i)
+    if [ ! -z "$r" ]; then
+      echo -e "$h\t$r" >> $out
+    fi
+  done
+}
+function plot() {
+  out=OUT_${2}D-mu-${3}-*-${13}_${11}
+  echo "set logscale"
+  echo "set title \"Res from $out\""
+  echo "set datafile missing \"-\""
+  echo "set xtics nomirror rotate by -45"
+  echo "set key noenhanced"
+  echo -n "plot ";
+  for i in `ls $out`;
+    do 
+      echo -n "\"$i\" using \"hSize\":\"err_rel\" w lp,"; 
+  done
+  echo ""
 }
 
 NPROCS=6
@@ -31,10 +59,15 @@ do
     do
       # LU
       res $NPROCS $D $mu $h gmres lu gmres lu gmres lu $OUTFILE $appDir $poly
+      nbIter $NPROCS $D $mu $h gmres lu gmres lu gmres lu $OUTFILE $appDir $poly
       # Block : LU LU
       res $NPROCS $D $mu $h gmres blockms gmres lu gmres lu $OUTFILE $appDir $poly
+      nbIter $NPROCS $D $mu $h gmres blockms gmres lu gmres lu $OUTFILE $appDir $poly
       # Block : Gamg Gamg
       res $NPROCS $D $mu $h gmres blockms gmres gamg gmres gamg $OUTFILE $appDir $poly
+      nbIter $NPROCS $D $mu $h gmres blockms gmres gamg gmres gamg $OUTFILE $appDir $poly
+      # Generate Gnuplot Command
+      # plot $NPROCS $D $mu $h gmres blockms gmres gamg gmres gamg $OUTFILE $appDir $poly
     done
   done
 done
