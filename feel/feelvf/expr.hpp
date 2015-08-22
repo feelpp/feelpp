126ae539f74cc6219d22e1f921adc064d9ebc720
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2015-08-22 12:00:09 +0200

    up #643 implementation of convolution  - implement convolve keyword, use #603  - added testsuite example  - parallel version still buggy, seems ok in sequential (need more tests though)  - fix evaluate function so that the return is associated to the integrand  tensor rank/order and  not the input parameters
    /cc @feelpp/sivibirpp @cdaversin

--- feel/feelvf/expr.hpp
+++ feel/feelvf/expr.hpp
@@ -639,7 +639,7 @@ public:
         return M_expr.evaluate( parallel,worldcomm );
     }
     template<typename T, int M, int N=1>
-    std::vector<Eigen::Matrix<T,M,N> >
+    decltype(auto)
     evaluate( std::vector<Eigen::Matrix<T,M,N>> const& v ) const
         {
             return M_expr.evaluate( v );
