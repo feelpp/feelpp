9e9108335918c2a619c82b89c9c27639b72de761
Vincent Huber <vincent.huber@cemosis.fr>
2015-11-05 17:36:00 +0100

    ModelProperties will load as default --mod-file

--- feel/feelmodels/modelproperties.cpp
+++ feel/feelmodels/modelproperties.cpp
@@ -35,7 +35,11 @@ namespace Feel {
 
 ModelProperties::ModelProperties( std::string const& filename )
 {
-    if ( !fs::exists( filename ) ) return;
+    if ( !fs::exists( filename ) ) 
+    {
+      LOG(INFO) << "Could not find " << filename << std::endl;
+      return;
+    }
 
     auto json_str_wo_comments = removeComments(readFromFile(filename));
     LOG(INFO) << "json file without comment:" << json_str_wo_comments;
