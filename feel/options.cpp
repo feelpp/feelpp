ee32dd6835826f1e0b16e2a85dea466d8f016af5
Vincent Huber <vincent.huber@cemosis.fr>
2015-11-16 15:27:32 +0100

    options for ams

--- feel/options.cpp
+++ feel/options.cpp
@@ -628,7 +628,7 @@ ams_options( std::string const& prefix )
         ( prefixvm( prefix, "useEdge" ).c_str(), Feel::po::value<bool>()->default_value(true), "true: SetConstantEdgeVector, false: SetCoordinates" )
         ( prefixvm( prefix, "threshold" ).c_str(), Feel::po::value<bool>()->default_value(false), "remove near null values in Grad" )
         ( prefixvm( prefix, "setAlphaBeta" ).c_str(), Feel::po::value<bool>()->default_value(false), "Use locally constructed A_alpha and A_beta" )
-        ( prefixvm( prefix, "singular" ).c_str(), Feel::po::value<bool>()->default_value(false), "zero relaxation parameter ?" )
+        ( prefixvm( prefix, "singular" ).c_str(), Feel::po::value<bool>()->default_value(false), "Force the relaxation parameter to be zero" )
         ;
     return _options
         .add( backend_options( prefix.c_str() ))  // for AMS
