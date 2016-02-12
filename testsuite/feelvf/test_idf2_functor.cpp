d8e3f52e113f1f1de38dfca17a75011222f66a50
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2016-02-12 08:07:02 +0100

    fixes for @Trophime problems with const, rank and some member data/functions of geomap

--- testsuite/feelvf/test_idf2_functor.cpp
+++ testsuite/feelvf/test_idf2_functor.cpp
@@ -2,11 +2,11 @@
 
    This file is part of the Feel library
 
-   Author(s): Vincent HUBER <vincent.huber@cemosis.fr>
+   Author(s): Christophe Trophime
 
-   Date 2013-02-18
+   Date 2016-02-18
 
-   Copyright (C) 2013 Université de Strasbourg
+   Copyright (C) 2016 Feel++ Consortium
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -25,7 +25,6 @@
 #include <vector>
 #include <Eigen/StdVector>
 
-//! [all]
 #include <feel/feelcore/environment.hpp>
 #include <feel/feelfilters/loadmesh.hpp>
 #include <feel/feeldiscr/pch.hpp>
@@ -34,69 +33,71 @@
 #include <feel/feeldiscr/operatorlagrangep1.hpp>
 #include <feel/feelfilters/exporter.hpp>
 #include <feel/feelvf/function.hpp>
-using namespace Feel;
 
-namespace Feel {
-    struct b_ana
-    {
-        static const size_type context = vm::JACOBIAN|vm::POINT|vm::NORMAL;
-        typedef double value_type;
-        typedef value_type evaluate_type;
-        typedef Feel::uint16_type uint16_type;
-        static const uint16_type rank = 0;
-        static const uint16_type imorder = 1;
-        static const bool imIsPoly = true;
-        using B_t = Eigen::VectorXd; //Matrix<value_type,Eigen::Dynamic,1>; 
-        //typedef Eigen::Matrix<value_type,Eigen::Dynamic,Eigen::Dynamic> B_t;
 
-        b_ana()
-            {}
-        template<typename Geo_t>
-        void init( Geo_t g )
+
+namespace Feel
+{
+struct b_ana
+{
+    static const size_type context = vm::JACOBIAN|vm::POINT|vm::NORMAL;
+    typedef double value_type;
+    typedef value_type evaluate_type;
+    typedef Feel::uint16_type uint16_type;
+    static const uint16_type rank = 1;
+    static const uint16_type imorder = 1;
+    static const bool imIsPoly = true;
+    using B_t = Eigen::Matrix<value_type,Eigen::Dynamic,1>;
+
+    b_ana()
+        {}
+    template<typename Geo_t>
+    void init( Geo_t g )
+        {
+            B.resize(g->nPoints());
+            std::for_each( B.begin(), B.end(), [&g]( auto& m ) { m = B_t::Zero( g->N() );  });
+        }
+    template<typename Geo_t>
+    void update( Geo_t g )
+        {
+            for(int q = 0; q < g->nPoints();++q )
             {
-                B.resize(g->nPoints());
-                std::for_each( B.begin(), B.end(), [&g]( auto& m ) { m = B_t::Zero( g->nRealDim );  });
-            }
-        template<typename Geo_t>
-        void update( Geo_t g )
-            {
-                for(int q = 0; q < g->nPoints();++q )
-                {
-                    auto const& x=g->xRealq( q );
-                    double r = math::sqrt(x[0]*x[0] + x[1]*x[1]);
-                    double z = x[2];
-                    double theta = math::atan2(x[1], x[0]);
+                auto const& x=g->xReal( q );
+                double r = math::sqrt(x[0]*x[0] + x[1]*x[1]);
+                double z = x[2];
+                double theta = math::atan2(x[1], x[0]);
 
-                    auto Cos_Theta = cos(theta);
-                    auto Sin_Theta = sin(theta);
+                auto Cos_Theta = std::cos(theta);
+                auto Sin_Theta = std::sin(theta);
 
-                    B[q](0) = r*Cos_Theta;
-                    B[q](1) = r*Sin_Theta;
-                    B[q](2) = z;
-                }
-
-            }
-        template<typename Geo_t>
-        void update( Geo_t g, uint16_type f )
-            {}
-
-        // from : testsuite/test_integration  value_type operator()( uint16_type c1, uint16_type c2, ublas::vector<double> const& x, ublas::vector<double> const& n ) const
-        double evalq( uint16_type c1, uint16_type c2, uint16_type q )
-            {
-                return B[q](c1);
+                B[q](0) = r*Cos_Theta;
+                B[q](1) = r*Sin_Theta;
+                B[q](2) = z;
             }
 
-        B_t const& evalq( uint16_type q )
-            {
-                return B[q]; //.row(q);
-            }
+        }
+    template<typename Geo_t>
+    void update( Geo_t g, uint16_type f )
+        {}
 
-        std::vector<B_t> B;
-    };
+    double evalq( uint16_type c1, uint16_type c2, uint16_type q ) const
+        {
+            return B[q](c1);
+        }
 
-}
+    B_t const& evalq( uint16_type q ) const
+        {
+            return B[q]; //.row(q);
+        }
+
+    std::vector<B_t> B;
+};
+
+} // Feel
+
 int main(int argc, char**argv )
 {
+    using namespace Feel;
     Environment env( _argc=argc, _argv=argv,
                      _about=about(_name="myfunctor",
                                   _author="Feel++ Consortium",
@@ -104,10 +105,9 @@ int main(int argc, char**argv )
 
     auto mesh = loadMesh(_mesh=new Mesh<Simplex<3>>);
 
-    auto Vh= Pchv<3>(mesh); 
+    auto Vh= Pchv<3>(mesh);
     auto b = Vh->element();
     b_ana bfield;
     b.on( _range=elements(mesh), _expr=idf2(bfield));
 
 }
-//! [all]
