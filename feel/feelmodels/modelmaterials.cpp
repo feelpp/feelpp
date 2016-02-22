32f0a34afeb2791422b51b5128f4bd61c3f931f8
romainhild <hild.romain@gmail.com>
2016-02-22 12:58:36 +0100

    add some properties to ModelMaterial

--- feel/feelmodels/modelmaterials.cpp
+++ feel/feelmodels/modelmaterials.cpp
@@ -1,22 +1,22 @@
 /* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-
- 
+
  This file is part of the Feel++ library
- 
+
  Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
  Date: 16 Mar 2015
- 
+
  Copyright (C) 2015 Feel++ Consortium
- 
+
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
  version 2.1 of the License, or (at your option) any later version.
- 
+
  This library is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public
  License along with this library; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -50,6 +50,9 @@ std::ostream& operator<<( std::ostream& os, ModelMaterial const& m )
        << ", E: " << m.E()
        << ", nu: " << m.nu()
        << ", sigma: " << m.sigma()
+       << ", mu_mag: " << m.mu_mag()
+       << ", Bs: " << m.Bs()
+       << ", kappa_ri: " << m.kappa_ri()
        << "]";
     return os;
 }
@@ -70,11 +73,11 @@ ModelMaterials::setup()
     for( auto const& v : M_p )
     {
         LOG(INFO) << "Material Physical/Region :" << v.first  << "\n";
-        
+
         if ( auto fname = v.second.get_optional<std::string>("filename") )
         {
             LOG(INFO) << "  - filename = " << Environment::expand( fname.get() ) << std::endl;
-            
+
             this->insert( std::make_pair( v.first, this->loadMaterial( Environment::expand( fname.get() ) ) ) );
         }
         else
@@ -104,6 +107,9 @@ ModelMaterials::getMaterial( pt::ptree const& v )
     m.setE( v.get( "E", 1.f ) );
     m.setNu( v.get( "nu", 1.f ) );
     m.setSigma( v.get( "sigma", 1.f ) );
+    m.setMu_mag( v.get( "mu_mag", "1." ) );
+    m.setBs( v.get( "Bd", "1." ) );
+    m.setKappa_ri( v.get( "kappa_ri", "1." ) );
     m.setC( v.get( "C", 1.f ) );
 
     LOG(INFO) << "adding material " << m;
@@ -133,10 +139,11 @@ ModelMaterials::saveMD(std::ostream &os)
        << "|" << it->second.E()
        << "|" << it->second.nu()
        << "|" << it->second.sigma()
+       << "|" << it->second.mu_mag()
+       << "|" << it->second.Bs()
+       << "|" << it->second.kappa_ri()
        << "|\n";
   os << "\n";
 }
 
 }
-
-
