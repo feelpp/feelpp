822a856566c5ba1f43d29a859f2812506e8b7138
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2016-02-28 15:31:59 +0100

    fix stencil for #711 in case test related to trial still it seems that trial related to test misses some entries @vincentchabannes but I currently don't see what is missing

--- feel/feeldiscr/stencil.hpp
+++ feel/feeldiscr/stencil.hpp
@@ -522,38 +522,27 @@ private:
             {
                 size_type face_id = _M_X1->mesh()->subMeshToMesh( test_eid );
                 auto const& theface = _M_X2->mesh()->face(face_id);
-                size_type domain_eid = invalid_size_type_value;
                 if ( theface.isConnectedTo1() )
                 {
                     if ( !theface.element0().isGhostCell() && !theface.element1().isGhostCell() )
                     {
-                        domain_eid = theface.element0().id();
-                        if ( domain_eid != invalid_size_type_value )
-                            idsFind.insert( domain_eid );
-                        if ( theface.isConnectedTo1() )
-                        {
-                            domain_eid = theface.element1().id();
-                            if ( domain_eid != invalid_size_type_value )
-                                idsFind.insert( domain_eid );
-                        }
+                        idsFind.insert( theface.element0().id() );
+                        idsFind.insert( theface.element1().id() );
                     }
                     else if ( !theface.element0().isGhostCell() && theface.element1().isGhostCell() )
                     {
-                        domain_eid = theface.element0().id();
-                        if ( domain_eid != invalid_size_type_value )
-                            idsFind.insert( domain_eid );
+                        idsFind.insert( theface.element0().id() );
+                        idsFind.insert( theface.element1().id() );
                     }
                     else if ( theface.element0().isGhostCell() && !theface.element1().isGhostCell() )
                     {
-                        domain_eid = theface.element1().id();
-                        if ( domain_eid != invalid_size_type_value )
-                            idsFind.insert( domain_eid );
+                        idsFind.insert( theface.element0().id() );
+                        idsFind.insert( theface.element1().id() );
                     }
                 }
                 else if ( theface.isConnectedTo0() )
                     idsFind.insert( theface.element0().id() );
-
-                //DVLOG(2) << "[test_related_to_trial<1>] test element id: "  << test_eid << " trial element id : " << domain_eid << "\n";
+                DVLOG(1) << "[test_related_to_trial<1>] test element id: "  << test_eid << " trial element ids : " << idsFind << "\n";
 
             }
             else if( trial_related_to_test )
@@ -564,7 +553,8 @@ private:
                     const size_type idFind = _M_X2->mesh()->meshToSubMesh( eltTest.face(f).id() );
                     if ( idFind != invalid_size_type_value ) idsFind.insert( idFind );
                 }
-                DVLOG(2) << "[trial_related_to_test<1>] test element id: "  << test_eid << " idsFind.size() "<< idsFind.size() << "\n";
+                DVLOG(1) << "[trial_related_to_test<1>] ids : " << idsFind;
+                DVLOG(1) << "[trial_related_to_test<1>] test element id: "  << test_eid << " idsFind.size() "<< idsFind.size() << "\n";
             }
             else if ( trial_sibling_of_test )
             {
@@ -583,6 +573,7 @@ private:
             {
                 CHECK ( false ) << "[trial_related_to_test<1>] : test and trial mesh can not be the same here\n";
             }
+            google::FlushLogFiles( google::GLOG_INFO );
             return idsFind;
         }
     std::set<size_type> trialElementId( size_type test_eid, mpl::int_<2> /**/ )
