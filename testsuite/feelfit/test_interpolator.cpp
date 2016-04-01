fd125d1af1d18b6e79a9445b753515906d0b15a5
Vincent Huber <vincent.huber@cemosis.fr>
2016-04-01 22:54:43 +0200

    up #733 - compare results against GSL implementation - to test it only

--- testsuite/feelfit/test_interpolator.cpp
+++ testsuite/feelfit/test_interpolator.cpp
@@ -1,7 +1,7 @@
 #include <feel/feelfit/interpolator.hpp>
 #define GSL 1
 
-#if GSL==1
+#if GSL == 1
 // to compare against GSL
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_spline.h>
@@ -22,11 +22,16 @@ int main(int argc, char **argv)
     gsl_y[i] = b;
     i++;
   }
-#if GSL==1
-  gsl_interp_accel *acc = gsl_interp_accel_alloc ();
+#if GSL == 1
+  // Cspline
+  gsl_interp_accel *acc_spline = gsl_interp_accel_alloc ();
   //gsl_spline *spline = gsl_spline_alloc (gsl_interp_akima, size);
-  gsl_spline *spline = gsl_spline_alloc (gsl_interp_cspline, size);
-  gsl_spline_init (spline, gsl_x, gsl_y, size);
+  gsl_spline *cspline = gsl_spline_alloc (gsl_interp_cspline, size);
+  gsl_spline_init (cspline, gsl_x, gsl_y, size);
+  // Akima
+  gsl_interp_accel *acc_akima = gsl_interp_accel_alloc ();
+  gsl_spline *akima = gsl_spline_alloc (gsl_interp_akima, size);
+  gsl_spline_init (akima, gsl_x, gsl_y, size);
 #endif
   Interpol* po = Interpol::New(P0,data);
   Interpol* p1 = Interpol::New(P1,data);
@@ -34,19 +39,31 @@ int main(int argc, char **argv)
   Interpol* ak = Interpol::New(Akima,data);
   std::ofstream ofile("data_interp.txt");
   ofile << "x\tP0\tP1\tSpline\tAkima\tGSL\n";
-  for(double d = 0.3; d < 6; d+= 0.02)
+  double gsl_ak = 0., err_ak = 0.;
+  double gsl_cs = 0., err_cs = 0.;
+  for(double d = gsl_x[0]; d < gsl_x[i-1]; d+= 0.02)
   {
-#if GLS==1
-    b = gsl_spline_eval (spline, d, acc);
+#if GSL == 1
+    gsl_cs = gsl_spline_eval (cspline, d, acc_spline);
+    gsl_ak = gsl_spline_eval (akima, d, acc_akima);
+    err_cs += std::fabs(gsl_cs-(*cb)(d));
+    err_ak += std::fabs(gsl_ak-(*ak)(d));
 #endif
     ofile << d << "\t" << (*po)(d) << "\t" << (*p1)(d) << "\t" << (*cb)(d) << "\t" << (*ak)(d) << "\t" 
-#if GLS==1
-      << b 
+#if GSL == 1
+      << gsl_cs << "\t"  
+      << gsl_ak << "\t"  
 #endif
       << std::endl;
   }
-  gsl_spline_free (spline);
-  gsl_interp_accel_free (acc);
+#if GSL == 1
+  gsl_spline_free (cspline);
+  gsl_interp_accel_free (acc_spline);
+  gsl_spline_free (akima);
+  gsl_interp_accel_free (acc_akima);
+  std::cout << "err_ak = " << err_ak << std::endl;
+  std::cout << "err_cs = " << err_cs << std::endl;
+#endif
   return 0;
 }
 
