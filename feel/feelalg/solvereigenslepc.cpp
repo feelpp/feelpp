a6ed6601124ecc4ff6f80fa5f9bd5727c9f21d89
Vincent Huber <vincent.huber@cemosis.fr>
2016-04-27 14:49:24 +0200

    up

--- feel/feelalg/solvereigenslepc.cpp
+++ feel/feelalg/solvereigenslepc.cpp
@@ -1,11 +1,11 @@
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=cpp:et:sw=4:ts=4:sts=4
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8ft=cpp:et:sw=4:ts=4:sts=4
 
   This file is part of the Feel library
 
   Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
        Date: 2007-07-04
 
-  Copyright (C) 2012 UniversitÃ© Joseph Fourier (Grenoble I)
+  Copyright (C) 2007, 2009 Université Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
