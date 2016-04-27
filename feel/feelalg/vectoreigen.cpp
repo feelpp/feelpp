0adfc72753a08980dbeae15cd0f3db1366e9f7e0
Vincent Huber <vincent.huber@cemosis.fr>
2016-04-27 17:37:34 +0200

    Update vectoreigen.cpp

--- feel/feelalg/vectoreigen.cpp
+++ feel/feelalg/vectoreigen.cpp
@@ -5,7 +5,7 @@
   Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
        Date: 2012-06-20
 
-  Copyright (C) 2012 UniversitÃ© Joseph Fourier (Grenoble I)
+  Copyright (C) 2012 Université Joseph Fourier (Grenoble I)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
