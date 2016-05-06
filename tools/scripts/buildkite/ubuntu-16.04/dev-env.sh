2581c43035ab749ce5a6b5df3ea8494d425bfb37
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2016-05-06 08:31:51 +0200

    up

--- tools/scripts/buildkite/ubuntu-16.04/dev-env.sh
+++ tools/scripts/buildkite/ubuntu-16.04/dev-env.sh
@@ -3,18 +3,24 @@
 set -e
 
 echo '--- setup module'
-if [ -f /usr/share/modules/init/sh ]; then
-    . /usr/share/modules/init/sh
-    echo '--- setup module done'
-fi
+case "$0" in
+    -sh|sh|*/sh)	modules_shell=sh ;;
+    -ksh|ksh|*/ksh)	modules_shell=ksh ;;
+    -zsh|zsh|*/zsh)	modules_shell=zsh ;;
+    -bash|bash|*/bash)	modules_shell=bash ;;
+
+esac
+module() { eval `/usr/bin/modulecmd $modules_shell $*`; }
+
 export FEELPP_HPCNAME=sd-87660
 export FEELPP_CONFIG_PATH=/opt/config
 export FEELPP_SHARE_PATH=/opt/software/install
 export FEELPP_MODULE_PATH=/opt/config/modules
+export MODULEPATH=/opt/config/modules/files/sd-87660:/etc/environment-modules/modules:/usr/share/modules/versions:
 
 echo '--- setup feelpp module'
 if [ -f  /opt/config/etc/feelpprc.sh  ]; then
-    . /opt/config/etc/feelpprc.sh
+    #. /opt/config/etc/feelpprc.sh
     module load libs/boost-1.60.feelpp   libs/petsc-3.6.3.feelpp    mpi/openmpi-1.10.2.feelpp
     module load libs/hdf5-1.8.16.feelpp  science/gmsh-2.10.1.feelpp
 fi
