3c097fa23bdbdfb2653bb6470c26492d00f4f27c
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2016-05-06 07:59:41 +0200

    up

--- tools/scripts/buildkite/ubuntu-16.04/dev-env.sh
+++ tools/scripts/buildkite/ubuntu-16.04/dev-env.sh
@@ -5,9 +5,14 @@ set -e
 if [ -f /usr/share/modules/init/sh ]; then
     . /usr/share/modules/init/sh
 fi
+export FEELPP_HPCNAME=sd-87660
+export FEELPP_CONFIG_PATH=/opt/config
+export FEELPP_SHARE_PATH=/opt/software/install
+
 if [ -f  /opt/config/etc/feelpprc.sh  ]; then
     . /opt/config/etc/feelpprc.sh
 fi
+
 module load libs/boost-1.60.feelpp   libs/petsc-3.6.3.feelpp    mpi/openmpi-1.10.2.feelpp
 module load libs/hdf5-1.8.16.feelpp  science/gmsh-2.10.1.feelpp
 
