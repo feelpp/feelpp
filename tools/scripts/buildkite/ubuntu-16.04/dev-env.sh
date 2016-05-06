5bd7eb390d317de4961667ec3e1694a9bb2f9766
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2016-05-06 08:34:28 +0200

    up

--- tools/scripts/buildkite/ubuntu-16.04/dev-env.sh
+++ tools/scripts/buildkite/ubuntu-16.04/dev-env.sh
@@ -20,7 +20,7 @@ export MODULEPATH=/opt/config/modules/files/sd-87660:/etc/environment-modules/mo
 
 echo '--- setup feelpp module'
 if [ -f  /opt/config/etc/feelpprc.sh  ]; then
-    #. /opt/config/etc/feelpprc.sh
+    . /opt/config/etc/feelpprc.sh
     module load libs/boost-1.60.feelpp   libs/petsc-3.6.3.feelpp    mpi/openmpi-1.10.2.feelpp
     module load libs/hdf5-1.8.16.feelpp  science/gmsh-2.10.1.feelpp
 fi
