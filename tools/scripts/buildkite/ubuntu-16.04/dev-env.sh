207e0399ecb7faf13df9d75f9fb6674a9879c438
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2016-05-06 08:36:46 +0200

    one more time

--- tools/scripts/buildkite/ubuntu-16.04/dev-env.sh
+++ tools/scripts/buildkite/ubuntu-16.04/dev-env.sh
@@ -3,14 +3,7 @@
 set -e
 
 echo '--- setup module'
-case "$0" in
-    -sh|sh|*/sh)	modules_shell=sh ;;
-    -ksh|ksh|*/ksh)	modules_shell=ksh ;;
-    -zsh|zsh|*/zsh)	modules_shell=zsh ;;
-    -bash|bash|*/bash)	modules_shell=bash ;;
-
-esac
-module() { eval `/usr/bin/modulecmd $modules_shell $*`; }
+. /usr/share/modules/init/sh
 
 export FEELPP_HPCNAME=sd-87660
 export FEELPP_CONFIG_PATH=/opt/config
