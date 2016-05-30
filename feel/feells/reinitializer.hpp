65f712ed9500714ba9f77b01f32aa0b0b4bf85f2
Thibaut Metivet <thibaut.metivet@gmail.com>
2016-05-30 12:02:33 +0200

    Add reinitializer interface.

---
+++ feel/feells/reinitializer.hpp
@@ -0,0 +1,50 @@
+#ifndef _REINITIALIZER_HPP
+#define _REINITIALIZER_HPP 1
+
+namespace Feel
+{
+
+enum ReinitializerType
+{
+    FM,
+    HJ
+};
+
+template<typename FunctionSpaceType>
+class Reinitializer
+{
+public:
+    //--------------------------------------------------------------------//
+    // Typedefs
+    typedef Reinitializer<FunctionSpaceType> reinitializer_type;
+    typedef boost::shared_ptrtype<reinitializer_type> reinitializer_ptrtype;
+
+    typedef FunctionSpaceType functionspace_type;
+    typedef typename functionspace_type::element_type element_type;
+    typedef boost::shared_ptr<element_type> element_ptrtype;
+    
+    //--------------------------------------------------------------------//
+    // Constructor/Destructor
+    Reinitializer( std::string const& prefix );
+    virtual ~Reinitializer() {}
+
+    static reinitializer_ptrtype build( std::string const& type, std::string const& prefix="" );
+    //--------------------------------------------------------------------//
+    std::string const& prefix() const { return M_prefix; } 
+    ReinitializerType type() const { return M_reinitializerType; }
+    //--------------------------------------------------------------------//
+    // Run reintialization
+    virtual element_type run( element_type const& phi ) =0;
+
+    element_type operator() ( element_type const& phi ) { return this->run(phi); }
+
+protected:
+    ReinitializerType M_reinitializerType;
+
+private:
+    std::string M_prefix;
+};
+
+} // Feel
+
+#endif
