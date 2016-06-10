02ce96b84b72f1ff288b6d9b1d6769d1fe4b7cfb
Thibaut Metivet <thibaut.metivet@gmail.com>
2016-06-10 10:53:19 +0200

    Add smoother-related options.

--- feel/feelmodels/modelcore/options.cpp
+++ feel/feelmodels/modelcore/options.cpp
@@ -346,6 +346,7 @@ levelset_options(std::string const& prefix)
     Feel::po::options_description levelsetOptions("Levelset options");
     levelsetOptions.add_options()
         (prefixvm(prefix,"fm-use-markerdirac").c_str(), Feel::po::value<bool>()->default_value( false ), "use markerDirac to mark initially done elements in fast-marching")
+        (prefixvm(prefix,"fm-smooth-coeff").c_str(), Feel::po::value<double>()->default_value(-1.), "smoothing coefficient for interface local projection, if <0 nodal projection is done instead")
         (prefixvm(prefix,"use-regularized-phi").c_str(), Feel::po::value<bool>()->default_value( false ), "use grad(phi)/|grad(phi)| to evaluate Dirac and Heaviside functions")
         (prefixvm(prefix,"h-d-nodal-proj").c_str(), Feel::po::value<bool>()->default_value( true ), "use nodal projection to compute dirac and heaviside functions (if false, use L2 projection)")
         (prefixvm(prefix,"thickness-interface").c_str(), Feel::po::value<double>()->default_value( 0.1 ), "thickness of the interface (support for Dirac and Heaviside functions)")
@@ -354,7 +355,10 @@ levelset_options(std::string const& prefix)
         (prefixvm(prefix,"export-advection").c_str(), Feel::po::value<bool>()->default_value( false ), "export advection results")
         ;
 
-    levelsetOptions.add( advection_options( prefix ) );
+    levelsetOptions
+        .add( advection_options( prefix ) )
+        .add( backend_options( prefixvm(prefix, "ls-smooth") ) )
+        ;
 
     return levelsetOptions;
 }
