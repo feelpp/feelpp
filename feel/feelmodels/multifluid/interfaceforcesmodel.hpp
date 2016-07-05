f492abddd3d3adb6176b481b3a6ab2890e9f3fbc
Thibaut Metivet <thibaut.metivet@gmail.com>
2016-07-05 19:24:04 +0200

    Up InterfaceForcesModel code

--- feel/feelmodels/multifluid/interfaceforcesmodel.hpp
+++ feel/feelmodels/multifluid/interfaceforcesmodel.hpp
@@ -4,23 +4,77 @@
 namespace Feel {
 namespace FeelModels {
 
-template<class SpaceType>
+//enum class InterfaceForcesModels {
+    //NONE, HELFRICH
+//};
+
+template<class LevelSetType>
 class InterfaceForcesModel
 {
-    typedef InterfaceForcesModel<SpaceType> self_type;
+    typedef InterfaceForcesModel<LevelSetType> self_type;
+    typedef boost::shared_ptr<self_type> self_ptrtype;
 public:
-    typedef SpaceType space_type;
-    typedef boost::shared_ptr<SpaceType> space_ptrtype;
+    typedef LevelSetType levelset_type;
+    typedef boost::shared_ptr<levelset_type> levelset_ptrtype;
+
+    typedef typename levelset_type::space_levelset_vectorial_type space_type;
+    typedef boost::shared_ptr<space_type> space_ptrtype;
+
+    typedef typename space_type::mesh_type mesh_type;
+    typedef typename space_type::mesh_ptrtype mesh_ptrtype;
+
     typedef typename space_type::element_type element_type;
     typedef typename space_type::element_ptrtype element_ptrtype;
-    typedef typename space_type::mesh_type mesh_type;
-    typedef typename space_type mesh_ptrtype mesh_ptrtype;
 
     //--------------------------------------------------------------------//
     // Construction
-    InterfaceForcesModel( std::string const& prefix );
+    InterfaceForcesModel( std::string const& prefix, levelset_ptrtype const& ls );
+    InterfaceForcesModel( InterfaceForcesModel const& i ) = default;
+
+    static self_ptrtype build( 
+            std::string const& type, 
+            std::string const& prefix, 
+            levelset_ptrtype const& levelset );
+
+    //--------------------------------------------------------------------//
+    std::string const& prefix() const { return M_prefix; }
+    levelset_ptrtype const& levelset() const { return M_levelset; }
+    //--------------------------------------------------------------------//
+    void updateInterfaceForces( element_ptrtype & F, bool overwrite = false);
+
+    //--------------------------------------------------------------------//
+    //--------------------------------------------------------------------//
+    //--------------------------------------------------------------------//
+private:
+    virtual void loadParametersFromOptionsVm() =0;
+    virtual void updateInterfaceForcesImpl( element_ptrtype & F ) =0;
+
+    //--------------------------------------------------------------------//
+    std::string M_prefix;
+
+    levelset_ptrtype M_levelset;
 
 };
+
+template<typename LevelSetType>
+InterfaceForcesModel<LevelSetType>::InterfaceForcesModel( 
+        std::string const& prefix,
+        levelset_ptrtype const& ls 
+        ) :
+    M_prefix( prefix ),
+    M_levelset( ls )
+{
+    this->loadParametersFromOptionsVm();
+}
+
+template<typename LevelSetType>
+void
+InterfaceForcesModel<LevelSetType>::updateInterfaceForces( element_ptrtype & F, bool overwrite )
+{
+    if( overwrite )
+        F.zero();
+    this->updateInterfaceForcesImpl( F );
+}
 
 } // namespace FeelModels
 } // namespace Feel
