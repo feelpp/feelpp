10d20dca7b19d6430ba27b17cca51ff01ee64dcf
Thibaut Metivet <thibaut.metivet@gmail.com>
2016-07-07 17:42:31 +0200

    Add helfrich force factory registration

--- feel/feelmodels/multifluid/multifluid_inst.cpp
+++ feel/feelmodels/multifluid/multifluid_inst.cpp
@@ -2,11 +2,19 @@
 #include "multifluidconfig.h"
 
 #include <feel/feelmodels/multifluid/multifluid.cpp>
+#include <feel/feelmodels/multifluid/helfrichforcemodel.hpp>
 
 namespace Feel {
 namespace FeelModels {
 
-template class MultiFluid< FLUIDMECHANICS_CLASS_TYPE, LEVELSET_CLASS_TYPE >;
+//template class MultiFluid< FLUIDMECHANICS_CLASS_TYPE, LEVELSET_CLASS_TYPE >;
+template class MULTIFLUID_CLASS_INSTANTIATION;
+
+// Register interface forces models
+const bool helfrich_interfaceforcesmodel = 
+    MULTIFLUID_CLASS_INSTANTIATION::interfaceforces_factory_type::instance().registerProduct( 
+            "helfrich", 
+            &detail::createInterfaceForcesModel<HelfrichForceModel, typename MULTIFLUID_CLASS_INSTANTIATION::levelset_type> );
 
 }
 }
