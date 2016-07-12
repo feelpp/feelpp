25cab63c5ba4fe98ec8225acb2413518bd4af2b4
Vincent Chabannes <vincent.chabannes@feelpp.org>
2016-07-12 20:15:45 +0200

    ptr can be null

--- feel/feelmesh/geoelement.hpp
+++ feel/feelmesh/geoelement.hpp
@@ -1298,8 +1298,6 @@ public:
     edge_type const* facePtr( uint16_type i ) const
     {
         DCHECK( i < numLocalEdges ) << "invalid local edge index " << i << " should be less than " << numLocalEdges ;
-        DCHECK( M_edges[i] != nullptr ) << "invalid edge (null pointer) for edge local id " << i << " in element " << this->id();
-
         return M_edges[i];
     }
 
