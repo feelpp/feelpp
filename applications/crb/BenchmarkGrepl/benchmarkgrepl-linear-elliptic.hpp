efbadefa133b24ed44b0f2d19ca408ce2ea101b4
Vincent Chabannes <vincent.chabannes@feelpp.org>
2016-10-02 08:47:24 +0200

    put options in lib (else duplicated symbol issue when use BenchmarkGrepl model in external lib)

--- applications/crb/BenchmarkGrepl/benchmarkgrepl-linear-elliptic.hpp
+++ applications/crb/BenchmarkGrepl/benchmarkgrepl-linear-elliptic.hpp
@@ -42,36 +42,10 @@
 
 #include <feel/feelcrb/modelcrbbase.hpp>
 
+#include <applications/crb/BenchmarkGrepl/benchmarkgrepl-options.hpp>
 
 namespace Feel
 {
-
-po::options_description
-makeBenchmarkGreplLinearEllipticOptions()
-{
-    po::options_description bgoptions( "BenchmarkGreplLinearElliptic options" );
-    bgoptions.add_options()
-        ( "mshfile", Feel::po::value<std::string>()->default_value( "" ), "name of the gmsh file input")
-        ( "hsize", Feel::po::value<double>()->default_value( 1e-1 ), "hsize")
-        ( "trainset-eim-size", Feel::po::value<int>()->default_value( 40 ), "EIM trainset is built using a equidistributed grid 40 * 40 by default")
-        ( "gamma", Feel::po::value<double>()->default_value( 10 ), "penalisation parameter for the weak boundary Dirichlet formulation" )
-        ;
-    return bgoptions;
-}
-AboutData
-makeBenchmarkGreplLinearEllipticAbout( std::string const& str = "benchmarkGrepl" )
-{
-    Feel::AboutData about( str.c_str(),
-                           str.c_str(),
-                           "0.1",
-                           "Benchmark Grepl",
-                           Feel::AboutData::License_GPL,
-                           "Copyright (C) 2011-2014 Feel++ Consortium" );
-
-    about.addAuthor( "Christophe Prud'homme", "developer", "christophe.prudhomme@feelpp.org", "" );
-    about.addAuthor( "Stephane Veys", "developer", "stephane.veys@imag.fr", "" );
-    return about;
-}
 
 namespace BenchmarkGreplLinearElliptic_Definition
 {
