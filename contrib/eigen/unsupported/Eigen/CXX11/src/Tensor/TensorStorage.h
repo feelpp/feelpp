b9bc912456f1c425a2a2dbb2670123457a6bbf90
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-01-09 15:47:25 +0100

    try and fix eigen3 compilation with clang++-4.0

--- contrib/eigen/unsupported/Eigen/CXX11/src/Tensor/TensorStorage.h
+++ contrib/eigen/unsupported/Eigen/CXX11/src/Tensor/TensorStorage.h
@@ -31,12 +31,13 @@ namespace Eigen {
   *
   * \sa Tensor
   */
-template<typename T, typename Dimensions, int Options_> class TensorStorage;
+//template<typename T, typename Dimensions, int Options_> class TensorStorage;
 
 
 // Pure fixed-size storage
-template<typename T, int Options_, typename FixedDimensions>
-class TensorStorage<T, FixedDimensions, Options_>
+//template<typename T, int Options_, typename FixedDimensions>
+template<typename T, typename FixedDimensions, int Options_>
+class TensorStorage//<T, FixedDimensions, Options_>
 {
  private:
   static const std::size_t Size = FixedDimensions::total_size;
