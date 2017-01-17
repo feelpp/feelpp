b0209a2938a3fca71916da76af102b0f841ddb08
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-01-17 05:43:52 +0100

    added base #834 and #837 [ci skip]

---
+++ tools/scripts/buildkite/install-feelpp-base.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eo pipefail
+
+echo '--- clone/pull feelpp/docker'
+if [ -d docker ]; then (cd docker; git pull) else git clone --depth=1 https://github.com/feelpp/docker; fi
+
+echo '--- building feelpp-base'
+cd docker/feelpp-base && bash mkimg.sh -f ${TARGET} --jobs ${JOBS} --branch ${BUILDKITE_BRANCH} --cxx "${CXX}" --cc "${CC}"
+
