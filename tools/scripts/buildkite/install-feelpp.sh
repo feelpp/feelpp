9deaa5cd99cbf5b034222afd9509a4110fdce3ee
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-01-18 09:14:54 +0100

    grr fix tags #834 [ci skip]

--- tools/scripts/buildkite/install-feelpp.sh
+++ tools/scripts/buildkite/install-feelpp.sh
@@ -30,7 +30,7 @@ extratags_from_target() {
 echo '--- clone/pull feelpp/docker'
 if [ -d docker ]; then (cd docker; git pull) else git clone --depth=1 https://github.com/feelpp/docker; fi
 
-tag=$(echo "${BUILDKITE_BRANCH}" | sed -e 's/\//-/g')-$(`tag_from_target $TARGET`)
+tag=$(echo "${BUILDKITE_BRANCH}" | sed -e 's/\//-/g')-$(tag_from_target $TARGET)
 echo "--- Building feelpp-${component}:${tag}"
 
 
