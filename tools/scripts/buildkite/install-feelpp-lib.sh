7493fc2d075f27fd44366d48d01660789c1d782b
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-01-18 03:24:31 +0100

    forgot to define tag #834 [ci skip]

--- tools/scripts/buildkite/install-feelpp-lib.sh
+++ tools/scripts/buildkite/install-feelpp-lib.sh
@@ -28,7 +28,8 @@ extratags_from_target() {
 echo '--- clone/pull feelpp/docker'
 if [ -d docker ]; then (cd docker; git pull) else git clone --depth=1 https://github.com/feelpp/docker; fi
 
-echo '--- building feelpp-libs'
+tag=`tag_from_target $TARGET`
+echo '--- building feelpp-libs:${tag}'
 (cd docker/feelpp-libs && bash mkimg.sh -f ${TARGET} --jobs ${JOBS} --branch ${BUILDKITE_BRANCH} --cxx "${CXX}" --cc "${CC}")
 
 extratags=`extratags_from_target $TARGET`
