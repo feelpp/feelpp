e733968e19ae30df107e9f98680d11dad094f9fb
Vincent Chabannes <vincent.chabannes@feelpp.org>
2017-02-06 13:06:54 +0100

    remove processId in multi-index of Mesh::Elements

--- feel/feelmesh/elements.hpp
+++ feel/feelmesh/elements.hpp
@@ -106,7 +106,7 @@ public:
         multi_index::indexed_by<
             //multi_index::random_access<>,
             // sort by less<int> on id() + pid()
-#if 0
+#if 1
             multi_index::ordered_unique<multi_index::identity<element_type> >
 #else
             multi_index::ordered_unique<
@@ -390,32 +390,32 @@ public:
 
     element_iterator elementIterator( size_type i ) const
     {
-        return  M_elements.template get<0>().find( boost::make_tuple( this->worldCommElements().localRank(), i ) );
-        // return M_elements.find( element_type( i ) );
+        //return  M_elements.template get<0>().find( boost::make_tuple( this->worldCommElements().localRank(), i ) );
+        return M_elements.find( element_type( i ) );
     };
 
     element_iterator elementIterator( size_type i, rank_type p ) const
     {
-        return  M_elements.template get<0>().find( boost::make_tuple( p, i ) );
-        // return M_elements.find( element_type( i ) );
+        //return  M_elements.template get<0>().find( boost::make_tuple( p, i ) );
+        return M_elements.find( element_type( i ) );
     };
 
     element_iterator elementIterator( element_type const& elt ) const
     {
-        // return elementIterator( elt.id() );//, elt.processId() );
-        return elementIterator( elt.id(), elt.processId() );
+        return elementIterator( elt.id() );//, elt.processId() );
+        //return elementIterator( elt.id(), elt.processId() );
     };
 
     element_type const& element( size_type i ) const
     {
-        return *M_elements.template get<0>().find( boost::make_tuple( this->worldCommElements().localRank(), i ) );
-        // return *M_elements.find( element_type( i ) );
+        //return *M_elements.template get<0>().find( boost::make_tuple( this->worldCommElements().localRank(), i ) );
+        return *M_elements.find( element_type( i ) );
     };
 
     element_type const& element( size_type i, rank_type p ) const
     {
-        return *M_elements.template get<0>().find( boost::make_tuple( p, i ) );
-        // return *M_elements.find( element_type( i ) );
+        //return *M_elements.template get<0>().find( boost::make_tuple( p, i ) );
+        return *M_elements.find( element_type( i ) );
     };
 
     /**
@@ -423,14 +423,16 @@ public:
      */
     bool hasElement( size_type i, rank_type p = invalid_rank_type_value ) const
     {
-        const rank_type part = (p==invalid_rank_type_value)? this->worldCommElements().localRank() : p;
+        //const rank_type part = (p==invalid_rank_type_value)? this->worldCommElements().localRank() : p;
+#if 0
         return M_elements.template get<0>().find( boost::make_tuple( part, i ) ) !=
                M_elements.template get<0>().end();
-#if 0
+#endif
+#if 1
         return M_elements.find( element_type( i ) ) != M_elements.end();
         if ( M_elements.find( element_type( i ) ) != M_elements.end() )
             return false;
-        if ( this->element( i ).processId() != part )
+        if ( p!=invalid_rank_type_value && this->element( i ).processId() != p )
             return false;
         return true;
 #endif
