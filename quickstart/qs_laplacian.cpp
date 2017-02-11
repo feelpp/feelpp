14a173c0595e38c3ad51002b6966d78900c34b35
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-02-11 22:41:28 +0100

    Update [ci skip]

--- quickstart/qs_laplacian.cpp
+++ quickstart/qs_laplacian.cpp
@@ -21,7 +21,7 @@
    License along with this library; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
-//! [global]
+// tag::global[]
 #include <feel/feel.hpp>
 
 int main(int argc, char**argv )
@@ -101,4 +101,4 @@ int main(int argc, char**argv )
     return 0;
     // end::export[]
 }
-//! [global]
+// end::global[]
