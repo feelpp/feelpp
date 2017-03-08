5869715d4a8144fd893f69ecb408784ecd0abbf9
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-03-08 08:46:50 +0100

    up laplacian

--- quickstart/qs_laplacian.cpp
+++ quickstart/qs_laplacian.cpp
@@ -1,27 +1,28 @@
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*- vim:set fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
-
-   This file is part of the Feel++ library
-
-   Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
-   Date     : Tue Feb 25 11:58:30 2014
-
-   Copyright (C) 2014 Feel++ Consortium
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-// tag::global[]
+//! -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-   -*- vim:set fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
+//!
+//! This file is part of the Feel++ library
+//!
+//! This library is free software; you can redistribute it and/or
+//! modify it under the terms of the GNU Lesser General Public
+//! License as published by the Free Software Foundation; either
+//! version 2.1 of the License, or (at your option) any later version.
+//!
+//! This library is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//! Lesser General Public License for more details.
+//!
+//! You should have received a copy of the GNU Lesser General Public
+//! License along with this library; if not, write to the Free Software
+//! Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//!
+//! @file
+//! @author Christophe Prud'homme <christophe.prudhomme@feelpp.org>
+//! @date 25 Feb 2014
+//! @copyright 2014-2017 Feel++ Consortium
+//!
+//!
+// tag::all[]
 #include <feel/feel.hpp>
 
 int main(int argc, char**argv )
@@ -42,12 +43,14 @@ int main(int argc, char**argv )
                                 _email="feelpp-devel@feelpp.org"));
     // end::env[]
 
-    // tag::mesh_space[]
     tic();
+    // tag::mesh[]
     auto mesh = loadMesh(_mesh=new Mesh<Simplex<FEELPP_DIM,1>>);
+    // end::mesh[]
     toc("loadMesh");
 
     tic();
+    // tag::discr[]
     auto Vh = Pch<2>( mesh );
     auto u = Vh->element("u");
     auto mu = doption(_name="mu");
@@ -56,20 +59,26 @@ int main(int argc, char**argv )
     auto r_2 = expr( soption(_name="functions.b"), "b" ); // Robin right hand side expression
     auto n = expr( soption(_name="functions.c"), "c" ); // Neumann expression
     auto g = expr( soption(_name="functions.g"), "g" );
+    // tag::v[]
     auto v = Vh->element( g, "g" );
+    // end::v[]
+    // end::discr[]
     toc("Vh");
-    // end::mesh_space[]
 
-    // tag::forms[]
+
+
     tic();
+    // tag::vf[]
     auto l = form1( _test=Vh );
     l = integrate(_range=elements(mesh),
                   _expr=f*id(v));
     l+=integrate(_range=markedfaces(mesh,"Robin"), _expr=r_2*id(v));
     l+=integrate(_range=markedfaces(mesh,"Neumann"), _expr=n*id(v));
+    // tag::vf[]
     toc("l");
 
     tic();
+    // tag::vf[]
     auto a = form2( _trial=Vh, _test=Vh);
     a = integrate(_range=elements(mesh),
                   _expr=mu*gradt(u)*trans(grad(v)) );
@@ -79,26 +88,32 @@ int main(int argc, char**argv )
     //! impose Dirichlet boundary conditions over the entire boundary
     if ( !mesh->hasAnyMarker({"Robin", "Neumann","Dirichlet"}) )
         a+=on(_range=boundaryfaces(mesh), _rhs=l, _element=u, _expr=g );
+    // end::vf[]
     toc("a");
 
     tic();
+    // tag::solve[]
     //! solve the linear system, find u s.t. a(u,v)=l(v) for all v
     if ( !boption( "no-solve" ) )
         a.solve(_rhs=l,_solution=u);
+    // end::solve[]
     toc("a.solve");
 
-    // end::forms[]
-    cout << "||u-u_h||_L2=" << normL2(_range=elements(mesh), _expr=idv(u)-g) << std::endl;
+    // tag::ug[]
+    cout << "||u_h-g||_L2=" << normL2(_range=elements(mesh), _expr=idv(u)-g) << std::endl;
+    // end::ug[]
 
-    // tag::export[]
     tic();
+    // tag::export[]
     auto e = exporter( _mesh=mesh );
     e->addRegions();
     e->add( "u", u );
     e->add( "g", v );
     e->save();
+    // end::export[]
+
     toc("Exporter");
     return 0;
-    // end::export[]
+
 }
-// end::global[]
+// end::all[]
