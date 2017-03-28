623b13872cfc3428e67d5d55236fb39d539776c0
Vincent Chabannes <vincent.chabannes@feelpp.org>
2017-03-28 17:06:44 +0200

    up fit and interpolator :
    - include in Feel namespace
    - major clean
    - interpolator type given from a string
    - // : only master rank apply IO operations
    - Merge FitDiff into Fit (remove duplicated code)

--- feel/feelfit/interpolator.cpp
+++ feel/feelfit/interpolator.cpp
@@ -24,7 +24,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <feel/feelfit/interpolator.hpp>
 
-std::unique_ptr<Interpolator> Interpolator::New( InterpolationType type, std::vector<pair_type> data )
+#include <feel/feelalg/backendeigen.hpp>
+
+namespace Feel
+{
+
+std::unique_ptr<Interpolator> Interpolator::New( InterpolationType type, std::vector<pair_type> const& data )
 {
     switch ( type )
     {
@@ -55,37 +60,44 @@ std::unique_ptr<Interpolator> Interpolator::New( InterpolationType type, std::ve
     }
 }
 
-std::unique_ptr<Interpolator> Interpolator::New( InterpolationType type, std::string dataFile )
+std::unique_ptr<Interpolator> Interpolator::New( InterpolationType type, std::string const& dataFile, WorldComm const& worldComm )
 {
-    std::vector<std::pair<double, double>> data;
-    std::ifstream infile( Feel::Environment::expand( dataFile ) );
-    std::string line;
-    double a, b;
-    if ( infile.is_open() )
+    std::vector<pair_type> data;
+    std::string dataFileExpanded = Environment::expand( dataFile );
+    if ( worldComm.isMasterRank() )
     {
-        while ( getline( infile, line ) )
+        std::ifstream infile( dataFileExpanded );
+        std::string line;
+        double a, b;
+        if ( infile.is_open() )
         {
-            if ( line[0] != '#' )
+            while ( getline( infile, line ) )
             {
-                std::istringstream iss( line );
-                iss >> a >> b;
-                data.push_back( {a, b} );
+                if ( line[0] != '#' )
+                {
+                    std::istringstream iss( line );
+                    iss >> a >> b;
+                    data.push_back( {a, b} );
+                }
             }
+            infile.close();
         }
-        infile.close();
+        else
+        {
+            LOG(WARNING) << "Error opening data file" << dataFileExpanded;
+        }
     }
-    else
-    {
-        Feel::cout << "Error opening file" << Feel::Environment::expand( dataFile ) << "\n";
-    }
+    if ( worldComm.globalComm().size() > 1 )
+        boost::mpi::broadcast( worldComm.globalComm() , data , worldComm.masterRank() );
+
     return Interpolator::New( type, data );
 }
 
-Interpolator::value_type InterpolatorP0::diff( double _x )
+Interpolator::value_type InterpolatorP0::diff( double _x ) const
 {
     return 0.;
 }
-Interpolator::value_type InterpolatorP0::operator()( double _x )
+Interpolator::value_type InterpolatorP0::operator()( double _x ) const
 {
     pair_type lower( _x, 0. );
     double v1 = 0.;
@@ -130,30 +142,30 @@ Interpolator::value_type InterpolatorP0::operator()( double _x )
     return 0;
 }
 
-Interpolator::value_type InterpolatorP1::operator()( double _x )
+Interpolator::value_type InterpolatorP1::operator()( double _x ) const
 {
     double y1 = 0.;
     double y2 = 0.;
     double x1 = 0.;
     double x2 = 0.;
 
-    Coefficients( _x, y1, y2, x1, x2 );
+    computeCoefficients( _x, y1, y2, x1, x2 );
     return ( ( y1 - y2 ) * _x - ( x2 * y1 - x1 * y2 ) ) / ( x1 - x2 );
 }
-Interpolator::value_type InterpolatorP1::diff( double _x )
+Interpolator::value_type InterpolatorP1::diff( double _x ) const
 {
     double y1 = 0.;
     double y2 = 0.;
     double x1 = 0.;
     double x2 = 0.;
-    Coefficients( _x, y1, y2, x1, x2 );
+    computeCoefficients( _x, y1, y2, x1, x2 );
     return ( ( y1 - y2 ) ) / ( x1 - x2 );
 }
-void InterpolatorP1::Coefficients( value_type _x,
+void InterpolatorP1::computeCoefficients( value_type _x,
                                    value_type& y1,
                                    value_type& y2,
                                    value_type& x1,
-                                   value_type& x2 )
+                                   value_type& x2 ) const
 {
     pair_type lower( _x, 0. );
     // (it-1)->first < _x < it->first
@@ -227,41 +239,46 @@ void InterpolatorP1::Coefficients( value_type _x,
  * Interpolate with a*x**3 + b*x**2 + c*x + d
  * TODO: interpolate a(x-xi)**3 + ... instead of ax**3 (only to generate a easiest to inverse linear system)
  */
-InterpolatorSpline::InterpolatorSpline( std::vector<pair_type> data, ExtrapolationType_spline r = natural, ExtrapolationType_spline l = natural )
-    : super( data ), M_r_type( r ), M_l_type( l )
+InterpolatorSpline::InterpolatorSpline( std::vector<pair_type> const& data, ExtrapolationType_spline r, ExtrapolationType_spline l )
+    :
+    super( data ),
+    M_r_type( r ),
+    M_l_type( l )
 {
+    typedef Eigen::SparseMatrix<value_type> SpMat;
+    typedef Eigen::Triplet<value_type> triplet_type;
     SpMat A( ( this->M_data.size() - 1 ) * 4, ( this->M_data.size() - 1 ) * 4 );
-    std::vector<T> coef;
+    std::vector<triplet_type> coef;
     Eigen::VectorXd b = Eigen::VectorXd::Zero( ( this->M_data.size() - 1 ) * 4 );
     for ( int i = 0; i < this->M_data.size() - 1; i++ )
     {
         double x = this->M_data.at( i ).first;
         double xp = this->M_data.at( i + 1 ).first;
         //si(xi) = yi
-        coef.push_back( T( 1 + i * 4, i * 4 + 0, x * x * x ) );
-        coef.push_back( T( 1 + i * 4, i * 4 + 1, x * x ) );
-        coef.push_back( T( 1 + i * 4, i * 4 + 2, x ) );
-        coef.push_back( T( 1 + i * 4, i * 4 + 3, 1. ) );
+        coef.push_back( triplet_type( 1 + i * 4, i * 4 + 0, x * x * x ) );
+        coef.push_back( triplet_type( 1 + i * 4, i * 4 + 1, x * x ) );
+        coef.push_back( triplet_type( 1 + i * 4, i * 4 + 2, x ) );
+        coef.push_back( triplet_type( 1 + i * 4, i * 4 + 3, 1. ) );
         //si(xi+1) = yi+1
-        coef.push_back( T( 1 + i * 4 + 1, i * 4 + 0, xp * xp * xp ) );
-        coef.push_back( T( 1 + i * 4 + 1, i * 4 + 1, xp * xp ) );
-        coef.push_back( T( 1 + i * 4 + 1, i * 4 + 2, xp ) );
-        coef.push_back( T( 1 + i * 4 + 1, i * 4 + 3, 1. ) );
+        coef.push_back( triplet_type( 1 + i * 4 + 1, i * 4 + 0, xp * xp * xp ) );
+        coef.push_back( triplet_type( 1 + i * 4 + 1, i * 4 + 1, xp * xp ) );
+        coef.push_back( triplet_type( 1 + i * 4 + 1, i * 4 + 2, xp ) );
+        coef.push_back( triplet_type( 1 + i * 4 + 1, i * 4 + 3, 1. ) );
         if ( i < M_data.size() - 2 )
         {
             // si'(xi+1) - si+1'(xi+1) = 0
-            coef.push_back( T( 1 + i * 4 + 2, i * 4 + 0, 3. * xp * xp ) );
-            coef.push_back( T( 1 + i * 4 + 2, i * 4 + 1, 2. * xp ) );
-            coef.push_back( T( 1 + i * 4 + 2, i * 4 + 2, 1 ) );
-            coef.push_back( T( 1 + i * 4 + 2, i * 4 + 0 + 4, -3. * xp * xp ) );
-            coef.push_back( T( 1 + i * 4 + 2, i * 4 + 1 + 4, -2. * xp ) );
-            coef.push_back( T( 1 + i * 4 + 2, i * 4 + 2 + 4, -1 ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 2, i * 4 + 0, 3. * xp * xp ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 2, i * 4 + 1, 2. * xp ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 2, i * 4 + 2, 1 ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 2, i * 4 + 0 + 4, -3. * xp * xp ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 2, i * 4 + 1 + 4, -2. * xp ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 2, i * 4 + 2 + 4, -1 ) );
 
             // si''(xi+1) - si+1''(xi+1) = 0
-            coef.push_back( T( 1 + i * 4 + 3, i * 4 + 0, 6. * xp ) );
-            coef.push_back( T( 1 + i * 4 + 3, i * 4 + 1, 2. ) );
-            coef.push_back( T( 1 + i * 4 + 3, i * 4 + 0 + 4, -6. * xp ) );
-            coef.push_back( T( 1 + i * 4 + 3, i * 4 + 1 + 4, -2. ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 3, i * 4 + 0, 6. * xp ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 3, i * 4 + 1, 2. ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 3, i * 4 + 0 + 4, -6. * xp ) );
+            coef.push_back( triplet_type( 1 + i * 4 + 3, i * 4 + 1 + 4, -2. ) );
         }
         b( 1 + i * 4 ) = M_data.at( i ).second;
         b( 1 + i * 4 + 1 ) = M_data.at( i + 1 ).second;
@@ -271,17 +288,17 @@ InterpolatorSpline::InterpolatorSpline( std::vector<pair_type> data, Extrapolati
     {
     case natural:
     {
-        coef.push_back( T( 0, 0, 6. * x0 ) );
-        coef.push_back( T( 0, 1, 2. ) );
+        coef.push_back( triplet_type( 0, 0, 6. * x0 ) );
+        coef.push_back( triplet_type( 0, 1, 2. ) );
         b( 0 ) = 0.;
         break;
     }
     case clamped:
     {
         //s0'(x0) = 0
-        coef.push_back( T( 0, 0, 3. * x0 * x0 ) );
-        coef.push_back( T( 0, 1, 2. * x0 ) );
-        coef.push_back( T( 0, 2, 1 ) );
+        coef.push_back( triplet_type( 0, 0, 3. * x0 * x0 ) );
+        coef.push_back( triplet_type( 0, 1, 2. * x0 ) );
+        coef.push_back( triplet_type( 0, 2, 1 ) );
         b( 0 ) = 1.; //diff_l;
         break;
     }
@@ -291,72 +308,72 @@ InterpolatorSpline::InterpolatorSpline( std::vector<pair_type> data, Extrapolati
     {
     case natural:
     {
-        coef.push_back( T( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 0, 6. * xn ) );
-        coef.push_back( T( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 1, 2 ) );
+        coef.push_back( triplet_type( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 0, 6. * xn ) );
+        coef.push_back( triplet_type( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 1, 2 ) );
         b( 4 * ( M_data.size() - 1 ) - 1 ) = 0.;
         break;
     }
     case clamped:
     {
         //sn'(xn) = 0
-        coef.push_back( T( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 0, 3. * xn * xn ) );
-        coef.push_back( T( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 1, 2. * xn ) );
-        coef.push_back( T( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 2, 1 ) );
+        coef.push_back( triplet_type( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 0, 3. * xn * xn ) );
+        coef.push_back( triplet_type( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 1, 2. * xn ) );
+        coef.push_back( triplet_type( 4 * ( M_data.size() - 1 ) - 1, ( M_data.size() - 2 ) * 4 + 2, 1 ) );
         b( 4 * ( M_data.size() - 1 ) - 1 ) = 0.; //diff_r;
         break;
     }
     }
     A.setFromTriplets( coef.begin(), coef.end() );
     Eigen::SparseQR<SpMat, Eigen::COLAMDOrdering<int>> solver( A );
-    sol = solver.solve( b );
+    M_sol = solver.solve( b );
 }
-Interpolator::value_type InterpolatorSpline::operator()( double _x )
+Interpolator::value_type InterpolatorSpline::operator()( double _x ) const
 {
     double a = 0.;
     double b = 0.;
     double c = 0.;
     double d = 0.;
-    Coefficients( _x, a, b, c, d );
+    computeCoefficients( _x, a, b, c, d );
     return a * _x * _x * _x + b * _x * _x + c * _x + d;
 }
-Interpolator::value_type InterpolatorSpline::diff( double _x )
+Interpolator::value_type InterpolatorSpline::diff( double _x ) const
 {
     double a = 0.;
     double b = 0.;
     double c = 0.;
     double d = 0.;
-    Coefficients( _x, a, b, c, d );
+    computeCoefficients( _x, a, b, c, d );
     return 3. * a * _x * _x + 2. * b * _x + c;
 }
-void InterpolatorSpline::Coefficients( value_type _x,
-                                       value_type& a,
-                                       value_type& b,
-                                       value_type& c,
-                                       value_type& d )
+void InterpolatorSpline::computeCoefficients( value_type _x,
+                                              value_type& a,
+                                              value_type& b,
+                                              value_type& c,
+                                              value_type& d ) const
 {
     pair_type lower( _x, 0. );
     auto it = std::lower_bound( M_data.begin(), M_data.end(), lower, []( pair_type a, pair_type b ) { return a.first < b.first; } );
     if ( it == M_data.end() ) // _x > M_data.end()->first
     {
-        a = sol( ( M_data.size() - 2 ) * 4 + 0 );
-        b = sol( ( M_data.size() - 2 ) * 4 + 1 );
-        c = sol( ( M_data.size() - 2 ) * 4 + 2 );
-        d = sol( ( M_data.size() - 2 ) * 4 + 3 );
+        a = M_sol( ( M_data.size() - 2 ) * 4 + 0 );
+        b = M_sol( ( M_data.size() - 2 ) * 4 + 1 );
+        c = M_sol( ( M_data.size() - 2 ) * 4 + 2 );
+        d = M_sol( ( M_data.size() - 2 ) * 4 + 3 );
     }
     else if ( it == M_data.begin() ) // _x < M_data.begin()->first
     {
-        a = sol( 0 );
-        b = sol( 1 );
-        c = sol( 2 );
-        d = sol( 3 );
+        a = M_sol( 0 );
+        b = M_sol( 1 );
+        c = M_sol( 2 );
+        d = M_sol( 3 );
     }
     else
     {
         size_t index = std::distance( M_data.begin(), it ) - 1;
-        a = sol( index * 4 + 0 );
-        b = sol( index * 4 + 1 );
-        c = sol( index * 4 + 2 );
-        d = sol( index * 4 + 3 );
+        a = M_sol( index * 4 + 0 );
+        b = M_sol( index * 4 + 1 );
+        c = M_sol( index * 4 + 2 );
+        d = M_sol( index * 4 + 3 );
     }
 }
 
@@ -365,11 +382,14 @@ void InterpolatorSpline::Coefficients( value_type _x,
  * The Akima's interpolation differs with the Cspline by the first order derivative imposed at nodes
  * TODO: interpolate a(x-xi)**3 + ... instead of ax**3
  */
-InterpolatorAkima::InterpolatorAkima( std::vector<pair_type> data )
-    : super( data )
+InterpolatorAkima::InterpolatorAkima( std::vector<pair_type> const& data )
+    :
+    super( data )
 {
+    typedef Eigen::SparseMatrix<value_type> SpMat;
+    typedef Eigen::Triplet<value_type> triplet_type;
     SpMat A( ( this->M_data.size() - 1 ) * 4, ( this->M_data.size() - 1 ) * 4 );
-    std::vector<T> coef;
+    std::vector<triplet_type> coef;
     Eigen::VectorXd b = Eigen::VectorXd::Zero( ( this->M_data.size() - 1 ) * 4 );
     double x, xp;
     for ( int i = 0; i < this->M_data.size() - 1; i++ )
@@ -377,48 +397,48 @@ InterpolatorAkima::InterpolatorAkima( std::vector<pair_type> data )
         x = this->M_data.at( i ).first;
         xp = this->M_data.at( i + 1 ).first;
         //si(xi) = yi
-        coef.push_back( T( i * 4, i * 4 + 0, x * x * x ) );
-        coef.push_back( T( i * 4, i * 4 + 1, x * x ) );
-        coef.push_back( T( i * 4, i * 4 + 2, x ) );
-        coef.push_back( T( i * 4, i * 4 + 3, 1. ) );
+        coef.push_back( triplet_type( i * 4, i * 4 + 0, x * x * x ) );
+        coef.push_back( triplet_type( i * 4, i * 4 + 1, x * x ) );
+        coef.push_back( triplet_type( i * 4, i * 4 + 2, x ) );
+        coef.push_back( triplet_type( i * 4, i * 4 + 3, 1. ) );
         b( i * 4 ) = M_data.at( i ).second;
         //si(xi+1) = yi+1
-        coef.push_back( T( i * 4 + 1, i * 4 + 0, xp * xp * xp ) );
-        coef.push_back( T( i * 4 + 1, i * 4 + 1, xp * xp ) );
-        coef.push_back( T( i * 4 + 1, i * 4 + 2, xp ) );
-        coef.push_back( T( i * 4 + 1, i * 4 + 3, 1. ) );
+        coef.push_back( triplet_type( i * 4 + 1, i * 4 + 0, xp * xp * xp ) );
+        coef.push_back( triplet_type( i * 4 + 1, i * 4 + 1, xp * xp ) );
+        coef.push_back( triplet_type( i * 4 + 1, i * 4 + 2, xp ) );
+        coef.push_back( triplet_type( i * 4 + 1, i * 4 + 3, 1. ) );
         b( i * 4 + 1 ) = M_data.at( i + 1 ).second;
         // si'(xi) =
-        coef.push_back( T( i * 4 + 2, i * 4 + 0, 3 * x * x ) );
-        coef.push_back( T( i * 4 + 2, i * 4 + 1, 2 * x ) );
-        coef.push_back( T( i * 4 + 2, i * 4 + 2, 1 ) );
+        coef.push_back( triplet_type( i * 4 + 2, i * 4 + 0, 3 * x * x ) );
+        coef.push_back( triplet_type( i * 4 + 2, i * 4 + 1, 2 * x ) );
+        coef.push_back( triplet_type( i * 4 + 2, i * 4 + 2, 1 ) );
         b( i * 4 + 2 ) = sp( i );
         // si'(xi+1) =
-        coef.push_back( T( i * 4 + 3, i * 4 + 0, 3 * xp * xp ) );
-        coef.push_back( T( i * 4 + 3, i * 4 + 1, 2 * xp ) );
-        coef.push_back( T( i * 4 + 3, i * 4 + 2, 1 ) );
+        coef.push_back( triplet_type( i * 4 + 3, i * 4 + 0, 3 * xp * xp ) );
+        coef.push_back( triplet_type( i * 4 + 3, i * 4 + 1, 2 * xp ) );
+        coef.push_back( triplet_type( i * 4 + 3, i * 4 + 2, 1 ) );
         b( i * 4 + 3 ) = sp( i + 1 );
     }
     A.setFromTriplets( coef.begin(), coef.end() );
     Eigen::SparseQR<SpMat, Eigen::COLAMDOrdering<int>> solver( A );
-    sol = solver.solve( b );
+    M_sol = solver.solve( b );
 }
-Interpolator::value_type InterpolatorAkima::operator()( double _x )
+Interpolator::value_type InterpolatorAkima::operator()( double _x ) const
 {
     double a = 0.;
     double b = 0.;
     double c = 0.;
     double d = 0.;
-    Coefficients( _x, a, b, c, d );
+    computeCoefficients( _x, a, b, c, d );
     return a * _x * _x * _x + b * _x * _x + c * _x + d;
 }
-Interpolator::value_type InterpolatorAkima::diff( double _x )
+Interpolator::value_type InterpolatorAkima::diff( double _x ) const
 {
     double a = 0.;
     double b = 0.;
     double c = 0.;
     double d = 0.;
-    Coefficients( _x, a, b, c, d );
+    computeCoefficients( _x, a, b, c, d );
     return 3. * a * _x * _x + 2. * b * _x + c;
 }
 Interpolator::value_type InterpolatorAkima::sp( int i )
@@ -460,34 +480,36 @@ Interpolator::value_type InterpolatorAkima::d( int j )
     }
 }
 
-void InterpolatorAkima::Coefficients( value_type _x,
-                                      value_type& a,
-                                      value_type& b,
-                                      value_type& c,
-                                      value_type& d )
+void InterpolatorAkima::computeCoefficients( value_type _x,
+                                             value_type& a,
+                                             value_type& b,
+                                             value_type& c,
+                                             value_type& d ) const
 {
     pair_type lower( _x, 0. );
     auto it = std::lower_bound( M_data.begin(), M_data.end(), lower, []( pair_type a, pair_type b ) { return a.first < b.first; } );
     if ( it == M_data.end() ) // _x > M_data.end()->first
     {
-        a = sol( ( M_data.size() - 2 ) * 4 + 0 );
-        b = sol( ( M_data.size() - 2 ) * 4 + 1 );
-        c = sol( ( M_data.size() - 2 ) * 4 + 2 );
-        d = sol( ( M_data.size() - 2 ) * 4 + 3 );
+        a = M_sol( ( M_data.size() - 2 ) * 4 + 0 );
+        b = M_sol( ( M_data.size() - 2 ) * 4 + 1 );
+        c = M_sol( ( M_data.size() - 2 ) * 4 + 2 );
+        d = M_sol( ( M_data.size() - 2 ) * 4 + 3 );
     }
     else if ( it == M_data.begin() ) // _x < M_data.begin()->first
     {
-        a = sol( 0 );
-        b = sol( 1 );
-        c = sol( 2 );
-        d = sol( 3 );
+        a = M_sol( 0 );
+        b = M_sol( 1 );
+        c = M_sol( 2 );
+        d = M_sol( 3 );
     }
     else
     {
         size_t index = std::distance( M_data.begin(), it ) - 1;
-        a = sol( index * 4 + 0 );
-        b = sol( index * 4 + 1 );
-        c = sol( index * 4 + 2 );
-        d = sol( index * 4 + 3 );
+        a = M_sol( index * 4 + 0 );
+        b = M_sol( index * 4 + 1 );
+        c = M_sol( index * 4 + 2 );
+        d = M_sol( index * 4 + 3 );
     }
 }
+
+} // namespace Feel
