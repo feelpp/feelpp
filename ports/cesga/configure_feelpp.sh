7d57f208f32664094dab1a0e46a453ba83ac241b
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-04-17 15:56:02 +0200

    fix configure vars

--- ports/cesga/configure_feelpp.sh
+++ ports/cesga/configure_feelpp.sh
@@ -1,8 +1,14 @@
 #! /bin/sh
 
+BUILDDIR=$PWD
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+cd $BUILDDIR
+
+. $SCRIPTPATH/environment
+. $SCRIPTPATH/modules
+
 PROF=gcc630
 FEELPP_SOURCES=$LUSTRE/devel/feelpp.git
-
 export CMAKE_BUILD_TYPE=RELEASE
 export CLANG_DIR=/opt/cesga/easybuild/software/Clang/3.9.1-foss-2017a
 export GMSH_DIR=${INSDIR}/$PROF/gmsh/2.16.0
@@ -13,14 +19,6 @@ export CC=${CLANG_DIR}/bin/clang
 export CXX=${CLANG_DIR}/bin/clang++
 #export CC=`which gcc` \
     #export CXX=`which g++` \
-
-BUILDDIR=$PWD
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-cd $BUILDDIR
-
-. $SCRIPTPATH/environment
-. $SCRIPTPATH/modules
-
 
 ${INSDIR}/$PROF/cmake/3.8.0/bin/cmake \
     -DFEELPP_PETSC_ENABLE_TESTS=OFF \
