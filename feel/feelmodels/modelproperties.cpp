dbdc6f88d9217100abf65dfd0267840e38d841f1
Vincent Chabannes <vincent.chabannes@feelpp.org>
2017-04-19 14:46:56 +0200

    up ModelMaterials :
    - refactoring : use std::map as storage of mat property
    - up expr support

--- feel/feelmodels/modelproperties.cpp
+++ feel/feelmodels/modelproperties.cpp
@@ -37,6 +37,7 @@ ModelProperties::ModelProperties( std::string const& filename, std::string const
     :
     M_worldComm( world ),
     M_params( world ),
+    M_mat( world ),
     M_bc( world, false ),
     M_ic( world, false ),
     M_postproc( world )
@@ -133,8 +134,9 @@ ModelProperties::ModelProperties( std::string const& filename, std::string const
     if ( mat )
     {
         LOG(INFO) << "Model with materials\n";
+        if ( !directoryLibExpr.empty() )
+            M_mat.setDirectoryLibExpr( directoryLibExpr );
         M_mat.setPTree( *mat );
-        
     }
     else
     {
@@ -147,7 +149,6 @@ ModelProperties::ModelProperties( std::string const& filename, std::string const
         if ( !directoryLibExpr.empty() )
             M_postproc.setDirectoryLibExpr( directoryLibExpr );
         M_postproc.setPTree( *pp );
-        
     }
     else
     {
