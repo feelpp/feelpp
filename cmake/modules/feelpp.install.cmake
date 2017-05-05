1cceeb1c41da021a54061bed4640b08ea5a007a6
Vincent Chabannes <vincent.chabannes@feelpp.org>
2017-05-05 16:21:38 +0200

    up feel cmake config :  - use cmake generator expr for definde FEELPP_INCLUDE_DIR and FEELPP_COMPILE_DEFINITIONS  - up cling interpreter

--- cmake/modules/feelpp.install.cmake
+++ cmake/modules/feelpp.install.cmake
@@ -55,32 +55,31 @@ mark_as_advanced(
 set(INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
 set(FEELPP_PREFIX ${CMAKE_INSTALL_PREFIX})
 if (NOT FEELPP_DATADIR )
-  set(FEELPP_DATADIR ${CMAKE_INSTALL_PREFIX}/share/feel )
+  set(FEELPP_DATADIR ${CMAKE_INSTALL_PREFIX}/share/feelpp/feel )
 endif()
 
-FILE(GLOB files "${CMAKE_CURRENT_SOURCE_DIR}/applications/crb/templates/*")
-INSTALL(FILES ${files} DESTINATION share/feel/crb/templates COMPONENT Devel)
+#FILE(GLOB files "${CMAKE_CURRENT_SOURCE_DIR}/applications/crb/templates/*")
+#INSTALL(FILES ${files} DESTINATION share/feel/crb/templates COMPONENT Devel)
 
 # documentation and examples
 #  install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/doc/manual/feel_get_tutorial.sh DESTINATION bin COMPONENT Doc)
 #install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/doc/manual/manual/feelpp-manual.pdf DESTINATION share/doc/feel COMPONENT Doc)
 IF( EXISTS "${CMAKE_CURRENT_BINARY_DIR}/doc/api/html" )
-  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc/api/html DESTINATION share/doc/feel COMPONENT Doc
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc/api/html DESTINATION share/doc/feelpp/feel COMPONENT Doc
     PATTERN ".svn" EXCLUDE PATTERN ".git" EXCLUDE )
 ENDIF()
 
-FILE(GLOB files "quickstart/qs_*")
-INSTALL(FILES ${files} DESTINATION share/doc/feel/examples/ COMPONENT Doc)
 
-FILE(WRITE CMakeLists.txt.doc  "cmake_minimum_required(VERSION 2.8)
-set(CMAKE_MODULE_PATH \"${CMAKE_INSTALL_PREFIX}/share/feel/cmake/modules/\")
-Find_Package(Feel++)
+#FILE(GLOB files "quickstart/qs_*")
+#INSTALL(FILES ${files} DESTINATION share/doc/feel/examples/ COMPONENT Doc)
 
+#FILE(WRITE CMakeLists.txt.doc  "cmake_minimum_required(VERSION 2.8)
+#Find_Package(Feel++ PATHS ${CMAKE_INSTALL_PREFIX}/share/feelpp/feel/cmake/modules/ )
 
-feelpp_add_application( qs_laplacian SRCS qs_laplacian.cpp INCLUDE_IN_ALL)
-feelpp_add_application( qs_stokes SRCS qs_stokes.cpp INCLUDE_IN_ALL)
-feelpp_add_application( qs_ns SRCS qs_ns.cpp INCLUDE_IN_ALL)
-")
+#feelpp_add_application( qs_laplacian SRCS qs_laplacian.cpp INCLUDE_IN_ALL)
+#feelpp_add_application( qs_stokes SRCS qs_stokes.cpp INCLUDE_IN_ALL)
+#feelpp_add_application( qs_ns SRCS qs_ns.cpp INCLUDE_IN_ALL)
+#")
 
 #FILE(GLOB examples
 #  "${CMAKE_CURRENT_SOURCE_DIR}/doc/manual/tutorial/*.*pp")
@@ -88,22 +87,22 @@ feelpp_add_application( qs_ns SRCS qs_ns.cpp INCLUDE_IN_ALL)
 #  "${CMAKE_CURRENT_SOURCE_DIR}/doc/manual/tutorial/*.cfg"
 #  "${CMAKE_CURRENT_SOURCE_DIR}/doc/manual/tutorial/*.geo" )
 
-INSTALL(FILES ${examples} DESTINATION share/doc/feel/examples/ COMPONENT Doc)
-INSTALL(FILES ${examplescfg} DESTINATION share/doc/feel/examples/ COMPONENT Doc)
-foreach(example ${examples} )
-  get_filename_component( EXAMPLE_TARGET_NAME ${example} NAME_WE )
-  get_filename_component( EXAMPLE_SRCS_NAME ${example} NAME )
-  FILE(APPEND CMakeLists.txt.doc "
+#INSTALL(FILES ${examples} DESTINATION share/doc/feel/examples/ COMPONENT Doc)
+#INSTALL(FILES ${examplescfg} DESTINATION share/doc/feel/examples/ COMPONENT Doc)
+#foreach(example ${examples} )
+#  get_filename_component( EXAMPLE_TARGET_NAME ${example} NAME_WE )
+#  get_filename_component( EXAMPLE_SRCS_NAME ${example} NAME )
+#  FILE(APPEND CMakeLists.txt.doc "
 # target feelpp_doc_${EXAMPLE_TARGET_NAME}
-feelpp_add_application( doc_${EXAMPLE_TARGET_NAME} SRCS ${EXAMPLE_SRCS_NAME} INCLUDE_IN_ALL)
-" )
-endforeach()
+#feelpp_add_application( doc_${EXAMPLE_TARGET_NAME} SRCS ${EXAMPLE_SRCS_NAME} INCLUDE_IN_ALL)
+#" )
+#endforeach()
 #foreach( example myapp mymesh myintegrals myfunctionspace mylaplacian mystokes)
 #  FILE(APPEND CMakeLists.txt.doc "
 #add_dependencies(tutorial feelpp_doc_${example})
 #")
 #endforeach()
-INSTALL(FILES CMakeLists.txt.doc DESTINATION share/doc/feel/examples/ COMPONENT Doc RENAME CMakeLists.txt)
+#INSTALL(FILES CMakeLists.txt.doc DESTINATION share/doc/feel/examples/ COMPONENT Doc RENAME CMakeLists.txt)
 
 #
 # this target installs the libraries, header files and cmake files
--- cmake/modules/feelpp.module.cling.interpreter.cmake
+++ cmake/modules/feelpp.module.cling.interpreter.cmake
@@ -8,7 +8,7 @@ set(FEELPP_CLING_BIN ${CLING_BIN})
 set(FEELPP_HAS_CLING_INTERPRETER 1)
 set(FEELPP_ENABLED_MODULES "${FEELPP_ENABLED_MODULES} Cling/Interpreter" )
 
-add_custom_target(feelpp_generate_cling_interpreter
+add_custom_target(feelpp_cling_interpreter
   DEPENDS feelpp
   COMMAND ${CMAKE_COMMAND} -P "${CMAKE_SOURCE_DIR}/cmake/modules/feelpp.generate.cling.interpreter.bash.cmake"
   )
