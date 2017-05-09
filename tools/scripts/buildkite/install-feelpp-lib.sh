be0c31a77ff4b4ae4a67436c8e7911217f570ee6
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-05-09 07:26:26 +0200

    fixed #896

--- tools/scripts/buildkite/install-feelpp-lib.sh
+++ tools/scripts/buildkite/install-feelpp-lib.sh
@@ -7,7 +7,7 @@ tag_from_target() {
     fromos=${splitfrom[0]}
     fromtag=${splitfrom[1]}
 
-    tools/scripts/buildkite/list.sh | grep "${fromos}-${fromtag}"  | while read line ; do
+    tools/scripts/buildkite/list.sh | grep "${BUILDKITE_BRANCH}-${fromos}-${fromtag}"  | while read line ; do
         tokens=($line)
         image=${tokens[0]}
         printf "%s" "${image}"
@@ -18,7 +18,7 @@ extratags_from_target() {
     fromos=${splitfrom[0]}
     fromtag=${splitfrom[1]}
 
-    tools/scripts/buildkite/list.sh | grep "${fromos}-${fromtag}"  | while read line ; do
+    tools/scripts/buildkite/list.sh | grep "${BUILDKITE_BRANCH}-${fromos}-${fromtag}"  | while read line ; do
         tokens=($line)
         extratags=${tokens[@]:5}
         printf "%s" "${extratags}" 
@@ -28,7 +28,8 @@ extratags_from_target() {
 echo '--- clone/pull feelpp/docker'
 if [ -d docker ]; then (cd docker; git pull) else git clone --depth=1 https://github.com/feelpp/docker; fi
 
-tag=$(echo "${BUILDKITE_BRANCH}" | sed -e 's/\//-/g')-$(cut -d- -f 2- <<< $(tag_from_target $TARGET))
+#tag=$(echo "${BUILDKITE_BRANCH}" | sed -e 's/\//-/g')-$(cut -d- -f 2- <<< $(tag_from_target $TARGET))
+tag=$(cut -d- -f 2- <<< $(tag_from_target $TARGET))
 echo "--- building feelpp-libs:${tag}"
 (cd docker/feelpp-libs && bash mkimg.sh -f ${TARGET} --jobs ${JOBS} --branch ${BUILDKITE_BRANCH} --cxx "${CXX}" --cc "${CC}")
 
