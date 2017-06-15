17b3a355f98de6bd80edfd322f7ea02d5251d888
Vincent Chabannes <vincent.chabannes@feelpp.org>
2017-06-15 11:47:53 +0200

    up crb plugin interface

--- feel/feelcrb/crbplugin_interface.hpp
+++ feel/feelcrb/crbplugin_interface.hpp
@@ -29,10 +29,8 @@
 #include <feel/feelconfig.h>
 #include <feel/feelcrb/parameterspace.hpp>
 #include <feel/feelcrb/crbdata.hpp>
-#if defined(FEELPP_HAS_VTK)
-#include <vtkSmartPointer.h>
-#include <vtkUnstructuredGrid.h>
-#endif
+#include <feel/feelmesh/meshbase.hpp>
+#include <feel/feelcrb/crbmodelbase.hpp>
 
 namespace Feel {
 
@@ -52,11 +50,21 @@ public:
     //! load database
     //!
     virtual void loadDB( std::string ) = 0;
-    
+
     //!
     //! @return the parameter space
     //!
     virtual boost::shared_ptr<ParameterSpaceX> parameterSpace() const = 0;
+
+    //!
+    //! @return the crb model
+    //!
+    virtual boost::shared_ptr<CRBModelBase> crbmodel() const = 0;
+
+    //!
+    //! @return the meshes
+    //!
+    virtual std::vector<boost::shared_ptr<MeshBase>> meshes() const = 0;
 
     //!
     //! run the crb online code
@@ -74,7 +82,7 @@ public:
     //! initialize the exporter
     //!
     virtual void initExporter()  = 0;
-    
+
     //!
     //! write to disk the results of the run
     //! @param name the name of the field to exporter
@@ -86,13 +94,6 @@ public:
     //! save exporter
     //!
     virtual void saveExporter() const = 0;
-    
-#if defined(FEELPP_HAS_VTK)
-    //!
-    //! exporter to VTK data structure 
-    //!
-    virtual vtkSmartPointer<vtkUnstructuredGrid> exporterVTK() const = 0;
-#endif // FEELPP_HAS_VTK
 
     //!
     //! virtual destructor
