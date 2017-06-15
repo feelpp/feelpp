516662a0ab19efb1ad175acde825709317c35798
Vincent Chabannes <vincent.chabannes@feelpp.org>
2017-06-15 11:47:31 +0200

    add CRBModelBase (interface of CRBModel)

---
+++ feel/feelcrb/crbmodelbase.hpp
@@ -0,0 +1,42 @@
+//! -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*- vim:fenc=utf-8:ft=cpp:et:sw=4:ts=4:sts=4
+//!
+//! This file is part of the Feel++ library
+//!
+//! This library is free software; you can redistribute it and/or
+//! modify it under the terms of the GNU Lesser General Public
+//! License as published by the Free Software Foundation; either
+//! version 2.1 of the License, or (at your option) any later version.
+//!
+//! This library is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//! Lesser General Public License for more details.
+//!
+//! You should have received a copy of the GNU Lesser General Public
+//! License along with this library; if not, write to the Free Software
+//! Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//!
+//! @file
+//! @author Vincent Chabannes <vincent.chabannes@feelpp.org>
+//! @date 15 Jun 2017
+//! @copyright 2017 Feel++ Consortium
+//!
+#ifndef FEELPP_CRBMODELBASE_HPP
+#define FEELPP_CRBMODELBASE_HPP 1
+
+
+namespace Feel
+{
+
+struct CRBModelBase
+{
+    virtual bool isSteady() const = 0;
+    virtual int numberOfTimeStep() const = 0;
+    virtual double timeStep() const = 0;
+    virtual double timeInitial() const = 0;
+    virtual double timeFinal() const = 0;
+};
+
+}
+
+#endif
