86d2b2b1bc7ac07970b7ec976f3484e75ae75a0e
romainhild <hild.romain@gmail.com>
2017-06-15 16:36:29 +0200

    add copyright

--- applications/crb/biotsavart/biotsavart.hpp
+++ applications/crb/biotsavart/biotsavart.hpp
@@ -1,15 +1,37 @@
+//! -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-
+//!
+//! This file is part of the Feel++ library
+//!
+//! This library is free software; you can redistribute it and/or
+//! modify it under the terms of the GNU Lesser General Public
+//! License as published by the Free Software Foundation; either
+//! version 2.1 of the License, or (at your option) any later version.
+//!
+//! This library is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//! Lesser General Public License for more details.
+//!
+//! You should have received a copy of the GNU Lesser General Public
+//! License along with this library; if not, write to the Free Software
+//! Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//!
+//! @file
+//! @author <you>
+//! @date 15 Jun 2017
+//! @copyright 2017 Feel++ Consortium
+//!
+//!
+
+#ifndef FEELPP_BIOTSAVART_HPP
+#define FEELPP_BIOTSAVART_HPP
+
 #include <feel/feel.hpp>
 #include <feel/feelcrb/crb.hpp>
 #include <feel/feelcrb/eim.hpp>
 #include <feel/feelcrb/crbmodel.hpp>
 #include <feel/feelcrb/modelcrbbase.hpp>
 #include <feel/feelfilters/exporter.hpp>
-
-// #ifdef USE_HIFI_TE
-// #include "thermoelectricCRB.hpp"
-// #else
-// #include "thermoelectric.hpp"
-// #endif
 
 namespace Feel
 {
@@ -699,3 +721,5 @@ void BiotSavartCRB<te_rb_model_type>::exportResults()
 }
 
 } // namespace Feel
+
+#endif
