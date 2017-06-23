bbaaefa20bfe60583176a84cfd909f4cf3a49e39
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-06-23 16:59:16 +0200

    Add script to generate singularity images via buildkite

---
+++ tools/scripts/buildkite/singularity-generate.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+containers="feelpp/feelpp-toolboxes:latest feelpp/feelpp-libs feelpp/feelpp-crb"
+
+set -euo pipefail
+
+echo '--- clone/pull feelpp/docker'
+if [ -d docker ]; then (cd docker; git pull) else git clone --depth=1 https://github.com/feelpp/docker; fi
+
+cd ./docker/singularity
+
+for cont in ${containers}
+do
+    echo "--- generate singularity image for ${cont} docker container"
+    ./generate_bootstrap.sh "${cont}"
+    ./generate_image.sh "${cont}"
+done
