66c5d48bf801a3c3cf781288fe569dd8508145a6
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-07-03 06:36:37 +0200

    up #938
     . a bit more generic scripts
     . tag master and develop with Feel++ version eg feelpp/feelpp-libs:v0.104.0
     . start work on Feel++ external application (feelpp_dockerize)

--- tools/scripts/buildkite/install-feelpp-lib.sh
+++ tools/scripts/buildkite/install-feelpp-lib.sh
@@ -10,17 +10,14 @@ if [ -d docker ]; then (cd docker; git pull) else git clone --depth=1 https://gi
 #tag=$(echo "${BUILDKITE_BRANCH}" | sed -e 's/\//-/g')-$(cut -d- -f 2- <<< $(tag_from_target $TARGET))
 #tag=$(cut -d- -f 2- <<< $(tag_from_target $TARGET))
 BRANCHTAG=$(echo "${BUILDKITE_BRANCH}" | sed -e 's/\//-/g')
-export BRANCHTAG
-version=$(get_version)
-export version
-tag=$(tag_from_target $TARGET)
+tag=$(tag_from_target $TARGET $BRANCHTAG $FEELPP_VERSION)
 echo "--- building feelpp-libs:${tag}"
 
 (cd docker/feelpp-libs && bash mkimg.sh -t feelpp/feelpp-libs:$tag -f ${TARGET} --jobs ${JOBS} --branch ${BUILDKITE_BRANCH} --cxx "${CXX}" --cc "${CC}")
 
 if [ "${BUILDKITE_BRANCH}" = "develop" ]; then
     echo "--- Tagging feelpp-libs:${tag}"
-    extratags=`extratags_from_target $TARGET`
+    extratags=$(extratags_from_target $TARGET $BRANCHTAG $FEELPP_VERSION)
     # add extra tags
     for tagalias in ${extratags[@]}; do
         echo "Tagging feelpp/feelpp-libs:$tag as feelpp/feelpp-libs:$tagalias"
