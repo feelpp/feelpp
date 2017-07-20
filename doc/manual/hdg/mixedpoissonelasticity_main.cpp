b30ac497a4db8d3702093e7183fc96feea2b8a62
Lorenzo Sala <lorenzo.sala91@gmail.com>
2017-07-20 11:50:47 +0200

    Add possibility to have different submeshes for elasticity and darcy in the hdg coupling model

--- doc/manual/hdg/mixedpoissonelasticity_main.cpp
+++ doc/manual/hdg/mixedpoissonelasticity_main.cpp
@@ -41,26 +41,77 @@ int main(int argc, char *argv[])
 
     decltype( IPtr( _domainSpace=Pdhv<FEELPP_ORDER>(mesh), _imageSpace=Pdhv<FEELPP_ORDER>(mesh) ) ) Idh_el ;
     decltype( IPtr( _domainSpace=Pdhms<FEELPP_ORDER>(mesh), _imageSpace=Pdhms<FEELPP_ORDER>(mesh) ) ) Idhv_el;
-		
-	if ( soption( "gmsh.submesh" ).empty() )
-	{
-		mpe_type MPE( mesh );
-		MPE.run( mesh, Idh_el, Idhv_el, Idh_poi, Idhv_poi );
-	}
-	else
-	{
-		Feel::cout << "Using submesh: " << soption("gmsh.submesh") << std::endl;
-	    auto cmesh = createSubmesh( mesh, markedelements(mesh,soption("gmsh.submesh")), Environment::worldComm() );
 
-		Idh_poi = IPtr( _domainSpace=Pdh<FEELPP_ORDER>(cmesh), _imageSpace=Pdh<FEELPP_ORDER>(mesh) );
-    	Idhv_poi = IPtr( _domainSpace=Pdhv<FEELPP_ORDER>(cmesh), _imageSpace=Pdhv<FEELPP_ORDER>(mesh) );
+    std::list<std::string> listSubmesh;
+    std::string unseparatedList = soption( "mixedelasticity.gmsh.submesh");
 
-    	Idh_el = IPtr( _domainSpace=Pdhv<FEELPP_ORDER>(cmesh), _imageSpace=Pdhv<FEELPP_ORDER>(mesh) );
-		Idhv_el = IPtr( _domainSpace=Pdhms<FEELPP_ORDER>(cmesh), _imageSpace=Pdhms<FEELPP_ORDER>(mesh) );
+    char help;
+    std::string nameSubmesh;
+    for ( int i = 0; i < unseparatedList.size(); i++)
+    {
+
+        help = unseparatedList[i];
+        if ( help == ',' || i == unseparatedList.size()-1 )
+        {        
+            if ( i ==  unseparatedList.size()-1)
+                nameSubmesh.push_back(help);
+            listSubmesh.push_back(nameSubmesh);
+            nameSubmesh.erase();
+        }
+        else
+        {
+            nameSubmesh.push_back(help);
+        }
+    }
+
 		
-		mpe_type MPE( cmesh, mesh); 
-		MPE.run( mesh, Idh_el, Idhv_el, Idh_poi, Idhv_poi );
+	if ( soption( "gmsh.submesh" ).empty() && soption("mixedelasticity.gmsh.submesh").empty() )
+	{
+		mpe_type MPE( mesh, mesh, mesh, mesh);
+		MPE.run( Idh_el, Idhv_el, Idh_poi, Idhv_poi );
 	}
+	else if ( soption( "gmsh.submesh" ).empty() )
+    {
+		Feel::cout << "Using submesh for Elasticity: " << soption("mixedelasticity.gmsh.submesh") << std::endl;
+        auto cmeshElasticity = createSubmesh( mesh, markedelements(mesh,listSubmesh ), Environment::worldComm() );
+    	
+        Idh_el = IPtr( _domainSpace=Pdhv<FEELPP_ORDER>(cmeshElasticity), _imageSpace=Pdhv<FEELPP_ORDER>(mesh) );
+		Idhv_el = IPtr( _domainSpace=Pdhms<FEELPP_ORDER>(cmeshElasticity), _imageSpace=Pdhms<FEELPP_ORDER>(mesh) );
+		
+		mpe_type MPE( mesh, cmeshElasticity, cmeshElasticity, mesh ); 
+		MPE.run( Idh_el, Idhv_el, Idh_poi, Idhv_poi );
+
+    }
+	else if ( soption("mixedelasticity.gmsh.submesh").empty() )
+    {
+		Feel::cout << "Using submesh for Poisson: " << soption("gmsh.submesh") << std::endl;
+        auto cmeshPoisson = createSubmesh( mesh, markedelements(mesh,soption("gmsh.submesh")), Environment::worldComm() );
+
+		Idh_poi = IPtr( _domainSpace=Pdh<FEELPP_ORDER>(cmeshPoisson), _imageSpace=Pdh<FEELPP_ORDER>(mesh) );
+    	Idhv_poi = IPtr( _domainSpace=Pdhv<FEELPP_ORDER>(cmeshPoisson), _imageSpace=Pdhv<FEELPP_ORDER>(mesh) );
+		
+        mpe_type MPE( cmeshPoisson, mesh, cmeshPoisson, mesh ); 
+		MPE.run( Idh_el, Idhv_el, Idh_poi, Idhv_poi );
+	} 
+    else
+    {
+		Feel::cout << "Using submesh for Poisson: " << soption("gmsh.submesh") << std::endl;
+		Feel::cout << "Using submesh for Elasticity: " << soption("mixedelasticity.gmsh.submesh") << std::endl;
+	    
+        auto cmeshPoisson = createSubmesh( mesh, markedelements(mesh,soption("gmsh.submesh")), Environment::worldComm() );
+        auto cmeshElasticity = createSubmesh( mesh, markedelements(mesh,listSubmesh ), Environment::worldComm() );
+
+		Idh_poi = IPtr( _domainSpace=Pdh<FEELPP_ORDER>(cmeshPoisson), _imageSpace=Pdh<FEELPP_ORDER>(mesh) );
+    	Idhv_poi = IPtr( _domainSpace=Pdhv<FEELPP_ORDER>(cmeshPoisson), _imageSpace=Pdhv<FEELPP_ORDER>(mesh) );
+
+    	Idh_el = IPtr( _domainSpace=Pdhv<FEELPP_ORDER>(cmeshElasticity), _imageSpace=Pdhv<FEELPP_ORDER>(mesh) );
+		Idhv_el = IPtr( _domainSpace=Pdhms<FEELPP_ORDER>(cmeshElasticity), _imageSpace=Pdhms<FEELPP_ORDER>(mesh) );
+		
+        auto meshCommon = (soption("gmsh.submesh")<soption("mixedelasticity.gmsh.submesh")) ? cmeshPoisson : cmeshElasticity ;
+
+		mpe_type MPE( cmeshPoisson, cmeshElasticity, meshCommon, mesh); 
+		MPE.run( Idh_el, Idhv_el, Idh_poi, Idhv_poi );
+    }
 
 	
 	return 0;
