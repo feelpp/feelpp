dd284a6c4ca4ea70d875a00dc9a98c4980388d96
Vincent Chabannes <vincent.chabannes@feelpp.org>
2017-07-21 18:40:54 +0200

    Revert "fix compilation"
    This reverts commit 2f1d693d4d765343b0edc3e4f050245992af2a4a.

--- feel/feelpartition/partitionermetis_impl.hpp
+++ feel/feelpartition/partitionermetis_impl.hpp
@@ -221,7 +221,7 @@ PartitionerMetis<MeshType>::partitionImpl( mesh_ptrtype mesh, rank_type np, Iter
         dof_id_type gid = pairElt.second.first;
         rank_type initialPid = pairElt.second.second;
         rank_type newPid = static_cast<rank_type>(part[gid]);
-        auto & eltToUpdate = *mesh->elementIterator( eltId );
+        auto & eltToUpdate = mesh->elementIterator( eltId )->second;
         eltToUpdate.setProcessId( newPid );
     }
 }
