89d6cf1a72fca616d4b2af4a306d481318dbdd5c
Guillaume DOLLÉ <dolle.guillaume@gmail.com>
2017-08-23 17:20:48 +0200

    Merge

--- feel/feelcore/observer.hpp
+++ feel/feelcore/observer.hpp
@@ -1,7 +1,7 @@
 // This file is part of the Feel library
 //
-// Author(s): Guillaume Dolle <gdolle@unistra.fr>
-//      Date: 2012-02-14
+// Author(s): Feel++ Contortium
+//      Date: 2017-07-10
 //
 // @copyright (C) 2017 University of Strasbourg
 // @copyright (C) 2012-2017 Feel++ Consortium
@@ -20,38 +20,79 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-
 #ifndef FEELPP_OBSERVER_HPP
 #define FEELPP_OBSERVER_HPP 1
 
-#include <feel/feelcore/environment.hpp>
+//#include <feel/feelcore/environment.hpp>
+//#include <feel/feelcore/signal.hpp>
+#include <boost/signals2.hpp>
+#include <boost/any.hpp>
+#include <boost/property_tree/ptree.hpp>
 
 namespace Feel
 {
+namespace Observer
+{
+
 namespace pt =  boost::property_tree;
+
+//! Enable signals & slots to inherited objects.
+//! Add new functionnalities to connect objects between each others.
+//! Note: This class is used to generate benchmark database.
+class SignalManager
+{
+public:
+    template< typename Notify, typename Event >
+        using sig_type = boost::signals2::signal< Notify, Event >;
+
+
+    // Create a new Feel++ signal.
+    template< typename Notify, typename Event >
+    sig_type<Notify, Event>
+    newSignal( std::string s )
+    {
+        const auto v = std::make_shared< sig_type< Notify, Event > >;
+        M_sigs.insert(
+            std::pair<std::string,
+                      sig_type< Notify, Event > > ( s, v )
+            );
+    };
+
+    // Get an existing Feel++ signal. Use typeid for information, if you
+    // desire to dynamic casting to the signal real type.
+    // Otherwise, prefer using createSignal.
+    boost::any signal( std::string s )
+    {
+        return M_sigs[s];
+    };
+
+private:
+    //! Map containing signal name and object.
+    std::map< std::string, boost::any > M_sigs;
+};
+
+
+class SlotManager
+{
+};
+
 
 //! Retrieve information from child class (observer pattern)
 //!
 //! Each child must implement its inform() method. It returns a property tree
 //! containing objects information, aka simulations input/output parameters
 //! important for reproducibility.
-class Observer
-{
-protected:
-    //! Default constructor.
-    //!
-    //! When this constructor is called by a child class, the new child observer
-    //! is connected to the global signal contained by Environment, to inform
-    //! Environment to watch him.
-    Observer()
-    {
-        Environment::simDataSignals().connect(std::bind(&Observer::watch,this));
-    }
-public:
-    //! Watch child properties.
-    virtual pt::ptree& watch() const = 0;
-};
+//class SimulationInfo
+//    :
+//        public ObserverBase< pt::ptree ( std::string ), pt::ptree >
+//{
+//public:
+//    //! Watch child properties.
+//    virtual pt::ptree& notify() const = 0;
+//};
 
+
+} // Observer namespace
 } // Feel namespace.
 
 #endif // FEELPP_OBSERVER_HPP
