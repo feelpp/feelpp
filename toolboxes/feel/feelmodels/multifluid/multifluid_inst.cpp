5295436fe87fc89d98e3ddabf9b58cc08bf5e0b0
Thibaut Metivet <thibaut.metivet@gmail.com>
2017-09-01 14:30:00 +0200

    Add linear elastic force model

--- toolboxes/feel/feelmodels/multifluid/multifluid_inst.cpp
+++ toolboxes/feel/feelmodels/multifluid/multifluid_inst.cpp
@@ -5,6 +5,7 @@
 
 #include <feel/feelmodels/multifluid/helfrichforcemodel.hpp>
 #include <feel/feelmodels/multifluid/inextensibilityforcemodel.hpp>
+#include <feel/feelmodels/multifluid/linearelasticforcemodel.hpp>
 #include <feel/feelmodels/multifluid/skalakforcemodel.hpp>
 
 namespace Feel {
@@ -23,6 +24,11 @@ const bool inextensibility_interfaceforcesmodel =
     MULTIFLUID_CLASS_INSTANTIATION::interfaceforces_factory_type::instance().registerProduct( 
             "inextensibility-force", 
             &detail::createInterfaceForcesModel<InextensibilityForceModel, typename MULTIFLUID_CLASS_INSTANTIATION::levelset_type> );
+
+const bool linearelastic_interfaceforcesmodel = 
+    MULTIFLUID_CLASS_INSTANTIATION::interfaceforces_factory_type::instance().registerProduct( 
+            "linear-elastic-force", 
+            &detail::createInterfaceForcesModel<LinearElasticForceModel, typename MULTIFLUID_CLASS_INSTANTIATION::levelset_type> );
 
 const bool skalak_interfaceforcesmodel = 
     MULTIFLUID_CLASS_INSTANTIATION::interfaceforces_factory_type::instance().registerProduct( 
