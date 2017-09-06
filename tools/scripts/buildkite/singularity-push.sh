8ea845fd53fb5de1d2c072cd59a300b54834b474
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-09-06 22:04:51 +0200

    Rework singularity push script
    - Push into a LFS repo to make images available to the rest of the
      world #975.
    
    - Fix a max number of images to avoid messing with the repository.
      If they are more than 20 images for now (~200 GB on gitlab) no images
      are pushed, the CI is stopped with a clear warning message.
      NB: The repo should have a hook for guaranty.
    
    Remarks:
    
    - Note that images are pushed on local machine gitlab LFS. We have first
      to clone the repository. So for 200GB => 400GB of disk space is required.
      (We could check if it is possible to manipulate the lfs server
      associated to a repository directly (might not be a good idea).
      The best is to wait for singularity registry for a better solution).
    
    - Also note that the LFS is backed up.

--- tools/scripts/buildkite/singularity-push.sh
+++ tools/scripts/buildkite/singularity-push.sh
@@ -4,32 +4,47 @@ set -euo pipefail
 
 source $(dirname $0)/common.sh
 
+ROOT_DIR=`pwd`
 IMAGES_ROOT=./docker/singularity/images
 REPO_NAME=feelpp-singularity-images.git
+REPO=git@gitlab.math.unistra.fr:dolle/feelpp-singularity-images.git
+REPO_MAX_IMAGES=20 #( x10 GB )
 
 # Remove old local images (NOT LFS).
-if [ -d ${IMAGES_ROOT} ]; then
-    find ${IMAGES_ROOT} -name "*.img" -exec  rm -r {} \;
-else
+if [ ! -d ${IMAGES_ROOT} ]; then
     echo "./docker.git/singularity/images does not exist"
     pwd
     exit 1
 fi
 
 # Clone/update gitlab repository.
+echo "-- Clone Feel++ singularity images repository"
 if [ -d ${REPO_NAME} ]; then
-	cd ${REPO_NAME}
-	git lfs pull
+    cd ${REPO_NAME}
+    git lfs pull
+    cd ${ROOT_DIR}
 else
-	git lfs clone --depth=1 git@gitlab.math.unistra.fr:dolle/feelpp-singularity-images.git ${REPO_NAME}
+    git lfs clone --depth=1 ${REPO} ${REPO_NAME}
 fi
 
 # Copy (overide) generated images in the repository.
+echo "-- Prepare Feel++ singularity images"
 find ${IMAGES_ROOT} -name "*.img" \
--exec echo "--- push singularity image:" {} \; \
+-exec echo "- image:" {} \; \
 -exec cp -r {} ${REPO_NAME} \;
 
 cd ${REPO_NAME}
+echo "-- Push Feel++ singularity images"
 git add *.img
 git commit -am "[buildkite] Deploy new singularity images"
 git push
+cd ${ROOT_DIR}
+
+echo "-- Remove temporary Feel++ singularity images"
+find ${IMAGES_ROOT} -name "*.img" -exec  rm -r {} \;
+
+if [ `ls -l ${REPO_NAME}/*.img | wc -l` -ge ${REPO_MAX_IMAGES} ]; then
+    echo "WARNING: Maximum images limit reached ! => ${REPO}"
+    echo "INFO: You should remove some images or increase this 
+    REPO_MAX_IMAGES number in the script."
+fi
