49d372dafb7f4c678a0a117800e9750641058641
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-09-18 22:46:22 +0200

    use python 3 #1045 and compile feelpython code
    [ci skip]

--- cmake/modules/feelpp.dependencies.cmake
+++ cmake/modules/feelpp.dependencies.cmake
@@ -486,15 +486,15 @@ SET(BOOST_MIN_VERSION "1.61.0")
 
 # First we try to find boost with the python components
 if(FEELPP_ENABLE_PYTHON_WRAPPING)
-    FIND_PACKAGE(Boost ${BOOST_MIN_VERSION} COMPONENTS python )
-    if(Boost_PYTHON_FOUND)
-        set(FEELPP_HAS_BOOST_PYTHON 1)
-        set(FEELPP_LIBRARIES ${Boost_PYTHON_LIBRARY} ${FEELPP_LIBRARIES})
-        set(FEELPP_ENABLED_OPTIONS "${FEELPP_ENABLED_OPTIONS} Boost-Python-Wrapping" )
+    # FIND_PACKAGE(Boost ${BOOST_MIN_VERSION} COMPONENTS python )
+    # if(Boost_PYTHON_FOUND)
+    #     set(FEELPP_HAS_BOOST_PYTHON 1)
+    #     set(FEELPP_LIBRARIES ${Boost_PYTHON_LIBRARY} ${FEELPP_LIBRARIES})
+    #     set(FEELPP_ENABLED_OPTIONS "${FEELPP_ENABLED_OPTIONS} Boost-Python-Wrapping" )
 
-    else()
-        message(FATAL_ERROR "[feelpp] Boost.Python was not found on your system (Required for Python Wrapping)." )
-      endif()
+    # else()
+    #     message(FATAL_ERROR "[feelpp] Boost.Python was not found on your system (Required for Python Wrapping)." )
+#      endif()
 endif()
 
 # Then we try to find rest of the Boost components
@@ -731,7 +731,22 @@ endif()
 # Python libs
 option( FEELPP_ENABLE_PYTHON "Enable Python Support" ${FEELPP_ENABLE_PACKAGE_DEFAULT_OPTION} )
 if(FEELPP_ENABLE_PYTHON)
-  FIND_PACKAGE(PythonLibs 2.7 REQUIRED)
+  
+  #
+  # Python interp
+  #
+  FIND_PACKAGE(PythonInterp)
+  if(PYTHONINTERP_FOUND)
+    execute_process(COMMAND
+      ${PYTHON_EXECUTABLE}
+      -c "import sys; print(sys.version[0:3])"
+      OUTPUT_VARIABLE PYTHON_VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    message(STATUS "[feelpp] Found python version ${PYTHON_VERSION}")
+  endif()
+
+  FIND_PACKAGE(PythonLibs 3 REQUIRED)
   if ( PYTHONLIBS_FOUND )
     message(STATUS "[feelpp] PythonLibs: ${PYTHON_INCLUDE_DIRS} ${PYTHON_LIBRARIES}")
     INCLUDE_DIRECTORIES(${PYTHON_INCLUDE_DIRS})
@@ -739,20 +754,6 @@ if(FEELPP_ENABLE_PYTHON)
     SET(FEELPP_ENABLED_OPTIONS "${FEELPP_ENABLED_OPTIONS} Python" )
     set( FEELPP_HAS_PYTHON 1 )
 
-  endif()
-
-  #
-  # Python interp
-  #
-  FIND_PACKAGE(PythonInterp 2.7  REQUIRED)
-  if(PYTHONINTERP_FOUND)
-    execute_process(COMMAND
-      ${PYTHON_EXECUTABLE}
-      -c "import sys; print sys.version[0:3]"
-      OUTPUT_VARIABLE PYTHON_VERSION
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-    message(STATUS "[feelpp] Found python version ${PYTHON_VERSION}")
   endif()
 endif()
 
