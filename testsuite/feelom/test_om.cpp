fc1bef181b20b195ef8c77667994c812669cd71c
jbwahl <wahl.jb@gmail.com>
2017-12-07 11:21:46 +0100

    change test for OM with new macros / wrapper

--- testsuite/feelom/test_om.cpp
+++ testsuite/feelom/test_om.cpp
@@ -1,10 +1,11 @@
-#include "feeltest_om.hpp"
+#include "feel/feelfmi/fmu.hpp"
 
 
 int main( int argc, char* argv[] )
 {
     using namespace Feel;
 
-    Feeltest_om om;
-    om.run();
+    FMU my_fmu;
+    int ret = my_fmu.load("test_om/test_om.fmu");
+    std::cout << "all ok for now ! \n";
 }
