e078820117b9893882aff98853232f6f858b3afc
jbwahl <wahl.jb@gmail.com>
2017-12-10 16:33:29 +0100

    add virtual function in crb model to provide warp field (visualization of geometrical parametrization @vincentchabannes

--- feel/feelcrb/crbmodel.hpp
+++ feel/feelcrb/crbmodel.hpp
@@ -1776,7 +1776,7 @@ public:
      */
     bool hasEim()
     {
-        return M_has_eim;
+        return M_has_eim || hasDeim();
     }
 
     bool hasDeim()
@@ -2832,6 +2832,8 @@ public:
     void initializationField( element_ptrtype& initial_field,parameter_type const& mu,mpl::bool_<false> ) {};
 
 
+    typename model_type::warp_element_type fieldForWarp( parameter_type const& mu ) { return M_model->fieldForWarp(mu); }
+    bool hasWarpField() const { return M_model->hasWarpField(); }
     //@}
 
 
