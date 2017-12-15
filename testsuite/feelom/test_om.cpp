7a0fb9bcd183b4b748dfc7c26472a966acfcda57
jbwahl <wahl.jb@gmail.com>
2017-12-15 16:35:08 +0100

    up fmu test case. name to be changed

--- testsuite/feelom/test_om.cpp
+++ testsuite/feelom/test_om.cpp
@@ -4,8 +4,10 @@
 int main( int argc, char* argv[] )
 {
     using namespace Feel;
+    Environment env( _argc=argc, _argv=argv );
 
     FMU my_fmu;
-    int ret = my_fmu.load("test_om/test_om.fmu");
+    int ret = my_fmu.load();
     std::cout << "all ok for now ! \n";
+    my_fmu.simulate();
 }
