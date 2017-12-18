467461cbf2523f4d394508862d1c520205cb3f4d
jbwahl <wahl.jb@gmail.com>
2017-12-18 11:16:23 +0100

    move FMI code in cpp files

--- feel/feelfmi/fmu.hpp
+++ feel/feelfmi/fmu.hpp
@@ -1,14 +1,10 @@
 #ifndef FMIWRAPPER_HPP
 #define FMIWRAPPER_HPP
 
-
-#define BUFFER 1000
-
 #include "feel/feelcore/environment.hpp"
 
 #include "fmilib.h"
 #include <boost/filesystem.hpp>
-
 
 #include "feel/feelfmi/fmumodel2.hpp"
 #include "feel/feelfmi/cssolver.hpp"
@@ -29,92 +25,13 @@ public :
     typedef SolverBase solver_type;
     typedef boost::shared_ptr<solver_type> solver_ptrtype;
 
+    FMU( std::string prefix="" );
+    ~FMU();
 
-    FMU( std::string prefix="" ) :
-        M_prefix( prefix ),
-        M_verbose( boption(_name="fmu.verbose", _prefix=M_prefix) ),
-        M_callbacks( new callbacks_type )
-    {
-        M_callbacks->malloc = malloc;
-        M_callbacks->calloc = calloc;
-        M_callbacks->realloc = realloc;
-        M_callbacks->free = free;
-        M_callbacks->logger = importlogger;
-        M_callbacks->log_level = jm_log_level_all;//FLAGS_v;
-        M_callbacks->context = 0;
-
-        M_context = fmi_import_allocate_context( M_callbacks.get() );
-
-        // create temporary repository to extract fmu
-        M_tmp_dir = Environment::rootRepository() + "/fmu_tmpdir/";
-        if ( M_prefix!="" )
-            M_tmp_dir += M_prefix+"/";
-        boost::filesystem::path dir( M_tmp_dir );
-        if ( !(boost::filesystem::exists(dir)) )
-            boost::filesystem::create_directory(dir);
-    }
-
-    ~FMU()
-    {
-        fmi_import_free_context( M_context );
-        fmi_import_rmdir( M_callbacks.get(), M_tmp_dir.c_str() );
-    }
-
-    int load( std::string _path="" )
-    {
-        std::string path = _path;
-        if ( path=="" )
-            path = Environment::expand( soption( _name="fmu.filename", _prefix=M_prefix ) );
-        CHECK( path!="" ) << "No filename specified to load FMU. Either pass argument to the function load() or set the option fmu.filename\n";
-
-        // Read fmu version
-        auto version = fmi_import_get_fmi_version( M_context, path.c_str(), M_tmp_dir.c_str() );
-        CHECK( version != fmi_version_unknown_enu ) << "The FMU version could not be determined. "
-                                                    << jm_get_last_error( M_callbacks.get() )
-                                                    << std::endl;
-
-        if ( version == fmi_version_2_0_enu )
-            M_model = fmumodel_ptrtype( new FmuModel2( M_context, M_tmp_dir, M_callbacks ) );
-        else
-        {
-            Feel::cerr << "Error only version 2.0 FMU is supported for now\n";
-            return 0;
-        }
-
-        return 1;
-    }
-
-
-    void simulate( double t_init=-1, double t_final=-1, double tolerance=-1 )
-    {
-        if ( !M_solver )
-            initSolver();
-        if ( t_init==-1 )
-            t_init = M_model->defaultStartTime();
-        if ( t_final==-1 )
-            t_final = M_model->defaultFinalTime();
-        if ( tolerance==-1 )
-            tolerance = M_model->defaultTolerance();
-
-        M_solver->simulate( t_init, t_final, tolerance );
-    }
-
-    void initSolver()
-    {
-        auto kind = M_model->kind();
-        if ( kind=="CS" )
-            M_solver = solver_ptrtype( new CSSolver( M_model ) );
-
-        M_solver->setTimeStep( doption( _name="fmu.solver.time-step", _prefix=M_prefix ) );
-        M_solver->setRelativeTol( doption( _name="fmu.solver.rtol", _prefix=M_prefix) );
-    }
-
-    void setSolverTimeStep( double const& step )
-    {
-        if ( !M_solver )
-            initSolver();
-        M_solver->setTimeStep( step );
-    }
+    int load( std::string _path="" );
+    void simulate( double t_init=-1, double t_final=-1, double tolerance=-1 );
+    void initSolver();
+    void setSolverTimeStep( double const& step );
 
 private :
     static void importlogger(jm_callbacks* c,
@@ -122,7 +39,6 @@ private :
     {
         Feel::cout << "FeelFMI["<< module <<"] : " << message << std::endl;
     }
-
 
 
 private :
--- feel/feelfmi/fmumodel2.hpp
+++ feel/feelfmi/fmumodel2.hpp
@@ -2,10 +2,15 @@
 #define FMUMODEL2
 
 #include "fmilib.h"
+
+#include <boost/filesystem.hpp>
+
+#include "feel/feelcore/environment.hpp"
 #include "feel/feelfmi/fmumodelbase.hpp"
 
 namespace Feel
 {
+
 class FmuModel2 :
         public FmuModelBase
 {
@@ -15,126 +20,21 @@ public :
 
     typedef typename super_type::callbacks_ptrtype callbacks_ptrtype;
 
-    FmuModel2( fmi_import_context_t* context, std::string tmp_dir, callbacks_ptrtype callbacks ) :
-        super_type( callbacks )
-    {
-        fmi2_type_t type;
+    FmuModel2( fmi_import_context_t* context, std::string tmp_dir, callbacks_ptrtype callbacks );
+    ~FmuModel2();
 
-        M_version = 2;
-        M_callbackfunctions.logger = fmi2logger;
-        M_callbackfunctions.allocateMemory = calloc;
-        M_callbackfunctions.freeMemory = free;
-        //M_callbackfunctions.stepFinished = stepFinished;
-        M_callbackfunctions.componentEnvironment = 0;
+    void setupExperiment( double const& t_init, double const& t_final, double const& tol ) override;
+    void initialize() override;
+    void doStep( double t_cur, double step, bool newStep ) override;
 
-        // Read informations in xml file for the model
-        M_fmu = fmi2_import_parse_xml( context, tmp_dir.c_str(), 0 );
-        CHECK( M_fmu) << "Error parsing XML in tmp dir " << tmp_dir <<", "
-                      << jm_get_last_error( callbacks.get() ) << std::endl;
-        M_allocated_xml=true;
-
-        // Read Model name and ID
-        M_name = fmi2_import_get_model_name( M_fmu );
-        M_guid = fmi2_import_get_GUID( M_fmu );
-
-        // Read model kind : CS or ME
-        auto kind = fmi2_import_get_fmu_kind( M_fmu );
-        if ( kind == fmi2_fmu_kind_me )
-        {
-            M_kind = "ME";
-            M_id = fmi2_import_get_model_identifier_ME( M_fmu );
-            type = fmi2_model_exchange;
-        }
-        else if ( kind == fmi2_fmu_kind_cs )
-        {
-            M_kind = "CS";
-            M_id = fmi2_import_get_model_identifier_CS( M_fmu );
-            type = fmi2_cosimulation;
-        }
-        else
-            CHECK( false ) << "Unxepected FMU kind, exiting. " << jm_get_last_error( callbacks.get() ) << std::endl;
-
-        // Load DLL
-        auto status = fmi2_import_create_dllfmu( M_fmu, kind, &M_callbackfunctions );
-        CHECK( status!=jm_status_error ) << "Could not create the DLL loading mechanism(C-API). " << std::endl;
-        M_allocated_dll = true;
-
-        // Instatiate the model
-        std::string instance_name = "FMU model " + M_name;
-        status = fmi2_import_instantiate( M_fmu, instance_name.c_str(), type, 0, 0 );
-        CHECK( status!=jm_status_error ) << "FMUModel2 : fmi2_import_instantiate failed\n";
-
-
-        LOG(INFO) << "FMU Model loaded : name=" << M_name <<", guid=" << M_guid
-                  << ", kind="<< M_kind << ", id=" << M_id <<std::endl
-                  << "Version returned from FMU = " << fmi2_import_get_version(M_fmu) << std::endl;
-    }
-
-    ~FmuModel2()
-    {
-        if ( M_allocated_fmu )
-        {
-            fmi2_import_terminate(M_fmu);
-            fmi2_import_free_instance(M_fmu);
-        }
-        if ( M_allocated_xml )
-            fmi2_import_free( M_fmu );
-        if ( M_allocated_dll )
-            fmi2_import_destroy_dllfmu(M_fmu);
-    }
-
-    void setupExperiment( double const& t_init, double const& t_final, double const& tol ) override
-    {
-        auto status = fmi2_import_setup_experiment( M_fmu, fmi2_true, tol, t_init, fmi2_false, t_final );
-        CHECK( status==fmi2_status_ok )<< "FMUModel2 : fmi2_import_setup_experiment failed\n";
-    }
-
-    void initialize() override
-    {
-        auto status = fmi2_import_enter_initialization_mode(M_fmu);
-        CHECK( status==fmi2_status_ok ) << "FMUModel2 : fmi2_import_enter_initialization_mode failed\n";
-        status = fmi2_import_exit_initialization_mode(M_fmu);
-        CHECK( status==fmi2_status_ok ) << "FMUModel2 : fmi2_import_exit_initialization_mode failed\n";
-    }
-
-    void doStep( double t_cur, double step, bool newStep ) override
-    {
-        fmi2_import_do_step( M_fmu, t_cur, step, newStep );
-    }
-
-
-    double defaultStartTime() override
-    {
-        return fmi2_import_get_default_experiment_start(M_fmu);
-    }
-
-    double defaultFinalTime() override
-    {
-        return fmi2_import_get_default_experiment_stop(M_fmu);
-    }
-
-    double defaultTolerance() override
-    {
-        return fmi2_import_get_default_experiment_tolerance(M_fmu);
-    }
+    double defaultStartTime() override;
+    double defaultFinalTime() override;
+    double defaultTolerance() override;
 
 private :
     static void fmi2logger(fmi2_component_environment_t env, fmi2_string_t instanceName,
-                           fmi2_status_t status, fmi2_string_t category, fmi2_string_t message, ...)
-    {
-        char buffer[1000];
-        va_list args;
-        va_start( args, message );
-        vsnprintf( buffer, 256, message, args );
-        Feel::cout << "FMUModel2 : status="<<fmi2_status_to_string(status)
-                   << "; " << instanceName <<"("<<category<<") : " << buffer <<std::endl;
-        va_end (args);
-    }
-
-    static void stepFinished(fmi2_component_environment_t env, fmi2_status_t status)
-    {
-        Feel::cout << "FMUModel2 : stepFinished called with fmiStatus : "<< fmi2_status_to_string(status) << std::endl;
-    }
+                           fmi2_status_t status, fmi2_string_t category, fmi2_string_t message, ...);
+    static void stepFinished(fmi2_component_environment_t env, fmi2_status_t status);
 
 
 private :
