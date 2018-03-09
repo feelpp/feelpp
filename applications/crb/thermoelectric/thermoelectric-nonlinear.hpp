35ab16c9c517dd43222b8808929bb91f7c9060ce
romainhild <hild.romain@gmail.com>
2018-03-09 15:50:04 +0100

    update thermoelectric crb

--- applications/crb/thermoelectric/thermoelectric-nonlinear.hpp
+++ applications/crb/thermoelectric/thermoelectric-nonlinear.hpp
@@ -38,6 +38,8 @@ makeThermoElectricOptions()
 {
     po::options_description options( "Thermoelectric" );
     options.add_options()
+        ( "thermoelectric.basename", Feel::po::value<std::string>()->default_value("thermoelectric_linear"),
+          "name of the database" )
         ( "thermoelectric.filename", Feel::po::value<std::string>()->default_value("thermoelectric.json"),
           "json file containing application parameters and boundary conditions")
         ( "thermoelectric.penal-dir", po::value<double>()->default_value( 1e4 ), "penalisation term" )
@@ -79,9 +81,9 @@ public:
     using V_basis_type = bases<fct_base_type>;
     using V_space_type = FunctionSpace<mesh_type, V_basis_type, value_type>;
 
-    using fct_eim_base_type = Lagrange<Order+2, Scalar>;
-    using basis_eim_type = bases<fct_eim_base_type>;
-    using space_eim_type = FunctionSpace<mesh_type, basis_eim_type, value_type>;
+    // using fct_eim_base_type = Lagrange<Order+2, Scalar>;
+    // using basis_eim_type = bases<fct_eim_base_type>;
+    // using space_eim_type = FunctionSpace<mesh_type, basis_eim_type, value_type>;
 
     using J_basis_type = bases<Lagrange<Order+2, Scalar,Discontinuous> >;
     using J_space_type = FunctionSpace<mesh_type, J_basis_type, Discontinuous, value_type>;
@@ -94,12 +96,14 @@ public :
     typedef typename ParameterDefinition::parameterspace_type parameterspace_type;
     typedef typename FunctionSpaceDefinition::space_type space_type;
     typedef typename FunctionSpaceDefinition::J_space_type J_space_type;
-    using space_eim_type = typename FunctionSpaceDefinition::space_eim_type;
-    using element_eim_type = typename space_eim_type::element_type;
+    // using space_eim_type = typename FunctionSpaceDefinition::space_eim_type;
+    // using element_eim_type = typename space_eim_type::element_type;
+    using V_space_type = typename FunctionSpaceDefinition::V_space_type;
 
     /* EIM */
     // Scalar continuous //
-    typedef EIMFunctionBase<space_eim_type, space_type , parameterspace_type> fun_type;
+    typedef EIMFunctionBase<V_space_type, space_type , parameterspace_type> fun_type;
+    // typedef EIMFunctionBase<space_eim_type, space_type , parameterspace_type> fun_type;
     // Scalar Discontinuous //
     typedef EIMFunctionBase<J_space_type, space_type , parameterspace_type> fund_type;
 
@@ -132,9 +136,10 @@ public:
 
     using J_space_type = function_space_type::J_space_type;
     using J_space_ptrtype = boost::shared_ptr<J_space_type>;
-    using q_sigma_space_type = eim_definition_type::space_eim_type;
+    using q_sigma_space_type = eim_definition_type::V_space_type;
     using q_sigma_space_ptrtype = boost::shared_ptr<q_sigma_space_type>;
-    using q_sigma_element_type = eim_definition_type::element_eim_type;
+    using q_sigma_element_type = q_sigma_space_type::element_type;
+    using V_view_type = typename element_type::template sub_element_type<0>;
     using V_view_ptrtype = typename element_type::template sub_element_ptrtype<0>;
     using T_view_ptrtype = typename element_type::template sub_element_ptrtype<1>;
 
@@ -149,7 +154,7 @@ public:
     using parameter_type = super_type::parameter_type;
     using vectorN_type = super_type::vectorN_type;
     using beta_vector_type = typename super_type::beta_vector_type;
-    using beta_type = boost::tuple<beta_vector_type,  std::vector<beta_vector_type> >;
+    using beta_type = boost::tuple<beta_vector_type, std::vector<beta_vector_type> >;
     using affine_decomposition_type = typename super_type::affine_decomposition_type;
 
     using sparse_matrix_ptrtype = typename super_type::sparse_matrix_ptrtype;
@@ -158,6 +163,9 @@ private:
     mesh_ptrtype M_mesh;
     prop_ptrtype M_modelProps;
     map_mat_type M_materials;
+    map_mat_type M_elecMaterials;
+    map_mat_type M_therMaterials;
+
     std::vector< std::vector< element_ptrtype > > M_InitialGuess;
 
     element_ptrtype M_VT;
@@ -166,7 +174,7 @@ private:
     parameter_type M_mu;
 
     J_space_ptrtype M_Jh;
-    q_sigma_space_ptrtype M_Th;
+    // q_sigma_space_ptrtype M_Th;
 
     sparse_matrix_ptrtype M_M;
 
@@ -182,7 +190,8 @@ public:
     ThermoElectric();
     ThermoElectric( mesh_ptrtype mesh );
 
-    int indexOfMat(std::string mat ) const { return std::distance(M_materials.begin(),M_materials.find(mat)); }
+    int indexOfMatV(std::string mat ) const { return std::distance(M_elecMaterials.begin(),M_elecMaterials.find(mat)); }
+    int indexOfMatT(std::string mat ) const { return std::distance(M_therMaterials.begin(),M_therMaterials.find(mat)); }
     // Size of the decomposition
     int Qa() const;
     int Nl() const;
@@ -190,8 +199,10 @@ public:
     int mMaxA( int q ) const;
     int mMaxL( int l, int q ) const;
     int mMaxCompliant( int q ) const;
-    int mMaxIntensity( int q ) const;
-    int mMaxAverageTemp( int q ) const;
+    int QIntensity( ModelOutput const& out ) const;
+    int QAverageTemp( ModelOutput const& out ) const;
+    int mMaxIntensity( int q, ModelOutput const& out ) const;
+    int mMaxAverageTemp( int q, ModelOutput const& out ) const;
     int QInitialGuess() const override;
     int mMaxInitialGuess( int q ) const override;
     void resizeQm( bool resizeMatrix = true );
@@ -204,6 +215,9 @@ public:
     void initModel() override;
     void setupSpecificityModel( boost::property_tree::ptree const& ptree, std::string const& dbDir ) override;
 
+    // mesh support of functionspace
+    functionspace_type::mesh_support_vector_type functionspaceMeshSupport( mesh_ptrtype const& mesh ) const override;
+
     // Decomposition
     void assemble() override;
     affine_decomposition_type computeAffineDecomposition() override;
@@ -211,13 +225,13 @@ public:
     std::vector<std::vector<element_ptrtype> > computeInitialGuessAffineDecomposition() override;
 
     // Beta coefficients
+    beta_vector_type computeBetaLinearDecompositionA( parameter_type const& mu , double time=1e30 ) override;
     beta_vector_type computeBetaInitialGuess( parameter_type const& mu ) override;
     beta_type computeBetaQm( parameter_type const& mu ,  double time , bool only_terms_time_dependent=false) override;
     beta_type computeBetaQm( element_type const& T, parameter_type const& mu ) override;
     beta_type computeBetaQm( vectorN_type const& urb, parameter_type const& mu ) override;
     beta_type computeBetaQm( parameter_type const& mu ) override;
-    void fillBetaQm( parameter_type const& mu, std::vector<vectorN_type> betaEimsJoule, std::vector<vectorN_type> betaEimsSigma, std::vector<vectorN_type> betaEimsK );
-    beta_vector_type computeBetaLinearDecompositionA( parameter_type const& mu , double time=1e30 ) override;
+    void fillBetaQm( parameter_type const& mu, vectorN_type betaEimGrad, std::vector<vectorN_type> betaEimsSigma, std::vector<vectorN_type> betaEimsK );
 
     // FE resolution
     element_type solve( parameter_type const& mu ) override;
@@ -229,7 +243,7 @@ public:
 
     // Output
     value_type
-    output( int output_index, parameter_type const& mu , element_type& u, bool need_to_solve=false);
+    output( int output_index, parameter_type const& mu , element_type& u, bool need_to_solve=false) override;
 
     // BiotSavart API
     int mMaxJoule();
