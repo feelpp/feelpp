2bb5de1789854981be3e83e10ff98e2549e1cde3
Vincent Chabannes <vincent.chabannes@feelpp.org>
2018-03-21 09:09:10 +0100

    rename HeatTransfer -> Heat

--- toolboxes/heattransfer/main_ht.cpp
+++ toolboxes/heattransfer/main_ht.cpp
@@ -1,42 +1,42 @@
 
-#include <feel/feelmodels/heattransfer/heattransfer.hpp>
+#include <feel/feelmodels/heat/heat.hpp>
 
 int main(int argc, char**argv )
 {
     using namespace Feel;
-	po::options_description heattransferoptions( "heat-transfer options" );
-    heattransferoptions.add( toolboxes_options("heat-transfer") );
+	po::options_description heatoptions( "heat options" );
+    heatoptions.add( toolboxes_options("heat") );
 
 	Environment env( _argc=argc, _argv=argv,
-                     _desc=heattransferoptions,
-                     _about=about(_name="toolboxes_heattransfer",
+                     _desc=heatoptions,
+                     _about=about(_name="toolboxes_heat",
                                   _author="Feel++ Consortium",
                                   _email="feelpp-devel@feelpp.org"));
 
-    typedef FeelModels::HeatTransfer< Simplex<FEELPP_DIM,1>,
+    typedef FeelModels::Heat< Simplex<FEELPP_DIM,1>,
                                       Lagrange<1, Scalar,Continuous,PointSetFekete> > model_type;
-    boost::shared_ptr<model_type> heatTransfer( new model_type("heat-transfer") );
-    heatTransfer->init();
-    heatTransfer->printAndSaveInfo();
+    boost::shared_ptr<model_type> heat( new model_type("heat") );
+    heat->init();
+    heat->printAndSaveInfo();
 
-    if ( heatTransfer->isStationary() )
+    if ( heat->isStationary() )
     {
-        heatTransfer->solve();
-        heatTransfer->exportResults();
+        heat->solve();
+        heat->exportResults();
     }
     else
     {
-        for ( ; !heatTransfer->timeStepBase()->isFinished(); heatTransfer->updateTimeStep() )
+        for ( ; !heat->timeStepBase()->isFinished(); heat->updateTimeStep() )
         {
-            if (heatTransfer->worldComm().isMasterRank())
+            if (heat->worldComm().isMasterRank())
             {
                 std::cout << "============================================================\n";
-                std::cout << "time simulation: " << heatTransfer->time() << "s \n";
+                std::cout << "time simulation: " << heat->time() << "s \n";
                 std::cout << "============================================================\n";
             }
 
-            heatTransfer->solve();
-            heatTransfer->exportResults();
+            heat->solve();
+            heat->exportResults();
         }
     }
 
