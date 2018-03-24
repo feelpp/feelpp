f3f4e5db74b5934bf1e41b7e6262479b25637104
jbwahl <wahl.jb@gmail.com>
2018-03-24 17:14:16 +0100

    fix function databaseIndexFromContainerId @vincentchabannes

--- feel/feelalg/datamap.hpp
+++ feel/feelalg/datamap.hpp
@@ -548,8 +548,9 @@ public:
     /**
      * \return the mapping index which contains this global process id in container view
      */
-    int databaseIndexFromContainerId( size_type gpdof ) const
+    size_type databaseIndexFromContainerId( size_type gpdof ) const
         {
+#if 0
             size_type currentStartId = 0;
             for ( int tag=0;tag<this->numberOfDofIdToContainerId();++tag )
             {
@@ -559,6 +560,14 @@ public:
                 currentStartId+=nGpDof;
             }
             return 0;
+#endif
+            for ( int tag=0;tag<this->numberOfDofIdToContainerId();++tag )
+            {
+                auto it = std::find( M_dofIdToContainerId[tag].begin(), M_dofIdToContainerId[tag].end(), gpdof );
+                if ( it !=M_dofIdToContainerId[tag].end() )
+                    return tag;
+            }
+            return invalid_size_type_value;
         }
     /**
      * initialize the number of dofIdToContainerId mapping
@@ -593,7 +602,7 @@ public:
 
     size_type containerIdToDofId( int tag, size_type gpdof ) const
     {
-        size_type dof_id = -1;
+        size_type dof_id = invalid_size_type_value;
         auto it = std::find( M_dofIdToContainerId[tag].begin(), M_dofIdToContainerId[tag].end(), gpdof );
         if ( it !=M_dofIdToContainerId[tag].end() )
             dof_id = std::distance( M_dofIdToContainerId[tag].begin(), it );
