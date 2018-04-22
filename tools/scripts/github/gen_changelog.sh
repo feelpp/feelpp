1d17d9df3c14f4e0ea7a520a6e1eb410572c714c
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-04-22 08:49:20 +0200

    add release [ci skip]

--- tools/scripts/github/gen_changelog.sh
+++ tools/scripts/github/gen_changelog.sh
@@ -7,6 +7,7 @@ echo "computing ChangeLog since ${LATEST_VERSION}..."
 github_changelog_generator -p feelpp -t $TOKEN --release-branch develop \
   --since-tag $LATEST_VERSION \
   --no-issues-wo-labels \
+  --future-release v0.104.0 \
   --enhancement-labels type:feature,type:optimisation,type:question,type:refactoring,type:cleanup,type:update-3rdparty\
   --bug-labels type:bug,type:ftbs,type:fte
 
