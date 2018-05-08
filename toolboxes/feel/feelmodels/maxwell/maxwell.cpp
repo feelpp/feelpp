853670e999159cc27cbf5d78efdb3f999b0f9aa3
romainhild <hild.romain@gmail.com>
2018-05-08 17:52:42 +0200

    up #1155
    - init properties
    - check if directory is empty before creating it

--- toolboxes/feel/feelmodels/maxwell/maxwell.cpp
+++ toolboxes/feel/feelmodels/maxwell/maxwell.cpp
@@ -45,6 +45,7 @@ MAXWELL_CLASS_TEMPLATE_TYPE::Maxwell( std::string const& prefix,
                                       ModelBaseRepository const& modelRep )
     :
     super_type( prefix, worldComm, subPrefix, modelRep ),
+    M_maxwellProperties( new maxwellproperties_type( prefix ) ),
     M_epsilon( doption(_name="regularization-epsilon", _prefix=prefix) )
 {
     this->log("Maxwell","constructor", "start" );
