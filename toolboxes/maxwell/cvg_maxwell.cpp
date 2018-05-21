63483bd01c5e6d4ab20bff250545d441132ded6a
romainhild <hild.romain@gmail.com>
2018-05-21 18:40:22 +0200

    add convergence tests for maxwell toolox #1155

---
+++ toolboxes/maxwell/cvg_maxwell.cpp
@@ -0,0 +1,170 @@
+#include <feel/feelmodels/maxwell/maxwell.hpp>
+#include <feel/feelmodels/modelcore/options.hpp>
+#include <feel/feelfilters/loadmesh.hpp>
+
+using namespace Feel;
+
+void printCvg( std::ostream& out, std::vector<double> h, std::vector<std::vector<double>> err )
+{
+    auto errA = err[0];
+    auto errRelA = err[1];
+    auto errB = err[2];
+    auto errRelB = err[3];
+
+    boost::format fmter("%1% %|14t|");
+    boost::format fmter_endl("%1%\n");
+
+    out << fmter % "h";
+    out << fmter % "errA_L2";
+    out << fmter % "errA_Curl";
+    out << fmter % "errRelA_L2";
+    out << fmter % "errRelA_Curl";
+    out << fmter % "errB_L2";
+    out << fmter % "errB_H1";
+    out << fmter % "errRelB_L2";
+    out << fmter_endl % "errRelB_H1";
+
+    int maxiter = h.size();
+    for( int i = 0; i < maxiter; ++i )
+    {
+        out << fmter % h[i];
+        out << fmter % errA[2*i];
+        out << fmter % errA[2*i+1];
+        out << fmter % errRelA[2*i];
+        out << fmter % errRelA[2*i+1];
+        out << fmter % errB[2*i];
+        out << fmter % errB[2*i+1];
+        out << fmter % errRelB[2*i];
+        out << fmter_endl % errRelB[2*i+1];
+    }
+}
+
+// template <int OrderT>
+void
+runApplicationMaxwell()
+{
+    using model_type = FeelModels::Maxwell< Simplex<FEELPP_DIM> >;
+    using mesh_type = typename model_type::mesh_type;
+    using exporter_type = Exporter<mesh_type>;
+    using exporter_ptrtype = boost::shared_ptr<exporter_type>;
+
+    boost::shared_ptr<model_type> maxwell( new model_type("maxwell", false) );
+
+    int maxiter = ioption("cvg.nb-iter");
+    double factor = doption("cvg.factor");
+    double size = doption("gmsh.hsize");
+
+    exporter_ptrtype e( exporter_type::New("convergence") );
+
+    std::vector<double> h(maxiter);
+    std::vector<double> normA(2*maxiter), errA(2*maxiter), errRelA(2*maxiter);
+    std::vector<double> normB(2*maxiter), errB(2*maxiter), errRelB(2*maxiter);
+
+    for( int i = 0; i < maxiter; ++i )
+    {
+        h[i] = size;
+        auto mesh = loadMesh( _mesh=new mesh_type, _h=size );
+        maxwell->setMesh(mesh);
+        maxwell->init();
+        maxwell->printAndSaveInfo();
+        maxwell->solve();
+
+        auto functions = maxwell->modelProperties().functions();
+        auto params = maxwell->modelProperties().parameters().toParameterValues();
+        functions.setParameterValues( params );
+
+        auto e = exporter(_mesh=mesh, _name="convergence",_geo="static");
+        // e->step(i)->setMesh( mesh, EXPORTER_GEOMETRY_CHANGE );
+        // e->step(i)->setMesh(mesh);
+
+        auto A_h = maxwell->fieldMagneticPotential();
+        auto AExpr = expr<FEELPP_DIM,1>(functions["A_exact"].expressionString() );
+        AExpr.setParameterValues( params );
+        auto Ah = maxwell->spaceMagneticPotential();
+        auto A_ex = Ah->element(AExpr);
+        normA[2*i] = normL2( Ah->template rangeElements<0>(),
+                             idv(A_h) );
+        errA[2*i] = normL2( Ah->template rangeElements<0>(),
+                            idv(A_h)-idv(A_ex) );
+        errRelA[2*i] = errA[2*i]/normA[2*i];
+        normA[2*i+1] = normL2( Ah->template rangeElements<0>(),
+                               _expr=idv(A_h) + curlv(A_h) );
+        errA[2*i+1] = normL2( Ah->template rangeElements<0>(),
+                              _expr=idv(A_h)-idv(A_ex) + curlv(A_h)-curlv(A_ex) );
+        errRelA[2*i+1] = errA[2*i+1]/normA[2*i+1];
+        e->step(i)->add("A_h", "A_h", A_h);
+        e->step(i)->add("A_ex", "A_ex", A_ex);
+
+        auto B_h = maxwell->fieldMagneticField();
+#if FEELPP_DIM==3
+        auto BExpr = expr<FEELPP_DIM,1>(functions["B_exact"].expressionString() );
+#else
+        auto BExpr = expr(functions["B_exact"].expressionString() );
+#endif
+        BExpr.setParameterValues( params );
+        auto Bh = maxwell->spaceMagneticField();
+        auto B_ex = Bh->element(BExpr);
+        // normB[2*i] = normL2( Bh->template rangeElements<0>(),
+        //                      idv(B_h) );
+        // errB[2*i] = normL2( Bh->template rangeElements<0>(),
+        //                     idv(B_h)-idv(B_ex) );
+        // errRelB[2*i] = errB[2*i]/normB[2*i];
+        // normB[2*i+1] = normH1( Bh->template rangeElements<0>(),
+        //                        _expr=idv(B_h), _grad_expr=gradv(B_h) );
+        // errB[2*i+1] = normH1( Bh->template rangeElements<0>(),
+        //                       _expr=idv(B_h)-idv(B_ex), _grad_expr=gradv(B_h)-gradv(B_ex) );
+        // errRelB[2*i+1] = errB[2*i+1]/normB[2*i+1];
+        // e->step(i)->add("B_h", "B_h", B_h);
+        // e->step(i)->add("B_ex", "N_ex", B_ex);
+
+        e->save();
+        size /= factor;
+    }
+
+    std::vector<std::vector<double>> err = {errA, errRelA, errB, errRelB};
+    Feel::cout << std::endl;
+    if( Environment::isMasterRank() )
+    {
+        printCvg( std::cout, h, err );
+        std::ofstream file ( soption("cvg.filename") );
+        if( file )
+        {
+            printCvg( file, h, err );
+            file.close();
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    po::options_description maxwelloptions( "application maxwell options" );
+    maxwelloptions.add( toolboxes_options("maxwell") );
+    maxwelloptions.add_options()
+        ("fe-approximation", Feel::po::value<std::string>()->default_value( "P1" ), "fe-approximation : P1,P2,P3 ")
+        ("cvg.nb-iter", po::value<int>()->default_value(1), "number of convergence iteration")
+        ("cvg.factor", po::value<double>()->default_value(2), "factor of mesh size by iteration")
+        ("cvg.filename", po::value<std::string>()->default_value("cvg.dat"), "filename to store convergence results")
+        ;
+
+	Environment env( _argc=argc, _argv=argv,
+                     _desc=maxwelloptions,
+                     _about=about(_name="cvg_maxwell",
+                                  _author="Feel++ Consortium",
+                                  _email="feelpp-devel@feelpp.org"));
+
+    std::string feapprox = soption(_name="fe-approximation");
+    runApplicationMaxwell();
+    // if ( feapprox == "P1" )
+    //     runApplicationMaxwell<1>();
+// #if FEELPP_INSTANTIATION_ORDER_MAX >= 2
+//     else if ( feapprox == "P2" )
+//         runApplicationMaxwell<2>();
+// #endif
+// #if FEELPP_INSTANTIATION_ORDER_MAX >= 3
+//     else if ( feapprox == "P3" )
+//         runApplicationMaxwell<3>();
+// #endif
+//     else
+//         CHECK( false ) << "invalid feapprox " << feapprox;
+
+    return 0;
+}
