01f6578000d814530449522f40fe6c0d5820f087
Vincent Chabannes <vincent.chabannes@feelpp.org>
2018-05-22 14:26:34 +0200

    up alemesh : start work on Winslow

---
+++ toolboxes/feel/feelmodels/modelmesh/winslow_inst3d.cpp
@@ -0,0 +1,12 @@
+
+#include <feel/feelmodels/modelmesh/winslow.cpp>
+
+namespace Feel
+{
+namespace FeelModels
+{
+
+template class Winslow< Mesh<Simplex<3,1> >, 2 >;
+
+} // namespace FeelModels
+} // namespace Feel
