d1519f5c9190aaa7ec74b6d01bbdb22dfee4dbba
Vincent Chabannes <vincent.chabannes@feelpp.org>
2018-06-05 22:21:40 +0200

    fusion feelpp_toolbox_heatfluid_2d/3d -> feelpp_toolbox_heatfluid

--- toolboxes/heatfluid/main_heatfluid.cpp
+++ toolboxes/heatfluid/main_heatfluid.cpp
@@ -2,15 +2,15 @@
 
 #include <feel/feelmodels/heatfluid/heatfluid.hpp>
 
-template <int OrderT,int OrderV, int OrderP>
+template <int nDim, int OrderT, int OrderV, int OrderP>
 void
 runApplicationHeatFluid()
 {
     using namespace Feel;
 
-    typedef FeelModels::Heat< Simplex<FEELPP_DIM,1>,
-                                      Lagrange<OrderT, Scalar,Continuous,PointSetFekete> > model_heat_type;
-    typedef FeelModels::FluidMechanics< Simplex<FEELPP_DIM,1>,
+    typedef FeelModels::Heat< Simplex<nDim,1>,
+                              Lagrange<OrderT, Scalar,Continuous,PointSetFekete> > model_heat_type;
+    typedef FeelModels::FluidMechanics< Simplex<nDim,1>,
                                         Lagrange<OrderV, Vectorial,Continuous,PointSetFekete>,
                                         Lagrange<OrderP, Scalar,Continuous,PointSetFekete> > model_fluid_type;
     typedef FeelModels::HeatFluid< model_heat_type,model_fluid_type> model_type;
@@ -47,20 +47,33 @@ main( int argc, char** argv )
     po::options_description heatfluidoptions( "application heat-fluid options" );
     heatfluidoptions.add( toolboxes_options("heat-fluid") );
     heatfluidoptions.add_options()
-        ("fe-approximation", Feel::po::value<std::string>()->default_value( "P1-P2P1" ), "fe-approximation")
+        ("case.dimension", Feel::po::value<int>()->default_value( 3 ), "dimension")
+        ("case.discretization", Feel::po::value<std::string>()->default_value( "P1-P2P1" ), "discretization : P1-P2P1")
      ;
 
 	Environment env( _argc=argc, _argv=argv,
                      _desc=heatfluidoptions,
-                     _about=about(_name="application_heatfluid",
+                     _about=about(_name="feelpp_toolbox_heatfluid",
                                   _author="Feel++ Consortium",
                                   _email="feelpp-devel@feelpp.org"));
 
-    std::string feapprox = soption(_name="fe-approximation");
-    if ( feapprox == "P1-P2P1" )
-        runApplicationHeatFluid<1,2,1>();
-    else
-        CHECK( false ) << "invalid feapprox " << feapprox;
+    int dimension = ioption(_name="case.dimension");
+    std::string discretization = soption(_name="case.discretization");
+
+    auto dimt = hana::make_tuple(hana::int_c<2>,hana::int_c<3>);
+
+    auto discretizationt = hana::make_tuple( hana::make_tuple("P1-P2P1", hana::make_tuple( hana::int_c<1>,hana::int_c<2>,hana::int_c<1>) ) );
+
+    hana::for_each( hana::cartesian_product(hana::make_tuple(dimt,discretizationt)), [&discretization,&dimension]( auto const& d )
+                    {
+                        constexpr int _dim = std::decay_t<decltype(hana::at_c<0>(d))>::value;
+                        std::string const& _discretization = hana::at_c<0>( hana::at_c<1>(d) );
+                        constexpr int _torder = std::decay_t<decltype(hana::at_c<0>(hana::at_c<1>( hana::at_c<1>(d)) ))>::value;
+                        constexpr int _uorder = std::decay_t<decltype(hana::at_c<1>(hana::at_c<1>( hana::at_c<1>(d)) ))>::value;
+                        constexpr int _porder = std::decay_t<decltype(hana::at_c<2>(hana::at_c<1>( hana::at_c<1>(d)) ))>::value;
+                        if ( dimension == _dim && discretization == _discretization )
+                            runApplicationHeatFluid<_dim,_torder,_uorder,_porder>();
+                    } );
 
     return 0;
 }
