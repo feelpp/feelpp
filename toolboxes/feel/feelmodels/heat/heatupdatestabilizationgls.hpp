800f81296184b087a0f6ccc851272b67a1a69404
Vincent Chabannes <vincent.chabannes@feelpp.org>
2018-06-25 09:12:18 +0200

    add hpp guard

--- toolboxes/feel/feelmodels/heat/heatupdatestabilizationgls.hpp
+++ toolboxes/feel/feelmodels/heat/heatupdatestabilizationgls.hpp
@@ -1,5 +1,8 @@
 /* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=cpp:et:sw=4:ts=4:sts=4*/
 
+#ifndef FEELPP_TOOLBOXES_HEAT_UPDATESTABILIZATIONGLS_HPP
+#define FEELPP_TOOLBOXES_HEAT_UPDATESTABILIZATIONGLS_HPP 1
+
 #include <feel/feelmodels/modelvf/stabilizationglsparameter.hpp>
 #include <feel/feelmodels/modelcore/stabilizationglsparameter.hpp>
 #include <feel/feeldiscr/pchv.hpp>
@@ -295,3 +298,5 @@ Heat<ConvexType,BasisTemperatureType>::updateResidualStabilizationGLS( Expr<RhoC
 
 } // namespace Feel
 } // namespace FeelModels
+
+#endif
