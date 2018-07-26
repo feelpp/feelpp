ade78a4cc5e5ce98cd805b1de7eec06f7b37e06f
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-07-26 09:01:25 +0200

    script to use std:: instead of boost::, see #1202
     - shared_ptr [done]

---
+++ tools/scripts/stdcpp.sh
@@ -0,0 +1,5 @@
+find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::shared_ptr/std::shared_ptr/g' file
+find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::make_shared/std::make_shared/g' file
+find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::enable_shared/std::enable_shared/g' file
+find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::weak/std::weak/g' file
+find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::dynamic_pointer/std::dynamic_pointer/g' file
