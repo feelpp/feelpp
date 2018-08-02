5dcd0cfecd7ea671cb487fe2004510b3aeb60e15
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-08-02 14:48:44 +0200

    exporter in python up and running #930

--- pyfeelpp/pyfeelpp/test_exporter.py
+++ pyfeelpp/pyfeelpp/test_exporter.py
@@ -11,8 +11,10 @@ m2d=mesh.Mesh_2()
 m2d = mesh.load(m2d,"triangle.geo",0.1)
 
 Xh=discr.Pch_2D_P1(mesh=m2d)
-u=Xh.element()
-
+P0h = discr.Pdh_2D_P0(mesh=m2d)
+u=Xh.elementFromExpr("{sin(2*pi*x)*cos(pi*y)}:x:y")
 e = exporter.exporter(mesh=m2d)
-e.add()
+e.addScalar("un", 1.)
+e.addP1c("u",u);
+e.addP0d("pid",discr.pid( P0h ));
 e.save()
