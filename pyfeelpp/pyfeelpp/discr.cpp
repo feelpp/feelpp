c0e128107293837f8f6c433b4bf7e218fb0f028f
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-08-02 14:45:18 +0200

    more generic space data structure wrappers #930
    - add Pch
    - add Pdh
    - add pid P0d function
    
    can add as many instantiations in the future as we want

--- pyfeelpp/pyfeelpp/discr.cpp
+++ pyfeelpp/pyfeelpp/discr.cpp
@@ -24,6 +24,9 @@
 #include <pybind11/pybind11.h>
 
 #include <feel/feeldiscr/pch.hpp>
+#include <feel/feeldiscr/pchv.hpp>
+#include <feel/feeldiscr/pdh.hpp>
+#include <feel/feeldiscr/pdhv.hpp>
 #include <mpi4py/mpi4py.h>
 
 namespace py = pybind11;
@@ -36,28 +39,28 @@ public:
     using base = typename Pch_type<MeshT,Order>::element_type;
     MyElement() : base() {}
 };
-template<typename MeshT, int Order = 1>
+template<typename SpaceT>
 void defDiscr(py::module &m)
 {
     using namespace Feel;
     
-    using pch_t = Pch_type<MeshT,Order>;
-    using pch_ptr_t = Pch_ptrtype<MeshT,Order>;
-    using mesh_support_vector_t = typename pch_t::mesh_support_vector_type;
-    using periodicity_t = typename pch_t::periodicity_type;
-    using mesh_ptr_t = std::shared_ptr<MeshT>;
-    using element_t = typename pch_t::element_type;
+    using space_t = SpaceT;
+    using space_ptr_t = std::shared_ptr<space_t>;
+    using mesh_support_vector_t = typename space_t::mesh_support_vector_type;
+    using periodicity_t = typename space_t::periodicity_type;
+    using mesh_t = typename space_t::mesh_type;
+    using mesh_ptr_t = std::shared_ptr<mesh_t>;
+    using element_t = typename space_t::element_type;
     std::string pyclass_name;
+    int Order = space_t::basis_0_type::nOrder;
+    std::string suffix = std::to_string(mesh_t::nDim)+std::string("D_P") + std::to_string(Order);
+    std::cout << "suffix discr: " << suffix << std::endl;
 
-    std::string suffix = std::to_string(MeshT::nDim)+std::string("D_P") + std::to_string(Order);
-    pyclass_name = std::string("mesh_support_vector_")+suffix;
-    py::class_<mesh_support_vector_t>(m,pyclass_name.c_str()).def(py::init<>());
-    pyclass_name = std::string("periodicity_")+suffix;
-    py::class_<periodicity_t>(m,pyclass_name.c_str()).def(py::init<>());
-    
-    
-    pyclass_name = std::string("Pch_") + suffix;
-    py::class_<pch_t,std::shared_ptr<pch_t>>(m,pyclass_name.c_str())
+    if ( space_t::is_continuous )
+        pyclass_name = std::string("Pch_") + suffix;
+    else
+        pyclass_name = std::string("Pdh_") + suffix;
+    py::class_<space_t,std::shared_ptr<space_t>>(m,pyclass_name.c_str())
         .def(py::init<mesh_ptr_t const&,mesh_support_vector_t const&, size_type, periodicity_t, std::vector<WorldComm> const&, std::vector<bool>>(),
              py::arg("mesh"),
              py::arg("support")=mesh_support_vector_t(),
@@ -65,24 +68,39 @@ void defDiscr(py::module &m)
              py::arg("periodicity")=periodicity_t(),
              py::arg("worldsComm") = Environment::worldsComm(1),
              py::arg("extendedDofTable") = std::vector<bool>(1,false) )
-        .def("nDof",static_cast<size_type(pch_t::*)() const>(&pch_t::nDof), "get the number of degrees of freedom over the whole domain")
-        .def("nLocalDof",static_cast<size_type(pch_t::*)() const>(&pch_t::nLocalDof), "get the number of degrees of freedom over the current subdomain")
-        .def("nLocalDofWithGhost",static_cast<size_type(pch_t::*)() const>(&pch_t::nLocalDofWithGhost), "get the number of degrees of freedom over the current subdomain withthe ghost")
-        .def("nLocalDofWithoutGhost",static_cast<size_type(pch_t::*)() const>(&pch_t::nLocalDofWithoutGhost), "get the number of degrees of freedom over the current subdomain without the ghost")
-        .def("basisName",static_cast<std::string (pch_t::*)() const>(&pch_t::basisName), "get the basis function name")
+        .def("nDof",static_cast<size_type(space_t::*)() const>(&space_t::nDof), "get the number of degrees of freedom over the whole domain")
+        .def("nLocalDof",static_cast<size_type(space_t::*)() const>(&space_t::nLocalDof), "get the number of degrees of freedom over the current subdomain")
+        .def("nLocalDofWithGhost",static_cast<size_type(space_t::*)() const>(&space_t::nLocalDofWithGhost), "get the number of degrees of freedom over the current subdomain withthe ghost")
+        .def("nLocalDofWithoutGhost",static_cast<size_type(space_t::*)() const>(&space_t::nLocalDofWithoutGhost), "get the number of degrees of freedom over the current subdomain without the ghost")
+        .def("basisName",static_cast<std::string (space_t::*)() const>(&space_t::basisName), "get the basis function name")
 
-        .def("mesh",static_cast<mesh_ptr_t const&(pch_t::*)() const>(&pch_t::mesh), "get the mesh of the function space")
-        .def("element",static_cast<element_t (pch_t::*)(std::string const&, std::string const&)>(&pch_t::element), "get an element of the function space", py::arg("name")="u", py::arg("desc")="u")
+        .def("mesh",static_cast<mesh_ptr_t const&(space_t::*)() const>(&space_t::mesh), "get the mesh of the function space")
+        .def("element",static_cast<element_t (space_t::*)(std::string const&, std::string const&)>(&space_t::element), "get an element of the function space", py::arg("name")="u", py::arg("desc")="u")
+        .def("elementFromExpr",static_cast<element_t (space_t::*)(std::string const&, std::string const&, std::string const& )>(&space_t::elementFromExpr), "get an element of the function space interpolating the expression", py::arg("expr"),py::arg("name")="u", py::arg("desc")="u")
         ;
 
     // Element
-    pyclass_name = std::string("Element_Pch_") + suffix;
+    if ( space_t::is_continuous )
+        pyclass_name = std::string("Element_Pch_") + suffix;
+    else
+        pyclass_name = std::string("Element_Pdh_") + suffix;
     py::class_<element_t,std::shared_ptr<element_t>>(m,pyclass_name.c_str())
         .def(py::init<>())
-        .def(py::init<std::shared_ptr<pch_t> const&, std::string const&, std::string const&, size_type, ComponentType>(),py::arg("space"), py::arg("name"), py::arg("desc"), py::arg("start")=0, py::arg("ct")= ComponentType::NO_COMPONENT)
-        .def("functionSpace",static_cast<pch_ptr_t const&(element_t::*)() const>(&element_t::functionSpace), "Get funtion space from element")
+        .def(py::init<std::shared_ptr<space_t> const&, std::string const&, std::string const&, size_type, ComponentType>(),py::arg("space"), py::arg("name"), py::arg("desc"), py::arg("start")=0, py::arg("ct")= ComponentType::NO_COMPONENT)
+        .def("functionSpace",static_cast<space_ptr_t const&(element_t::*)() const>(&element_t::functionSpace), "Get funtion space from element")
         .def("size", static_cast< size_type (element_t::*)() const>(&element_t::size), "Get size of element")
         ;
+
+    
+}
+template<typename space_t>
+void defDiscrDiscontinuous(py::module &m )
+{
+    if ( (space_t::is_continuous == false) && ( space_t::basis_0_type::nOrder == 0 ) )
+    {
+        m.def( "pid", &regionProcess<space_t>, "get an piecewise constant function storing the process ids", py::arg("space") );
+        //m.def( "marker", &regionMarker<space_t>, "get an piecewise constant function storing the marker of the mesh", py::arg("space") );
+    }
 }
     
 PYBIND11_MODULE(discr, m )
@@ -93,9 +111,21 @@ PYBIND11_MODULE(discr, m )
 
     std::string pyclass_name = std::string("ComponentType");
     py::class_<ComponentType>(m,pyclass_name.c_str()).def(py::init<>());
+    pyclass_name = std::string("Periodic");
+    py::class_<Periodic<double>>(m,pyclass_name.c_str()).def(py::init<>());
+    pyclass_name = std::string("PeriodicityPeriodic");
+    py::class_<Periodicity<Periodic<double>>>(m,pyclass_name.c_str()).def(py::init<>());
+    pyclass_name = std::string("NoPeriodicity");
+    py::class_<NoPeriodicity>(m,pyclass_name.c_str()).def(py::init<>());
+    pyclass_name = std::string("PeriodicityNoPeriodicity");
+    py::class_<Periodicity<NoPeriodicity>>(m,pyclass_name.c_str()).def(py::init<>());
+
     
     //defDiscr<Mesh<Simplex<1>>,1>( m );
-    defDiscr<Mesh<Simplex<2>>,1>( m );
+    defDiscr<Pch_type<Mesh<Simplex<2>>,1>>( m );
+    defDiscr<Pdh_type<Mesh<Simplex<2>>,0>>( m );
+    defDiscrDiscontinuous<Pdh_type<Mesh<Simplex<2>>,0>>( m );
+    defDiscr<Pdh_type<Mesh<Simplex<2>>,1>>( m );
     //defDiscr<Mesh<Simplex<2>>,2>( m );
 }
 
