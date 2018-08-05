48c8235c08b9b6448e8f96adf25acca9d4151ebc
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-08-05 13:20:46 +0200

    add const version #1202

--- tools/scripts/stdcpp.sh
+++ tools/scripts/stdcpp.sh
@@ -4,3 +4,5 @@ find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::enable_shared/std::e
 find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::weak/std::weak/g' file
 find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::dynamic_pointer/std::dynamic_pointer/g' file
 find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::static_pointer/std::static_pointer/g' file
+find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::const_pointer/std::const_pointer/g' file
+
