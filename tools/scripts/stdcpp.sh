77a19a9f924b8189befacf2362b53f8c6a572e5b
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-08-05 09:12:38 +0200

    add std::static_pointer_cast #1202

--- tools/scripts/stdcpp.sh
+++ tools/scripts/stdcpp.sh
@@ -3,3 +3,4 @@ find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::make_shared/std::mak
 find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::enable_shared/std::enable_shared/g' file
 find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::weak/std::weak/g' file
 find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::dynamic_pointer/std::dynamic_pointer/g' file
+find . -name "*.[ch]pp" | xargs -Ifile sed -i.bak 's/boost::static_pointer/std::static_pointer/g' file
