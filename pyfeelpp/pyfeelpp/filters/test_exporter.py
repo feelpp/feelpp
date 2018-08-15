aff3ff17087450313e8fef7ddfcb5089b28ed8d8
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-08-15 19:50:52 +0200

    up example #930
    2D and 3D
    
    /cc @vincentchabannes

--- pyfeelpp/pyfeelpp/filters/test_exporter.py
+++ pyfeelpp/pyfeelpp/filters/test_exporter.py
@@ -1,22 +1,38 @@
 import sys
-from pyfeelpp import *
+from pyfeelpp import core
+
 e=core.Environment(sys.argv)
-import pyfeelpp.discr as discr
-import pyfeelpp.filters as filters
-import pyfeelpp.vf as vf
 
+from pyfeelpp import mesh,discr,filters,vf
 
-m2d=mesh.Mesh_2()
-m2d = mesh.load(m2d,"triangle.geo",0.1)
+rd = core.RemoteData("github:{repo:feelpp,path:quickstart/laplacian/feelpp2d/feelpp2d.geo}",worldComm=core.Environment.worldCommPtr())
+if rd.canDownload():
+    d=e.downloadsRepository()
+    geo={'2':rd.download( d )[0]}
+    if e.isMasterRank():
+        print(geo)
 
-Xh=discr.Pch_2D_P1(mesh=m2d)
-P0h = discr.Pdh_2D_P0(mesh=m2d)
-#u=Xh.elementFromExpr("{sin(2*pi*x)*cos(pi*y)}:x:y")
-u=Xh.element()
-u.on(range=mesh.elements(m2d),expr=vf.expr("x*x:x"))
+rd = core.RemoteData("github:{repo:feelpp,path:quickstart/laplacian/feelpp3d/feelpp3d.geo}",worldComm=core.Environment.worldCommPtr())
+if rd.canDownload():
+    d=e.downloadsRepository()
+    geo['3']=rd.download( d )[0]
+    if e.isMasterRank():
+        print(geo)        
 
-e = filters.exporter(mesh=m2d)
-e.addScalar("un", 1.)
-e.addP1c("u",u);
-e.addP0d("pid",discr.pid( P0h ));
-e.save()
+def run( m, geo ):
+    m2d = mesh.load(m,geo,0.1)
+
+    Xh=discr.functionSpace( type="Pch", mesh=m2d, order=1 )
+    P0h = discr.functionSpace( type="Pdh", mesh=m2d, order=0 )
+    #u=Xh.elementFromExpr("{sin(2*pi*x)*cos(pi*y)}:x:y")
+    u=Xh.element()
+    u.on(range=mesh.elements(m2d),expr=vf.expr("x*x:x"))
+
+    e = filters.exporter(mesh=m2d,name="feelpp"+str(m.dimension())+"d")
+    e.addScalar("un", 1.)
+    e.addP1c("u",u);
+    e.addP0d("pid",discr.pid( P0h ));
+    e.save()
+
+run( mesh.mesh( dim=2 ), geo['2'] )
+run( mesh.mesh( dim=3, realdim=3 ), geo['3'] )    
