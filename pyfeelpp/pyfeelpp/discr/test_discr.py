98aa902df19b4f11006e76b93dd904d86aff7d65
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-08-15 19:49:37 +0200

    add function space factories + up example #930
    /cc @vincentchabannes

--- pyfeelpp/pyfeelpp/discr/test_discr.py
+++ pyfeelpp/pyfeelpp/discr/test_discr.py
@@ -1,35 +1,42 @@
-from pyfeelpp import *
-import sys,time
+from pyfeelpp import core
+import sys
+
 e=core.Environment(sys.argv)
 
-import pyfeelpp.discr as discr
-import pyfeelpp.vf as vf
+from pyfeelpp import mesh,discr,vf
+rd = core.RemoteData("github:{repo:feelpp,path:quickstart/laplacian/feelpp2d/feelpp2d.geo}",worldComm=core.Environment.worldCommPtr())
+if rd.canDownload():
+    d=e.downloadsRepository()
+    geo={'2':rd.download( d )[0]}
+    if e.isMasterRank():
+        print(geo)
+rd = core.RemoteData("github:{repo:feelpp,path:quickstart/laplacian/feelpp3d/feelpp3d.geo}",worldComm=core.Environment.worldCommPtr())
+if rd.canDownload():
+    d=e.downloadsRepository()
+    geo['3']=rd.download( d )[0]
+    if e.isMasterRank():
+        print(geo)
 
+def run( m, geo ):
+    m2d = mesh.load(m,geo,0.1)
+    Xh=discr.functionSpace(mesh=m2d)
 
+    if e.isMasterRank():
+        print("Xh basisname: ", Xh.basisName())
+        print("Xh nDof: ", Xh.nDof())
+        print("Xh nLocalDof: ", Xh.nLocalDof())
+        print("Xh nLocalDofWithGhost: ", Xh.nLocalDofWithGhost())
+        print("Xh nLocalDofWithoutGhost: ", Xh.nLocalDofWithoutGhost())
 
-print("pid:",e.worldComm().localRank() )
-m2d=mesh.Mesh_2()
-m2d = mesh.load(m2d,"triangle.geo",0.1)
+    m3=Xh.mesh()
 
-Xh=discr.Pch_2D_P1(mesh=m2d)
-Yh=discr.Pch_2D_P1(mesh=m2d)
+    assert m3==m2d
 
+    u=Xh.element()
+    u.on(range=mesh.elements(m2d),expr=vf.expr("x:x"))
 
-print("Xh basisname: ", Xh.basisName())
-print("Xh nDof: ", Xh.nDof())
-print("Xh nLocalDof: ", Xh.nLocalDof())
-print("Xh nLocalDofWithGhost: ", Xh.nLocalDofWithGhost())
-print("Xh nLocalDofWithoutGhost: ", Xh.nLocalDofWithoutGhost())
+    assert u.functionSpace() == Xh
+    assert u.size() == Xh.nDof()
 
-m3=Xh.mesh()
-
-assert m3==m2d
-
-u=Xh.element()
-u.on(range=mesh.elements(m2d),expr=vf.expr("x:x"))
-
-assert u.functionSpace() == Xh
-assert u.size() == Xh.nDof()
-assert u.functionSpace() != Yh
-
-#help(u)
+run( mesh.mesh(dim=2), geo['2'] )
+run( mesh.mesh(dim=3,realdim=3), geo['3'] )
