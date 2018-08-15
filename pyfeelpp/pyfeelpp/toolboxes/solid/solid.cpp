233240100e5122b4a3d37003fb011b76a14f3fc3
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-08-15 19:51:13 +0200

    fix python binding toolbox compilation #930

--- pyfeelpp/pyfeelpp/toolboxes/solid/solid.cpp
+++ pyfeelpp/pyfeelpp/toolboxes/solid/solid.cpp
@@ -50,10 +50,10 @@ void defSM(py::module &m)
     
     std::string pyclass_name = std::string("Solid_") + std::to_string(nDim) + std::string("DP") + std::to_string(OrderDisp);
     py::class_<sm_t,std::shared_ptr<sm_t>,ModelNumerical>(m,pyclass_name.c_str())
-        .def(py::init<std::string const&,bool,WorldComm const&,std::string const&, ModelBaseRepository const&>(),
+        .def(py::init<std::string const&,bool,worldcomm_ptr_t const&,std::string const&, ModelBaseRepository const&>(),
              py::arg("prefix"),
              py::arg("buildmesh")=true,
-             py::arg("worldComm")=Environment::worldComm(),
+             py::arg("worldComm")=Environment::worldCommPtr(),
              py::arg("subprefix")=std::string(""),
              py::arg("modelRep") = ModelBaseRepository(),
              "Initialize the solid mechanics toolbox"
@@ -61,7 +61,7 @@ void defSM(py::module &m)
         .def_static( "create", &sm_t::New,
                      py::arg("prefix"),
                      py::arg("buildmesh")=true,
-                     py::arg("worldComm")=Environment::worldComm(),
+                     py::arg("worldComm")=Environment::worldCommPtr(),
                      py::arg("subprefix")=std::string(""),
                      py::arg("modelRep") = ModelBaseRepository(),
                      "Initialize the solid mechanics toolbox"
