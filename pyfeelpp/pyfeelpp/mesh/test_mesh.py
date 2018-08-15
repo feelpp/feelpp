d6f62ea911431af58bb376e1648e8a447afc6e65
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-08-15 19:50:24 +0200

    add mesh factory + up example #930
    /cc @vincentchabannes

--- pyfeelpp/pyfeelpp/mesh/test_mesh.py
+++ pyfeelpp/pyfeelpp/mesh/test_mesh.py
@@ -1,25 +1,39 @@
-from pyfeelpp import *
-import sys,time
-
+from pyfeelpp import core
+import sys
 e=core.Environment(sys.argv)
-m1d=mesh.Mesh_1()
-m2d=mesh.Mesh_2()
-if e.isMasterRank():
-    print("mesh dim:", m1d.dimension())
-    print("mesh dim:", m2d.dimension())
+rd = core.RemoteData("github:{repo:feelpp,path:quickstart/laplacian/feelpp2d/feelpp2d.geo}",worldComm=core.Environment.worldCommPtr())
+if rd.canDownload():
+    d=e.downloadsRepository()
+    print("download mesh in ", d);
+    geo={'2':rd.download( d )[0]}
+    print(geo)
+rd = core.RemoteData("github:{repo:feelpp,path:quickstart/laplacian/feelpp3d/feelpp3d.geo}",worldComm=core.Environment.worldCommPtr())
+if rd.canDownload():
+    d=e.downloadsRepository()
+    print("download mesh in ", d);
+    geo['3']=rd.download( d )[0]
+    print(geo)
+from pyfeelpp import mesh
 
-m2d=mesh.load(m2d,"triangle.geo",2)
-help(m2d)
-if e.isMasterRank():
-    print("mesh 2D nelts:", m2d.numGlobalElements() )
-    print("mesh 2D nfaces:", m2d.numGlobalFaces() )
-    print("mesh 2D hmin:", m2d.hMin())
-    print("mesh 2D havg:", m2d.hAverage())
-    print("mesh 2D hmax:", m2d.hMax())
-    print("mesh 2D measure:", m2d.measure())
+def run( m, geofile ):
+    if e.isMasterRank():
+        print("mesh dim:", m.dimension())
     
+    m=mesh.load(m,geofile,0.1)
+    if e.isMasterRank():
+        print("mesh ",m.dimension(),"D nelts:", m.numGlobalElements() )
+        print("mesh ",m.dimension(),"D nfaces:", m.numGlobalFaces() )
+        print("mesh ",m.dimension(),"D hmin:", m.hMin())
+        print("mesh ",m.dimension(),"D havg:", m.hAverage())
+        print("mesh ",m.dimension(),"D hmax:", m.hMax())
+        print("mesh ",m.dimension(),"D measure:", m.measure())
     
-r = mesh.elements(m2d)
-print("mesh elts:", mesh.nelements(r,True))
-r = mesh.boundaryfaces(m2d)
-print("mesh boundary faces:", mesh.nfaces(r,True))
+    r = mesh.elements(m)
+    print("mesh elts:", mesh.nelements(r,True))
+    r = mesh.boundaryfaces(m)
+    print("mesh boundary faces:", mesh.nfaces(r,True))
+
+
+run( mesh.mesh(dim=2), geo['2'] )
+run( mesh.mesh(dim=3,realdim=3), geo['3'] )
+
