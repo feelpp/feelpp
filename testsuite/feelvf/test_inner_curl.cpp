2e57ccc0037fb0bb847c3105c79a959c1bc1c9fb
romainhild <hild.romain@gmail.com>
2018-10-04 12:59:46 +0200

    up testsuite inner_curl #1227
    fail to compile do to normL2(elements(mesh), curlv(u)-idv(v)) in 2D
    @vincentchabannes @prudhomm

--- testsuite/feelvf/test_inner_curl.cpp
+++ testsuite/feelvf/test_inner_curl.cpp
@@ -19,28 +19,41 @@ BOOST_AUTO_TEST_CASE( test_3d )
 {
     auto mesh = unitSphere();
 
-    auto Vh = Pchv<1>(mesh);
-    auto u = Vh->element();
-    auto v = Vh->element();
+    auto Vh = Pchv<2>(mesh);
+    auto f = expr<3,1>("{2*x^2+3*y^2+4*z^2,3*x^2+4*y^2+2*z^2,4*x^2+2*y^2+3*z^2}:x:y:z");
+    auto g = expr<3,1>("{4*y-4*z,8*z-8*x,6*x-6*y}:x:y:z");
+    auto u = Vh->element(f);
+    auto v = Vh->element(g);
 
+    // check inner keyword
     auto a = form2( _test=Vh, _trial=Vh);
     a = integrate( _range=elements( mesh ), _expr=inner(curlt(u),curl(v)));
 
-    BOOST_CHECK( true );
+    auto err = normL2(_range=elements(mesh), _expr=curlv(u)-idv(v));
+    BOOST_CHECK_SMALL( err, 1e-8 );
 }
 
 BOOST_AUTO_TEST_CASE( test_2d )
 {
     auto mesh = unitCircle();
 
-    auto Vh = Pchv<1>(mesh);
-    auto u = Vh->element();
-    auto v = Vh->element();
+    auto Vh = Pchv<2>(mesh);
+    auto Wh = Pch<1>(mesh);
+    auto f = expr<2,1>("{2*x^2+3*y^2,3*x^2+2*y^2}:x:y");
+    auto g = expr("6*x-6*y:x:y");
+    auto u = Vh->element(f);
+    auto v = Wh->element(g);
 
+    // check inner keyword
     auto a = form2( _test=Vh, _trial=Vh);
-    a = integrate( _range=elements( mesh ), _expr=inner(curlxt(u),curlx(v)) );
+    a = integrate( _range=elements( mesh ), _expr=inner(curlt(u),curl(u)) );
 
-    BOOST_CHECK( true );
+    auto nu = normL2(elements(mesh), curlv(u));
+    auto nv = normL2(elements(mesh), idv(v));
+    BOOST_CHECK_CLOSE( nu, nv, 1e-4 );
+
+    auto err = normL2(_range=elements(mesh), _expr=curlv(u)-idv(v));
+    BOOST_CHECK_SMALL( err, 1e-8 );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
