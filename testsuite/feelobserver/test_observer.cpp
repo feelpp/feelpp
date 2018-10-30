713d263ff6b70675f5c45f689b9d655e4c6b80b7
Vincent Chabannes <vincent.chabannes@feelpp.org>
2018-10-30 16:23:07 +0100

    fix tests compilation

--- testsuite/feelobserver/test_observer.cpp
+++ testsuite/feelobserver/test_observer.cpp
@@ -21,7 +21,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #define BOOST_TEST_MODULE test_observers
-#include <testsuite.hpp>
+#include <feel/feelcore/testsuite.hpp>
 
 #include <iostream>
 #include <cassert>
