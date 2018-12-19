c7f53cfcb2a83bcb655461cf0743c9a45a34d318
Philippe <rickaphilippe@gmail.com>
2018-12-19 14:11:45 +0100

    deleted useless parts

--- feelpp/pyfeelpp/kalman/ukf-testcase-tracking.py
+++ feelpp/pyfeelpp/kalman/ukf-testcase-tracking.py
@@ -25,20 +25,8 @@ def run():
 
     plt.plot(Time,F.forecast[0,:],label="Predicted trajectory")
     plt.plot(Time,Signal,label="Signal")
-#    plt.plot(Time[0:-2],np.sin(theta*Time[0:-2])+2,label="True trajectory")
     leg = plt.legend(loc='upper right')
     leg.get_frame().set_alpha(0.5)
     plt.show()
-
-    from matplotlib2tikz import save as tikz_save
-    
-#    tikz_save("ukftc1.tex")
-
-#    plt.plot(np.log(Time[0:-2]),F.X[1,0:-2],label="R1")
-#    plt.plot(np.log(1+Time[0:-2]),F.X[2,0:-2],label="R2")
-#    plt.plot(np.log(1+Time[0:-2]),F.X[3,0:-2],label="C")
-#    leg = plt.legend(loc='upper right')
-#    leg.get_frame().set_alpha(1)
-#    plt.show()
 
 run()
