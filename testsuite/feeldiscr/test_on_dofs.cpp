4b90ec82fc499fb1e9368081773186c42f50128e
Vincent Chabannes <vincent.chabannes@feelpp.org>
2019-02-04 11:17:32 +0100

    up test for #1268 with poly order 4 [ci skip]

--- testsuite/feeldiscr/test_on_dofs.cpp
+++ testsuite/feeldiscr/test_on_dofs.cpp
@@ -184,7 +184,7 @@ BOOST_AUTO_TEST_SUITE( test_on_dofs )
 BOOST_AUTO_TEST_CASE( assign_3d )
 {
     runTestAssign<3>();
-    runTestAssignMeshRelated<3>();
+    runTestAssignMeshRelated<3,4>();
 }
 
 BOOST_AUTO_TEST_CASE( elimination_3d )
