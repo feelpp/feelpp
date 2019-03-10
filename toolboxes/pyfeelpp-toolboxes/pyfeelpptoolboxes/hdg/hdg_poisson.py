a3f410007404ead47bac256ec3fa2e99adb6f2c7
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2019-03-10 13:19:34 +0100

    interface changes and add OperatorInterpolation support #1279

--- toolboxes/pyfeelpp-toolboxes/pyfeelpptoolboxes/hdg/hdg_poisson.py
+++ toolboxes/pyfeelpp-toolboxes/pyfeelpptoolboxes/hdg/hdg_poisson.py
@@ -1,16 +1,11 @@
-from pyfeelpp import core
 import sys
-
-import pyfeelpptoolboxes.modelcore as modelcore
-
-from pyfeelpptoolboxes.hdg import hdg_poisson_options
-e=core.Environment(sys.argv,opts=hdg_poisson_options())
-
-#from pyfeelpp import discr,ts,filters
 from pyfeelpptoolboxes.hdg import *
 
-f=hdgpoisson(dim=2,order=1,worldComm=e.worldCommPtr())
+e=core.Environment(sys.argv,opts=hdg_poisson_options())
+
+
+f=hdgpoisson(dim=2,order=1)
 f.init()
 f.printAndSaveInfo()
 f.solve()
-#f.exportResults()
+f.exportResults()
