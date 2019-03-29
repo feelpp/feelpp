1cb745265ff8f5a304ba01f2ccdbc92210d1dcc8
Thibaut Metivet <thibaut.metivet@gmail.com>
2019-03-29 16:52:47 +0100

    Fix typo (phiElt)

--- toolboxes/feel/feelmodels/levelset/levelsetbase.cpp
+++ toolboxes/feel/feelmodels/levelset/levelsetbase.cpp
@@ -1018,7 +1018,7 @@ LEVELSETBASE_CLASS_TEMPLATE_TYPE::updateCurvature()
         {
             this->log("LevelSetBase", "updateCurvature", "perform L2 projection");
             //*M_levelsetCurvature = this->projectorL2()->project( _expr=divv(this->normal()) );
-            auto phi = this->phi();
+            auto const& phi = this->phiElt();
             *M_levelsetCurvature = this->projectorL2()->derivate( gradv(phi) / sqrt(gradv(phi) * trans(gradv(phi))) );
         }
         break;
@@ -1026,7 +1026,7 @@ LEVELSETBASE_CLASS_TEMPLATE_TYPE::updateCurvature()
         {
             this->log("LevelSetBase", "updateCurvature", "perform smooth projection");
             //*M_levelsetCurvature = this->smoother()->project( _expr=divv(this->normal()) );
-            auto phi = this->phi();
+            auto const& phi = this->phiElt();
             *M_levelsetCurvature = this->smoother()->derivate( gradv(phi) / sqrt(gradv(phi) * trans(gradv(phi))) );
         }
         break;
