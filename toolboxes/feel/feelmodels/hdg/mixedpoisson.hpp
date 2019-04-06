6f31e2026be2c17ae2892eb75cbe889ddbddadce
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2019-04-06 05:23:26 +0200

    fix interface in MixedPoisson
     - avoid copy of fields and add read-write flavor
     - enable more expression for robin condition

--- toolboxes/feel/feelmodels/hdg/mixedpoisson.hpp
+++ toolboxes/feel/feelmodels/hdg/mixedpoisson.hpp
@@ -197,8 +197,10 @@ public:
     M0h_ptr_t traceSpaceOrder0() const { return M_M0h; }
     Ch_ptr_t constantSpace() const {return M_Ch;}
 
-    Vh_element_t fluxField() const { return M_up; }
-    Wh_element_t potentialField() const { return M_pp; }
+    Vh_element_t const& fluxField() const { return M_up; }
+    Wh_element_t const& potentialField() const { return M_pp; }
+    Vh_element_t & fluxField() { return M_up; }
+    Wh_element_t & potentialField() { return M_pp; }
     integral_boundary_list_type integralBoundaryList() const { return M_IBCList; }
     int integralCondition() const { return M_integralCondition; }
     void setIBCList(std::vector<std::string> markersIbc);
@@ -266,7 +268,7 @@ public:
     template<typename ExprT> void assembleRhsNeumann( Expr<ExprT> expr, std::string marker);
     template<typename ExprT> void assembleRhsInterfaceCondition( Expr<ExprT> expr, std::string marker);
     // u.n + g1.p = g2
-    template<typename ExprT> void assembleRobin( Expr<ExprT> expr1, Expr<ExprT> expr2, std::string marker);
+    template<typename ExprT1, typename ExprT2> void assembleRobin( Expr<ExprT1> expr1, Expr<ExprT2> expr2, std::string marker);
     void assembleIBC(int i, std::string marker = "");
     virtual void assembleRhsIBC(int i, std::string marker = "", double intjn = 0);
 
@@ -424,9 +426,9 @@ MixedPoisson<Dim, Order, G_Order, E_Order>::assembleRhsInterfaceCondition( Expr<
  */
 
 template<int Dim, int Order, int G_Order, int E_Order>
-template<typename ExprT>
+template<typename ExprT1, typename ExprT2>
 void
-MixedPoisson<Dim, Order, G_Order, E_Order>::assembleRobin( Expr<ExprT> expr1, Expr<ExprT> expr2, std::string marker)
+MixedPoisson<Dim, Order, G_Order, E_Order>::assembleRobin( Expr<ExprT1> expr1, Expr<ExprT2> expr2, std::string marker)
 {
     auto bbf = blockform2( *M_ps, M_A_cst);
 
