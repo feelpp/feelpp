9f7e7c4537b357ecac79ca3f1ac42896e2fc0992
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2019-04-06 18:05:58 +0200

    fix compilation after change of interface
    do you agree @metivett @vincentchabannes ?

--- toolboxes/feel/feelmodels/multifluid/multifluid.cpp
+++ toolboxes/feel/feelmodels/multifluid/multifluid.cpp
@@ -1290,7 +1290,7 @@ MULTIFLUID_CLASS_TEMPLATE_TYPE::computeLevelsetForce( std::string const& name ) 
     auto u = solFluid->template element<0>();
     auto p = solFluid->template element<1>();
     std::string matName = this->fluidModel()->materialProperties()->rangeMeshElementsByMaterial().begin()->first;
-    auto sigmav = Feel::FeelModels::fluidMecNewtonianStressTensor<2*fluid_model_type::nOrderVelocity>(u,p,*this->fluidModel()->materialProperties(),matName,true);
+    auto sigmav = Feel::FeelModels::fluidMecNewtonianStressTensor(gradv(u),idv(p),*this->fluidModel()->materialProperties(),matName,true,2*fluid_model_type::nOrderVelocity,true);
 
     auto const& phi = this->levelsetModel(name)->phi();
     auto N_expr = trans(gradv(phi)) / sqrt( gradv(phi) * trans(gradv(phi)) );
