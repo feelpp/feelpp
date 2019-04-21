968d839ff06f8a7d692368f74844cdec185be9df
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2019-04-21 15:48:08 +0200

    up #1317
    various compilation and runtime fixes

--- feelpp/feel/feeldiscr/functionspace.hpp
+++ feelpp/feel/feeldiscr/functionspace.hpp
@@ -2053,7 +2053,7 @@ public:
                 m(i,0) = t(i);
             auto loc =  M_Xh->mesh()->tool_localization();
             loc->setExtrapolation( false );
-            auto analysis = loc->run_analysis( m, invalid_v<size_type> );
+            auto analysis = loc->run_analysis( m, invalid_v<index_type> );
             auto found_points = analysis.template get<0>();
             bool found = found_points[0];
 
