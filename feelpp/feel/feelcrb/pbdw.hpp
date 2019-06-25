ce457ccaa0a436dd30d74422b9f2f4aaa4976acc
niakh <niakhrek20@gmail.com>
2019-06-25 12:43:45 +0200

    pbdw online and offline stage

--- feelpp/feel/feelcrb/pbdw.hpp
+++ feelpp/feel/feelcrb/pbdw.hpp
@@ -1,43 +1,109 @@
 #ifndef _FEELPP_PBDW_HPP
 #define _FEELPP_PBDW_HPP 1
-
-
+#include <Eigen/Core>
 namespace Feel
 {
-template<typename MeshType, typename FunctionSpaceType, typename ParameterSpaceType>
-class ModelBase{};
 
-
-template<typename ModelType, typename SensorMap, typename ScalarProduct, typename LinearForm >
+using Eigen::MatrixXd;
+template<typename CRB_Model, typename SensorMap >
 class PBDW
-
 {
-    typedef ModelBase<SpaceType,typename ModelType::functionspace_type, typename ModelType::parameterspace_type> super;
+    typedf CRB_Model::element_type element_type;
 
 public:
 
-    typedef ModelType::functionspace_type::element_type element_type;
-    typedef typename ModelType::functionspace_type functionspace_type;
-    typedef typename ModelType::parameterspace_type  parameterspace_type;
-
-    PBDW( int Z_max_dim , int U_max_dim, bool do_offline = true):
+    PBDW( int U_max_dim, bool do_offline = true):
         M_M_max(U_max_dim),
-        M_N_max(Z_max_dim),
         M_do_offline(do_offline),
         M_M(1),
-        M_N(1)
         {}
     ~PBDW()
         {}
 
+    static std::vector<double> get_measurement( double time, std::vector<std::string> capteurs_name, int numberOfMeasure);
 
-    void offline();
+    void offline()
+    {
+        // construction of Z_N
+        if ( offlineStep()==false )
+        {
+            auto conv = CRB_Model.offline();
+        }
 
-    element_type online( std::array<double,M_M> y_obs);
+        auto N = CRB_model.dimension();
+        auto Z_N  = CRB_Model.wn();
+        auto res = CRB_Model.orthonormalize(N, Z_N, N);
+        // construction of U_M;
+        std::array<element_type, M_M_max> UM;
+        auto a = CRB_Model.algebraicInnerProduct();
+        i = 0;
+        for( auto const& sensor : SensorMap )
+        {
+            element_type q;
+            lm = sensor;
+            a.solve(_rhs=lm, _solution=q);
+            UM(i) = q;
+            i ++;
+        }
+
+        MatrixXd A(M_M,M_M);
+
+        for(int i=0; i<M_M; i++)
+        {
+          for(int j=0; j<M_M; j++)
+
+              A(i,j) = CRB_Model.scalarProduct(UM[j], UM[i]);
+        }
+
+        MatrixXd B(M_M,N);
+
+        for(int i=0; i<M_M; i++)
+        {
+          for(int j=0; j<N; j++)
+
+            B(i,j) = CRB_Model.scalarProduct(ZN[j], UM[i]);
+        }
+
+
+        MatrixXd K(M_M+N,M_M+N);
+
+        K.setZero();
+        K.block(0,0,M_M,M_M) = A;
+        K.block(0,M_M,M_M,N) = B;
+        K.block(M_M,0,N,M_M) = B.transpose();
+
+        // Save K
+        MatrixXd I_U(M_M,1);
+        for(int i=0; i<M_M; i++)
+          I_U(i) = CRB_Model.out(UM[i]);
+
+        for(int i=0; i<N; i++)
+          I_Z(i) = CRB_Model.out(ZN[i]);
+
+        //Save I_U and I_Z
+    }
+
+    element_type online(std::array<double,M_M> y_obs)
+    {
+
+        MatrixXd Y(M_M+N,1);
+        Y.setZero();
+        for(int i=0; i<M_M; i++)
+          Y(i,0)=y_obs[i];
+
+        MatrixXd S(M_M+N,1);
+        S = K.inverse()*Y; // load matrix K
+
+        auto eta = S.block(0,0,M_M,0);
+        auto z = S.block(M_M,0,N,0);
+
+        auto ouput = eta * I_U + z * I_Z; // load I_U and I_Z
+
+    }
 
 protected
 
-    int M_M_max, M_N_max, M_M,M_N;
+    int M_M_max, M_M;
     bool M_do_offline;
 };
 
