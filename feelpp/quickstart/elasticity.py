766955dcb67750c9ae43c0f799c11443a79261d5
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2019-07-11 21:16:19 +0200

    work on elasticity hdg
    still an issue but getting there

--- feelpp/quickstart/elasticity.py
+++ feelpp/quickstart/elasticity.py
@@ -5,45 +5,44 @@ from sympy2ginac import *
 #parameters={'dim':'2','mu':'1','lambda':'1','displ':'[(1/(2*pi*pi))*sin(pi*x)*cos(pi*y),(1/(2*pi*pi))*cos(pi*x)*sin(pi*y)]'}
 #parameters={'dim':'3','mu':'1','lambda':'1','displ':'[cos(Pi*x)*cos(Pi*y)*cos(Pi*z), cos(Pi*y)*sin(Pi*x)*sin(Pi*z), cos(Pi*x)*cos(Pi*z)*sin(Pi*y) ]'}
 
-dim=int(parameters['dim']);
+if 'dim' in locals():
+    dim=int(locals()['dim']);
+else:
+    dim=2
+
 s=syms(dim);
 print("s=",s);
 ns=nsyms(dim);
 
-Mu=sympify(parameters['mu']);
-Lambda=sympify(parameters['lambda']);
-
-displ=dict();
-displ['exact']=sympify(Array([x**2,0]));
-displ['Dirichlet']=displ['exact'];
+lam1=sympify(locals()['lam1']);
+print("lam1=",lam1);
+lam2=sympify(locals()['lam2']);
+print("Lambda=",lam2);
+displ=sympify(locals()['displ']);
 print("displ=",displ);
 
 # gradient
-grad_displ=dict();
-grad_displ['exact']=grad(displ['exact'],s);
+grad_displ=grad(displ,s);
 print("grad(displ)=",grad_displ);
 
 # strain
-strain=dict();
-strain['exact'] =0.5 * ( grad_displ['exact']+transpose(grad_displ['exact']) );
+strain =0.5 * ( grad_displ+transpose(grad_displ) );
 print("strain=",strain);
 
 # stress
-stress=dict();
 I=eye(dim);
-stress['exact']=Lambda * tensorcontraction(strain['exact'],(0,1)) * I + 2. * Mu *strain['exact'];
+print("I=",I);
+stress=Array((lam2 * tensorcontraction(strain,(0,1)) * I + 2. * lam1 *strain).tolist());
 print("stress=",stress);
 
 # surfacic forces
-stressn=dict();
-stressn['Neumann']=n(stress['exact'],1,ns);
+stressn=n(stress,1,ns);
 print("stressn=",stressn);
 
 # force density
-f=dict();
-f['exact'] = -div(stress['exact'],s);
+f = -div(stress,s);
 print("f=",f);
 
-c1['exact']     = 0.5/Mu;
-c2['exact']     = -Lambda/(2. * Mu * (dim*Lambda + 2.*Mu));
+c1     = 0.5/lam1;
+c2     = -lam2/(2. * lam1 * (dim*lam2 + 2.*lam1));
 
