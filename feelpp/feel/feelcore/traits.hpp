ba6a07f125826645c8b7a027250b84c227acbe9f
Vincent Chabannes <vincent.chabannes@feelpp.org>
2019-09-24 13:25:52 +0200

    add type trait is_iterable

--- feelpp/feel/feelcore/traits.hpp
+++ feelpp/feel/feelcore/traits.hpp
@@ -118,5 +118,14 @@ decltype(auto) remove_shared_ptr_f( T&& e )
                      []( auto&& x ) { return x; } )( std::forward<T>(e) );
 
 }
+
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::declval<T>().begin()),decltype(std::declval<T>().end())>>
+    : std::true_type {};
+template <typename T>
+constexpr bool is_iterable_v = is_iterable<T>::value;
+
 } // namespace Feel
 #endif
