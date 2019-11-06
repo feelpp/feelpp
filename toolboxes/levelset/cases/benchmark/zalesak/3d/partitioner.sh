b4e5a748939219fd64a6d24f95da4d9425af5be4
francoisdh <francois.mac@gmail.com>
2019-11-06 12:00:32 +0100

    Zalesak3d: Added mesh partitioner script: 	new file:   toolboxes/levelset/cases/benchmark/zalesak/3d/partitioner.sh

---
+++ toolboxes/levelset/cases/benchmark/zalesak/3d/partitioner.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PART_LIST="2 4 8 10 12 16 20 24"
+PART_LIST="1 2 4 8 10 12 16 20 24"
+H_SIZES="0.032 0.016 0.008 0.004"
+INPUT_MESH=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/zalesak/3d/domain3d.geo
+OUTPUT_DIR=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/zalesak/3d/meshes/
+OUTPUT_FILENAME=domain3d
+INIT_DIR=${PWD}
+
+mkdir -p ${OUTPUT_DIR}
+
+for HSIZE in ${H_SIZES}
+do
+    cd ${OUTPUT_DIR}
+    mkdir -p ${HSIZE}
+    cd ${HSIZE}
+    printf "feelpp_mesh_partitioner --dim 3 --part ${PART_LIST} --ifile ${INPUT_MESH} --ofile ${OUTPUT_FILENAME}_${HSIZE}  --gmsh.hsize ${HSIZE} \n"
+    (time feelpp_mesh_partitioner --dim 3 --part ${PART_LIST} --ifile ${INPUT_MESH} --ofile "${OUTPUT_FILENAME}_${HSIZE}" --gmsh.hsize ${HSIZE}) 2>&1 | tee partitioner.log &
+done
+cd ${INIT_DIR}
+wait
