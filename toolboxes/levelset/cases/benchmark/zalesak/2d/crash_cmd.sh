009b6392821c33adf5e0165f1006618aef3116d2
francoisdh <francois.mac@gmail.com>
2019-11-06 10:49:25 +0100

    Zalesak2d: updated paths in crash_cmd.sh

--- toolboxes/levelset/cases/benchmark/zalesak/2d/crash_cmd.sh
+++ toolboxes/levelset/cases/benchmark/zalesak/2d/crash_cmd.sh
@@ -1,6 +1,8 @@
-mpirun -np 1 /ssd/derhovsepian/feelpp/Debug_install_develop_rpath_custom_ls_toolbox/bin/feelpp_toolbox_levelset_2d \
-       --config-file=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/benchmark/zalesak/2d/slotteddisk.cfg \
-       --levelset.mesh.filename=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/benchmark/zalesak/2d/domain.geo \
+BIN_DIR="/ssd/derhovsepian/feelpp/Debug_install_develop_rpath_custom_ls_toolbox/bin/feelpp_toolbox_levelset_2d"
+CFG_DIR="/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/zalesak/2d"
+mpirun -np 1 ${BIN_DIR} \
+       --config-file=${CFG_DIR}/slotteddisk.cfg \
+       --levelset.mesh.filename=${CFG_DIR}/domain.geo \
        --directory=toolboxes/levelset/zalesak2d/test_exportsspace_convergence/GALS_reinitevery_-1/h_0.32/quadorder_2 \
        --ts.time-step=36.57 \
        --ts.time-final=1.0 \
