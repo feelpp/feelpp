4200f2c80f94c0fac1b7c2f42f90bd90b3c94e4d
francoisdh <francois.mac@gmail.com>
2019-11-07 11:04:46 +0100

    Zalesak2d: Added missing partitioner script for the 2d domain: 	new file:   toolboxes/levelset/cases/benchmark/zalesak/2d/partitioner.sh

---
+++ toolboxes/levelset/cases/benchmark/zalesak/2d/partitioner.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PART_LIST="2 4 8 10 12 16 20 24"
+PART_LIST="1 2 4 8 10 12 16 20 24"
+#H_SIZES="0.32 0.16 0.08 0.04 0.032 0.016 0.008 0.004 0.0032 0.0016 0.0008 0.0004"
+#H_SIZES="0.32"
+H_SIZES="0.0032 0.0016 0.0008 0.0004"
+INPUT_MESH=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/zalesak/2d/domain.geo
+OUTPUT_DIR=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/zalesak/2d/meshes/
+OUTPUT_FILENAME=domain
+
+mkdir -p ${OUTPUT_DIR}
+cd ${OUTPUT_DIR}
+
+for HSIZE in ${H_SIZES}
+do
+    #INPUT_MESH=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/benchmark/zalesak/2d/meshes/${HSIZE}/domain.msh
+    mkdir -p ${HSIZE}
+    cd ${HSIZE}
+    printf "feelpp_mesh_partitioner --dim 2 --part ${PART_LIST} --ifile ${INPUT_MESH} --ofile ${OUTPUT_FILENAME}_${HSIZE}  --gmsh.hsize ${HSIZE} \n"
+    (time feelpp_mesh_partitioner --dim 2 --part ${PART_LIST} --ifile ${INPUT_MESH} --ofile "${OUTPUT_FILENAME}_${HSIZE}" --gmsh.hsize ${HSIZE}) 2>&1 | tee partitioner.log &
+    cd ${OUTPUT_DIR}
+done
+wait
