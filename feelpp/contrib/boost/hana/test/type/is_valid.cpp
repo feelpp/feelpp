e7889b545d3e86eb95f296b497a331dbe752190e
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2019-11-11 22:51:13 +0100

    import latest boost.hana (1.71) and use it. Fixes #1386

--- feelpp/contrib/boost/hana/test/type/is_valid.cpp
+++ feelpp/contrib/boost/hana/test/type/is_valid.cpp
@@ -1,13 +1,14 @@
-// Copyright Louis Dionne 2013-2016
+// Copyright Louis Dionne 2013-2017
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
 #include <boost/hana/assert.hpp>
 #include <boost/hana/equal.hpp>
 #include <boost/hana/not.hpp>
+#include <boost/hana/traits.hpp>
 #include <boost/hana/type.hpp>
 
-#include <laws/base.hpp>
+#include <support/tracked.hpp>
 
 #include <type_traits>
 namespace hana = boost::hana;
@@ -34,6 +35,24 @@ int main() {
 
         auto from_object = hana::is_valid([](auto&& t) -> decltype(
             t.member
+        ) { });
+        BOOST_HANA_CONSTANT_CHECK(from_object(yes{}));
+        BOOST_HANA_CONSTANT_CHECK(hana::not_(from_object(no{})));
+    }
+
+    // Check for a non-static member function
+    {
+        struct yes { int member() const; };
+        struct no { };
+
+        auto from_type = hana::is_valid([](auto t) -> decltype(
+            hana::traits::declval(t).member()
+        ) { });
+        BOOST_HANA_CONSTANT_CHECK(from_type(hana::type_c<yes>));
+        BOOST_HANA_CONSTANT_CHECK(hana::not_(from_type(hana::type_c<no>)));
+
+        auto from_object = hana::is_valid([](auto&& t) -> decltype(
+            t.member()
         ) { });
         BOOST_HANA_CONSTANT_CHECK(from_object(yes{}));
         BOOST_HANA_CONSTANT_CHECK(hana::not_(from_object(no{})));
@@ -162,8 +181,8 @@ int main() {
 
     // Make sure `is_valid` works with non-PODs.
     {
-        hana::is_valid(undefined{})(hana::test::Tracked{1});
-        hana::is_valid([t = hana::test::Tracked{1}](auto) { return 1; })(hana::test::Tracked{1});
+        hana::is_valid(undefined{})(Tracked{1});
+        hana::is_valid([t = Tracked{1}](auto) { return 1; })(Tracked{1});
     }
 
     // Check `is_valid` with a nullary function.
