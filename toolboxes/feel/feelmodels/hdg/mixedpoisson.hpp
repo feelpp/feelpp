d15585a2c9fca8da7b01feafae53dfc58d39dd70
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2019-11-12 14:56:59 +0100

    remove update Robin, and add parameter to assembleRobin

--- toolboxes/feel/feelmodels/hdg/mixedpoisson.hpp
+++ toolboxes/feel/feelmodels/hdg/mixedpoisson.hpp
@@ -288,8 +288,8 @@ public:
     template<typename ExprT> void assembleRhsNeumann( Expr<ExprT> expr, std::string marker);
     template<typename ExprT> void assembleRhsInterfaceCondition( Expr<ExprT> expr, std::string marker);
     // u.n + g1.p = g2
-    template<typename ExprT1, typename ExprT2> void assembleRobin( Expr<ExprT1> const& expr1, Expr<ExprT2> const& expr2, std::string const& marker);
-    template<typename ExprT1, typename ExprT2> void updateRobin( Expr<ExprT1> const& expr1, Expr<ExprT2> const& expr2, std::string const& marker);
+    template<typename ExprT1, typename ExprT2> void assembleRobin( Expr<ExprT1> const& expr1, Expr<ExprT2> const& expr2, std::string const& marker, bool update_only = false );
+
     void assembleIBC(int i, std::string marker = "");
     virtual void assembleRhsIBC(int i, std::string marker = "", double intjn = 0);
 
@@ -463,7 +463,7 @@ MixedPoisson<Dim, Order, G_Order, E_Order>::assembleRhsInterfaceCondition( Expr<
 template<int Dim, int Order, int G_Order, int E_Order>
 template<typename ExprT1, typename ExprT2>
 void
-MixedPoisson<Dim, Order, G_Order, E_Order>::assembleRobin( Expr<ExprT1> const& expr1, Expr<ExprT2> const& expr2, std::string const& marker)
+MixedPoisson<Dim, Order, G_Order, E_Order>::assembleRobin( Expr<ExprT1> const& expr1, Expr<ExprT2> const& expr2, std::string const& marker, bool update_only )
 {
     tic();
     auto bbf = blockform2( *M_ps, M_A_cst);
@@ -485,15 +485,18 @@ MixedPoisson<Dim, Order, G_Order, E_Order>::assembleRobin( Expr<ExprT1> const& e
     // stabilisation parameter
     auto tau_constant = cst(M_tauCst);
 
-    // <j.n,mu>_Gamma_R
-    bbf( 2_c, 0_c ) += integrate(_range=markedfaces(M_mesh,marker),
-                                 _expr=id(l)*normalt(u) );
-    // <tau p, mu>_Gamma_R
-    bbf( 2_c, 1_c ) += integrate(_range=markedfaces(M_mesh,marker),
-                                 _expr=tau_constant * id(l) * idt(p)  );
-    // <-tau phat, mu>_Gamma_R
-    bbf( 2_c, 2_c ) += integrate(_range=markedfaces(M_mesh,marker),
-                                 _expr=-tau_constant * idt(phat) * id(l) );
+    if ( !update_only )
+    {
+        // <j.n,mu>_Gamma_R
+        bbf( 2_c, 0_c ) += integrate(_range=markedfaces(M_mesh,marker),
+                                     _expr=id(l)*normalt(u) );
+        // <tau p, mu>_Gamma_R
+        bbf( 2_c, 1_c ) += integrate(_range=markedfaces(M_mesh,marker),
+                                     _expr=tau_constant * id(l) * idt(p)  );
+        // <-tau phat, mu>_Gamma_R
+        bbf( 2_c, 2_c ) += integrate(_range=markedfaces(M_mesh,marker),
+                                     _expr=-tau_constant * idt(phat) * id(l) );
+    }
     // <g_R^1 phat, mu>_Gamma_R
     bbf( 2_c, 2_c ) += integrate(_range=markedfaces(M_mesh,marker),
                                  _expr=expr1*idt(phat) * id(l) );
@@ -503,39 +506,6 @@ MixedPoisson<Dim, Order, G_Order, E_Order>::assembleRobin( Expr<ExprT1> const& e
     toc("assembleRobin", this->verbose() || FLAGS_v > 0);
 }
 
-template<int Dim, int Order, int G_Order, int E_Order>
-template<typename ExprT1, typename ExprT2>
-void
-MixedPoisson<Dim, Order, G_Order, E_Order>::updateRobin( Expr<ExprT1> const& expr1, Expr<ExprT2> const& expr2, std::string const& marker)
-{
-    tic();
-    auto bbf = blockform2( *M_ps, M_A_cst);
-
-    auto blf = blockform1( *M_ps, M_F );
-    auto u = M_Vh->element( "u" );
-    auto p = M_Wh->element( "p" );
-    auto phat = M_Mh->element( "phat" );
-    auto l = M_Mh->element( "lambda" );
-    // auto H = M_M0h->element( "H" );
-    // if ( ioption(prefixvm(prefix(), "hface") ) == 0 )
-    //     H.on( _range=elements(M_M0h->mesh()), _expr=cst(M_Vh->mesh()->hMax()) );
-    // else if ( ioption(prefixvm(prefix(), "hface") ) == 1 )
-    //     H.on( _range=elements(M_M0h->mesh()), _expr=cst(M_Vh->mesh()->hMin()) );
-    // else if ( ioption(prefixvm(prefix(), "hface") ) == 2 )
-    //     H.on( _range=elements(M_M0h->mesh()), _expr=cst(M_Vh->mesh()->hAverage()) );
-    // else
-    //     H.on( _range=elements(M_M0h->mesh()), _expr=h() );
-    // stabilisation parameter
-    auto tau_constant = cst(M_tauCst);
-
-    // <g_R^1 phat, mu>_Gamma_R
-    bbf( 2_c, 2_c ) += integrate(_range=markedfaces(M_mesh,marker),
-                                 _expr=expr1*idt(phat) * id(l) );
-    // <g_R^2,mu>_Gamma_R
-    blf(2_c) += integrate( _range=markedfaces(M_mesh, marker),
-                           _expr=id(l)*expr2);
-    toc("assembleRobin", this->verbose() || FLAGS_v > 0);
-}
 
 template<int Dim, int Order, int G_Order, int E_Order>
 template<typename ExprT>
