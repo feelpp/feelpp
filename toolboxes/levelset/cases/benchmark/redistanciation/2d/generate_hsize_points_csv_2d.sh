8104794e105cc535b4d109db847db1ceb2c88df1
francoisdh <francois.mac@gmail.com>
2019-11-27 18:37:37 +0100

    Redistanciation Benchmark 2D: Added hsize/points csv table generation script

---
+++ toolboxes/levelset/cases/benchmark/redistanciation/2d/generate_hsize_points_csv_2d.sh
@@ -0,0 +1,3 @@
+TABLE_FILE_NAME="hsize_points_2d.csv"
+printf "hsize, points\n" > ${TABLE_FILE_NAME}
+grep points meshes/*/partitioner.log --color | sed s,meshes/,, | sed -e s#/partitioner.log#,# -e 's|:      number of points  in memory : | |' | sort -r >> ${TABLE_FILE_NAME}
