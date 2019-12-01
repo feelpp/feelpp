2ef5b8ebc8c7b78c372d29ca949f0df849c8d241
Vincent Chabannes <vincent.chabannes@feelpp.org>
2019-12-01 16:16:00 +0100

    fix compilation

--- toolboxes/levelset/main_ls.cpp
+++ toolboxes/levelset/main_ls.cpp
@@ -12,8 +12,8 @@ runLevelsetApplication()
 
     typedef Simplex<FEELPP_DIM,1> convex_type;
     typedef Lagrange<OrderLevelset, Scalar, Continuous, PointSetFekete> basis_type;
-    //typedef FunctionSpace<Mesh<convex_type>, bases<typename FeelModels::detail::ChangeBasisPolySet<Vectorial, basis_type>::type>, Periodicity<NoPeriodicity>> space_advection_velocity_type;
-    typedef FunctionSpace<Mesh<convex_type>, Feel::detail::bases<Lagrange<OrderLevelset, Vectorial, Continuous, PointSetFekete>>, double, Periodicity<NoPeriodicity>, mortars<NoMortar>> space_advection_velocity_type;
+    typedef FunctionSpace<Mesh<convex_type>, bases<typename FeelModels::detail::ChangeBasisPolySet<Vectorial, basis_type>::type>/*, Periodicity<NoPeriodicity>*/ > space_advection_velocity_type;
+    //typedef FunctionSpace<Mesh<convex_type>, Feel::detail::bases<Lagrange<OrderLevelset, Vectorial, Continuous, PointSetFekete>>, double, Periodicity<NoPeriodicity>, mortars<NoMortar>> space_advection_velocity_type;
     typedef Lagrange<OrderLevelsetPN, Scalar, Continuous, PointSetFekete> basis_PN_type;
 
     typedef FeelModels::LevelSet<
