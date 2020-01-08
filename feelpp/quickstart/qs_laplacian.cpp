5df079b89440ad009e1458cdefad862769a8c9e9
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2020-01-08 12:11:02 +0100

    Fixes #1412

--- feelpp/quickstart/qs_laplacian.cpp
+++ feelpp/quickstart/qs_laplacian.cpp
@@ -35,8 +35,11 @@ int main( int argc, char** argv )
     
     laplacianoptions.add_options()( "no-solve", po::value<bool>()->default_value( false ), "No solve" )
         ( "k", po::value<std::string>()->default_value( "1" ), "diffusion coefficient" )
-        ( "r_1", po::value<std::string>()->default_value( "1" ), "Robin lhs coefficient" )
-        ( "r_2", po::value<std::string>()->default_value( "" ), "Robin rhs coefficient" )
+        ( "f", po::value<std::string>()->default_value( "" ), "right hand side" )
+        ( "g", po::value<std::string>()->default_value( "" ), "Dirichlet boundary condition" )
+        ( "un", po::value<std::string>()->default_value( "" ), "Neumann boundary condition" )
+        ( "r_1", po::value<std::string>()->default_value( "1" ), "Robin left hand side coefficient" )
+        ( "r_2", po::value<std::string>()->default_value( "" ), "Robin right hand side  coefficient" )
         ( "pyexpr.filename", po::value<std::string>()->default_value( "$cfgdir/../python/laplacian.py" ), "python filename to execute" );
     laplacianoptions.add_options()( "marker.name", po::value<std::string>(), "marker on which to solve problem" );
     laplacianoptions.add_options()( "marker.levelset", po::value<std::string>(), "marker on which to solve problem" );
@@ -69,14 +72,40 @@ int main( int argc, char** argv )
     else
         Vh = Pch<2>( mesh );
     auto u = Vh->element( "u" );
-    Feel::cout << "Dirichlet:" << nelements( markedfaces( support( Vh ), "Dirichlet" ) ) << std::endl;
-    Feel::cout << "Neumann:" << nelements( markedfaces( support( Vh ), "Neumann" ) ) << std::endl;
-    Feel::cout << "Robin:" << nelements( markedfaces( support( Vh ), "Robin" ) ) << std::endl;
-    std::map<std::string,std::string> locals{{"dim",std::to_string(FEELPP_DIM)},{"k",soption("k")},{"p",soption("checker.solution")},{"grad_p",""}, {"u",""}, {"un",""}, {"f",""}, {"r_1",soption("r_1")}, {"r_2",soption("r_2")}};
-    Feel::pyexprFromFile( Environment::expand(soption("pyexpr.filename")), locals );
 
-    for( auto d: locals )
-        cout << d.first << ":" << d.second << std::endl;
+    // if we do not check the results with a manufactured solution,
+    // the right hand side is given by functions.f otherwise it is computed by the python script
+    auto thechecker = checker( "L1/H1 convergence", soption("checker.solution") );
+    auto check_data = [&thechecker]( std::string data, std::string helper, bool defined = true ) {
+                          using namespace std::string_literals;
+                          if ( defined )
+                          {
+                              if ( !thechecker.check() && data.empty() )
+                                  throw std::invalid_argument( "Invalid data "s + data + " "s + helper);
+                          }
+                      };
+    try {
+        check_data( soption( _name = "k" ), " set diffusion coefficient using option --k (eg. --k=1)");
+        check_data( soption( _name = "f" ), " set source term using option --f (eg. --f=1)");
+        check_data( soption( _name = "g" ), " set Dirichlet condition using option --g (eg. --g=0)",  support( Vh )->hasAnyMarker( {"Dirichlet"} ) );
+        check_data( soption( _name = "un" ), " set Neumann condition using option --un (eg. --un=0)", support( Vh )->hasAnyMarker( {"Neumann"} ) );
+        check_data( soption( _name = "r_2" ), " set Robin right hand side condition using option --r_2 (eg. --r_2=0)", support( Vh )->hasAnyMarker( {"Robin"} ) );
+    }
+    catch( std::invalid_argument const& e )
+    {
+        Feel::cout << e.what() << std::endl;
+        Feel::cout << "Exiting..." << std::endl;
+        return 1;
+    }
+    auto opt = [&thechecker]( std::string data ) {
+                   using namespace std::string_literals;
+                   if ( thechecker.check() )
+                       return ""s;
+                   return data;
+              };
+    std::map<std::string,std::string> locals{{"dim",std::to_string(FEELPP_DIM)},{"k",opt(soption("k"))},{"p",soption("checker.solution")},{"grad_p",""}, {"u",""}, {"un",opt(soption("un"))}, {"f",opt(soption("f"))}, {"g",opt(soption("g"))}, {"r_1",soption("r_1")}, {"r_2",opt(soption("r_2"))}};
+    
+    Feel::pyexprFromFile( Environment::expand(soption("pyexpr.filename")), locals );
 
     std::string p_exact_str = locals.at("p");
     std::string u_exact_str = locals.at("u");
@@ -85,19 +114,39 @@ int main( int argc, char** argv )
     auto k = expr( locals.at("k") );
     auto un = expr( locals.at("un") );
     auto f = expr( locals.at("f") );
+    auto g = expr( locals.at("g") );
     auto r_1 = expr( locals.at("r_1") );
     auto r_2 = expr( locals.at("r_2") );
-#if 0
-    auto mu = expr( soption( _name = "functions.mu" ) ); // diffusion term
-    auto f = expr( soption( _name = "functions.f" ), "f" );
-    auto r_1 = expr( soption( _name = "functions.a" ), "a" ); // Robin left hand side expression
-    auto r_2 = expr( soption( _name = "functions.b" ), "b" ); // Robin right hand side expression
-    auto n = expr( soption( _name = "functions.c" ), "c" );   // Neumann expression
-#endif
-    auto thechecker = checker( "L1/H1 convergence", p_exact_str );
-    auto solution = expr( p_exact_str, "solution" );
-    auto g = thechecker.check() ? solution : expr( soption( _name = "functions.g" ), "g" );
+    thechecker.setSolution( locals.at("p") );
 
+    Feel::cout << "----------------------------------------" << std::endl;
+    Feel::cout << "Solving -div(( k grad p ) = f with the following boundary conditions" << std::endl;
+    Feel::cout << "                         k=" << locals.at("k") << std::endl;
+    Feel::cout << "                         f=" << locals.at("f") << std::endl;
+
+    if ( support( Vh )->hasAnyMarker( {"Dirichlet"} ) ) 
+    {
+        Feel::cout << "     Dirichlet (nfaces:" << nelements( markedfaces( support( Vh ), "Dirichlet" ) ) << ")" << std::endl;
+        Feel::cout << "                     p=" << locals.at("g") << std::endl;
+    }
+    if ( support( Vh )->hasAnyMarker( {"Neumann"} ) ) 
+    {
+        Feel::cout << "       Neumann (nfaces:" << nelements( markedfaces( support( Vh ), "Neumann" ) ) << ")" << std::endl;
+        Feel::cout << "              -k*dn(p)=" << locals.at("un") << std::endl;
+    }
+    if ( support( Vh )->hasAnyMarker( {"Robin"} ) ) 
+    {
+        Feel::cout << "         Robin (nfaces:" << nelements( markedfaces( support( Vh ), "Robin" ) ) << ")" << std::endl;
+        Feel::cout << "              -k*dn(p)+" << locals.at("r_1") << "*u=" << locals.at("r_2") << std::endl;
+    }
+    if ( thechecker.check() )
+    {
+        Feel::cout << std::endl
+                   << "      Exact solution to be checked with L2/H1 norms" << std::endl;
+        Feel::cout << "                     p=" << locals.at("p") << std::endl;
+        Feel::cout << "            -k*grad(p)=" << locals.at("u") << std::endl;
+    }
+    Feel::cout << "----------------------------------------" << std::endl;
     // tag::v[]
     auto v = Vh->element( g, "g" );
     // end::v[]
@@ -144,34 +193,42 @@ int main( int argc, char** argv )
     tic();
     auto e = exporter( _mesh = mesh );
     e->addRegions();
-    e->add( "uh", u );
-    v.on( _range = elements(support(Vh)), _expr = k );
-    e->add( "mu", v );
+    e->add( "p", u );
+    e->add( "u", -k*gradv(u), "element" );
+    e->add( "k", k );
+    e->add( "f", f );
+    e->add( "g", g );
+    e->add( "r_1", r_1 );
+    e->add( "r_2", r_2 );
     if ( thechecker.check() )
     {
-        v.on( _range = elements(support(Vh)), _expr = solution );
-        e->add( "solution", v );
+        e->add( "solution", p_exact );
+        e->add( "flux", u_exact );
     }
     e->save();
     toc( "Exporter" );
     // end::export[]
 
+    int status = 0;
     // tag::check[]
-    // compute l2 and h1 norm of u-u_h where u=solution
-    auto norms = [=]( std::string const& solution ) -> std::map<std::string, double> {
-        tic();
-        double l2 = normL2( _range = elements( support( Vh ) ), _expr = idv( u ) - expr( solution ) );
-        toc( "L2 error norm" );
-        tic();
-        double h1 = normH1( _range = elements( support( Vh ) ), _expr = idv( u ) - expr( solution ), _grad_expr = gradv( u ) - grad<FEELPP_DIM>( expr( solution ) ) );
-        toc( "H1 error norm" );
-        return {{"L2", l2}, {"H1", h1}};
-    };
+    if ( thechecker.check() )
+    {
+        // compute l2 and h1 norm of u-u_h where u=solution
+        auto norms = [=]( std::string const& solution ) -> std::map<std::string, double> {
+            tic();
+            double l2 = normL2( _range = elements( support( Vh ) ), _expr = idv( u ) - expr( solution ) );
+            toc( "L2 error norm" );
+            tic();
+            double h1 = normH1( _range = elements( support( Vh ) ), _expr = idv( u ) - expr( solution ), _grad_expr = gradv( u ) - grad<FEELPP_DIM>( expr( solution ) ) );
+            toc( "H1 error norm" );
+            return {{"L2", l2}, {"H1", h1}};
+        };
 
-    int status = thechecker.runOnce( norms, rate::hp( mesh->hMax(), Vh->fe()->order() ) );
+        status = !thechecker.runOnce( norms, rate::hp( mesh->hMax(), Vh->fe()->order() ) );
+    }
     // end::check[]
 
     // exit status = 0 means no error
-    return !status;
+    return status;
 }
 // end::global[]
