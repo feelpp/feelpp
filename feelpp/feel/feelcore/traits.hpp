3e7b196f23b62c4fdd64845ccfb0f381e9f7da5a
Thibaut METIVET <thibaut.metivet@inria.fr>
2020-01-21 10:24:38 +0100

    Add is_iterable traits

--- feelpp/feel/feelcore/traits.hpp
+++ feelpp/feel/feelcore/traits.hpp
@@ -100,5 +100,28 @@ struct remove_std_vector<std::vector<T> >
 template<typename T>
 using remove_std_vector_t = typename remove_std_vector<T>::type;
 
+namespace detail
+{
+    // To allow ADL with custom begin/end
+    using std::begin;
+    using std::end;
+
+    template <typename T>
+    auto is_iterable_impl(int)
+    -> decltype (
+        begin(std::declval<T&>()) != end(std::declval<T&>()), // begin/end and operator !=
+        void(), // operator ,
+        ++std::declval<decltype(begin(std::declval<T&>()))&>(), // operator ++
+        void(*begin(std::declval<T&>())), // operator*
+        std::true_type{});
+
+    template <typename T>
+    std::false_type is_iterable_impl(...);
+
+}
+
+template <typename T>
+using is_iterable = decltype(detail::is_iterable_impl<T>(0));
+
 } // namespace Feel
 #endif
