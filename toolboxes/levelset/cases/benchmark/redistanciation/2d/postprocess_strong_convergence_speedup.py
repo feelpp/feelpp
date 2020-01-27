c07295010e79fba75cdc32f946ea9f5199ac82f8
francoisdh <francois.mac@gmail.com>
2020-01-27 14:47:29 +0100

    Redistanciation Benchmark 2D: Updated strong convergence speedup python script to include more columns (processes, speedup, incrementalspeedup, linearincrementalspeedup) 	modified:   toolboxes/levelset/cases/benchmark/redistanciation/2d/postprocess_strong_convergence_speedup.py

--- toolboxes/levelset/cases/benchmark/redistanciation/2d/postprocess_strong_convergence_speedup.py
+++ toolboxes/levelset/cases/benchmark/redistanciation/2d/postprocess_strong_convergence_speedup.py
@@ -7,28 +7,32 @@ if __name__ == '__main__':
     
     dictrows=[]
     arglength = len(sys.argv)
-    #print("length = " + str(arglength))
     sourcefilename = sys.argv[1]
     targetfilename = sourcefilename.replace("time_per_core", "speedup_per_core")
-    
-    #print("source filename = " + sourcefilename)
-    #print("target filename = " + targetfilename)
     
     with open(sourcefilename, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            #print(row)
             dictrows.append(row)
 
     # We only compute speedup if we find sequential time
     if dictrows[0]["processes"] == "1":
-        sequential_time = float(dictrows[0][" time"])
+        sequentialtime = float(dictrows[0][" time"])
         with open(targetfilename, mode='w') as targetcsv:
-            targetcsv.write("processes, speedup\n")
+            targetcsv.write("processes, speedup, incrementalspeedup, linearincrementalspeedup\n")
+            targetcsv.write("1, 1.0, , \n" )# first line
+            lasttime = sequentialtime
+            lastnproc = 1
+            dictrows.pop(0)
             for d in dictrows:
-                targetcsv.write(d["processes"] + ", " + str( sequential_time / float(d[" time"])) + "\n" )
-
+                currentnproc = int(d["processes"])
+                currenttime = float(d[" time"])
+                speedup = sequentialtime / currenttime
+                incrementalspeedup = lasttime / currenttime
+                linearincrementalspeedup = currentnproc / lastnproc
+                targetcsv.write(str(currentnproc) + ", " + str(speedup) + ", " + str(incrementalspeedup) + ", " + str(linearincrementalspeedup) + "\n" )
+                lasttime = currenttime
+                lastnproc = currentnproc
     else:
-        print("Could not find sequential time in " + sourcefilename)
-    #print("sequential time is : " + str(sequential_time))
-
+        print(__file__ + ": could not find sequential time in " + sourcefilename)
+    
