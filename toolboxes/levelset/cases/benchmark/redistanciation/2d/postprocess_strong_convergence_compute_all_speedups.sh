82214b937083945442f620fec6496a6d6a4e9ffb
francoisdh <francois.mac@gmail.com>
2020-01-27 14:40:23 +0100

    Redistanciation Benchmark 2D: Updated strong convergence postprocessing script to use find + pattern 	modified:   postprocess_strong_convergence_compute_all_speedups.sh

--- toolboxes/levelset/cases/benchmark/redistanciation/2d/postprocess_strong_convergence_compute_all_speedups.sh
+++ toolboxes/levelset/cases/benchmark/redistanciation/2d/postprocess_strong_convergence_compute_all_speedups.sh
@@ -1,13 +1,6 @@
-ROOT_DIR=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/redistanciation/2d
-H_SIZES="0.1024 0.0512 0.0256 0.0128 0.0064 0.0032 0.0016 0.0008 0.0004 0.0002 0.0001"
-SOURCE_FILE_NAME_BASE=results_2d_strong_convergence_time_per_core_h
-for HSIZE in ${H_SIZES}
-do
-    SOURCE_FILE_NAME=${ROOT_DIR}/${SOURCE_FILE_NAME_BASE}_$HSIZE.csv
-    if [ -f ${SOURCE_FILE_NAME} ];
-    then
-        ./postprocess_strong_convergence_speedup.py ${SOURCE_FILE_NAME}
-    else
-        printf "Could not find ${SOURCE_FILE_NAME}.\n"
-    fi
-done
+#!/usr/bin/env bash
+ROOT_DIR=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/redistanciation/2d/results
+FILENAME_PATTERN=redistanciation_2d_time_per_core
+SPEEDUP_SCRIPT=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/redistanciation/2d/postprocess_strong_convergence_speedup.py
+
+find ${ROOT_DIR} -name "${FILENAME_PATTERN}*" -exec ${SPEEDUP_SCRIPT} {} \; 
