f5596086259ebd49121d03eb02b79024cf2381e5
francoisdh <francois.mac@gmail.com>
2020-01-27 14:44:00 +0100

    Redistanciation Benchmark 2D: Updated time sorting script: it now has to be called for each file to process 	modified:   postprocess_strong_convergence_sort_times.sh

--- toolboxes/levelset/cases/benchmark/redistanciation/2d/postprocess_strong_convergence_sort_times.sh
+++ toolboxes/levelset/cases/benchmark/redistanciation/2d/postprocess_strong_convergence_sort_times.sh
@@ -1,16 +1,11 @@
-ROOT_DIR=/home/u2/derhovsepian/git/feelpp/toolboxes/levelset/cases/benchmark/redistanciation/2d
-H_SIZES="0.1024 0.0512 0.0256 0.0128 0.0064 0.0032 0.0016 0.0008 0.0004 0.0002 0.0001"
-SOURCE_FILE_NAME_BASE=redistanciation_2d_timer_h
-TARGET_FILE_NAME_BASE=results_2d_strong_convergence_time_per_core_h
-for HSIZE in ${H_SIZES}
-do
-    SOURCE_FILE_NAME=${ROOT_DIR}/${SOURCE_FILE_NAME_BASE}_$HSIZE.csv
-    TARGET_FILE_NAME=${ROOT_DIR}/${TARGET_FILE_NAME_BASE}_$HSIZE.csv
-    if [ -f ${SOURCE_FILE_NAME} ];
-    then
-        printf "processes, hsize, points, elements, time\n" > ${TARGET_FILE_NAME}
-        sort -n ${SOURCE_FILE_NAME} >> ${TARGET_FILE_NAME}
-    else
-        printf "Could not find ${SOURCE_FILE_NAME}.\n"
-    fi
-done
+#!/usr/bin/env bash
+SOURCE_FILE_NAME=$1
+TARGET_FILE_NAME=${1/times/time_per_core}
+if [ -f ${SOURCE_FILE_NAME} ];
+then
+    printf "processes, hsize, points, elements, time\n" > ${TARGET_FILE_NAME}
+    sort -n ${SOURCE_FILE_NAME} >> ${TARGET_FILE_NAME}
+else
+    printf "Could not find ${SOURCE_FILE_NAME}.\n"
+fi
+
