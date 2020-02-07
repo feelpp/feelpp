49ad774ad1f8a3fabe157197e411b2bf6cdf1f26
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2020-02-07 06:16:06 +0100

    assume yes for apt install
    [ci skip]

--- feelpp/tools/scripts/pkg/add_key.sh
+++ feelpp/tools/scripts/pkg/add_key.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pbuilder-dist $DIST login --save-after-login << EOF
-apt install wget gnupg ca-certificates
+apt install -y wget gnupg ca-certificates
 echo "deb https://dl.bintray.com/feelpp/ubuntu $DIST $CHANNEL" | tee -a /etc/apt/sources.list
 wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | apt-key add -
 apt update
