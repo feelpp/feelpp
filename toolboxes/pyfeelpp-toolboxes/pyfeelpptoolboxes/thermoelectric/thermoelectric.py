4341ca2f0da4abfccf3f22f0e8ecd1cef3ff5423
Vincent Chabannes <vincent.chabannes@feelpp.org>
2020-02-28 09:57:21 +0100

    fix some pyfeelpp-toolboxes

--- toolboxes/pyfeelpp-toolboxes/pyfeelpptoolboxes/thermoelectric/thermoelectric.py
+++ toolboxes/pyfeelpp-toolboxes/pyfeelpptoolboxes/thermoelectric/thermoelectric.py
@@ -1,12 +1,8 @@
-from pyfeelpp import core
 import sys
+from pyfeelpptoolboxes.thermoelectric import *
 
-import pyfeelpp.toolboxes.modelcore as modelcore
+e=core.Environment(sys.argv,opts=toolboxes_options("thermoelectric"))
 
-e=core.Environment(sys.argv,opts=modelcore.toolboxes_options("thermoelectric"))
-
-#from pyfeelpp import discr,ts,filters
-from pyfeelpp.toolboxes.thermoelectric import *
 
 f=thermoelectric(dim=2,orderDisp=1,worldComm=e.worldCommPtr())
 f.init()
