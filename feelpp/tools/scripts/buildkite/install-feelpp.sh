656f7c30e7fa542b85852ed49ff54484db1fe5aa
Vincent Chabannes <vincent.chabannes@feelpp.org>
2020-03-10 07:44:40 +0100

    remove verbose with qs testsuite + add verbose for feelpp testsuite [ci skip]

--- feelpp/tools/scripts/buildkite/install-feelpp.sh
+++ feelpp/tools/scripts/buildkite/install-feelpp.sh
@@ -41,11 +41,11 @@ else
 fi
 
 if [ "${component}" = "feelpp" ] ; then
-    CTEST_FLAGS="-R feelpp_qs_ -T test --no-compress-output -VV"
+    CTEST_FLAGS="-R feelpp_qs_ -T test --no-compress-output"
 elif [ "${component}" = "toolboxes" ] ; then
     CTEST_FLAGS="-R feelpp_toolbox_ -T test --no-compress-output"
 elif [ "${component}" = "testsuite" ] ; then
-    CTEST_FLAGS="-R feelpp_test_ -T test --no-compress-output"
+    CTEST_FLAGS="-R feelpp_test_ -T test --no-compress-output -VV"
 else
     CTEST_FLAGS="-T test --no-compress-output"
 fi
