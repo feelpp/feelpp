c67b8a3e437fc46f18832a13a14356bb1f3c8cc6
Thibaut METIVET <thibaut.metivet@inria.fr>
2020-04-29 11:13:08 +0200

    Use levelsetspacemanager in levelsetredistanciation*_inst to improve modularity remove dependence to levelsetbase.hpp

--- toolboxes/feel/feelmodels/levelset/levelsetredistanciation_fm_inst.cpp
+++ toolboxes/feel/feelmodels/levelset/levelsetredistanciation_fm_inst.cpp
@@ -1,12 +1,21 @@
 
 #include "levelsetbaseconfig.h"
-#include <feel/feelmodels/levelset/levelsetbase.hpp>
+#include <feel/feelmodels/levelset/levelsetspacemanager.hpp>
 #include <feel/feelmodels/levelset/levelsetredistanciation_fm.cpp>
+
+#define LEVELSETSPACEMANAGER_CLASS_TYPE \
+    LevelSetSpaceManager< \
+        LEVELSETBASE_CONVEX_TYPE, \
+        LEVELSETBASE_BASIS_TYPE, \
+        LEVELSETBASE_PERIODICITY_TYPE, \
+        LEVELSETBASE_BASISPN_TYPE \
+        >                    \
+    /**/
 
 namespace Feel {
 
 template class LevelSetRedistanciationFM< 
-    typename FeelModels::LEVELSETBASE_CLASS_TYPE::space_levelset_type
+    typename FeelModels::LEVELSETSPACEMANAGER_CLASS_TYPE::space_scalar_type
     >;
 
 }
