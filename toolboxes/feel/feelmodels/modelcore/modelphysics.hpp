11d59762c248b0594ba22c173b0cae734429c2f8
Thibaut METIVET <thibaut.metivet@inria.fr>
2020-05-13 15:47:12 +0200

    Fix vim modelines

--- toolboxes/feel/feelmodels/modelcore/modelphysics.hpp
+++ toolboxes/feel/feelmodels/modelcore/modelphysics.hpp
@@ -1,4 +1,5 @@
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=cpp:et:sw=4:ts=4:sts=4*/
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=cpp:et:sw=4:ts=4:sts=4
+ */
 
 #ifndef FEELPP_TOOLBOXES_MODELCORE_MODELPHYSICS_H
 #define FEELPP_TOOLBOXES_MODELCORE_MODELPHYSICS_H 1
