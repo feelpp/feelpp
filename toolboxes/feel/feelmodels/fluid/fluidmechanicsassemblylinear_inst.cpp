6be37738a241906561d8d5383e01188b017e81c1
Vincent Chabannes <vincent.chabannes@feelpp.org>
2020-07-08 11:16:36 +0200

    rename/delete files

--- toolboxes/feel/feelmodels/fluid/fluidmechanicsassemblylinear_inst.cpp
+++ toolboxes/feel/feelmodels/fluid/fluidmechanicsassemblylinear_inst.cpp
@@ -1,6 +1,6 @@
 
 #include "fluidmechanicsconfig.h"
-#include <feel/feelmodels/fluid/fluidmechanicsupdatelinear.cpp>
+#include <feel/feelmodels/fluid/fluidmechanicsassemblylinear.cpp>
 
 namespace Feel {
 namespace FeelModels {
