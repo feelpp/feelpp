277bdca8021cb87e548f3cb14b03fb1e4feec1b3
Philippe Ricka <rickaphilippe@gmail.com>
2020-08-14 10:22:22 +0200

    any dimensional tracking ok ; introduced time-dependancy of operators

--- feelpp/pyfeelpp/kalman/testcases/advectiondiffusion/testcase.py
+++ feelpp/pyfeelpp/kalman/testcases/advectiondiffusion/testcase.py
@@ -4,37 +4,50 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
+mu, v = 0.05, 0.5
+a, b = 1, 0.25
+omega = 10
 N = 101
 dx = 1/(N-1)
-dt = 0.000000001
+dt = 0.15 # min( dx**2/(2*mu), dx/mu )/50
 
-def computeMatrix( mu, v ):
+def computeMatrix( v, mu ):
     M = np.eye(N) - v*dt*(np.eye(N,N,1)+np.eye(N,N,-1))+mu*dt*(np.eye(N,N,1)+np.eye(N,N,-1)-2*np.eye(N))/(dx**2)
     return M
 
-def A( x ):
-    BC = [2,1] # x[[2,-1]]
-    matrix = computeMatrix( x[0], x[1] )
-    x[2:] = matrix @ x[2:]
-    x[[2,-1]] = BC
+def computeCovariance( theta ):
+    return np.diag(np.concatenate((0.125*theta,np.zeros(N)))) # 12.5% of given initial values, model assumed exact
+
+def A( x, t ):
+    x[5:] = computeMatrix( x[0], x[1] ) @ x[5:]
+    x[5] = x[2] + x[3]*np.sin( x[4]*t )
+    x[-1] = 2*x[-2] - x[-3]
+    x[0:5] += np.random.normal( 0, 1e-8, 5 ) # random walk for constant unknown parameters
     return x
 
-def H( x ):
-    return x[2:N+2]
+def H( x, t ):
+    return x[[9,29,49,84]]
 
-initialState = np.zeros((N+2,1))
-initialState[[0,1,2,-1],0] = [1,1,2,1]
-flt = ukf.Filter( dynamics = A, observe = H, defect = 0.0001, stateDim = N+2, obsDim = N )
+def h( x, t ):
+    return x[[5,25,45,80]]
+
+initialState = np.ones((N+5,1))
+#initialState[0:4,0] = [1,1,a,b]
+flt = ukf.Filter( dynamics = A, observe = H, defect = 0.002, stateDim = N+5, obsDim = 4 )
 flt.setState( initialState )
 #flt.setSigmaHk( np.zeros((N,N)) )
-advectionSystem = ad.Adv_Diff( N = N, dx = dx, dt = dt, v=1, mu = 0.01, a=1, b=0 )
+advectionSystem = ad.Adv_Diff( N = N, dx = dx, dt = dt, v=v, mu = mu, a = a, b = b )
+advectionSystem._state = np.ones(N) # as prescribed in Lal
 
-flt.step( advectionSystem.getState() )
-for i in range (50):
+flt.step( h(advectionSystem.getState(), 0) )
+for i in range (667):
     print(i)
-    flt.step( advectionSystem.getState() )
+    print("observation : {}".format(h(advectionSystem.getState(), flt.time)))
+    flt.step( h(advectionSystem.getState(), flt.time) )
     advectionSystem.step()
-    print( flt.getStateEstimate()[0:2] )
+    advectionSystem.setBC( a + b*np.sin( omega*i*dt ), 2*advectionSystem.getState()[-2] - advectionSystem.getState()[-3] )
+    advectionSystem.resetBC()
+    print( flt.getStateEstimate()[0:5] )
     
 est = flt.getStateEstimateList()
 #for state in est:
--- feelpp/pyfeelpp/kalman/ukf.py
+++ feelpp/pyfeelpp/kalman/ukf.py
@@ -5,9 +5,11 @@ from base import *
 
 class Filter:
 
-    def __init__( self, dynamics = lambda x : x, observe = lambda x : x, defect = 1, stateDim = 1, obsDim = 1 ):
+    def __init__( self, dynamics = lambda x : x, observe = lambda x : x, defect = 1, stateDim = 1, obsDim = 1, w0 = 0.5 ):
+        
         self.observe = observe
         self.transform = dynamics
+        self.time = 0
         
         self.stateDim = stateDim
         self.obsDim = obsDim
@@ -15,7 +17,7 @@ class Filter:
         self._stateEstimateList = numpy.zeros((stateDim,0))
         self._obsEstimateList = numpy.zeros((obsDim,0))
 
-        self._stateEstimate = numpy.zeros((stateDim,1))    
+        self._stateEstimate = numpy.ones((stateDim,1))    
         self._stateForecast = numpy.zeros((stateDim,1))
         self._obsEstimate = numpy.zeros((obsDim,1))
 
@@ -33,24 +35,37 @@ class Filter:
 
         self._alpha, self._beta, self._kappa, self._lambda, self._factor = 0, 0, 0, 0, 0
         
-        if 0: # method 1
+        if 0: # method 1 - seems unstable
             self.setNumericalParameters( 1, 0, 3-stateDim )
-            self._weightsMean = numpy.ones((1,2*stateDim+1))/(2*(stateDim+self._lambda))
+            self._weightsMean = numpy.ones((1,2*stateDim+1))/(2*(stateDim+self._kappa))
             self._weightsMean[0] = self._kappa/(self.stateDim+self._kappa)
-            # self._lambda/(self.stateDim+self._lambda)
-            self._weightsCov = numpy.copy(self._weightsMean)
-            self._weightsCov[0] += 1 - self._alpha**2 + self._beta
+            self._weightsCov = self._weightsMean.copy()
 
-        if 1: # method 2
+        if 0: # method 2 - seems unstable
+            self.setNumericalParameters( 1, 0, 3-stateDim )
+            self._weightsMean = numpy.ones((1,2*stateDim+1))*self._lambda/(2*(stateDim+self._lambda))
+            self._weightsCov = self._weightsMean.copy()
+            self._weightsCov[0] += 1 - self._alpha**2 + self._beta
+            self._factor = stateDim + self._lambda
+            
+        if 1: # method 3
             self._weightsMean = numpy.zeros((1,2*stateDim+1))
-            self._weightsMean[0,0] = 0.9 # or any value < 1
+            self._weightsMean[0,0] = w0 # or any value < 1
             self._weightsMean[0,1:] = (1-self._weightsMean[0,0])/(2*stateDim)
             self._weightsCov = self._weightsMean
             self._factor = stateDim/(1-self._weightsMean[0,0])
 
-    def setNumericalParameters( self, a, b, k ):
-        self._alpha, self._beta, self._kappa, self._lambda, self._factor = a, b, k, (self.stateDim + k)*a**2 - self.stateDim, self.stateDim + self._kappa
+        print("    Filter initialized with state dimension {} and observation dimension {}".format(stateDim, obsDim))
 
+    def setNumericalParameters( self, a, b, k ):
+        self._alpha, self._beta, self._kappa, self._lambda, self._factor = a, b, k, (self.stateDim + k)*a**2 - self.stateDim, self.stateDim + k
+
+    def stepTime( dt ):
+        self.time += dt
+
+    def resetTime():
+        self.time = 0
+        
     def setState( self, state ):
         self._stateEstimate = state
         
@@ -72,13 +87,13 @@ class Filter:
         return self._sigmaPoints
 
     def getStateEstimate( self ):
-        return self._stateEstimate
+        return self._stateEstimate.reshape(self.stateDim,1)
 
     def getStateForecast( self ):
-        return self._stateForecast
+        return self._stateForecast.reshape(self.stateDim,1)
 
     def getObsEstimate( self ): # this is the weighted mean of H(A(sigma-points))
-        return self._obsEstimate
+        return self._obsEstimate.reshape(self.obsDim,1)
 
     def getObsForecast( self ): # this is the set of H(A(sigma-points))
         return self._obsForecast
@@ -96,13 +111,13 @@ class Filter:
         return self._obsEstimateList
 
     def getNiter( self ):
-        return len(self.getStateEstimateList())
+        return self.getStateEstimateList().shape[1]
         
     def transformSet( self, stateSet ): # stateSet is a numpy matrix representing points as columns ; to be parallelized
-        self._sigmaPoints = numpy.apply_along_axis( self.transform, axis = 0, arr = stateSet )
+        self._sigmaPoints = numpy.apply_along_axis( self.transform, axis = 0, arr = stateSet, t = self.time )
 
     def observeSet( self, stateSet ): # stateSet is a numpy matrix representing points as columns ; to be parallelized
-        self._obsForecast = numpy.apply_along_axis( self.observe, axis = 0, arr = stateSet )
+        self._obsForecast = numpy.apply_along_axis( self.observe, axis = 0, arr = stateSet, t = self.time )
 
     def _sigmaCov( self, x ):
         return numpy.outer( x, x )
@@ -114,7 +129,7 @@ class Filter:
         return numpy.inner( self._weightsCov, x )
     
     def saveStep( self ):
-        self._stateEstimateList = numpy.hstack( (self._stateEstimateList, self.getStateEstimate().copy() ) )
+        self._stateEstimateList = numpy.hstack( ( self._stateEstimateList, self.getStateEstimate().copy() ) )
         self._obsEstimateList = numpy.hstack( (self._obsEstimateList,  self.getObsEstimate().copy() ) )
 
     def step( self, measurement ):
@@ -134,21 +149,40 @@ class Filter:
                 axis = 0,
                 arr = self._sigmaPoints-self._stateForecast
             )
-        ).reshape((self.stateDim,self.stateDim))
-        + self._sigmaAk
+        ).reshape((self.stateDim,self.stateDim)) + self._sigmaAk
 
         assert self._stateCov.dtype == "float64"
 
         self._obsEstimate = self._obsForecast @ self._weightsMean.T
-        self._obsCov =  numpy.apply_along_axis( self._weightedCov, axis = 2, arr = numpy.apply_along_axis( self._sigmaCov, axis = 0, arr = self._obsForecast-self._obsEstimate ) ).reshape((self.obsDim,self.obsDim)) + self._sigmaHk
+        
+        self._obsCov =  numpy.apply_along_axis(
+            self._weightedCov,
+            axis = 2,
+            arr = numpy.apply_along_axis(
+                self._sigmaCov,
+                axis = 0,
+                arr = (self._obsForecast-self._obsEstimate).reshape(self.obsDim,1+2*self.stateDim)
+            )
+        ).reshape((self.obsDim,self.obsDim)) + self._sigmaHk
 
-        assert [self._obsCov.dtype, self._obsCov.shape] == ["float64", (self.obsDim,self.obsDim)]
+        assert self._obsCov.dtype == "float64"
 
-        assert [self._crossCov.dtype, self._crossCov.shape] == ["float64", (self.stateDim, self.obsDim)]
+        assert self._crossCov.dtype == "float64"
+        
+        self._crossCov = numpy.apply_along_axis(
+            self._weightedCov,
+            axis = 2,
+            arr = numpy.apply_along_axis(
+                self._sigmaCrossCov,
+                axis = 0,
+                arr = numpy.vstack(
+                    (self._sigmaPoints - self._stateEstimate, self._obsForecast - self._obsEstimate)
+                ).reshape(self.stateDim+self.obsDim, 1+2*self.stateDim),
+                cut = self.stateDim
+            )
+        ).reshape((self.stateDim,self.obsDim))
 
-        self._crossCov = numpy.apply_along_axis( self._weightedCov, axis = 2, arr = numpy.apply_along_axis( self._sigmaCrossCov, axis = 0, arr = numpy.vstack( (self._sigmaPoints - self._stateEstimate, self._obsForecast - self._obsEstimate) ), cut = self.stateDim ) ).reshape((self.stateDim,self.obsDim))
-
-        assert [self._crossCov.dtype, self._crossCov.shape] == ["float64", (self.stateDim, self.obsDim)]
+        assert self._crossCov.dtype == "float64"
 
         self._gain = self._crossCov @ numpy.linalg.inv(self._obsCov)
 
@@ -156,7 +190,7 @@ class Filter:
 
         self._stateCov -= self._gain @ self._obsCov @ self._gain.T
         
-        self._stateEstimate += self._gain @ ( numpy.matrix(measurement).T - self._obsEstimate )
+        self._stateEstimate += self._gain @ ( measurement.reshape((self.obsDim,1)) - self._obsEstimate )
 
         self.saveStep()
 
