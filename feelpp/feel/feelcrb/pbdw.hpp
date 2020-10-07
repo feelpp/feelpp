b87dc087ac5026b7aa6d5596b0c3513d046eb67d
romainhild <hild.romain@gmail.com>
2020-10-07 11:23:15 +0200

    add database system to PBDW #1364 #1325

--- feelpp/feel/feelcrb/pbdw.hpp
+++ feelpp/feel/feelcrb/pbdw.hpp
@@ -30,6 +30,7 @@
 #include <feel/feelcore/unwrapptr.hpp>
 #include <feel/feeldiscr/reducedbasisspace.hpp>
 #include <feel/feeldiscr/sensors.hpp>
+#include <feel/feelcore/json.hpp>
 
 #include <limits>
 
@@ -40,30 +41,47 @@ namespace Feel
  * Class to support Parameterized Background Data-Weak method
  */
 template<typename RBSpace>
-class PBDW
+class PBDW : CRBDB
 {
 public:
+    using super_type = CRBDB;
     using reducedspace_type = RBSpace;
     using reducedspace_ptrtype = std::shared_ptr<reducedspace_type>;
     using space_type = typename reducedspace_type::fespace_type;
+    using space_ptrtype = std::shared_ptr<space_type>;
+    using mesh_type = typename space_type::mesh_type;
+    using node_t = typename mesh_type::node_type;
     using element_type = typename space_type::element_type;
     using vectorN_type = Eigen::VectorXd;
     using matrixN_type = Eigen::MatrixXd;
     using sensorbase_type = SensorBase<space_type>;
     using sensorbase_ptrtype = std::shared_ptr<sensorbase_type>;
+    static const int nDim = space_type::mesh_type::nDim;
 
     /**
-     * Constructor
+     * Constructor for the online phase
+     * @param name Name of pbdw
+     * @param l Loading type (rb,fe,all)
+     * @param uuid Uuid to use for the db
+     */
+    PBDW(std::string const& name,
+         crb::load l = crb::load::rb,
+         uuids::uuid const& uuid = uuids::nil_uuid());
+    /**
+     * Constructor for the offline phase
      * @param name Name of pbdw
      * @param XR Reduced Basis
      * @param sigmas Sensors to use
+     * @param uuid Uuid to use for the db
      */
     PBDW(std::string const& name,
          reducedspace_ptrtype const& XR,
-         std::vector<sensorbase_ptrtype> sigmas);
-    int dimensionN() const { return M_XR->size(); } /**< Dimension of Reduced Basis */
-    int dimensionM() const { return M_sigmas.size(); } /**< Number of sensors */
-    int dimension() const { return this->dimensionN()+this->dimensionM(); } /**< Dimension of PBDW */
+         std::vector<sensorbase_ptrtype> sigmas,
+         uuids::uuid const& uuid = uuids::nil_uuid());
+    int dimensionN() const { return M_N; } /**< Dimension of Reduced Basis */
+    int dimensionM() const { return M_M; } /**< Number of sensors */
+    int dimension() const { return M_N+M_M; } /**< Dimension of PBDW */
+    space_ptrtype functionSpace() const { return M_XR->functionSpace(); } /**< Function Space */
     matrixN_type matrix() const { return M_matrix; } /**< Matrix of PBDW */
     void offline(); /** Do offline phase */
     /**
@@ -79,21 +97,67 @@ public:
      */
     element_type solution(vectorN_type const& yobs) const; /**< Retrieve solution */
 
+protected:
+    void loadDB( std::string const& filename, crb::load l ) override;
+    void saveDB() override;
+
+
 private:
     std::string M_name;
     reducedspace_ptrtype M_XR;
     std::vector<sensorbase_ptrtype> M_sigmas;
     matrixN_type M_matrix;
+    int M_M;
+    int M_N;
+
+    bool M_rebuildDb;
+    int M_dbLoad;
+    std::string M_dbFilename;
+    std::string M_dbId;
+    crb::stage M_stage;
 };
 
 template<typename RBSpace>
 PBDW<RBSpace>::PBDW(std::string const& name,
+                    crb::load l,
+                    uuids::uuid const& uuid):
+    super_type(name, "pbdw", uuid),
+    M_name(name),
+    M_rebuildDb(boption("pbdw.rebuild-database")),
+    M_dbLoad(ioption("pbdw.db.load")),
+    M_dbFilename(soption("pbdw.db.filename")),
+    M_dbId(soption("pbdw.db.id")),
+    M_stage(crb::stage::online)
+{
+    if( ! this->findDBUuid(M_dbLoad, M_dbLoad ? M_dbId : M_dbFilename) )
+        throw std::invalid_argument("Database not found during online phase");
+
+    this->loadDB(this->absoluteDbFilename(), l );
+}
+
+template<typename RBSpace>
+PBDW<RBSpace>::PBDW(std::string const& name,
                     reducedspace_ptrtype const& XR,
-                    std::vector<sensorbase_ptrtype> sigmas):
+                    std::vector<sensorbase_ptrtype> sigmas,
+                    uuids::uuid const& uuid):
+    super_type(name, "pbdw", uuid),
     M_name(name),
     M_XR(XR),
-    M_sigmas(sigmas)
-{}
+    M_sigmas(sigmas),
+    M_M(M_sigmas.size()),
+    M_N(M_XR->size()),
+    M_rebuildDb(boption("pbdw.rebuild-database")),
+    M_dbLoad(ioption("pbdw.db.load")),
+    M_dbFilename(soption("pbdw.db.filename")),
+    M_dbId(soption("pbdw.db.id")),
+    M_stage(crb::stage::offline)
+{
+    if( ! this->findDBUuid(M_dbLoad, M_dbLoad ? M_dbId : M_dbFilename) )
+        this->setDBDirectory(Environment::randomUUID(true));
+
+    if( ! M_rebuildDb )
+        this->loadDB(this->absoluteDbFilename(), crb::load::all );
+}
 
 template<typename RBSpace>
 void
@@ -115,6 +179,8 @@ PBDW<RBSpace>::offline()
             M_matrix(i, M+j) = (*M_sigmas[i])(M_XR->primalBasisElement(j));
     }
     M_matrix.bottomLeftCorner(N, M) = M_matrix.topRightCorner(M, N).transpose();
+
+    this->saveDB();
 }
 
 template<typename RBSpace>
@@ -143,6 +209,136 @@ PBDW<RBSpace>::solution(vectorN_type const& yobs) const
     for(int i = 0; i < N; ++i )
         I.add(vn(M+i), unwrap_ptr(wn[i]));
     return I;
+}
+
+template<typename RBSpace>
+void
+PBDW<RBSpace>::saveDB()
+{
+    if ( this->worldComm().isMasterRank() )
+    {
+        if( fs::exists(this->dbLocalPath()) )
+        {
+            if( !fs::is_directory(this->dbLocalPath()) )
+                throw std::exception();
+        }
+        else
+            fs::create_directories(this->dbLocalPath());
+
+        json j;
+        j["name"] = this->name();
+        std::time_t t = std::time(nullptr);
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&t), "%c %Z");
+        j["date"] = ss.str();
+        j["mesh"] = this->absoluteMeshFilename();
+        std::ofstream o(this->absoluteJsonFilename());
+        o << j << std::endl;
+
+        fs::ofstream ofs( this->absoluteDbFilename() );
+        if ( ofs )
+        {
+            Feel::cout << "saving DB at " << this->absoluteDbFilename() << std::endl;
+            boost::archive::binary_oarchive oa( ofs );
+            oa << this->dimensionN();
+            oa << this->dimensionM();
+            oa << M_matrix;
+        }
+    }
+    if( ! fs::exists(this->absoluteMeshFilename()) )
+    {
+        Feel::cout << "Saving mesh to " << this->absoluteMeshFilename() << std::endl;
+        M_XR->functionSpace()->mesh()->saveHDF5( this->absoluteMeshFilename() );
+    }
+    fs::ofstream ofsp( this->absoluteDbFilenameProc() );
+    if( ofsp )
+    {
+        boost::archive::binary_oarchive oa( ofsp );
+        for( int n = 0; n < M_N; ++n )
+            oa << M_XR->primalBasisElement(n);
+        for( int m = 0; m < M_M; ++m )
+        {
+            auto t = M_sigmas[m]->type();
+            oa << t;
+            oa << M_sigmas[m]->name();
+            oa << M_sigmas[m]->position();
+            if( t == "pointwise" )
+            {
+            }
+            else if( t == "gaussian" )
+            {
+                auto s = std::dynamic_pointer_cast<SensorGaussian<space_type>>(M_sigmas[m]);
+                oa << s->radius();
+            }
+        }
+    }
+}
+
+template<typename RBSpace>
+void
+PBDW<RBSpace>::loadDB( std::string const& filename, crb::load l )
+{
+    if( !fs::exists(filename) )
+        return;
+
+    fs::ifstream ifs( filename );
+    if ( ifs )
+    {
+        Feel::cout << "loading DB at " << filename << std::endl;
+        boost::archive::binary_iarchive ia( ifs );
+        if( l > crb::load::none )
+        {
+            ia >> M_N;
+            ia >> M_M;
+            M_matrix.resize(M_M+M_N, M_M+M_N);
+            ia >> M_matrix;
+        }
+    }
+    if( l > crb::load::rb )
+    {
+        if( ! M_XR )
+        {
+            std::ifstream i(this->absoluteJsonFilename());
+            json j = json::parse(i);
+            auto meshfilename = j["mesh"].get<std::string>();
+            auto mesh = loadMesh(_mesh=new mesh_type, _filename=meshfilename);
+            auto Xh = space_type::New(mesh);
+            M_XR = std::make_shared<reducedspace_type>(Xh);
+            fs::ifstream ifsp( this->absoluteDbFilenameProc() );
+            if( ifsp )
+            {
+                boost::archive::binary_iarchive ia( ifsp );
+                auto u = Xh->element();
+                for( int n = 0; n < M_N; ++n )
+                {
+                    ia >> u;
+                    M_XR->addPrimalBasisElement(u);
+                }
+                node_t n(nDim);
+                for( int m = 0; m < M_M; ++m )
+                {
+                    std::string type;
+                    ia >> type;
+                    std::string name;
+                    ia >> name;
+                    node_t n(nDim);
+                    ia >> n;
+                    if( type == "pointwise" )
+                    {
+                        auto s = std::make_shared<SensorPointwise<space_type> >(Xh, n, name);
+                        M_sigmas.push_back(s);
+                    }
+                    else if( type == "gaussian" )
+                    {
+                        double r;
+                        ia >> r;
+                        auto s = std::make_shared<SensorGaussian<space_type> >(Xh, n, r, name);
+                        M_sigmas.push_back(s);
+                    }
+                }
+            }
+        }
+    }
 }
 
 } // namespace Feel
--- testsuite/feelcrb/test_pbdw.cpp
+++ testsuite/feelcrb/test_pbdw.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE pbdw testsuite
 
-//#include <feel/feelcore/testsuite.hpp>
-#define USE_BOOST_TEST 0
+#include <feel/feelcore/testsuite.hpp>
+#define USE_BOOST_TEST 1
 
 #include <feel/feelfilters/exporter.hpp>
 #include <feel/feelcrb/pbdw.hpp>
@@ -18,10 +18,14 @@ po::options_description makeOptions()
 
     options.add( feel_options() );
     options.add_options()
-        ("nb-sensors", po::value<int>()->default_value(3), "nb of sensors in x direction")
+        ("nb-sensors", po::value<int>()->default_value(4), "nb of sensors in x direction")
         ("radius", po::value<double>()->default_value(0.025), "radius of sensors")
         ("trainset-size", po::value<int>()->default_value(7), "number of parameter in the trainset")
         ( "do-ortho", po::value<bool>()->default_value(false), "")
+        ( "pbdw.rebuild-database", po::value<bool>()->default_value(true), "rebuild the database" )
+        ( "pbdw.db.load", po::value<int>()->default_value(2), "=0 use db.filename, =1 use last DB created =2 use last DB modified =3 use db.id =4 create new db" )
+        ( "pbdw.db.filename", po::value<std::string>()->default_value(""), "path to the db when db.load or db.update = 0" )
+        ( "pbdw.db.id", po::value<std::string>()->default_value(""), "id of the db when db.load or db.update = 3" )
         ;
     options.add(backend_options("pbdw"));
 
@@ -91,8 +95,176 @@ BOOST_AUTO_TEST_SUITE( pbdwsuite )
 
 BOOST_AUTO_TEST_CASE( test_pbdw_offline )
 {
+    using parameterspace_type = ParameterSpaceX;
+    using parameter_type = typename parameterspace_type::element_type;
+    using mesh_type = Mesh<Simplex<2> >;
+    using space_type = Pch_type<mesh_type, 1>;
+    using reducedspace_type = ReducedBasisSpace<space_type>;
+    using element_type = typename space_type::element_type;
+    using element_ptrtype = std::shared_ptr<element_type>;
+    using sensorbase_type = SensorBase<space_type>;
+    using sensorbase_ptrtype = std::shared_ptr<sensorbase_type>;
+    using sensor_type = SensorGaussian<space_type>;
+    using node_t = typename sensor_type::node_t;
+
+    int nbSensors = ioption("nb-sensors");
+    double r = doption("radius");
+    int N = ioption("trainset-size");
+    int M = nbSensors*nbSensors;
+
+    auto mesh = loadMesh( _mesh=new mesh_type, _filename="test_geim.geo");
+    auto Xh = Pch<1>(mesh);
+    auto u = Xh->element();
+    auto XR = std::make_shared<reducedspace_type>(Xh);
+
+    auto Dmu = parameterspace_type::New(4);
+    auto muMin = Dmu->element();
+    muMin << 0.1, -2, -2, -2;
+    Dmu->setMin(muMin);
+    auto muMax = Dmu->element();
+    muMax << 3, 2, 2, 2;
+    Dmu->setMax(muMax);
+    auto Pset = Dmu->sampling();
+    Pset->randomize(N);
+
+    std::vector<sensorbase_ptrtype> sigmas;
+    for( int i = 1; i < nbSensors+1; ++i )
+    {
+        for( int j = 1; j < nbSensors+1; ++j )
+        {
+            node_t n(2);
+            n(0) = i*1./(nbSensors+1);
+            n(1) = j*1./(nbSensors+1);
+            auto s = std::make_shared<sensor_type>(Xh, n, r,
+                                                   "sensors_"+std::to_string((i-1)*nbSensors+(j-1)));
+            sigmas.push_back(s);
+        }
+    }
+
+    auto solver = [mesh,Xh](parameter_type const& mu) -> element_type {
+                      auto u = Xh->element();
+                      auto a = form2(_test=Xh, _trial=Xh);
+                      auto f = form1(_test=Xh);
+                      a = integrate(_range=markedelements(mesh, "Omega1"),
+                                    _expr=inner(gradt(u),grad(u)) );
+                      a += integrate(_range=markedelements(mesh, "Omega2"),
+                                     _expr=mu(0)*inner(gradt(u),grad(u)) );
+                      f = integrate(_range=markedfaces(mesh, "left"), _expr=mu(1)*id(u));
+                      f += integrate(_range=markedfaces(mesh, "right"), _expr=mu(2)*id(u));
+                      a += on(_range=markedfaces(mesh, std::list<std::string>({"bottom","top"})),
+                              _rhs=f, _element=u, _expr=cst(mu(3)));
+                      a.solve(_rhs=f, _solution=u, _name="pbdw");
+                      return u;
+                  };
+
+    for( auto const& mu : *Pset )
+    {
+        auto u = solver(mu);
+        XR->addPrimalBasisElement(u);
+    }
+    if( boption("do-ortho") )
+    {
+        auto wn = XR->primalRB();
+        do {} while( !orthonormalize(wn) );
+    }
+
+    PBDW pbdw("test_pbdw", XR, sigmas);
+    pbdw.offline();
+    auto m = pbdw.matrix();
+
+    Pset->randomize(20);
+    std::vector<double> errors;
+    for( auto const& mu : *Pset )
+    {
+        auto phi = solver(mu);
+        auto vn = vectorN_type(M);
+        for(int i = 0; i < M; ++i)
+            vn(i) = (*sigmas[i])(phi);
+        auto I = pbdw.solution(vn);
+        errors.push_back( normL2(_range=elements(mesh),_expr=idv(phi)-idv(I)) );
+    }
+    double max = *max_element(errors.begin(), errors.end());
+    BOOST_CHECK_SMALL( max , 1e-5 );
 }
+
+BOOST_AUTO_TEST_CASE( test_pbdw_online )
+{
+    using parameterspace_type = ParameterSpaceX;
+    using parameter_type = typename parameterspace_type::element_type;
+    using mesh_type = Mesh<Simplex<2> >;
+    using space_type = Pch_type<mesh_type, 1>;
+    using reducedspace_type = ReducedBasisSpace<space_type>;
+    using element_type = typename space_type::element_type;
+    using element_ptrtype = std::shared_ptr<element_type>;
+    using sensorbase_type = SensorBase<space_type>;
+    using sensorbase_ptrtype = std::shared_ptr<sensorbase_type>;
+    using sensor_type = SensorGaussian<space_type>;
+    using node_t = typename sensor_type::node_t;
+
+    double r = doption("radius");
+    auto pbdw = PBDW<reducedspace_type>("test_pbdw", crb::load::fe);
+    auto M = pbdw.dimensionM();
+    auto nbSensors = std::sqrt(M);
+    auto N = pbdw.dimensionN();
+    auto Xh = pbdw.functionSpace();
+    auto mesh = Xh->mesh();
+
+    std::vector<sensorbase_ptrtype> sigmas;
+    for( int i = 1; i < nbSensors+1; ++i )
+    {
+        for( int j = 1; j < nbSensors+1; ++j )
+        {
+            node_t n(2);
+            n(0) = i*1./(nbSensors+1);
+            n(1) = j*1./(nbSensors+1);
+            auto s = std::make_shared<sensor_type>(Xh, n, r,
+                                                   "sensors_"+std::to_string((i-1)*nbSensors+(j-1)));
+            sigmas.push_back(s);
+        }
+    }
+
+    auto solver = [mesh,Xh](parameter_type const& mu) -> element_type {
+                      auto u = Xh->element();
+                      auto a = form2(_test=Xh, _trial=Xh);
+                      auto f = form1(_test=Xh);
+                      a = integrate(_range=markedelements(mesh, "Omega1"),
+                                    _expr=inner(gradt(u),grad(u)) );
+                      a += integrate(_range=markedelements(mesh, "Omega2"),
+                                     _expr=mu(0)*inner(gradt(u),grad(u)) );
+                      f = integrate(_range=markedfaces(mesh, "left"), _expr=mu(1)*id(u));
+                      f += integrate(_range=markedfaces(mesh, "right"), _expr=mu(2)*id(u));
+                      a += on(_range=markedfaces(mesh, std::list<std::string>({"bottom","top"})),
+                              _rhs=f, _element=u, _expr=cst(mu(3)));
+                      a.solve(_rhs=f, _solution=u, _name="pbdw");
+                      return u;
+                  };
+
+    auto Dmu = parameterspace_type::New(4);
+    auto muMin = Dmu->element();
+    muMin << 0.1, -2, -2, -2;
+    Dmu->setMin(muMin);
+    auto muMax = Dmu->element();
+    muMax << 3, 2, 2, 2;
+    Dmu->setMax(muMax);
+    auto Pset = Dmu->sampling();
+    Pset->randomize(20);
+
+    std::vector<double> errors;
+    for( auto const& mu : *Pset )
+    {
+        auto phi = solver(mu);
+        auto vn = vectorN_type(M);
+        for(int i = 0; i < M; ++i)
+            vn(i) = (*sigmas[i])(phi);
+        auto I = pbdw.solution(vn);
+        errors.push_back( normL2(_range=elements(mesh),_expr=idv(phi)-idv(I)) );
+    }
+    double max = *max_element(errors.begin(), errors.end());
+    BOOST_CHECK_SMALL( max , 1e-5 );
+}
+
 BOOST_AUTO_TEST_SUITE_END()
+
 #else
 int main(int argc, char** argv)
 {
@@ -172,7 +344,7 @@ int main(int argc, char** argv)
         do {} while( !orthonormalize(wn) );
     }
 
-    auto e1 = exporter(_mesh=mesh,_name="test_geim_rb");
+    auto e1 = exporter(_mesh=mesh,_name="test_pbdw_rb");
     int i = 0;
     for( auto const& b : XR->primalRB() )
         e1->add("b_"+std::to_string(i++), *b);
