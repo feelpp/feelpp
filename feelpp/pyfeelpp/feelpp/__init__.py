17face7667e9add5518142175d99e6408fe7ae2b
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2020-10-22 13:12:01 +0200

    work on python-feelpp #930
    - start support for  MeshStructured
    - define FEELPP_PYTHON_MODEULE_PATH
    - add trait value for is_hypercube

--- feelpp/pyfeelpp/feelpp/__init__.py
+++ feelpp/pyfeelpp/feelpp/__init__.py
@@ -19,17 +19,18 @@ def download(data,worldComm):
         raise RuntimeError("Remote Data " + data + " cannot be downloaded")
 
 _meshes={
-    'mesh(3,1,3)':Mesh_3DG1R3,
+    'mesh(3,1,3)':Mesh_S3DG1R3,
     #'mesh(3,2,3)':Mesh_3DG2R3,
     #'mesh(2,1,3)':Mesh_2DG1R3,
     #'mesh(2,2,3)':Mesh_2DG2R3,
     #'mesh(1,1,3)':Mesh_1DG1R3,
     #'mesh(1,2,3)':Mesh_1DG2R3,
-    'mesh(2,1,2)':Mesh_2DG1R2,
-    'mesh(2,2,2)':Mesh_2DG2R2,
+    'mesh(2,1,2)':Mesh_S2DG1R2,
+    'meshstructured(2,1,2)':Mesh_H2DG1R2,
+    'mesh(2,2,2)':Mesh_S2DG2R2,
     #'mesh(1,1,2)':Mesh_1DG1R2,
     #'mesh(1,2,2)':Mesh_1DG2R2,
-    'mesh(1,1,1)':Mesh_1DG1R1,
+    'mesh(1,1,1)':Mesh_S1DG1R1,
     #'mesh(1,2,1)':Mesh_1DG2R1
 }
 
@@ -49,6 +50,24 @@ def mesh( dim=2, geo=1, realdim=2, worldComm=None ):
     if key not in _meshes:
         raise RuntimeError('Mesh '+key+' no existing')
     return _meshes[key]( worldComm )
+
+def meshstructured( dim=2, geo=1, realdim=2, worldComm=None ):
+    """create a structured mesh 
+    The mesh is of topological dimension 'dim', in real dimension 'realdim' with geometric order 'geo'.
+    The mesh is configured with a WorldComm which provides the parallel process layout.
+    Keyword arguments:
+    dim -- the topological dimension (default: 2)
+    geo -- the geometrical order (default: 1)
+    realdim -- the real dimension (default: 2)
+    worldComm -- the parallel communicator for the mesh (default: Environment::worldCommPtr())
+    """
+    if worldComm is None:
+        worldComm=Environment.worldCommPtr()
+    key='meshstructured('+str(dim)+','+str(geo)+','+str(realdim)+')'
+    if key not in _meshes:
+        raise RuntimeError('Mesh '+key+' no existing')
+    return _meshes[key]( worldComm )
+
 
 
 _spaces={
