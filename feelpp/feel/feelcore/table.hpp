fa0719aa92262655c046c27382c49287e17c2152
Vincent Chabannes <vincent.chabannes@feelpp.org>
2021-01-06 20:23:29 +0100

    up Table : refactoring + add font color

--- feelpp/feel/feelcore/table.hpp
+++ feelpp/feel/feelcore/table.hpp
@@ -28,141 +28,78 @@
 #include <string>
 #include <vector>
 #include <optional>
+#include <tuple>
+
+#include <numeric>
+
+#include <feel/feelcore/feel.hpp>
 
 namespace Feel
 {
-struct TableBase
-{
-    virtual ~TableBase() {}
-};
 
 namespace Font
 {
 enum class Align { left, right, center };
 
 enum class FloatingPoint { defaultfloat, fixed, scientific, hexfloat };
+
+enum class Color { none, grey, red, green, yellow, blue, magenta, cyan, white };
+
 }
 
-class Table : public TableBase
+namespace Printer
+{
+class OutputText
 {
 public :
-    class Cell
-    {
-    public:
+    OutputText() = default;
+    explicit OutputText( std::string const& s ) { this->push_back( s ); }
+    OutputText( OutputText && ) = default;
+    OutputText( OutputText const& ) = default;
 
-        struct Format
+    OutputText& operator=( OutputText && ) = default;
+    OutputText& operator=( OutputText const& ) = default;
+
+    void push_back( std::string const& s ) { M_data.push_back( std::make_pair( s, std::make_tuple( Font::Color::none ) ) ); }
+    void push_back( OutputText const& ot )
         {
-            Format() = default;
-            Format( Format const& ) = default;
-            Format( Format && ) = default;
-            Format& operator=( Format const& ) = default;
-            Format& operator=( Format && ) = default;
+            for ( auto const& d : ot.data() )
+                M_data.push_back( d );
+        }
+    void push_front( std::string const& s ) { M_data.insert( M_data.begin(), std::make_pair( s, std::make_tuple( Font::Color::none ) ) ); }
+    size_t size() const { return std::accumulate( M_data.begin(), M_data.end(), 0, []( size_t res, auto const& e ) { return res + e.first.size(); } ); }
 
-            Font::Align fontAlign() const { return M_fontAlign? *M_fontAlign : Font::Align::left; }
-            Format& setFontAlign( Font::Align a ) { M_fontAlign = a; return *this; }
+    std::vector<std::pair<std::string,std::tuple<Font::Color>> > const& data() const { return M_data; }
+    std::vector<std::pair<std::string,std::tuple<Font::Color>> > & data() { return M_data; }
 
-            int paddingLeft() const { return M_paddingLeft? *M_paddingLeft : 1; }
-            int paddingRight() const { return M_paddingRight? *M_paddingRight : 1; }
-            Format& setPaddingLeft( int v ) { if ( v < 0 ) return *this; M_paddingLeft = v; return *this; }
-            Format& setPaddingRight( int v ) { if ( v < 0 ) return *this; M_paddingRight = v; return *this; }
-            Format& setAllPadding( int v ) { if ( v < 0 ) return *this; M_paddingLeft = v; M_paddingRight = v; return *this; }
+    OutputText& operator<<( std::string const& s ) { this->push_back( s ); return *this; }
+    OutputText& operator<<( OutputText const& ot ) { this->push_back( ot ); return *this; }
 
-            Font::FloatingPoint floatingPoint() const { return M_fontFloatingPoint? *M_fontFloatingPoint : Font::FloatingPoint::scientific; }
-            Format& setFloatingPoint( Font::FloatingPoint a ) { M_fontFloatingPoint = a; return *this; }
+    void setColor( Font::Color c ) { for ( auto & [s,opt] : M_data ) std::get<0>( opt ) = c; }
+private:
+    std::vector<std::pair<std::string,std::tuple<Font::Color>> > M_data;
+};
 
-            int widthMax() const { return M_widthMax? *M_widthMax : -1; }
-            Format& setWidthMax( int i ) { M_widthMax = i; return *this; }
-
-            Format newFromParent( Format const& parentFormat ) const;
-
-        private :
-            std::optional<Font::Align> M_fontAlign;
-            std::optional<Font::FloatingPoint> M_fontFloatingPoint;
-            std::optional<int> M_paddingLeft, M_paddingRight;
-
-            std::optional<int> M_widthMax;
-        };
-        using variant_value_type = std::variant<std::string,int,size_t,double,Feel::TableBase>;
-
-        Cell() = default;
-        explicit Cell( variant_value_type const& v ) : M_value( v ) {}
-        Cell( Cell const& ) = default;
-        Cell( Cell && ) = default;
-
-        Cell& operator=( Cell const& ) = default;
-        Cell& operator=( Cell && ) = default;
-
-        Format& format() { return M_format; }
-        Format const& format() const { return M_format; }
-
-        std::vector<std::string> toRawString( Format const& format ) const;
-
-        static void updateStream( std::ostream &o, std::string const& input, size_t width, Format const& format );
-
-    private:
-        friend std::ostream& operator<<(std::ostream& o, Cell const& c );
-
-    private:
-        variant_value_type M_value;
-        Format M_format;
-    };
-
-    /**
-     * Format of a Table
-     * inherits of Cell::Format which define the defaut format of cell (can be override in each cell
-     */
-    struct Format : public Cell::Format
-    {
-        Format() = default;
-        Format( Format const& ) = default;
-        Format( Format && ) = default;
-        Format& operator=( Format const& ) = default;
-        Format& operator=( Format && ) = default;
-
-        bool hasRowSeparator() const { return M_hasRowSeparator; }
-        Format& setHasRowSeparator( bool b ) { M_hasRowSeparator = b; return *this; }
-        bool hasColumnSeparator() const { return M_hasColumnSeparator; }
-        Format& setHasColumnSeparator( bool b ) { M_hasColumnSeparator = b; return *this; }
-
-        std::string const& rowSeparator() const { return M_rowSeparator; }
-        Format& setRowSeparator( std::string const& s ) { M_rowSeparator = s; return *this; }
-        std::string const& columnSeparator() const { return M_columnSeparator; }
-        Format& setColumnSeparator( std::string const& s ) { M_columnSeparator = s; return *this; }
-
-        bool showLeftBorder() const { return M_showLeftBorder; }
-        bool showRightBorder() const { return M_showRightBorder; }
-        bool showTopBorder() const { return M_showTopBorder; }
-        bool showBottomBorder() const { return M_showBottomBorder; }
-        Format& setShowLeftBorder( bool b ) { M_showLeftBorder = b; return *this; }
-        Format& setShowRightBorder( bool b ) { M_showRightBorder = b; return *this; }
-        Format& setShowTopBorder( bool b ) { M_showTopBorder = b; return *this; }
-        Format& setShowBottomBorder( bool b ) { M_showBottomBorder = b; return *this; }
-        Format& setShowAllBorders( bool b ) { this->setShowLeftBorder( b ); this->setShowRightBorder( b ); this->setShowTopBorder( b ); this->setShowBottomBorder( b ); return *this; }
+}
 
 
-        bool firstRowIsHeader() const { return M_firstRowIsHeader; }
-        Format& setFirstRowIsHeader( bool b ) { M_firstRowIsHeader = b; return *this; }
-        bool firstColumnIsHeader() const { return M_firstColumnIsHeader; }
-        Format& setFirstColumnIsHeader( bool b ) { M_firstColumnIsHeader = b; return *this; }
-    private :
+namespace TableImpl
+{
+class Cell;
+struct Format;
+}
 
-        bool M_hasRowSeparator = true;
-        bool M_hasColumnSeparator = true;
-        std::string M_rowSeparator = "-";
-        std::string M_columnSeparator = "|";
-        bool M_showLeftBorder = true;
-        bool M_showRightBorder = true;
-        bool M_showTopBorder = true;
-        bool M_showBottomBorder = true;
-        bool M_firstRowIsHeader = false;
-        bool M_firstColumnIsHeader = false;
-    };
+class Table
+{
+public :
+    using variant_value_type = std::variant<std::string,int,size_t,double,Feel::Table,Printer::OutputText,std::vector<Printer::OutputText>>;
 
-    Table() : M_nRow(0), M_nCol(0) {}
-    Table( Table const& ) = default;
+    Table();
+    //Table( Table const& ) = default;
+    Table( Table const& t );
     Table( Table && ) = default;
-
-    using variant_value_type = typename Cell::variant_value_type;
+    Table& operator=( Table const& ) = default;
+    Table& operator=( Table && ) = default;
 
     //! number of row in table
     int nRow() const { return M_nRow; }
@@ -180,36 +117,165 @@ public :
         }
 
     //! get cell at indices i,j in table
-    Cell const& operator()(int i,int j) const { return M_cells[i*M_nCol+j]; }
+    TableImpl::Cell const& operator()(int i,int j) const { return M_cells[i*M_nCol+j]; }
 
     //! get cell at indices i,j in table
-    Cell & operator()(int i,int j) { return M_cells[i*M_nCol+j]; }
+    TableImpl::Cell & operator()(int i,int j) { return M_cells[i*M_nCol+j]; }
 
     //! add row in table
     void add_row( std::initializer_list<variant_value_type> const& rowValues );
 
     //! get table format
-    Format& format() { return M_format; }
+    TableImpl::Format& format() { return *M_format; }
     //! get table format
-    Format const& format() const { return M_format; }
+    TableImpl::Format const& format() const { return *M_format; }
 
     void exportAsciiDoc( std::ostream &o ) const;
 
-    std::vector<std::string> toRawString( Cell::Format const& format ) const;
-private :
-    static void updateOutputStreamOfCellUsingAsciiDoc( std::ostream &o, Cell const& c, Cell::Format const& format );
+    std::vector<Printer::OutputText> toOutputText() const { return this->toOutputText( this->format() ); }
+    //private :
+    std::vector<Printer::OutputText> toOutputText( TableImpl::Format const& format ) const;
 
 private :
     int M_nRow, M_nCol;
-    std::vector<Cell> M_cells;
+    std::vector<TableImpl::Cell> M_cells;
+    std::unique_ptr<TableImpl::Format> M_format;
+};
+
+std::ostream&
+operator<<(std::ostream& o, Printer::OutputText const& cb );
+
+std::ostream&
+operator<<(std::ostream& o, Table const& cb );
+
+std::ostream&
+operator<<( std::ostream& o, TableImpl::Cell const& c );
+
+
+namespace TableImpl
+{
+class Cell
+{
+public:
+
+    struct Format
+    {
+        Format() = default;
+        Format( Format const& ) = default;
+        Format( Format && ) = default;
+        Format& operator=( Format const& ) = default;
+        Format& operator=( Format && ) = default;
+
+        Font::Align fontAlign() const { return M_fontAlign? *M_fontAlign : Font::Align::left; }
+        Format& setFontAlign( Font::Align a ) { M_fontAlign = a; return *this; }
+
+        Font::Color fontColor() const { return M_fontColor? *M_fontColor : Font::Color::none; }
+        Format& setFontColor( Font::Color a ) { M_fontColor = a; return *this; }
+
+        int paddingLeft() const { return M_paddingLeft? *M_paddingLeft : 1; }
+        int paddingRight() const { return M_paddingRight? *M_paddingRight : 1; }
+        Format& setPaddingLeft( int v ) { if ( v < 0 ) return *this; M_paddingLeft = v; return *this; }
+        Format& setPaddingRight( int v ) { if ( v < 0 ) return *this; M_paddingRight = v; return *this; }
+        Format& setAllPadding( int v ) { if ( v < 0 ) return *this; M_paddingLeft = v; M_paddingRight = v; return *this; }
+
+        Font::FloatingPoint floatingPoint() const { return M_fontFloatingPoint? *M_fontFloatingPoint : Font::FloatingPoint::scientific; }
+        Format& setFloatingPoint( Font::FloatingPoint a ) { M_fontFloatingPoint = a; return *this; }
+
+        int widthMax() const { return M_widthMax? *M_widthMax : -1; }
+        Format& setWidthMax( int i ) { M_widthMax = i; return *this; }
+
+        Format newFromParent( Format const& parentFormat ) const;
+
+    private :
+        std::optional<Font::Align> M_fontAlign;
+        std::optional<Font::Color> M_fontColor;
+        std::optional<Font::FloatingPoint> M_fontFloatingPoint;
+        std::optional<int> M_paddingLeft, M_paddingRight;
+
+        std::optional<int> M_widthMax;
+    };
+    using variant_value_type = Table::variant_value_type;
+
+    Cell() = default;
+    explicit Cell( variant_value_type const& v ) : M_value( v ) {}
+    Cell( Cell const& ) = default;
+    Cell( Cell && ) = default;
+
+    Cell& operator=( Cell const& ) = default;
+    Cell& operator=( Cell && ) = default;
+
+    Format& format() { return M_format; }
+    Format const& format() const { return M_format; }
+
+    std::vector<Printer::OutputText> toOutputText( Format const& format, bool enableWidthMax = true ) const;
+
+    static void updateWidthAndHorizontalAlign( Printer::OutputText & ot, size_t width, Format const& format );
+
+private:
+    friend std::ostream& operator<<(std::ostream& o, Cell const& c );
+
+private:
+    variant_value_type M_value;
     Format M_format;
 };
 
 
-std::ostream&
-operator<<(std::ostream& o, TableBase const& cb );
+/**
+ * Format of a Table
+ * inherits of Cell::Format which define the defaut format of cell (can be override in each cell
+ */
+struct Format : public Cell::Format
+{
+    Format() = default;
+    Format( Format const& ) = default;
+    Format( Format && ) = default;
+    Format& operator=( Format const& ) = default;
+    Format& operator=( Format && ) = default;
 
-std::ostream&
-operator<<( std::ostream& o, Table::Cell const& c );
+    Format( Cell::Format const& f )
+        : Cell::Format( f ) {}
+
+    bool hasRowSeparator() const { return M_hasRowSeparator; }
+    Format& setHasRowSeparator( bool b ) { M_hasRowSeparator = b; return *this; }
+    bool hasColumnSeparator() const { return M_hasColumnSeparator; }
+    Format& setHasColumnSeparator( bool b ) { M_hasColumnSeparator = b; return *this; }
+
+    std::string const& rowSeparator() const { return M_rowSeparator; }
+    Format& setRowSeparator( std::string const& s ) { M_rowSeparator = s; return *this; }
+    std::string const& columnSeparator() const { return M_columnSeparator; }
+    Format& setColumnSeparator( std::string const& s ) { M_columnSeparator = s; return *this; }
+
+    bool showLeftBorder() const { return M_showLeftBorder; }
+    bool showRightBorder() const { return M_showRightBorder; }
+    bool showTopBorder() const { return M_showTopBorder; }
+    bool showBottomBorder() const { return M_showBottomBorder; }
+    Format& setShowLeftBorder( bool b ) { M_showLeftBorder = b; return *this; }
+    Format& setShowRightBorder( bool b ) { M_showRightBorder = b; return *this; }
+    Format& setShowTopBorder( bool b ) { M_showTopBorder = b; return *this; }
+    Format& setShowBottomBorder( bool b ) { M_showBottomBorder = b; return *this; }
+    Format& setShowAllBorders( bool b ) { this->setShowLeftBorder( b ); this->setShowRightBorder( b ); this->setShowTopBorder( b ); this->setShowBottomBorder( b ); return *this; }
+
+
+    bool firstRowIsHeader() const { return M_firstRowIsHeader; }
+    Format& setFirstRowIsHeader( bool b ) { M_firstRowIsHeader = b; return *this; }
+    bool firstColumnIsHeader() const { return M_firstColumnIsHeader; }
+    Format& setFirstColumnIsHeader( bool b ) { M_firstColumnIsHeader = b; return *this; }
+private :
+
+    bool M_hasRowSeparator = true;
+    bool M_hasColumnSeparator = true;
+    std::string M_rowSeparator = "-";
+    std::string M_columnSeparator = "|";
+    bool M_showLeftBorder = true;
+    bool M_showRightBorder = true;
+    bool M_showTopBorder = true;
+    bool M_showBottomBorder = true;
+    bool M_firstRowIsHeader = false;
+    bool M_firstColumnIsHeader = false;
+};
+
+void updateOutputStreamOfCellUsingAsciiDoc( std::ostream &o, Cell const& c, Cell::Format const& format );
+
+} // namespace TableImpl
 
 } // namespace Feel
