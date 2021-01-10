9b0bab97975e0a41a06a105114d6787eef9f2dc6
Luca Berti <lberti21@gmail.com>
2021-01-10 12:10:40 +0100

    test_remesh update

--- testsuite/feelmesh/test_remesh.cpp
+++ testsuite/feelmesh/test_remesh.cpp
@@ -139,4 +139,301 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( testnD, T, dim_types )
     ein->save();
 
 }
+
+
+// testcase with several successive remeshing steps on a square/cube
+BOOST_AUTO_TEST_CASE_TEMPLATE( testMultipleRemesh, T, dim_types )
+{
+    using namespace Feel;
+    
+    if ( Environment::isParallel() && T::first_type::value == 2 )
+        return;
+
+    typedef Mesh<Simplex<T::first_type::value,1,T::second_type::value>> mesh_type;
+    typedef std::shared_ptr<  mesh_type > mesh_ptrtype;
+
+    double meshSize = doption("gmsh.hsize");
+
+    auto create_mesh = [&](){
+                           if constexpr ( T::first_type::value == 2 && T::second_type::value == 2 ) 
+                           {
+                               return unitSquare();
+                           }
+                           else if constexpr ( T::first_type::value == 2 && T::second_type::value == 3 )
+                           {
+                               auto m = unitCube();
+                               auto mS = createSubmesh( _mesh=m, _range=boundaryfaces( m ) );
+                               return mS;
+                           }
+                           else if constexpr ( T::first_type::value == 3 )
+                           {
+                               return unitCube();
+                           }
+                       };
+    auto m = create_mesh();
+    auto out = create_mesh();
+    auto ein = exporter( _mesh=m, _name="initial",_geo="change");
+    auto eout = exporter( _mesh=out, _name="remeshed",_geo="change" );
+    std::string metric[4] = { "(abs(x)+1):x",
+                              "x*x:x",
+                              "exp(x):x",
+                              "(x*x+y*y+1):x:y"};
+    int ind =0;
+    for(int i=0;i<20;i++)
+    {
+        
+        auto Xh = Pch<1>( m );
+        auto met = Xh->element();
+        met.on( _range=elements(m), _expr=cst(meshSize)*expr(metric[ind]) );
+        auto r =  remesher( m );
+    
+        r.setMetric( met );
+        auto out = r.execute();
+        out->updateForUse();
+        auto Vh = Pch<1>( out );
+        auto interp = I( _domainSpace=Xh, _imageSpace=Vh );
+        auto v= interp.operator()( met );
+        auto w= Vh->element( cst(meshSize)*expr(metric[ind] ));
+        double err = normL2(_range=elements(out), _expr=idv(v)-idv(w));
+        BOOST_MESSAGE( "L2 error norm: " << err );
+        std::cout << "L2 error norm: " << err << std::endl;
+        eout->step(i)->setMesh(out);
+        eout->step(i)->add( "metricout", v );
+        eout->save();
+        
+        ein->step(i)->setMesh(m);
+        ein->step(i)->add( "metric", met );
+        ein->save();
+        m = out;
+        ind++;
+        ind = ind%4; std::cout << "index = " << ind <<std::endl;
+    }
+}
+// test checking vector interpolation after multiple remeshing
+BOOST_AUTO_TEST_CASE_TEMPLATE( testVecInterp, T, dim_types )
+{
+    using namespace Feel;
+    
+    if ( Environment::isParallel() && T::first_type::value == 2 )
+        return;
+
+    typedef Mesh<Simplex<T::first_type::value,1,T::second_type::value>> mesh_type;
+    typedef std::shared_ptr<  mesh_type > mesh_ptrtype;
+
+    double meshSize = doption("gmsh.hsize");
+
+    auto create_mesh = [&](){
+                           if constexpr ( T::first_type::value == 2 && T::second_type::value == 2 ) 
+                           {
+                               return unitSquare();
+                           }
+                           else if constexpr ( T::first_type::value == 2 && T::second_type::value == 3 )
+                           {
+                               auto m = unitCube();
+                               auto mS = createSubmesh( _mesh=m, _range=boundaryfaces( m ) );
+                               return mS;
+                           }
+                           else if constexpr ( T::first_type::value == 3 )
+                           {
+                              // auto mesh = loadMesh(_mesh=new Mesh<Simplex<3,1>>( "mesh" ),_filename="cube_cubeHole.geo");
+                               //return mesh;
+                               return unitCube();
+                           }
+                       };
+    auto m = create_mesh();
+    auto out = create_mesh();
+    auto ein = exporter( _mesh=m, _name="initial",_geo="change");
+    auto eout = exporter( _mesh=out, _name="remeshed",_geo="change" );
+    std::string metric[4] = { "(abs(x)+1):x",
+                              "x*x:x",
+                              "exp(x):x",
+                              "(x*x+y*y+1):x:y"};
+    int ind =0;
+    for(int i=0;i<20;i++)
+    {
+        
+        auto Xh = Pch<1>( m );
+        auto met = Xh->element();
+        met.on( _range=elements(m), _expr=cst(meshSize)*expr(metric[ind]) );
+        auto Th = Pchv<1>(m);
+        auto fieldToInterp = Th->element();
+        fieldToInterp.on(_range=elements(m), _expr=P()*cst(meshSize)*expr(metric[ind]) );
+        auto r =  remesher( m);
+    
+        r.setMetric( met );
+        out = r.execute();
+        out->updateForUse();
+        auto Vh = Pchv<1>( out );
+        auto interp = I( _domainSpace=Th, _imageSpace=Vh );
+        auto v= interp.operator()( fieldToInterp );
+        auto w= Vh->element( P()*cst(meshSize)*expr(metric[ind] ));
+        double err = normL2(_range=elements(out), _expr=idv(v)-idv(w));
+        BOOST_MESSAGE( "L2 error norm: " << err );
+        std::cout << "L2 error norm: " << err << std::endl;
+        eout->step(i)->setMesh(out);
+        eout->step(i)->add( "interpField", v );
+        eout->save();
+        
+        ein->step(i)->setMesh(m);
+        ein->step(i)->add( "fieldToInterp", fieldToInterp );
+        ein->save();
+        
+        m = out;
+        ind++;
+        ind = ind%4; std::cout << ind<<std::endl;
+    }
+     
+    
+} 
+
+// testcase with hole and fixed discretization on hole boundary
+BOOST_AUTO_TEST_CASE_TEMPLATE( testRemeshMarker, T, dim_types )
+{
+    using namespace Feel;
+    
+    if ( Environment::isParallel() && T::first_type::value == 2 )
+        return;
+
+    typedef Mesh<Simplex<T::first_type::value,1,T::second_type::value>> mesh_type;
+    typedef std::shared_ptr<  mesh_type > mesh_ptrtype;
+
+    double meshSize = 0.05;//doption("gmsh.hsize");
+
+    auto create_mesh = [&](){
+                           if constexpr ( T::first_type::value == 2 && T::second_type::value == 2 ) 
+                           {
+                               auto mesh = loadMesh(_mesh=new Mesh<Simplex<2>>,_filename="$cfgdir/square_squareHole.geo");
+                               std::cout << mesh->hasMarkers({"BdryFixedDiscr"}) << std::endl;
+                               return mesh;
+                           }
+                           else if constexpr ( T::first_type::value == 2 && T::second_type::value == 3 )
+                           {
+                               auto m = unitCube();
+                               auto mS = createSubmesh( _mesh=m, _range=boundaryfaces( m ) );
+                               return mS;
+                           }
+                           else if constexpr ( T::first_type::value == 3 )
+                           {
+                               auto mesh = loadMesh(_mesh=new Mesh<Simplex<3,1>>( "mesh" ),_filename="cube_cubeHole.geo");
+                               return mesh;
+                           }
+                       };
+    auto m = create_mesh();
+    auto out = create_mesh();
+    auto ein = exporter( _mesh=m, _name="initial",_geo="change");
+    auto eout = exporter( _mesh=out, _name="remeshed",_geo="change" );
+    std::string metric[4] = { "(abs(x)+1):x",
+                              "x*x:x",
+                              "exp(x):x",
+                              "(x*x+y*y+1):x:y"};
+    int ind =0;
+    for(int i=0;i<20;i++)
+    {
+        
+        auto Xh = Pch<1>( m );
+        auto met = Xh->element();
+        met.on( _range=elements(m), _expr=cst(meshSize)*expr(metric[ind]) );
+        auto r =  remesher( m,"",m->markerName("BdryFixedDiscr"));
+    
+        r.setMetric( met );
+        out = r.execute();
+        out->updateForUse();
+        auto Vh = Pch<1>( out );
+        auto interp = I( _domainSpace=Xh, _imageSpace=Vh );
+        auto v= interp.operator()( met );
+        auto w= Vh->element( cst(meshSize)*expr(metric[ind] ));
+        double err = normL2(_range=elements(out), _expr=idv(v)-idv(w));
+        BOOST_MESSAGE( "L2 error norm: " << err );
+        std::cout << "L2 error norm: " << err << std::endl;
+        eout->step(i)->setMesh(out);
+        eout->step(i)->add( "metricout", v );
+        eout->save();
+        
+        ein->step(i)->setMesh(m);
+        ein->step(i)->add( "metric", met );
+        ein->save();
+        
+        m = out;
+        ind++;
+        ind = ind%4; std::cout << ind<<std::endl;
+    }
+     
+    
+}  
+
+// testcase with two materials and fixed discretization on inner surface and elements
+BOOST_AUTO_TEST_CASE_TEMPLATE( testRemeshTwoMat, T, dim_types )
+{
+    using namespace Feel;
+    
+    if ( Environment::isParallel() && T::first_type::value == 2 )
+        return;
+
+    typedef Mesh<Simplex<T::first_type::value,1,T::second_type::value>> mesh_type;
+    typedef std::shared_ptr<  mesh_type > mesh_ptrtype;
+
+    double meshSize = 0.05;//doption("gmsh.hsize");
+
+    auto create_mesh = [&](){
+                           if constexpr ( T::first_type::value == 2 && T::second_type::value == 2 ) 
+                           {
+                               auto mesh = loadMesh(_mesh=new Mesh<Simplex<2>>,_filename="$cfgdir/square_twoMaterials.geo");
+                               std::cout << mesh->hasMarkers({"BdryFixedDiscr"}) << std::endl;
+                               return mesh;
+                           }
+                           else if constexpr ( T::first_type::value == 2 && T::second_type::value == 3 )
+                           {
+                               auto m = unitCube();
+                               auto mS = createSubmesh( _mesh=m, _range=boundaryfaces( m ) );
+                               return mS;
+                           }
+                           else if constexpr ( T::first_type::value == 3 )
+                           {
+                               auto mesh = loadMesh(_mesh=new Mesh<Simplex<3,1>>( "mesh" ),_filename="cube_twoMaterials.geo");
+                               return mesh;
+                           }
+                       };
+    auto m = create_mesh();
+    auto out = create_mesh();
+    auto ein = exporter( _mesh=m, _name="initial",_geo="change");
+    auto eout = exporter( _mesh=out, _name="remeshed",_geo="change" );
+    std::string metric[4] = { "(abs(x)+1):x",
+                              "x*x:x",
+                              "exp(x):x",
+                              "(x*x+y*y+1):x:y"};
+    int ind =0;
+    for(int i=0;i<20;i++)
+    {
+        
+        auto Xh = Pch<1>( m );
+        auto met = Xh->element();
+        met.on( _range=elements(m), _expr=cst(meshSize)*expr(metric[ind]) );
+        auto r =  remesher( m,m->markerName("MatTwo"),m->markerName("BdryFixedDiscr"));
+    
+        r.setMetric( met );
+        out = r.execute();
+        out->updateForUse();
+        auto Vh = Pch<1>( out );
+        auto interp = I( _domainSpace=Xh, _imageSpace=Vh );
+        auto v= interp.operator()( met );
+        auto w= Vh->element( cst(meshSize)*expr(metric[ind] ));
+        double err = normL2(_range=elements(out), _expr=idv(v)-idv(w));
+        BOOST_MESSAGE( "L2 error norm: " << err );
+        std::cout << "L2 error norm: " << err << std::endl;
+        eout->step(i)->setMesh(out);
+        eout->step(i)->add( "metricout", v );
+        eout->save();
+        
+        ein->step(i)->setMesh(m);
+        ein->step(i)->add( "metric", met );
+        ein->save();
+        
+        m = out;
+        ind++;
+        ind = ind%4; std::cout << ind<<std::endl;
+    }
+     
+    
+}  
+
 BOOST_AUTO_TEST_SUITE_END()
