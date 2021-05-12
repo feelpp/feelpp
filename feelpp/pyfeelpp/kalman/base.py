1b44455598740b70f9138b30258307ce75bc51c4
Philippe <rickaphilippe@gmail.com>
2021-05-12 11:10:52 +0200

    gain ok - filtering in progress

--- feelpp/pyfeelpp/kalman/base.py
+++ feelpp/pyfeelpp/kalman/base.py
@@ -25,6 +25,9 @@ def toc(message = ''):
 def isempty(liste: list) -> bool:
     return not bool(len(liste))
 
+def isnumber(variable) -> bool:
+    return type(variable) is float or type(variable) is int
+
 def balancedpartition(nb_data,nb_procs):
     """ For parallelization purpose using MPI
     
@@ -52,16 +55,25 @@ def displacements(partition):
     toc()
     return shift
 
+
 """ Here starts the implementation of the filter """
 
-def weighted_mean(list_of_states, weights = None):
-    result = State(list_of_states[0].get_dim())
+def magnitude_order(array):
+    return 10**np.floor(np.log10(np.linalg.norm(array)))
+
+def weighted_sum(element_list, weights = None):
     if weights is None:
-        weights = np.ones(len(list_of_states))/len(list_of_states)
-    elif type(weights) is list and len(weights) != len(list_of_states):
-        raise TypeError('There must be the same number of states and weights')
-    for state, weight in zip(list_of_states,weights):
-        result += state * weight
+        weights = np.ones(len(element_list))/len(element_list)
+    elif type(weights) is list and len(weights) != len(element_list):
+        raise ValueError('There must be the same number of states and weights')
+    if type(element_list[0]) is State:
+        result = State(element_list[0].get_dim())
+        for state, weight in zip(element_list,weights):
+            result += state * weight
+    else:
+        result = 0
+        for element, weight in zip(element_list,weights):
+            result += weight * element
     return result
 
 class State:
@@ -71,7 +83,7 @@ class State:
     This class is used for States and for Observations since only their
     respective dimention may differ.
     """
-    def __init__(self, input): #dimension: int = 1, values: list = [0]):
+    def __init__(self, input):
         self._values = None
         self._dim = None
         if type(input) is int:
@@ -91,9 +103,9 @@ class State:
         and sets the associated set to an array of zeros of that length 
         """
         if integer < 1:
-            raise TypeError('the dimension must be at least 1')
+            raise ValueError('the dimension must be at least 1')
         if type(integer) is not int:
-            raise TypeError('the dimension must be an integer')
+            raise ValueError('the dimension must be an integer')
         self._dim = integer
         self._values = np.zeros((integer))
 
@@ -106,23 +118,41 @@ class State:
         return self._dim
 
     def __add__(self, other):
-        return State(self.get_values() + other.get_values())
+        if type(other) is State:
+            return State(self.get_values() + other.get_values())
+        elif isnumber(other):
+            return State(self.get_values() + other)
+        else:
+            raise TypeError('invalid sum term')
 
     def __iadd__(self, other):
-        self._values += other.get_values()
+        if type(other) is State:
+            self._values += other.get_values()
+        elif isnumber(other):
+            self._values += other
+        else:
+            raise TypeError('invalid increment')
         return self
 
     def __mul__(self, factor: float):
         return State(factor * self.get_values())
 
     def __sub__(self, other):
-        return self + (-1)*other
+        if type(other) is State:
+            return self + (-1)*other
+        elif isnumber(other):
+            return State(self.get_values() - other)
+        else:
+            raise TypeError('invalid substraction term')
 
     def __rmul__(self, factor: float):
         return self * factor
 
     def __truediv__(self, divider: float):
         return self*(1/divider)
+
+    def __matmul__(self, matrix):
+        return State(np.array(matrix @ self.get_values()))
 
     def __str__(self):
         return "State of dimension {}".format(self.get_dim())
@@ -136,15 +166,31 @@ class EnsembleTools:
     sigma_scheme is an iterable list of shift states (default is UKF friendly)
     """
 
-    def __init__(self, factor: float, main_weight: float, dim: int):
+    def __init__(self, 
+                 factor: float, 
+                 main_weight: float, 
+                 dim: int):
         self.ensemble = None
         self.size = 2*dim+1
         self.factor = factor
         self.weights = self.set_weights(main_weight, dim)
-        self.state_cov = np.eye(dim)
+        self.state_cov = self.set_state_cov(1)
+        self.obs_cov = self.set_state_cov(1)
 
     def set_weights(self, value: float, dim: int):
         return [value] + (self.size-1)*[(1-value)/(2*dim)]
+
+    def set_state_cov(self, value):
+        if isnumber(value):
+            self.state_cov = value * np.eye(dim)
+        else:
+            self.state_cov = value
+
+    def set_obs_cov(self, value):
+        if isnumber(value):
+            self.obs_cov = value * np.eye(dim)
+        else:
+            self.obs_cov = value
 
     def produce_ensemble(self, state: State):
         self.ensemble = [state]
@@ -153,7 +199,6 @@ class EnsembleTools:
         for column in range(len(shift_matrix)):
             self.ensemble.append(state + State(shift_matrix[:,column]))
             self.ensemble.append(state - State(shift_matrix[:,column]))
-        return self.ensemble
 
 class Filter:
     """ A Filter object is the data of:
@@ -175,9 +220,10 @@ class Filter:
                  forecast_state = lambda x : x, 
                  forecast_obs = lambda x : x):
 
-        self.states = [State(dim)]
-        self.observations = None
-        self.ensemble = None
+        self.estimate_states = [State(dim)]
+        self.estimate_observations = []
+        self.real_observations = None
+        self.gain = None
         self.ts = 0
         self.tools = EnsembleTools(factor = dim/(1-main_weight), 
                                    main_weight = main_weight,
@@ -186,23 +232,95 @@ class Filter:
         self.forecast_obs = forecast_obs
 
     def set_state(self, state: State):
-        self.states.append(state)
+        if type(state) is State:
+            self.estimate_states.append(state)
+        else:
+            self.estimate_states.append(State(state))
 
-    def set_obs(self, obs: State):
-        self.observations.append(obs)
+    def load_real_observations(self, list):
+        self.real_observations = []
+        for state in list:
+            if type(state) is State:
+                self.real_observations.append(state)
+            else:
+                self.real_observations.append(State(state))
+
+    def set_forecast_function(self, function):
+        self.forecast_state = function
+    
+    def set_observation_function(self, function):
+        self.forecast_obs = function
         
     def get_last_state(self) -> State:
-        return self.states[-1]
+        return self.estimate_states[-1]
 
     def get_last_obs(self) -> State:
-        return self.observations[-1]
+        return self.estimate_observations[-1]
 
     def get_ts(self) -> int:
         return self.ts
 
-    def forecast(state: State):
-        self.state = weighted_mean(self.tools.produce_ensemble(state),
-                                   weights = self.tools.weights)
+    def step_ts(self):
+        self.ts += 1
+
+    def compute_gain(self, x_f, x_dag, y_dag):
+        """ Computes the transposed gain """
+        S_xy = []
+        S_yy = []
+        for x, y in zip(x_dag, y_dag):
+            S_xy.append(np.mat(x.get_values() - x_f.get_values()).T \
+                               @ np.mat(y.get_values() - self.get_last_obs().get_values()))
+            S_yy.append(np.mat(y.get_values() - self.get_last_obs().get_values()).T \
+                        @ np.mat(y.get_values() - self.get_last_obs().get_values()))
+    
+        self.gain = weighted_sum(S_xy, weights = self.tools.weights) \
+               @ np.linalg.inv(weighted_sum(S_yy, weights = self.tools.weights) \
+                               + sci.sqrtm(self.tools.obs_cov))
+
+    def analyze(self, x_f, x_dag, y_dag):
+        """ Computes the analysis formula using most recent data 
+        and provided forecast state
+        """
+
+        self.compute_gain(x_f, x_dag, y_dag)
+        self.estimate_states.append(x_f + ((self.real_observations[self.ts] - self.get_last_obs()) \
+                                            @ self.gain))
+
+    def forecast(self):
+        """ The first part computes the sigma-points and transforms them.
+        Then the associated preficted observations are computed.
+
+        The second part uses these informations along with a real observation
+        to compute the analyzed (i.e. best knowledge) state
+        """
+
+        if self.real_observations is None:
+            raise ValueError('Obervation data missing')
+        
+        ensemble_state = []
+        ensemble_obs = []
+
+        self.tools.set_state_cov(np.outer(self.get_last_state().get_values(),
+                                          self.get_last_state().get_values()))
+        self.tools.set_obs_cov(np.outer(self.real_observations[self.ts],
+                                        self.real_observations[self.ts]))
+
+        self.tools.produce_ensemble(self.get_last_state())
+        for guess in self.tools.ensemble:
+            sigma_point = self.forecast_state(guess)
+            ensemble_state.append(sigma_point)
+            ensemble_obs.append(self.forecast_obs(sigma_point))
+
+        forecast_state = weighted_sum(ensemble_state,
+                                      weights = self.tools.weights)
+        self.estimate_observations.append(weighted_sum(ensemble_obs,
+                                          weights = self.tools.weights))
+
+        self.analyze(forecast_state, ensemble_state, ensemble_obs)
+        self.step_ts()
+
+    def filter(self, initial_guess, data):
+        pass
 
     def __str__(self):
         message = "Filter at time step {}\n".format(self.get_ts()) \
