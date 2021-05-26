702467c36edf26b91537bb8ec7a6fbb8b8959649
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-05-26 08:41:14 +0200

    Update add_key.sh
    ci skip

--- feelpp/tools/scripts/pkg/add_key.sh
+++ feelpp/tools/scripts/pkg/add_key.sh
@@ -3,6 +3,6 @@
 pbuilder-dist $DIST login --save-after-login << EOF
 apt install -y wget gnupg ca-certificates
 echo "deb https://dl.bintray.com/feelpp/$FLAVOR $DIST $CHANNEL" | tee -a /etc/apt/sources.list
-wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | apt-key add -
+wget -qO  - https://feelpp.jfrog.io/artifactory/api/security/keypair/debian/public | apt-key add -
 apt update
 EOF
