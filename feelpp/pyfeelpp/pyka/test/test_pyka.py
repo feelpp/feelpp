3f674f627ea1e7b3d2576be0206c8f5b67056509
Philippe <rickaphilippe@gmail.com>
2021-05-26 12:27:26 +0200

    testcases documentation

--- feelpp/pyfeelpp/pyka/test/test_pyka.py
+++ feelpp/pyfeelpp/pyka/test/test_pyka.py
@@ -5,26 +5,22 @@ import pytest
 class TestState:
 
     def test_init(self):
-        print('Testing State initialization...')
+        """ Test: state initialization """
         s = State(input = [1,2,3,4,5])
         assert s.get_dim() == 5
         assert np.array_equal(s.get_values(), np.array([1,2,3,4,5]))
         assert str(s) == "State of dimension 5"
 
-    def test_set_dim(self):
-        s = State(dim = 3)
-        assert np.array_equal(s.get_values(), np.array(3*[0]))
-
     def test_addition(self):
-        print('Testing state addition...')
+        """ Test: state addition """
         assert State(input = [1,2]) + State(input = [2,3]) == State(input = [3,5])
 
     def test_multiplication(self):
-        print('Testing scalar multiplication...')
+        """ Test: scalar multiplication """
         assert 3 * State(input = [1,2,3]) == State(input = [3,6,9])
 
     def test_weighted_sum(self):
-        print('Testing state weighted sum')
+        """ Test: state weighted sum """
         list = [State(input = [1,2]), State(input = [2,3]), State(input = [3,-4])]
         weights = [2, -1, 1]
         assert weighted_sum(list, weights) == State(input = [3, -3])
@@ -32,25 +28,29 @@ class TestState:
 class TestFilter():
 
     def test_init(self):
+        """ Test: filter initialization """
         f = Filter(initial_state = [1,1,1])
         assert f.get_last_state().get_dim() == 3
         assert np.array_equal(f.get_last_state().get_values(), np.array([1,1,1]))
         assert str(f) == "Filter at time step 0\n    State dimension = 3\n    no observations"
 
     def test_load_data(self):
+        """ Test: data loading """
         f = Filter()
         data = [np.array([1,2,3]),np.array([2,3,4]),np.array([3,4,5]),np.array([4,5,6]),np.array([5,6,7])]
         f.load_measurements(data)
         assert f.real_observations[0] == State(input = [1,2,3])
         assert f.max_ts == 5
 
-    def test_simple_dircet_filter(self):
+    def test_simple_direct_filter(self):
+        """ Test: direct 1D filter with identity forecasts """
         f = Filter(initial_state = 0)
         f.load_measurements(100*[1])
         f.filter()
         assert f.get_last_state().round(10) == State(input = [1.]).round(10)
 
     def test_multiple_direct_filter(self):
+        """ Test: direct nD filter with identity forecasts """
         dim = 1 + np.random.randint(100)
         start = np.random.random(dim)
         goal = np.random.random(dim)
@@ -60,6 +60,7 @@ class TestFilter():
         assert f.get_last_state().round(10) == State(input = goal).round(10)
 
     def test_sum_diff(self):
+        """ Test: real hidden state is [1,1], observation is [sum,difference] with small uncertainty """
         forecast_obs = lambda tva : State(np.array([tva[0]+tva[1], tva[0]-tva[1]])) # tva: Two-Valued Array
         forecast_state = lambda tva : tva + State(input=np.random.random(2) - 0.5)
         f = Filter(
