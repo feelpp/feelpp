54d3e167927a0ed5ed9529e865f8612ccdd111f4
Vincent Chabannes <vincent.chabannes@feelpp.org>
2021-05-28 15:06:09 +0200

    full rewrite of test_meshstructured

--- testsuite/feeldiscr/test_meshstructured.cpp
+++ testsuite/feeldiscr/test_meshstructured.cpp
@@ -25,279 +25,247 @@
    \date 2015-04-27
  */
 
-#define USE_BOOST_TEST 1
-#if defined(USE_BOOST_TEST)
 #define BOOST_TEST_MODULE test_meshstructured
 #include <feel/feelcore/testsuite.hpp>
-#endif
 
-#include <feel/feelfilters/loadmesh.hpp>
+#include <feel/feeldiscr/meshstructured.hpp>
+//#include <feel/feelfilters/loadmesh.hpp>
 #include <feel/feeldiscr/pch.hpp>
-#include <feel/feeldiscr/pdh.hpp>
-#include <feel/feelvf/msi.hpp>
+//#include <feel/feeldiscr/pdh.hpp>
+//#include <feel/feelvf/msi.hpp>
 #include <feel/feelvf/integrate.hpp>
 #include <feel/feelvf/form.hpp>
 #include <feel/feelvf/norml2.hpp>
 #include <feel/feelvf/operators.hpp>
 #include <feel/feelvf/operations.hpp>
-#include <feel/feelvf/projectors.hpp>
-#include <feel/feelpoly/multiscalequadrature.hpp>
-#include <feel/feelvf/ginac.hpp>
+//#include <feel/feelvf/projectors.hpp>
+//#include <feel/feelpoly/multiscalequadrature.hpp>
+//#include <feel/feelvf/ginac.hpp>
 #include <feel/feelfilters/exporter.hpp>
-#include <feel/feelfilters/hbf.hpp>
-#if defined(FEELPP_HAS_FFTW)
-#include <fftw3.h>
+//#include <feel/feelfilters/hbf.hpp>
+// #if defined(FEELPP_HAS_FFTW)
+// #include <fftw3.h>
+// #endif
+
+
+using namespace Feel;
+
+FEELPP_ENVIRONMENT_NO_OPTIONS
+
+
+#if 0
+void runImageHbf  (std::string hbf1, std::string hbf2, std::string solution, double pixelsize) 
+{
+    auto x = readHBF( hbf1 );
+    auto y = readHBF( hbf2 );
+    auto z = readHBF( solution );
+
+    auto mesh = std::make_shared<MeshStructured>( x.rows(),
+                                                  x.cols(),
+                                                  pixelsize,
+                                                  cx,cy,
+                                                  /*NULL,*/
+                                                  Environment::worldCommPtr());
+    mesh->components().reset();
+    mesh->components().set( size_type(MESH_NO_UPDATE_MEASURES) );
+    mesh->updateForUse();
+
+    auto Vh = Pch<1> ( mesh ) ;
+    auto u = Vh->element () ;
+    auto v = Vh->element () ;
+    auto px = Vh->element () ;
+    auto py = Vh->element () ;
+    Hbf2FeelppStruc h2f( nx, ny, Vh );
+    Hbf2FeelppStruc h2fS( z.cols(), z.rows(), Vh );
+    px = h2f( x);
+    py = h2f( y );
+    auto s = h2fS ( z);
+
+    auto l = form1( _test=Vh );
+    l = integrate(_range=elements(mesh),
+                  _expr=grad(v)*vec(idv(px),idv(py)));
+
+    auto a = form2( _trial=Vh, _test=Vh);
+    a = integrate( _range=elements(mesh),
+                   //_expr=gradt(u)*trans(grad(v)));
+                   _expr=gradt(u)*trans(grad(v)));
+
+    a.solve(_rhs=l,_solution=u) ;
+
+    std::cout << "L2 error norm : " << normL2( _range=elements(mesh), _expr=idv(u)-idv(s) ) << "\n";
+}
 #endif
-#include <feel/feeldiscr/meshstructured.hpp>
 
-namespace Feel
+
+template <typename GeoShapeType>
+auto createStructuredMesh( nl::json const& jsonDesc )
 {
-
-inline
-AboutData
-makeAbout()
-{
-    AboutData about( "test_meshstructured" ,
-                     "test_meshstructured" ,
-                     "8.9e-3",
-                     "Test structured mesh with image",
-                     Feel::AboutData::License_GPL,
-                     "Copyright (c) 2014 Feel++ Consortium" );
-
-    about.addAuthor( "Thomas Lantz", "student", "", "" );
-    return about;
+    auto mesh = std::make_shared<MeshStructured<GeoShapeType>>( jsonDesc );
+    mesh->components().reset();
+    mesh->components().set( size_type( MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES ) );
+    mesh->updateForUse();
+    Feel::cout <<"  mesh->numGlobalElements() " << mesh->numGlobalElements() << std::endl;
+    Feel::cout <<"  mesh->numGlobalPoints()   " << mesh->numGlobalPoints()   << std::endl;
+    return mesh;
 }
 
-template < typename T  >
-using holo3_image = Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> ;
 
-class Test_MeshStructured
+template </*int Dim,int PolyOrder=2,*/typename MeshStructuredType >
+void runLaplacian( std::shared_ptr<MeshStructuredType> mesh, std::string const& exportName )
 {
-    public :
+    static const int Dim = MeshStructuredType::nDim;
+    std::string u_exact_str = Dim==1? "3*x^2:x" : (Dim==2? "3*x^2+5*y^2:x:y" : "3*x^2+5*y^2+9*z^2:x:y:z");
+    auto u_exact = expr( u_exact_str );
+    auto f_str = Dim==1? "-6" : (Dim==2? "-6-10" : "-6-10-18");
+    auto f = expr( f_str );
+
+    auto Vh = Pch<2>(mesh/*,true*/);
+    // std::cout << "Rank - nDof \t" << Environment::worldComm().localRank() << "\t" <<  Vh->nLocalDof() << std::endl;
+    // std::cout << "Rank - Ghost Local Dof :" << Environment::worldComm().localRank() << "\t"<< Vh->dof()->nLocalGhosts() << std::endl;
+    auto u = Vh->element();
+    auto v = Vh->element();
+    auto l = form1( _test=Vh );
+    l = integrate(_range=elements(mesh),
+                  _expr=f*id(v));
+    auto a = form2( _trial=Vh, _test=Vh);
+    a = integrate( _range=elements(mesh),
+                   _expr=inner(gradt(u),grad(v)) );
+
+    a += on( _range = boundaryfaces(mesh), _rhs = l, _element = u, _expr = u_exact/*cst(0)*/ );
+
+    a.solve(_rhs=l,_solution=u,_rebuild=true) ;
+
+    auto e = exporter( _mesh=mesh,_name= exportName+"_"+MeshStructuredType::shape_type::name() );
+    e->addRegions();
+    e->add( "u", u );
+    e->add( "u_exact", u_exact );
+    e->save();
+
+    double err = normL2( _range=elements(mesh), _expr=idv(u)-u_exact );
+    BOOST_CHECK_SMALL( err,1e-5 );
+}
 
 
-    Test_MeshStructured(int nx,int ny)
-    {
-        ima = holo3_image<float>(ny,nx);
-        ima2 = holo3_image<float>(ny,nx);
-        for (int i=0;i<nx;i++)
-        {
-            for (int j=0;j<ny;j++)
-            {
-                ima(j,i)=1;
-                ima2(j,i)=0;
+
+
+BOOST_AUTO_TEST_SUITE( meshstructured )
+
+BOOST_AUTO_TEST_CASE( test_json )
+{
+    nl::json jsonDesc1d = {
+        { "Geometry", {
+                { "shape", "hyperrectangle" },
+                { "domain", {
+                        { "origin", { -1.0 } },
+                        { "length", { 12 } }
+                    } }
+            }
+        },
+        { "Discretisation", {
+                { "n_points", 42 }
             }
         }
+    };
+    nl::json jsonDesc2d = {
+        { "Geometry", {
+                { "shape", "hyperrectangle" },
+                { "domain", {
+                        { "origin", { -1.0,3.2 } },
+                        { "length", { 12,45.5 } }
+                    } }
+            }
+        },
+        { "Discretisation", {
+                { "n_points", { 42,18 } }
+            }
+        }
+    };
+    nl::json jsonDesc3d = {
+        { "Geometry", {
+                { "shape", "hyperrectangle" },
+                { "domain", {
+                        { "origin", { -1.0,3.2,5.5 } },
+                        { "length", { 12,45.5,26.2 } }
+                    } }
+            }
+        },
+        { "Discretisation", {
+                { "n_points", { 23,12,7 } }
+            }
+        }
+    };
+
+    auto mesh1d = createStructuredMesh<Hypercube<1>>(jsonDesc1d);
+    BOOST_CHECK_CLOSE( mesh1d->measure(), 12, 1e-7 );
+    runLaplacian( mesh1d, "json" );
+
+    auto mesh2d = createStructuredMesh<Hypercube<2>>(jsonDesc2d);
+    BOOST_CHECK_CLOSE( mesh2d->measure(), 12*45.5, 1e-7 );
+    runLaplacian( mesh2d, "json" );
+
+    auto mesh3d = createStructuredMesh<Hypercube<3>>(jsonDesc3d);
+    BOOST_CHECK_CLOSE( mesh3d->measure(), 12*45.5*26.2, 1e-7 );
+    runLaplacian( mesh3d, "json" );
+}
+
+
+
+BOOST_AUTO_TEST_CASE( test_usercoords )
+{
+    eigen_vector_type<2,double> p0,p1,p2,p3;
+    p0[0] = 0; p0[1] = 0;
+    p1[0] = 5; p1[1] = 1;
+    p2[0] = 4; p2[1] = 4;
+    p3[0] = 1; p3[1] = 2;
+
+    double l0 = (p0-p1).norm();
+    double l1 = (p1-p2).norm();
+    double l2 = (p2-p3).norm();
+    double l3 = (p3-p0).norm();
+
+    eigen_vector_type<2,double> dir0,dir1,dir2,dir3;
+    dir0 = (p1-p0).normalized();
+    dir1 = (p2-p1).normalized();
+    dir2 = (p2-p3).normalized();
+    dir3 = (p3-p0).normalized();
+
+    int nx = 15;
+    int ny = 10;
+
+    double h0 = l0/(nx-1);
+    double h1 = l1/(ny-1);
+    double h2 = l2/(nx-1);
+    double h3 = l3/(ny-1);
+
+    holo3_image<float> coords_x(nx,ny);
+    holo3_image<float> coords_y(nx,ny);
+    eigen_vector_type<2,double> q0,q1,q2,q3;
+    for (int i=0;i<nx;++i)
+    {
+        q0 = p0 + i*h0*dir0;
+        q2 = p3 + i*h2*dir2;
+        for (int j=0;j<ny;++j)
+        {
+            q1 = p1 + j*h1*dir1;
+            q3 = p0 + j*h3*dir3;
+            // compute intersection of 2 segment
+            // see https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+            double t = ( (q0(0)-q1(0))*(q1(1)-q3(1)) - (q0(1)-q1(1))*(q1(0)-q3(0)) )/( (q0(0)-q2(0))*(q1(1)-q3(1))-(q0(1)-q2(1))*(q1(0)-q3(0)) );
+            auto qij = q0+t*(q2-q0);
+            coords_x(i,j) = qij(0);
+            coords_y(i,j) = qij(1);
+        }
     }
-    
-    
-    void run (double pixelsize) 
-        {
-            tic();
-            auto mesh = std::make_shared<MeshStructured>( ima.rows(), ima.cols(), pixelsize, ima,ima,
-                                                          Environment::worldCommPtr());
-            
-            mesh->components().reset();
-            mesh->components().set( size_type(MESH_NO_UPDATE_MEASURES) );
-            mesh->updateForUse();
-            toc("mesh");
-    
-            auto Vh = Pch<1> ( mesh ) ;
-            auto u = Vh->element () ;
-            auto v = Vh->element () ;
-     
-            auto imaP = vf::project(_space=Vh,_range=elements(mesh),_expr=Px()); 
-            auto l = form1( _test=Vh );
-            l = integrate(_range=elements(mesh),
-                          _expr=id(v)*idv(imaP));
-                     
-            auto a = form2( _trial=Vh, _test=Vh);
-            a = integrate( _range=elements(mesh),
-                           _expr=idt(u)*id(v));
-                      
-            a.solve(_rhs=l,_solution=u) ;
-
-            auto e = exporter( _mesh=mesh );
-            e->add( "u", u );
-            e->addRegions(); 
-            e->save();
-
-            auto n = normL2 ( _range=elements( mesh ), _expr=idv(u)-idv(imaP));
-            std::cout << " Norm2 " << n << std::endl;
-
-        }
-
-    void runImage  (double pixelsize) 
-        {
-
-            auto mesh = std::make_shared<MeshStructured>( ima.rows(), ima.cols(), pixelsize, ima,ima,
-                                                          Environment::worldCommPtr());
-            mesh->components().reset();
-            mesh->components().set( size_type(MESH_NO_UPDATE_MEASURES) );
-            mesh->updateForUse();
-    
-            auto Vh = Pch<1> ( mesh ) ;
-            auto u = Vh->element () ;
-            auto v = Vh->element () ;
-     
-            auto imaP = vf::project(_space=Vh,_range=elements(mesh),_expr=cst(1.)); 
-            auto imaP2 = vf::project(_space=Vh,_range=elements(mesh),_expr=cst(0.)); 
-            auto l = form1( _test=Vh );
-            l = integrate(_range=elements(mesh),
-                          //_expr=grad(v)*vec(idv(ima),idv(ima)));
-                          _expr=grad(v)*vec(idv(imaP),idv(imaP2)));
-    
-            auto a = form2( _trial=Vh, _test=Vh);
-            a = integrate( _range=elements(mesh),
-                           //_expr=gradt(u)*trans(grad(v)));
-                           _expr=gradt(u)*trans(grad(v)));
-    
-            a.solve(_rhs=l,_solution=u) ;
-    
-            auto value  = vf::project(_space=Vh,_range=elements(mesh),_expr=Py()); 
-            auto n = normL2 ( _range=elements( mesh ), _expr=idv(u)-idv(value)); 
-            std::cout << " Norm2 " << n << std::endl;
-
-        }
-  
-    void runImageHbf  (std::string hbf1, std::string hbf2, std::string solution, double pixelsize) 
-        {
-#if 0
-            auto x = readHBF( hbf1 );
-            auto y = readHBF( hbf2 );
-            auto z = readHBF( solution );
-
-            auto mesh = std::make_shared<MeshStructured>( x.rows(),
-                                                            x.cols(),
-                                                            pixelsize,
-                                                            cx,cy,
-                                                            /*NULL,*/
-                                                            Environment::worldCommPtr());
-            mesh->components().reset();
-            mesh->components().set( size_type(MESH_NO_UPDATE_MEASURES) );
-            mesh->updateForUse();
-    
-            auto Vh = Pch<1> ( mesh ) ;
-            auto u = Vh->element () ;
-            auto v = Vh->element () ;
-            auto px = Vh->element () ;
-            auto py = Vh->element () ;
-            Hbf2FeelppStruc h2f( nx, ny, Vh );
-            Hbf2FeelppStruc h2fS( z.cols(), z.rows(), Vh );
-            px = h2f( x);
-            py = h2f( y );
-            auto s = h2fS ( z);
-      
-            auto l = form1( _test=Vh );
-            l = integrate(_range=elements(mesh),
-                          _expr=grad(v)*vec(idv(px),idv(py)));
-    
-            auto a = form2( _trial=Vh, _test=Vh);
-            a = integrate( _range=elements(mesh),
-                           //_expr=gradt(u)*trans(grad(v)));
-                           _expr=gradt(u)*trans(grad(v)));
-    
-            a.solve(_rhs=l,_solution=u) ;
-    
-            std::cout << "L2 error norm : " << normL2( _range=elements(mesh), _expr=idv(u)-idv(s) ) << "\n"; 
-#endif   
-
-        }
 
 
-    void runParallel ( double pixelsize )
-        {
-            tic();
-            Feel::cout << "[nbPt X] x [nbPt Y] = " << ima.rows() << "\t" << ima.cols() << std::endl;
-            auto mesh = std::make_shared<MeshStructured>( ima.rows(), ima.cols(), pixelsize, ima,ima,
-                                                          Environment::worldCommPtr());
-            Feel::cout <<"  mesh->numGlobalElements() " << mesh->numGlobalElements() << std::endl;
-            Feel::cout <<"  mesh->numGlobalPoints()   " << mesh->numGlobalPoints()   << std::endl;
-            mesh->components().reset();
-            Feel::cout <<"  mesh->numGlobalElements() " << mesh->numGlobalElements() << std::endl;
-            Feel::cout <<"  mesh->numGlobalPoints()   " << mesh->numGlobalPoints()   << std::endl;
-            //mesh->components().set( size_type(MESH_NO_UPDATE_MEASURES) );
-            mesh->components().set( size_type(MESH_UPDATE_ELEMENTS_ADJACENCY|MESH_NO_UPDATE_MEASURES) );
-            Feel::cout <<"  mesh->numGlobalElements() " << mesh->numGlobalElements() << std::endl;
-            Feel::cout <<"  mesh->numGlobalPoints()   " << mesh->numGlobalPoints()   << std::endl;
-            mesh->updateForUse();
-            Feel::cout <<"  mesh->numGlobalElements() " << mesh->numGlobalElements() << std::endl;
-            Feel::cout <<"  mesh->numGlobalPoints()   " << mesh->numGlobalPoints()   << std::endl;
- 
+    using geoshape_type = Hypercube<2>;
+    auto mesh = std::make_shared<MeshStructured<geoshape_type>>( coords_x, coords_y );
+    mesh->components().reset();
+    mesh->components().set( size_type( MESH_CHECK|MESH_UPDATE_FACES|MESH_UPDATE_EDGES ) );
+    mesh->updateForUse();
 
-            auto Vh = Pch<1>(mesh,true);
-            std::cout << "Rank - nDof \t" << Environment::worldComm().localRank() << "\t" <<  Vh->nLocalDof() << std::endl;
-            //Vh->map().showMe(true,std::cout);
-            std::cout << "Rank - Ghost Local Dof :" << Environment::worldComm().localRank() << "\t"<< Vh->dof()->nLocalGhosts() << std::endl;
-            //auto a = form2(Vh,Vh);
-            //a.matrixPtr()->printMatlab("theMat");
-
-            auto e = exporter( _mesh=mesh );
-            e->addRegions();
-            e->save(); 
-
-        }
-
-
-
-    private :
-
-    holo3_image<float> ima;
-    holo3_image<float> ima2;
-    
-};
-
-#if defined(USE_BOOST_TEST)
-FEELPP_ENVIRONMENT_WITH_OPTIONS( makeAbout(), feel_options() )
-BOOST_AUTO_TEST_SUITE( multiscale_suite )
-
-double ps=1e-2;
-
-BOOST_AUTO_TEST_CASE( test_run0 )
-{
-    Test_MeshStructured tms0(13,4);
-    tms0.runImage(ps);
-    //tms0.runParallel(ps);
+    BOOST_CHECK_CLOSE( mesh->measure(), 10, 1e-5 );
+    runLaplacian( mesh, "usercoords" );
 }
-
-
-BOOST_AUTO_TEST_CASE( test_run2 )
-{
-    Test_MeshStructured tms2(12,24);
-    tms2.runParallel(ps);
-}
-
-BOOST_AUTO_TEST_CASE( test_run3 )
-{
-    Test_MeshStructured tms3(42,18);
-    tms3.runParallel(ps);
-}
-
-/*
-BOOST_AUTO_TEST_CASE( test_run4 )
-{
-    Test_MeshStructured tms4(43,9);
-    tms4.runParallel(ps);
-}
-*/
-
-
-/*
-BOOST_AUTO_TEST_CASE( test_run1 )
-{
-    Test_MeshStructured tms1(18,22);
-    tms1.runImage(8.9e-3);
-}
-*/
-
-
-
-
 
 BOOST_AUTO_TEST_SUITE_END()
-#else
-std::cout << "USE_BOOST_TEST non define" << std::endl;
-#endif
-
-
-}
