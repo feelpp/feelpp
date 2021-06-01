30fcda730e81f35e2c333926cf8b455ea300fc2b
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-06-01 11:20:59 +0000

    Review, update python scripts and add tests #1626
    configure tests for pytest with fixture to configure Feel++ once for all at beginning of session

---
+++ feelpp/pyfeelpp/tests/conftest.py
@@ -0,0 +1,36 @@
+from logging import getLogger
+import sys
+
+import py
+import pytest
+import feelpp
+
+log = getLogger(__name__)
+MPI_ARGS = ("mpirun", "-n")
+PYTEST_ARGS = (sys.executable, "-mpytest")
+
+
+@pytest.fixture
+def has_mpi4py():
+    try:
+        import mpi4py
+        return True
+    except ImportError:
+        return False
+
+
+
+class InitFeelpp:
+    def __init__(self):
+        try:
+            print('xxx call init_feelpp')
+            sys.argv=['test_feelpp']
+            self.e = feelpp.Environment(sys.argv)
+            print('proc:',feelpp.Environment.isMasterRank())
+        except Exception:
+            return 
+
+
+@pytest.fixture(scope="session", autouse=True)
+def init_feelpp():
+    return InitFeelpp()
