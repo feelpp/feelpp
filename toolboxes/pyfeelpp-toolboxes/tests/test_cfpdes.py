1fe9ba5e14dc27c84eb85cf8faa94e3f474062d0
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-06-04 21:13:35 +0000

    up cfpdes and hdg py toolboxes + fix cmake
    #1626 and #1628

--- toolboxes/pyfeelpp-toolboxes/tests/test_cfpdes.py
+++ toolboxes/pyfeelpp-toolboxes/tests/test_cfpdes.py
@@ -5,8 +5,29 @@ import feelpp.toolboxes.cfpdes as cfpdes
 import pandas as pd
 
 def test_cfpdes():
-    e.setConfigFile('cfpdes/square/square2d.cfg')   
+    e.setConfigFile()   
     f=cfpdes.simulate(dim=2)
 
-    df = pd.read_csv("cfpdes.heat.measures.csv", sep=",")
-    df.head(10)
+
+
+
+def test_heat():
+    feelpp.Environment.setConfigFile('cfpdes/square/square2d.cfg')
+    f = cfpdes(dim=2)
+    f.init()
+    #f.printAndSaveInfo()
+    if f.isStationary():
+        f.solve()
+        f.exportResults()
+    else:
+        if not f.doRestart():
+            f.exportResults(f.timeInitial())
+        while not f.timeStepBase().isFinished():
+            if f.worldComm().isMasterRank():
+                print("============================================================\n")
+                print("time simulation: ", f.time(), "s \n")
+                print("============================================================\n")
+            f.solve()
+            f.exportResults()
+            f.updateTimeStep()
+    return not f.checkResults()
