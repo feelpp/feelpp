817cc3154c40872e5ccb04d79061dbe383533be4
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-06-17 07:20:36 +0000

    Review, update python scripts and add tests #1626
    fix cfpdes python wrapper
    enable python cfpde test

--- toolboxes/pyfeelpp-toolboxes/feelpp/toolboxes/coefficientformpdes/cfpdes.cpp
+++ toolboxes/pyfeelpp-toolboxes/feelpp/toolboxes/coefficientformpdes/cfpdes.cpp
@@ -21,11 +21,10 @@
 //! @date 25 Jul 2018
 //! @copyright 2018 Feel++ Consortium
 //!
-#include <pybind11/pybind11.h>
-
-#include <feel/feelmodels/modelcore/modelnumerical.hpp>
 #include <feel/feelmodels/coefficientformpdes/coefficientformpdes.hpp>
-
+#include <feel/feelmodels/coefficientformpdes/coefficientformpdes_registered_type.hpp>
+#include <feel/feelmodels/modelcore/modelnumerical.hpp>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 using namespace Feel;
@@ -35,25 +34,18 @@ void defSM(py::module &m)
 {
     using namespace Feel;
     using namespace Feel::FeelModels;
-    using toolbox_t =  CoefficientFormPDEs< Simplex<nDim,1>,
-                                            //Lagrange<0,Scalar,Continuous,PointSetFekete>,
-                                            Lagrange<1,Scalar,Continuous,PointSetFekete>,
-                                            Lagrange<2,Scalar,Continuous,PointSetFekete>,
-                                            Lagrange<1,Vectorial,Continuous,PointSetFekete> >;
-    //using element_temperature_t = typename toolbox_t::element_temperature_type;
-    //using element_temperature_ptr_t = typename toolbox_t::element_temperature_ptrtype;
+    using toolbox_t = FeelModels::coefficient_form_PDEs_t<Simplex<nDim, 1>>;
 
     std::string pyclass_name = std::string("cfpdes_") + std::to_string(nDim) + std::string("D");
-    py::class_<toolbox_t,std::shared_ptr<toolbox_t>,ModelNumerical>(m,pyclass_name.c_str())
-        .def(py::init<std::string const&,std::string const&,worldcomm_ptr_t const&,std::string const&, ModelBaseRepository const&>(),
-             py::arg("prefix"),
-             py::arg("keyword")=std::string("cfpdes"),
-             py::arg("worldComm")=Environment::worldCommPtr(),
-             py::arg("subprefix")=std::string(""),
-             py::arg("modelRep") = ModelBaseRepository(),
-             "Initialize the coefficient form pdes toolbox"
-             )
-        .def("init",&toolbox_t::init, "initialize the heat  toolbox",py::arg("buildModelAlgebraicFactory")= true)
+    py::class_<toolbox_t, std::shared_ptr<toolbox_t>, ModelNumerical>( m, pyclass_name.c_str() )
+        .def( py::init<std::string const&, std::string const&, worldcomm_ptr_t const&, std::string const&, ModelBaseRepository const&>(),
+              py::arg( "prefix" ),
+              py::arg( "keyword" ) = std::string( "cfpdes" ),
+              py::arg( "worldComm" ) = Environment::worldCommPtr(),
+              py::arg( "subprefix" ) = std::string( "" ),
+              py::arg( "modelRep" ) = ModelBaseRepository(),
+              "Initialize the coefficient form pdes toolbox" )
+        .def( "init", &toolbox_t::init, "initialize the heat  toolbox", py::arg( "buildModelAlgebraicFactory" ) = true )
 
         // mesh
         .def( "mesh", &toolbox_t::mesh, "get the mesh" )
@@ -65,15 +57,15 @@ void defSM(py::module &m)
         //.def( "fieldTemperaturePtr", static_cast<element_temperature_ptr_t const& (toolbox_t::*)() const>(&toolbox_t::fieldTemperaturePtr), "returns the temperature field shared_ptr" )
 
         // time stepping
-        .def("timeStepBase",static_cast<std::shared_ptr<TSBase> (toolbox_t::*)() const>(&toolbox_t::timeStepBase), "get time stepping base")
-        .def("updateTimeStep",&toolbox_t::updateTimeStep, "update time stepping")
-
+        .def( "timeStepBase", static_cast<std::shared_ptr<TSBase> ( toolbox_t::* )() const>( &toolbox_t::timeStepBase ), "get time stepping base" )
+        .def( "startTimeStep", &toolbox_t::startTimeStep, "start time stepping" )
+        .def( "updateTimeStep", &toolbox_t::updateTimeStep, "update time stepping" )
         // solve
-        .def("solve",&toolbox_t::solve, "solve the heat mechanics problem, set boolean to true to update velocity and acceleration")
-        .def("exportResults",static_cast<void (toolbox_t::*)()>(&toolbox_t::exportResults), "export the results of the heat mechanics problem")
-//        .def("exportResults",static_cast<void (toolbox_t::*)( double )>(&toolbox_t::exportResults), "export the results of the heat mechanics problem", py::arg("time"))
+        .def( "solve", &toolbox_t::solve, "solve the cfpde problem" )
+        .def( "exportResults", static_cast<void ( toolbox_t::* )()>( &toolbox_t::exportResults ), "export the results of the cfpde problem" )
+        .def( "checkResults", static_cast<bool ( toolbox_t::* )() const>( &toolbox_t::checkResults ), "check the results of the cfpde problem" )
+        //        .def("exportResults",static_cast<void (toolbox_t::*)( double )>(&toolbox_t::exportResults), "export the results of the heat mechanics problem", py::arg("time"))
         ;
-        
 }
     
 
