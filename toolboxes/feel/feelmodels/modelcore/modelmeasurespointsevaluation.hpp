0366d2c863bfd27f45f97a9d9c0c8abcbffa2d40
Vincent Chabannes <vincent.chabannes@feelpp.org>
2021-06-21 14:34:22 +0200

    work on MeasurePointsEvaluation

--- toolboxes/feel/feelmodels/modelcore/modelmeasurespointsevaluation.hpp
+++ toolboxes/feel/feelmodels/modelcore/modelmeasurespointsevaluation.hpp
@@ -59,13 +59,14 @@ class MeasurePointsEvaluation
         struct apply {
             using context_type = typename T::second_type::element_type::Context;
             using context_ptrtype = std::shared_ptr<context_type>;
-            using map_fieldname_to_nodeid_type = std::map<std::string, std::map<int,std::string>>; // fieldname -> ( (nodeIdInCtx1 -> pointName1) , (nodeIdInCtx2 -> pointName2 ), ... )
-            using map_pointsname_to_expr_nodeid_type = std::map<std::string, std::tuple< std::map<std::string,ModelExpression>, std::set<int> > >; // pointName1 -> ( ((exprname1->expr1),(exprname2->expr2),...), (nodeIdInCtx1,nodeIdInCtx2,...) )
+            //using map_fieldname_to_nodeid_type = std::map<std::string, std::map<int,std::string>>; // fieldname -> ( (nodeIdInCtx1 -> pointName1) , (nodeIdInCtx2 -> pointName2 ), ... )
+            using map_fieldname_to_nodeid_type = std::map<std::string, std::set<std::string> >; // fieldname -> ( pointName1,pointName2,...)
+            using map_pointsname_to_expr_nodeid_type = std::map<std::string, std::tuple< std::map<std::string,ModelExpression> /*,std::set<index_type>*/ > >; // pointName1 -> ( ((exprname1->expr1),(exprname2->expr2),...), (nodeIdInCtx1,nodeIdInCtx2,...) )
             // exprname -> ( (nodeIdInCtx1 -> pointName1) , (nodeIdInCtx2 -> pointName2 ), ... )
             // exprname -> ( ( pointName1 -> ( nodeIdInCtx1,nodeIdInCtx1b,...) ), ( pointName2 -> (nodeIdInCtx2,nodeIdInCtx2b,...) ) )
             // pointName1 -> ( (exprname1,exprname2,...), (nodeIdInCtx1,nodeIdInCtx2,...) ),
             // pointName1 -> ( ((expr,exprname1),(expr,exprname2),...), (nodeIdInCtx1,nodeIdInCtx2,...) )
-            using type = std::tuple<context_ptrtype, map_fieldname_to_nodeid_type, map_pointsname_to_expr_nodeid_type >;
+            using type = std::tuple<context_ptrtype, std::map<std::string,std::set<index_type>>, map_fieldname_to_nodeid_type, map_pointsname_to_expr_nodeid_type >;
         };
 
         template <typename T>
@@ -78,7 +79,7 @@ class MeasurePointsEvaluation
                 map_fieldname_to_nodeid_type mapFieldToExport;
                 for ( std::string const& s : t.first )
                     mapFieldToExport[s];
-                return typename apply_type::type( std::make_shared<context_type>( t.second->context() ), mapFieldToExport, {} );
+                return typename apply_type::type( std::make_shared<context_type>( t.second->context() ), {}, mapFieldToExport, {} );
             }
     };
 #endif
@@ -140,11 +141,15 @@ public :
             hana::for_each( M_geoContexts,
                             [&evalPoints,&fields,&ptPos,&ptCoord,&nodeAddedInGeoCtx,&ctxId](auto & x) {
                                 auto & geoctx = std::get<0>( x );
-                                auto & fieldsInCtx = std::get<1>( x );
+                                auto & ptPosNameToNodeIds = std::get<1>( x );
+                                auto & fieldsInCtx = std::get<2>( x );
+
+                                std::set<std::string> fieldsUsed;
                                 for ( std::string const& field : fields )
                                 {
                                     if ( true ) // TODO check compatibility between field/geoctx
                                     {
+#if 0
                                         if ( nodeAddedInGeoCtx.find( ctxId ) == nodeAddedInGeoCtx.end() )
                                         {
                                             int nodeIdInCtx = geoctx->nPoints();
@@ -154,10 +159,13 @@ public :
                                         //nodeAddedInCtx[ ctxId ] = nodeIdInCtx;
 
                                         fieldsInCtx[field][nodeAddedInGeoCtx.find( ctxId )->second] = ptPos.name();
+#else
+                                        fieldsUsed.insert( field );
+#endif
                                     }
                                 }
 
-                                auto & exprInCtx = std::get<2>( x );
+                                auto & exprInCtx = std::get<3>( x );
                                 std::map<std::string,ModelExpression> mapExprNameToExpr;
                                 for ( auto const& [name,exprData] : evalPoints.expressions() )
                                 {
@@ -167,18 +175,21 @@ public :
                                         mapExprNameToExpr.emplace( name, mexpr );
                                     }
                                 }
-                                if ( !mapExprNameToExpr.empty() )
+                                if ( !mapExprNameToExpr.empty() || !fieldsUsed.empty() )
                                 {
-                                    std::set<int> nodeIds;
-                                    if ( nodeAddedInGeoCtx.find( ctxId ) == nodeAddedInGeoCtx.end() )
+                                    std::set<index_type> & nodeIds = ptPosNameToNodeIds[ptPos.name()];
+                                    //std::set<index_type> nodeIds;
+                                    if ( true )//nodeAddedInGeoCtx.find( ctxId ) == nodeAddedInGeoCtx.end() )
                                     {
                                         int nodeIdInCtx = geoctx->nPoints();
                                         geoctx->add( ptCoord );
                                         nodeIds.insert( nodeIdInCtx );
                                         nodeAddedInGeoCtx[ ctxId ] = nodeIdInCtx;
                                     }
-
-                                    exprInCtx[ptPos.name()] = std::make_tuple( std::move(mapExprNameToExpr), std::move( nodeIds ) );
+                                    for ( std::string const& field : fieldsUsed )
+                                        fieldsInCtx[field].insert( ptPos.name() );
+                                    if ( !mapExprNameToExpr.empty() )
+                                        exprInCtx[ptPos.name()] = std::make_tuple( std::move(mapExprNameToExpr)/*, std::move( nodeIds )*/ );
                                 }
                                 ++ctxId;
                             });
@@ -202,8 +213,11 @@ public :
             // update gmc ctx
             hana::for_each( M_geoContexts,
                             [this,&se](auto & x) {
+                                
+                                std::get<0>( x )->updateForUse(); // TODO VINCENT!!!!!!!!!!!!!!!!!
+                                
                                 size_type dynctx=0;
-                                auto const& exprInCtx = std::get<2>( x );
+                                auto const& exprInCtx = std::get<3>( x );
                                 for ( auto const& [ptPosName, exprDataAndNodeIds ] : exprInCtx )
                                 {
                                     for ( auto const& exprData : std::get<0>( exprDataAndNodeIds ) )
@@ -236,38 +250,74 @@ public :
 #endif
             hana::for_each( M_geoContexts,
                             [this,&se,&fieldTuple,&res](auto const& x) {
+                                auto const& geoctx = std::get<0>( x );
                                 // fields
-                                hana::for_each( fieldTuple.tuple(),
-                                                [this,&x,&res](auto const& y) {
-                                                    if constexpr ( is_iterable_v<decltype(y)> )
+                                auto const& fieldsInCtx = std::get<2>( x );
+                                for ( auto const& fieldInCtx : fieldsInCtx )
+                                {
+                                    auto const& fieldName = fieldInCtx.first;
+                                    auto const& ptPosSet = fieldInCtx.second;
+                                    hana::for_each( fieldTuple.tuple(), [this,&x,&geoctx,&fieldName,&ptPosSet,&res](auto const& y)
+                                                    {
+                                                    for ( auto const& mfield : y )
+                                                    {
+#if 0
+                                                        this->evalFieldImpl( x, mfield, res );
+#else
+                                                        if ( fieldName != mfield.nameWithPrefix() )
+                                                            continue;
+                                                        //std::string fieldName = mfield.nameWithPrefix();
+                                                        // auto itFindField = fieldInCtx.find( fieldName );
+                                                        // if ( itFindField == fieldsInCtx.end() )
+                                                        //     continue;
+
+                                                        auto const& fieldFunc = mfield.field();
+
+                                                        using FieldType = std::decay_t<decltype(fieldFunc)>;
+                                                        if constexpr ( is_shared_ptr< FieldType >::value )
                                                         {
-                                                            for ( auto const& mfield : y )
+                                                            if ( !fieldFunc )
+                                                                continue;
+                                                        }
+                                                        if constexpr( std::is_same_v< typename Feel::decay_type<FieldType>::functionspace_type::mesh_type,
+                                                                      typename std::decay_t<decltype(*geoctx)>::functionspace_type::mesh_type > )
+                                                        {
+                                                            //std::set<index_type> nodeIds;
+                                                            for ( std::string const& ptPosName : ptPosSet )
                                                             {
-                                                                this->evalFieldImpl( x, mfield, res );
+                                                                auto itFindNodeIds = std::get<1>( x ).find( ptPosName );
+                                                                CHECK( itFindNodeIds != std::get<1>( x ).end() ) << "ptPosName not find";
+                                                                auto const& nodeIds = itFindNodeIds->second;
+
+                                                                std::string outputNameBase = (boost::format("Points_%1%_field_%2%")%ptPosName %fieldName).str();
+                                                                this->evalImpl( geoctx, nodeIds, idv(fieldFunc), outputNameBase, res );
                                                             }
                                                         }
-                                                    else
-                                                    {
-                                                        //this->evalFieldImpl( x,y.first,y.second,res );
+#endif
                                                     }
-                                                }); // for_each fieldTuple
+                                                    }); // for_each fieldTuple
+                                }
                                 // expressions
-                                auto const& exprInCtx = std::get<2>( x );
+                                auto const& exprInCtx = std::get<3>( x );
                                 for ( auto const& [ptPosName, exprDataAndNodeIds ] : exprInCtx )
                                 {
+                                    //auto const& nodeIds = std::get<1>( exprDataAndNodeIds );
+                                    auto itFindNodeIds = std::get<1>( x ).find( ptPosName );
+                                    CHECK( itFindNodeIds != std::get<1>( x ).end() ) << "ptPosName not find";
+                                    auto const& nodeIds = itFindNodeIds->second;
                                     for ( auto const& exprData : std::get<0>( exprDataAndNodeIds ) )
                                     {
                                         auto const& exprName = std::get<0>( exprData );
                                         auto const& mexpr = std::get<1>( exprData );
                                         std::string outputNameBase = (boost::format("Points_%1%_expr_%2%")%ptPosName %exprName).str();
-                                        hana::for_each( ModelExpression::expr_shapes, [this,&se,&x,&outputNameBase,&mexpr,&res]( auto const& e_ij )
+                                        hana::for_each( ModelExpression::expr_shapes, [this,&se,&x,&nodeIds,&outputNameBase,&mexpr,&res]( auto const& e_ij )
                                                         {
                                                             constexpr int ni = std::decay_t<decltype(hana::at_c<0>(e_ij))>::value;
                                                             constexpr int nj = std::decay_t<decltype(hana::at_c<1>(e_ij))>::value;
                                                             if ( mexpr.template hasExpr<ni,nj>() )
                                                             {
                                                                 auto theExpr = expr( mexpr.template expr<ni,nj>(), se );
-                                                                this->evalImpl( std::get<0>( x ), theExpr, outputNameBase, res );
+                                                                this->evalImpl( std::get<0>( x ), nodeIds, theExpr, outputNameBase, res );
                                                             }
                                                         });
                                     }
@@ -277,6 +327,7 @@ public :
         }
 
 private :
+#if 0
     template <typename ContextDataType,typename MFieldType>
     void
     evalFieldImpl( ContextDataType const& x, MFieldType const& mfield, std::map<std::string,double> & res )
@@ -293,10 +344,12 @@ private :
 
             auto const& fectx = std::get<0>( x );
 
-            if constexpr( std::is_same_v< typename Feel::decay_type<FieldType>::functionspace_type::Context, std::decay_t<decltype(*fectx)> > || true )
+            if constexpr( std::is_same_v< typename Feel::decay_type<FieldType>::functionspace_type::Context, std::decay_t<decltype(*fectx)> > ||
+                          std::is_same_v< typename Feel::decay_type<FieldType>::functionspace_type::mesh_type, typename std::decay_t<decltype(*fectx)>::functionspace_type::mesh_type >
+                          )
                 {
-                    auto itFindField = std::get<1>( x ).find( fieldName );
-                    if ( itFindField != std::get<1>( x ).end() && !itFindField->second.empty() )
+                    auto itFindField = std::get<2>( x ).find( fieldName );
+                    if ( itFindField != std::get<2>( x ).end() && !itFindField->second.empty() )
                     {
                         mfield.applyUpdateFunction();
                         auto expr = idv( fieldFunc );
@@ -331,17 +384,19 @@ private :
                 }
 
         }
-
+#endif
     template <typename ContextDataType,typename ExprType>
     void
-    evalImpl( ContextDataType const& ctx, ExprType const& theExpr, std::string const& outputNameBase, std::map<std::string,double> & res )
+    evalImpl( ContextDataType const& ctx, std::set<index_type> const& nodeIds, ExprType const& theExpr, std::string const& outputNameBase, std::map<std::string,double> & res )
         {
+            std::cout << "ModelMeasurePointEval evalImpl " << outputNameBase << " nodeIds " << nodeIds << std::endl;
             auto evalAtNodes = evaluateFromContext( _context=*ctx,
-                                                    _expr=theExpr );
+                                                    _expr=theExpr,
+                                                    _points_used=nodeIds );
 
             typedef typename ExprTraitsFromContext<std::decay_t<decltype(*ctx)>,std::decay_t<decltype(theExpr)>>::shape shape_type;
 
-            for ( int nodeId=0;nodeId<ctx->nPoints();++nodeId )
+            for ( int nodeId=0;nodeId<nodeIds.size()/*ctx->nPoints()*/;++nodeId )
             {
                 for ( int i=0;i<shape_type::M ;++i )
                 {
