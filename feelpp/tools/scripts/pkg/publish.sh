68065cfaba0c3e280d635fa8acfc87a4f9ee494a
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-07-26 05:34:21 +0000

    rename pkg script and publish to reprepro
    skip toolboxes
    skip mor

---
+++ feelpp/tools/scripts/pkg/publish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+version=$1
+incoming=${2:-.}
+repo=${3:-$HOME/debian}
+distribution=${4:-focal}
+channel=${5:-latest}
+component=${6:-feelpp}
+arch=${7:-amd64}
+repository=$repo/$distribution
+
+for i in ${incoming}/*.deb do
+    newpackage=`dpkg-deb -I $i | grep Package | awk '{print $2}'`
+    newversion=`dpkg-deb -I $i | grep Version | awk '{print $2}'`
+    currentpackage=`reprepro -Vb $repository -C $channel list $distribution | grep "\b$newpackage\b"`
+    if ! test  -z  "$currentpackage"; then 
+        #check version
+        currentversion=`reprepro -Vb $repository -C $channel list $distribution | grep "\b$newpackage\b" | awk '{print $3}'`
+        if test $currentversion = $newversion; then
+            # remove package before adding the new version
+            reprepro -Vb $repository -C $channel remove $distribution $newpackage
+        fi
+    fi
+    reprepro -Vb $repository -C $channel includedeb $distribution $i
+done
