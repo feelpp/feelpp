83976e71bf3b10f75b3e65799e1fad7707c8292f
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-09-20 07:57:57 +0000

    Improve Exporter python wrapper #1687

--- feelpp/pyfeelpp/feelpp/exporter.cpp
+++ feelpp/pyfeelpp/feelpp/exporter.cpp
@@ -52,7 +52,9 @@ std::shared_ptr<Exporter<MeshT,Order>> exporter_impl( std::shared_ptr<MeshT> con
     using mesh_ptr_t = std::shared_ptr<MeshT>;
     return exporter( _mesh=m, _name=n, _geo=geo );
 }
-template<typename MeshT, int Order = 1>
+
+
+template <typename MeshT, int Order = 1>
 class PyExporter : public Exporter<MeshT, Order>
 {
 public:
@@ -82,11 +84,66 @@ void defExporter(py::module &m)
     
     using exporter_t = Exporter<MeshT,Order>;
     using exporter_ptr_t = std::shared_ptr<exporter_t>;
+    using ts_t = typename exporter_t::timeset_type;
+    using step_t = typename exporter_t::step_type;
+    using step_ptr_t = typename exporter_t::step_ptrtype;
+
     using mesh_t = MeshT;
     using mesh_ptr_t = std::shared_ptr<MeshT>;
     std::string pyclass_name;
 
-    std::string suffix = std::to_string(MeshT::nDim)+"D";
+    std::string suffix = std::to_string( MeshT::nDim ) + "D";
+    pyclass_name = std::string( "TimeSet::Step" ) + suffix;
+    py::class_<step_t, step_ptr_t>( m, pyclass_name.c_str() )
+        .def( "time", &step_t::time, "get time of the step" )
+        .def( "index", &step_t::index, "get index of the step" )
+        .def( "mesh", &step_t::mesh, "get the mesh of the step" )
+        .def( "setMesh", &step_t::setMesh, "set the mesh of the step" )
+
+        // continuous
+        .def(
+            "add", []( step_ptr_t& self, std::string const& n, typename Pch_type<mesh_t, 1>::element_type const& v, std::string const& reps )
+            { self->add( n, v, reps ); },
+            "add P1 continuous Lagrange function  to exported data",
+            py::arg( "name" ), py::arg( "element" ), py::arg( "reps" ) = "" )
+
+        .def(
+            "add", []( step_ptr_t& self, std::string const& n, typename Pch_type<mesh_t, 2>::element_type const& v, std::string const& reps )
+            { self->add( n, v, reps ); },
+            "add P2 continuous Lagrange function  to exported data",
+            py::arg( "name" ), py::arg( "element" ), py::arg( "reps" ) = "" )
+
+        .def(
+            "add", []( step_ptr_t& self, std::string const& n, typename Pch_type<mesh_t, 3>::element_type const& v, std::string const& reps )
+            { self->add( n, v, reps ); },
+            "add P3 continuous Lagrange function  to exported data",
+            py::arg( "name" ), py::arg( "element" ), py::arg( "reps" ) = "" )
+
+        .def(
+            "add", []( step_ptr_t& self, std::string const& n, typename Pdh_type<mesh_t, 0>::element_type const& v, std::string const& reps )
+            { self->add( n, v, reps ); },
+            "add P0 discontinuous Lagrange function  to exported data",
+            py::arg( "name" ), py::arg( "element" ), py::arg( "reps" ) = "" )
+
+        .def(
+            "add", []( step_ptr_t& self, std::string const& n, typename Pdh_type<mesh_t, 1>::element_type const& v, std::string const& reps )
+            { self->add( n, v, reps ); },
+            "add P1 discontinuous Lagrange function  to exported data",
+            py::arg( "name" ), py::arg( "element" ), py::arg( "reps" ) = "" )
+
+        .def(
+            "add", []( step_ptr_t& self, std::string const& n, typename Pdh_type<mesh_t, 2>::element_type const& v, std::string const& reps )
+            { self->add( n, v, reps ); },
+            "add P2 discontinuous Lagrange function  to exported data",
+            py::arg( "name" ), py::arg( "element" ), py::arg( "reps" ) = "" )
+
+        .def(
+            "add", []( step_ptr_t& self, std::string const& n, typename Pdh_type<mesh_t, 3>::element_type const& v, std::string const& reps )
+            { self->add( n, v, reps ); },
+            "add P3 discontinuous Lagrange function  to exported data",
+            py::arg( "name" ), py::arg( "element" ), py::arg( "reps" ) = "" )
+
+        ;
     pyclass_name = std::string("Exporter") + suffix;
     py::class_<exporter_t,PyExporter<MeshT,1>,exporter_ptr_t>(m,pyclass_name.c_str())
         .def(py::init<>() )
@@ -105,6 +162,8 @@ void defExporter(py::module &m)
         .def("addP1d", &exporter_t::template add<typename Pdh_type<mesh_t,1>::element_type>,"add P1 discontinuous Lagrange function  to exported data", py::arg("name"),py::arg("element"),py::arg("reps")="",py::arg("enable_if")=nullptr)
         .def("addP2d", &exporter_t::template add<typename Pdh_type<mesh_t,2>::element_type>,"add P2 discontinuous Lagrange function  to exported data", py::arg("name"),py::arg("element"),py::arg("reps")="",py::arg("enable_if")=nullptr)
         .def("addP3d", &exporter_t::template add<typename Pdh_type<mesh_t,3>::element_type>,"add P3 discontinuous Lagrange function  to exported data", py::arg("name"),py::arg("element"),py::arg("reps")="",py::arg("enable_if")=nullptr)
+        .def( "step", static_cast<step_ptr_t (exporter_t::*)(double)>(&exporter_t::step), "get time step at time t associated to default timeset", py::arg("time")=0.)
+        .def( "step", static_cast<step_ptr_t (exporter_t::*)(double, int)>(&exporter_t::step), "get time step  at time t associated to timeset with index", py::arg("time")=0., py::arg("index")=1)
 
         ;
 
@@ -114,7 +173,6 @@ void defExporter(py::module &m)
            py::arg("name") = "Exporter",
            py::arg("geo") = "change_coords_only",
            "create an Exporter object" );
-        
 }
     
 PYBIND11_MODULE(_exporter, m )
@@ -123,6 +181,11 @@ PYBIND11_MODULE(_exporter, m )
 
     if (import_mpi4py()<0) return ;
     
+    py::enum_<ExporterGeometry>( m, "ExporterGeometry", py::arithmetic() )
+        .value( "STATIC", ExporterGeometry::EXPORTER_GEOMETRY_STATIC )
+        .value( "CHANGE_COORDS_ONLY", ExporterGeometry::EXPORTER_GEOMETRY_CHANGE_COORDS_ONLY )
+        .value( "CHANGE", ExporterGeometry::EXPORTER_GEOMETRY_CHANGE );
+
     defExporter<Mesh<Simplex<2>>,1>( m );
     defExporter<Mesh<Simplex<3>>,1>( m );
     defHBF( m );
