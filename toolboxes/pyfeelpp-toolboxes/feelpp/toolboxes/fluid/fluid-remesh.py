a21e8e179c526b6adb96c76bae88c72327bd0ffa
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-09-30 10:30:58 +0000

    up remesh + fix cfg

--- toolboxes/pyfeelpp-toolboxes/feelpp/toolboxes/fluid/fluid-remesh.py
+++ toolboxes/pyfeelpp-toolboxes/feelpp/toolboxes/fluid/fluid-remesh.py
@@ -13,24 +13,46 @@ e = feelpp.Environment(
 
 #    feelpp.Environment.setConfigFile('fluid/TurekHron/cfd3.cfg')
 feelpp.Environment.setConfigFile('cases/swimmers/3-sphere/2d/three_sphere_2D.cfg')
-#feelpp.Environment.setConfigFile('fluid/moving_body/gravity/cfd.cfg')
-#feelpp.Environment.setConfigFile(
-#    'cases/moving_body/gravity/cylinder_under_gravity/cylinder_under_gravity.cfg')
+#feelpp.Environment.setConfigFile('cases/moving_body/gravity/cfd.cfg')
+#feelpp.Environment.setConfigFile('cases/swimmers/spermatozoon/2d/spermatozoon_2d.cfg')
+#feelpp.Environment.setConfigFile('cases/moving_body/gravity/cylinder_under_gravity/cylinder_under_gravity.cfg')
 f = fluid(dim=2, orderVelocity=2, orderPressure=1)
 f.init()
 
+#hfar = 3
+#hclose = hfar/10
+# 
+hfar=0.2
+hclose=0.02
+# gravity/cfd
+hfar=0.2
+hclose=hfar/4
+# swimmer spermatozzon
+hfar=1
+hclose=0.75
+# 3 spheres
 hfar = 3
-hclose = hfar/10
+hclose = hfar/5
 def remesh_toolbox(f, hclose, hfar, parent_mesh):
     required_facets=["CircleLeft","CircleCenter","CircleRight"]
     required_elts=["CirLeft","CirCenter","CirRight"]
+#    required_facets=["CylinderSurface"]
+#    required_elts = ["CylinderVolume"]
+    #gravity
+    #required_facets=["wall_body"]
+    #required_elts = ["Omega_body"]
+    #spermatozoon
+#    required_facets=["Tail","Head"]
+#    required_elts = ["Swimmer"]
     Xh = feelpp.functionSpace(mesh=f.mesh())
     n_required_elts_before=feelpp.nelements(feelpp.markedelements(f.mesh(),required_elts))
     n_required_facets_before=feelpp.nfaces(feelpp.markedfaces(f.mesh(),required_facets))
     print(" . [before remesh]   n required elts: {}".format(n_required_elts_before))
     print(" . [before remesh] n required facets: {}".format(n_required_facets_before))
+#    metric=feelpp.
     metric = feelpp.gradedls(Xh, feelpp.markedfaces(Xh.mesh(), required_facets), hclose, hfar)
-    R = feelpp.remesher(mesh=f.mesh(), required_elts=required_elts, required_facets=required_facets, parent=parent_mesh)
+    #R = feelpp.remesher(mesh=f.mesh(), required_elts=required_elts, required_facets=required_facets, parent=parent_mesh)
+    R = feelpp.remesher(mesh=f.mesh(), required_elts=required_elts, required_facets=required_facets)
     R.setMetric(metric)
     new_mesh = R.execute()
     n_required_elts_after=feelpp.nelements(feelpp.markedelements(new_mesh,required_elts))
@@ -40,19 +62,27 @@ def remesh_toolbox(f, hclose, hfar, parent_mesh):
     assert(n_required_elts_before==n_required_elts_after)
     assert(n_required_facets_before==n_required_facets_after)
     f.applyRemesh(new_mesh)
+    #assert(feelpp.nfaces(feelpp.markedfaces(f.mesh(), "wall_top"))>0)
+    #assert(feelpp.nfaces(feelpp.markedfaces(f.mesh(), "wall_bottom"))>0)
+    #assert(feelpp.nfaces(feelpp.markedfaces(f.mesh(), "wall_left"))>0)
+    #assert(feelpp.nfaces(feelpp.markedfaces(f.mesh(), "wall_right"))>0)
+
 
 parent_mesh=f.mesh()
-remesh_toolbox(f, hclose, hfar )
+remesh_toolbox(f, hclose, hfar, None )
+#remesh_toolbox(f, hclose, hfar, None)
+#f.exportResults()
 f.startTimeStep()
 while not f.timeStepBase().isFinished():
-#    if q.etaQ(f.mesh()).min() < 0.6:
-    if f.timeStepBase().iteration()%1 == 0:
-        remesh_toolbox( f, hclose, hfar, parent_mesh )
+    if q.etaQ(f.mesh()).min() < 0.4:
+#    if f.timeStepBase().iteration() % 10 == 0:
+        remesh_toolbox(f, hclose, hfar, None)
     if feelpp.Environment.isMasterRank():
         print("============================================================\n")
-        print("time simulation: ", f.time(), "s \n")
+        print("time simulation: {}s iteration : {}\n".format(f.time(), f.timeStepBase().iteration()))
         print("  -- mesh quality: {}s\n".format(q.etaQ(f.mesh()).min()))
         print("============================================================\n")
     f.solve()
     f.exportResults()
+    
     f.updateTimeStep()
