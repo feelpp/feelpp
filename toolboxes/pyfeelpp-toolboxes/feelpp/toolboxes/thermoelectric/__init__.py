975f99639afcbeb82f597a4588c8f3369e9d8f93
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-10-15 21:19:33 +0000

    fix thermoelectric wrapper lib init
    [ci skip]

--- toolboxes/pyfeelpp-toolboxes/feelpp/toolboxes/thermoelectric/__init__.py
+++ toolboxes/pyfeelpp-toolboxes/feelpp/toolboxes/thermoelectric/__init__.py
@@ -1,15 +1,24 @@
 import feelpp
-import feelpp.toolboxes as *
-from _thermoelectric import *
+from feelpp.toolboxes.core import *
 
-_thermoelectrics={
-    'thermoelectric(2,1)':Thermoelectric_2DP1,
-    'thermoelectric(2,2)':Thermoelectric_2DP2,
-    'thermoelectric(3,1)':Thermoelectric_3DP1,
-    'thermoelectric(3,2)':Thermoelectric_3DP2,
-}
+has_te = False
+_thermoelectrics = None
+try:
+    from _thermoelectric import *
 
-def thermoelectric( dim=2, orderPotential=1, worldComm=None ):
+    _thermoelectrics={
+        'thermoelectric(2,1)':Thermoelectric_2DP1,
+        'thermoelectric(2,2)':Thermoelectric_2DP2,
+        'thermoelectric(3,1)':Thermoelectric_3DP1,
+        'thermoelectric(3,2)':Thermoelectric_3DP2,
+    }
+    has_te = True
+except ImportError as e:
+    print('Import feelpp.toolboxes.thermoelectric failed: Feel++ Toolbox ThermoElectric is not available')
+    pass  # module doesn't exist, deal with it.
+
+
+def thermoelectric(dim=2, orderPotential=1, worldComm=None, keyword="thermo-electric", subprefix="", modelRep=None):
     """create a thermoelectric toolbox solver
     Keyword arguments:
     dim -- the dimension (default: 2)
@@ -23,4 +32,6 @@ def thermoelectric( dim=2, orderPotential=1, worldComm=None ):
         print(key)
     if key not in _thermoelectrics:
         raise RuntimeError('Thermoelectric solver '+key+' not existing')
-    return _thermoelectrics[key]( "thermo-electric", "thermo-electric", worldComm )
+    if modelRep is None:
+        modelRep = ModelBaseRepository()
+    return _thermoelectrics[key](prefix="thermo-electric", keyword=keyword, worldComm=worldComm, subprefix="", modelRep=modelRep)
