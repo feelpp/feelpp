125a2aa900972cb39b0634e7d32f490a46ea1c9a
romainhild <romain.hild@cemosis.fr>
2021-10-18 19:30:26 +0200

    update python script

--- toolboxes/pyfeelpp-toolboxes/feelpp/toolboxes/hdg/mixedpoisson.py
+++ toolboxes/pyfeelpp-toolboxes/feelpp/toolboxes/hdg/mixedpoisson.py
@@ -1,12 +1,15 @@
 import sys
 import feelpp
-from feelpp.toolboxes.hdg import *
+from feelpp.toolboxes.core import toolboxes_options
 
 e=feelpp.Environment(sys.argv, opts=toolboxes_options("mixedpoisson", "hdg.poisson"))
+
+from feelpp.toolboxes.hdg import *
+
 
 f=mixedpoisson(dim=2,order=1)
 f.init()
 # f.printAndSaveInfo()
 f.solve()
 f.exportResults()
-
+f.checkResults()
