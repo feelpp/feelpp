46b9c08a7f408f73a7630d08a8556f765f07adfd
Thibaut METIVET <thibaut.metivet@inria.fr>
2021-10-28 10:02:31 +0200

    Improve cases management

--- toolboxes/multifluid/main_mf.cpp
+++ toolboxes/multifluid/main_mf.cpp
@@ -3,13 +3,13 @@
 namespace Feel {
 
 // Warning: OrderPNLevelset must equal the FEELPP_MODELS_LEVELSET_PN_ORDER cmake variable or you must instantiate the appropriate lib
-template< uint16_type OrderVelocity, uint16_type OrderPressure, uint16_type OrderLevelset = 1, uint16_type OrderPNLevelset = LEVELSET_PN_ORDER>
+template< int Dim, uint16_type OrderVelocity, uint16_type OrderPressure, uint16_type OrderLevelset = 1, uint16_type OrderPNLevelset = LEVELSET_PN_ORDER>
 void
 runApplicationMultiFluid()
 {
     using namespace Feel;
 
-    typedef Simplex<FEELPP_DIM,FEELPP_GEO_ORDER> convex_type;
+    typedef Simplex< Dim, FEELPP_GEO_ORDER > convex_type;
     typedef FeelModels::FluidMechanics< convex_type,
                                         Lagrange<OrderVelocity, Vectorial, Continuous, PointSetFekete>,
                                         Lagrange<OrderPressure, Scalar, Continuous, PointSetFekete>
@@ -56,8 +56,9 @@ main( int argc, char** argv )
     po::options_description multifluidoptions( "application multifluid options" );
     multifluidoptions.add( Feel::toolboxes_options("multifluid") );
     multifluidoptions.add_options()
-        ("fluid-fe-approximation", Feel::po::value<std::string>()->default_value( "P2P1" ), "fluid-fe-approximation : P2P1,P1P1 ")
-        ("levelset-fe-approximation", Feel::po::value<std::string>()->default_value( "P1" ), "levelset-fe-approximation : P1,P2 ")
+        ("case.dimension", Feel::po::value<int>()->default_value( 2 ), "dimension")
+        ("case.fm-discretization", Feel::po::value<std::string>()->default_value( "P2P1" ), "fluid discretization : P2P1")
+        ("case.ls-discretization", Feel::po::value<std::string>()->default_value( "P1" ), "levelset discretization : P1")
         ("export-velocity-surface-div", Feel::po::value<bool>()->default_value( false ), "compute and export the velocity surface divergence" )
         ;
 
@@ -67,20 +68,35 @@ main( int argc, char** argv )
                 _author="Feel++ Consortium",
                 _email="feelpp-devel@feelpp.org"));
 
+    int dim = ioption( _name="case.dimension" );
+    std::string fmDiscretization = soption(_name="case.fm-discretization");
+    std::string lsDiscretization = soption(_name="case.ls-discretization");
 
-    std::string fluid_feapprox = soption(_name="fluid-fe-approximation");
-    std::string levelset_feapprox = soption(_name="levelset-fe-approximation");
-    if ( fluid_feapprox == "P2P1" )
-    {
-        if( levelset_feapprox == "P1" )
-            runApplicationMultiFluid<2,1,1>();
-        //else if( levelset_feapprox == "P2" )
-            //runApplicationMultiFluid<2,1,2>();
-        //else if( levelset_feapprox == "P3" )
-            //runApplicationMultiFluid<2,1,3>();
-        else CHECK( false ) << "invalid levelset-feapprox " << levelset_feapprox;
-    }
-    else CHECK( false ) << "invalid fluid-feapprox " << fluid_feapprox;
+    auto dim_t = hana::make_tuple( hana::int_c<2> );
+
+    auto fmDiscretization_t = hana::make_tuple( 
+            //hana::make_tuple( "P1P1", hana::make_tuple( hana::int_c<1>, hana::int_c<1>) ),
+            hana::make_tuple( "P2P1", hana::make_tuple( hana::int_c<2>, hana::int_c<1> ) ) 
+            );
+    auto lsDiscretization_t = hana::make_tuple( 
+            hana::make_tuple( "P1", hana::make_tuple( hana::int_c<1> ) ) 
+            );
+
+    hana::for_each( 
+            hana::cartesian_product( hana::make_tuple( dim_t, fmDiscretization_t, lsDiscretization_t ) ), 
+            [&fmDiscretization, &lsDiscretization, &dim]( auto const& d )
+                {
+                    constexpr int _dim = std::decay_t<decltype(hana::at_c<0>(d))>::value;
+                    std::string const& _fmDiscretization = hana::at_c<0>( hana::at_c<1>(d) );
+                    std::string const& _lsDiscretization = hana::at_c<0>( hana::at_c<2>(d) );
+
+                    constexpr int _uorder = std::decay_t<decltype(hana::at_c<0>(hana::at_c<1>( hana::at_c<1>(d)) ))>::value;
+                    constexpr int _porder = std::decay_t<decltype(hana::at_c<1>(hana::at_c<1>( hana::at_c<1>(d)) ))>::value;
+                    constexpr int _lsorder = std::decay_t<decltype(hana::at_c<0>(hana::at_c<1>( hana::at_c<2>(d)) ))>::value;
+                    if ( dim == _dim && fmDiscretization == _fmDiscretization && lsDiscretization == _lsDiscretization )
+                        runApplicationMultiFluid<_dim, _uorder, _porder, _lsorder>();
+                } 
+            );
 
     return 0;
 }
