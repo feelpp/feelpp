7d999efff8cb05c66952ce6f728f54ea1500fa28
Thomas Saigre <saigre@math.unistra.fr>
2022-02-04 09:11:25 +0100

    update tests using parameters

--- mor/pyfeelpp-mor/tests/test_reducedbasis.py
+++ mor/pyfeelpp-mor/tests/test_reducedbasis.py
@@ -1,6 +1,8 @@
-import sys
+import shutil, os
 import pytest
 import pandas as pd
+
+FEEL_PATH = os.environ['HOME'] + '/feel'
 
 
 from feelpp.toolboxes.heat import *
@@ -10,31 +12,49 @@ import feelpp
 
 from feelpp.mor.reducedbasis.reducedbasis import *
 
-
+#        (( prefix, case, casefile, dim, use_cache, time_dependant), name     )
 cases = [
-         (('thermal-fin', '2d', 'thermal-fin.cfg', 2, False), 'thermal-fin-2d'),
-        #  (('thermal-fin', '2d', 'thermal-fin.cfg', 2, True), 'thermal-fin-2d-ts'),
-        #  (('thermal-fin', '3d', 'thermal-fin.cfg', 3, False), 'thermal-fin-3d')
+         (('thermal-fin', '2d', 'thermal-fin.cfg', 2, False, False), 'thermal-fin-2d'),
+         (('thermal-fin', '2d', 'thermal-fin.cfg', 2, True, False), 'thermal-fin-2d-cached'),
+         (('thermal-fin', '3d', 'thermal-fin.cfg', 3, False, False), 'thermal-fin-3d'),
+         (('thermal-fin', '3d', 'thermal-fin.cfg', 3, True, False), 'thermal-fin-3d-cached')
         ]
 cases_params, cases_ids = list(zip(*cases))
 
 
 
-@pytest.fixture(params=cases_params, ids=cases_ids)
-def init_toolbox(request):
-    prefix, case, casefile, dim, time_dependant = request.param
+def init_toolbox(prefix, case, casefile, dim, use_cache):
+
+    if not use_cache:
+        print('removing cache...')
+        try:
+            shutil.rmtree(FEEL_PATH + '/crbdb')
+            shutil.rmtree(FEEL_PATH + f'{prefix}/{case}')
+        except FileNotFoundError:
+            print('You asked to remove cache, but there was none, so no problem !')
+
     feelpp.Environment.setConfigFile(f'{prefix}/{case}/{casefile}')
+    feelpp.Environment.changeRepository(directory=f'{prefix}/{case}')
 
     heatBox = heat(dim=dim, order=1)
     heatBox.init()
 
-    return heatBox,dim,time_dependant
+    return heatBox, dim
 
 
-@pytest.fixture()
-def init_model(init_toolbox):
-    heatBox,dim,time_dependant = init_toolbox
-    model = toolboxmor(dim=dim, time_dependent=time_dependant)
+
+def init_model(prefix, case, casefile, dim, use_cache, time_dependent):
+    heatBox, dim = init_toolbox(prefix, case, casefile, dim, use_cache)
+    model = toolboxmor(dim=dim, time_dependent=time_dependent)
+
+    Dmu = model.parameterSpace()
+    mubar = Dmu.element()
+
+    modelProperties = heatBox.modelProperties()
+    param = modelProperties.parameters()
+    for p in param:
+        assert(p[1].hasMinMax())    # check that parameters can vary
+        mubar.setParameterNamed(p[1].name(), p[1].value())
 
     model.setFunctionSpaces(Vh=heatBox.spaceTemperature())
 
@@ -57,7 +77,6 @@ def init_model(init_toolbox):
     model.setAssembleMDEIM(fct=assembleMDEIM)
 
     model.initModel()
-
 
     heatBoxDEIM = heat(dim=dim, order=1)
     meshDEIM = model.getDEIMReducedMesh()
@@ -88,27 +107,17 @@ def init_model(init_toolbox):
     model.postInitModel()
     model.setInitialized(True)
 
-    return heatBox, model, time_dependant
+    return heatBox, model, time_dependent, mubar
 
 
-
-def test_init_environment(init_model):
-    heatBox, model, time_dependant = init_model
-
-    Dmu = model.parameterSpace()
-    mubar = Dmu.element()
-
-    modelProperties = heatBox.modelProperties()
-    param = modelProperties.parameters()
-    for p in param:
-        print(p[1].name(), p[1].hasMinMax())
-        assert(p[1].hasMinMax())    # check that parameters can vary
-        mubar.setParameterNamed(p[1].name(), p[1].value())
+@pytest.mark.parametrize("prefix,case,casefile,dim,use_cache,time_dependent", cases_params, ids=cases_ids)
+def test_init_environment(prefix, case, casefile, dim, use_cache, time_dependent):
+    heatBox, model, time_dependent, mubar = init_model(prefix, case, casefile, dim, use_cache, time_dependent)
 
     decomposition = model.getAffineDecomposition()
-    assert len(decomposition) == [2,3][time_dependant]
+    assert len(decomposition) == [2,3][time_dependent]
 
-    return decomposition
+    # return decomposition
 
 
 
