ba673e32d66bb856234a751500b5b2ef63e62ca4
Thomas Saigre <saigre@math.unistra.fr>
2022-02-11 19:08:54 +0100

    attemp to fix test_mor [ci skip]

--- mor/pyfeelpp-mor/tests/test_mor.py
+++ mor/pyfeelpp-mor/tests/test_mor.py
@@ -9,11 +9,11 @@ from feelpp.toolboxes.core import *
 from feelpp.mor import *
 
 cases = [
-         (('thermal-fin', '2d', 'thermal-fin.cfg', 2,
+         (('thermal-fin/2d/thermal-fin.cfg', 2,
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01},
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01},
             {"k_1": 10, "k_2": 10, "k_3": 10, "k_4": 10, "k_0": 1, "Bi": 1}), 'thermal-fin-2d'),
-         (('thermal-fin', '3d', 'thermal-fin.cfg', 3,
+         (('thermal-fin/3d/thermal-fin.cfg', 3,
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01},
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01},
             {"k_1": 10, "k_2": 10, "k_3": 10, "k_4": 10, "k_0": 1, "Bi": 1}), 'thermal-fin-3d'),
@@ -25,25 +25,35 @@ mumin_th = {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01
 mumax_th = {"k_1": 10, "k_2": 10, "k_3": 10, "k_4": 10, "k_0": 1, "Bi": 1}
 
 
-def init_toolbox(prefix, case, casefile, dim):
+def init_toolbox(casefile, dim):
 
-    feelpp.Environment.setConfigFile(f'{prefix}/{case}/{casefile}')
-    feelpp.Environment.changeRepository(directory=f'{prefix}/{case}')
+    feelpp.Environment.setConfigFile(casefile)
+    feelpp.Environment.changeRepository(casefile)
 
     heatBox = heat(dim=dim, order=1)
     heatBox.init()
 
-    return heatBox, dim
+    return heatBox
 
 
 
-@pytest.mark.parametrize("prefix,case,casefile,dim,mubar_th,mumin_th,mumax_th", cases_params, ids=cases_ids)
-def test_init_environment(prefix, case, casefile, dim, mubar_th, mumin_th, mumax_th,  init_feelpp):
+@pytest.mark.parametrize("casefile,dim,mubar_th,mumin_th,mumax_th", cases_params, ids=cases_ids)
+def test_init_from_ModelPropeties(casefile, dim, mubar_th, mumin_th, mumax_th,  init_feelpp):
+    """Tests the initialisation from ModelProperties
+
+    Args:
+        casefile (string): path to config file
+        dim (int): dimention of the case
+        mubar_th (dict): dict containing the true values of mu_bar (="value" field in the json)
+        mumin_th (dict): true minimal values for parameters (="min" field in the json)
+        mumax_th (dict): true maximal values for parameters (="max" fiels in the json)
+        init_feelpp (_fixture_):
+    """
     e = init_feelpp
-    heatBox = init_toolbox(prefix, case, casefile, dim)
+    heatBox = init_toolbox(casefile, dim)
 
-    # modelProperties = heatBox.modelProperties()
-    model = toolboxmor(dim=dim, time_dependent=False)
+    modelProperties = heatBox.modelProperties()
+    Dmu = feelpp.mor.ParameterSpace(modelProperties)
 
     Dmu = model.parameterSpace()
     mubar = Dmu.element()
