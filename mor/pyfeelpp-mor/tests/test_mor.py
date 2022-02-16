8f5a9b4c6105d56feb78a052184091000ff6777b
Thomas Saigre <saigre@math.unistra.fr>
2022-02-16 12:05:57 +0100

    fix test #1777 Now it works ! /cc @romainhild

--- mor/pyfeelpp-mor/tests/test_mor.py
+++ mor/pyfeelpp-mor/tests/test_mor.py
@@ -10,15 +10,15 @@ from feelpp.mor import *
 
 # desc : (('path/to/cfg/file', dimension, {dict of mubar values in the model}, {mumin}, {mumax}), 'name-of-the-test')
 cases = [
-         (('thermal-fin/2d/thermal-fin.cfg', 2,
+         (('testcase/thermal-fin/2d/thermal-fin.cfg', 2,
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01},
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01},
             {"k_1": 10, "k_2": 10, "k_3": 10, "k_4": 10, "k_0": 1, "Bi": 1}), 'thermal-fin-2d'),
-         (('thermal-fin/3d/thermal-fin.cfg', 3,
+         (('testcase/thermal-fin/3d/thermal-fin.cfg', 3,
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01},
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1, "k_0": 1, "Bi": 0.01},
             {"k_1": 10, "k_2": 10, "k_3": 10, "k_4": 10, "k_0": 1, "Bi": 1}), 'thermal-fin-3d'),
-         (('testcase/thermal-fin.cfg', 3,
+         (('testcase/testcase/test.cfg', 3,
             {"k_1": 5, "k_2": 2, "k_3": 10, "k_4": 0.1},
             {"k_1": 0.1, "k_2": 0.1, "k_3": 0.1, "k_4": 0.1},
             {"k_1": 10, "k_2": 10, "k_3": 10, "k_4": 10}), 'test-case'),
@@ -32,7 +32,6 @@ def init_toolbox(casefile, dim):
     feelpp.Environment.changeRepository(casefile)
 
     heatBox = heat(dim=dim, order=1)
-    heatBox.init()
 
     return heatBox
 
