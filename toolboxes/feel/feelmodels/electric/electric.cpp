2b55da04d8a3e76f6518d2cdd964e35b0f8e217c
Vincent Chabannes <vincent.chabannes@feelpp.org>
2022-03-08 09:35:44 +0100

    fix mesh setup

--- toolboxes/feel/feelmodels/electric/electric.cpp
+++ toolboxes/feel/feelmodels/electric/electric.cpp
@@ -80,6 +80,8 @@ ELECTRIC_CLASS_TEMPLATE_TYPE::initMesh()
     this->log("Electric","initMesh", "start");
     this->timerTool("Constructor").start();
 
+    if ( auto ptMeshes = this->modelProperties().pTree().get_child_optional("Meshes") )
+        super_type::super_model_meshes_type::setup( *ptMeshes, {this->keyword()} );
     if ( this->doRestart() )
         super_type::super_model_meshes_type::setupRestart( this->keyword() );
     super_type::super_model_meshes_type::updateForUse<mesh_type>( this->keyword() );
@@ -130,8 +132,8 @@ ELECTRIC_CLASS_TEMPLATE_TYPE::init( bool buildModelAlgebraicFactory )
         M_materialsProperties->updateForUse( this->modelProperties().materials() );
     }
 
-    if ( !this->mesh() )
-        this->initMesh();
+    //if ( !this->mesh() )
+    this->initMesh();
 
     this->materialsProperties()->addMesh( this->mesh() );
 
--- toolboxes/feel/feelmodels/electric/electricboundaryconditions.hpp
+++ toolboxes/feel/feelmodels/electric/electricboundaryconditions.hpp
@@ -1,8 +1,8 @@
 /* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=cpp:et:sw=4:ts=4:sts=4
  */
 
-#ifndef FEELPP_TOOLBOXES_HEAT_HEATBOUNDARYCONDITIONS_HPP
-#define FEELPP_TOOLBOXES_HEAT_HEATBOUNDARYCONDITIONS_HPP
+#ifndef FEELPP_TOOLBOXES_ELECTRIC_ELECTRICBOUNDARYCONDITIONS_HPP
+#define FEELPP_TOOLBOXES_ELECTRIC_ELECTRICBOUNDARYCONDITIONS_HPP
 
 #include <feel/feelmodels/modelcore/genericboundaryconditions.hpp>
 
