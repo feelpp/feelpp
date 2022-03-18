a800fa099ef28932a7ea3b441506ff4ec9901069
Vincent Chabannes <vincent.chabannes@feelpp.org>
2022-03-18 20:26:26 +0100

    fix heat/electric/thermoelectric

--- toolboxes/feel/feelmodels/electric/electric.cpp
+++ toolboxes/feel/feelmodels/electric/electric.cpp
@@ -80,9 +80,9 @@ ELECTRIC_CLASS_TEMPLATE_TYPE::initMesh()
     this->log("Electric","initMesh", "start");
     this->timerTool("Constructor").start();
 
-    if ( auto ptMeshes = this->modelProperties().pTree().get_child_optional("Meshes") )
-        super_type::super_model_meshes_type::setup( *ptMeshes, {this->keyword()} );
-    if ( this->doRestart() )
+    if ( this->modelProperties().jsonData().contains("Meshes") )
+        super_type::super_model_meshes_type::setup( this->modelProperties().jsonData().at("Meshes"), {this->keyword()} );
+     if ( this->doRestart() )
         super_type::super_model_meshes_type::setupRestart( this->keyword() );
     super_type::super_model_meshes_type::updateForUse<mesh_type>( this->keyword() );
 
@@ -119,6 +119,8 @@ ELECTRIC_CLASS_TEMPLATE_TYPE::init( bool buildModelAlgebraicFactory )
 {
     this->log("Electric","init", "start" );
     this->timerTool("Constructor").start();
+
+    this->initModelProperties();
 
     // physics
     this->initPhysics( this->shared_from_this(), this->modelProperties().models() );
