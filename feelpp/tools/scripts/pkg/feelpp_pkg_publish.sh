d2fc8e81855220a03d8550f8d6a05372c5f1aec0
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-04-03 08:30:52 +0200

    --verbose
    [ci skip]

--- feelpp/tools/scripts/pkg/feelpp_pkg_publish.sh
+++ feelpp/tools/scripts/pkg/feelpp_pkg_publish.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 set -eo pipefail
-set -x
+#set -x
 source $(dirname $0)/feelpp_pkg_common.sh
 
 echo $BUILDKITE_PASSPHRASE > $(pwd)/pp
-cat $(pwd)/pp
 aptly publish update -batch -passphrase-file=pp -force-overwrite ${DIST} s3:apt.feelpp.org:${FLAVOR}
 rm $(pwd)/pp
