6513e4ca22c1ae692b76120f87dd554c9b6b93da
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-06-08 00:32:25 +0200

    up pkg build
    [ci skip]

--- feelpp/tools/scripts/pkg/feelpp_pbuilder.sh
+++ feelpp/tools/scripts/pkg/feelpp_pbuilder.sh
@@ -19,7 +19,7 @@ wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc  | apt-key add
 wget -O - http://apt.feelpp.org/apt.gpg | apt-key add -
 
 echo "--- add-apt-repository  "
-add-apt-repository  'deb [trusted=yes] http://apt.feelpp.org/$FLAVOR $DIST $CHANNEL'
+add-apt-repository  'deb [trusted=yes] http://apt.feelpp.org/$FLAVOR/$DIST $DIST $CHANNEL'
 add-apt-repository  'deb https://apt.kitware.com/ubuntu/ $DIST main'
 if [ "$DIST" = "bullseye" ]; then
 add-apt-repository  'deb http://deb.debian.org/debian $DIST-backports main'
