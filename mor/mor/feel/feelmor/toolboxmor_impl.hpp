eda85ebbb97921833ef7a0391440d8e286db70f2
Thomas Saigre <saigre@math.unistra.fr>
2022-07-15 17:39:04 +0200

    up modification missed in 75f118b8963456bae2acbd17680b1587a5801597
    [ci skip]

--- mor/mor/feel/feelmor/toolboxmor_impl.hpp
+++ mor/mor/feel/feelmor/toolboxmor_impl.hpp
@@ -3,40 +3,6 @@
 namespace Feel
 {
 
-po::options_description
-makeToolboxMorOptions()
-{
-    po::options_description options( "ToolboxMor options" );
-    options.add_options()
-        ( "toolboxmor.filename", po::value<std::string>()->default_value("toolbox.json"), "json file defining the parameters" )
-        ( "toolboxmor.trainset-deim-size", po::value<int>()->default_value(40), "size of the deim trainset" )
-        ( "toolboxmor.trainset-mdeim-size", po::value<int>()->default_value(40), "size of the mdeim trainset" )
-        ( "toolboxmor.test-deim", po::value<bool>()->default_value(false), "test deim decomposition" )
-        ;
-    options.add(crbOptions())
-        .add(crbSEROptions())
-        .add(eimOptions())
-        .add(podOptions())
-        // .add(deimOptions("vec"))
-        // .add(deimOptions("mat"))
-        .add(backend_options("backend-primal"))
-        .add(backend_options("backend-dual"))
-        .add(backend_options("backend-l2"))
-        .add(bdf_options("OpusHeatTB"));
-    return options;
-}
-AboutData
-makeToolboxMorAbout( std::string const& str )
-{
-    Feel::AboutData about( str.c_str(),
-                           str.c_str(),
-                           "0.1",
-                           "toolboxmor",
-                           Feel::AboutData::License_GPL,
-                           "Copyright (C) 2009-2014 Feel++ Consortium");
-    return about;
-}
-
 template<typename ToolboxType>
 typename DeimMorModelToolbox<ToolboxType>::deim_function_type
 DeimMorModelToolbox<ToolboxType>::deimFunction()
