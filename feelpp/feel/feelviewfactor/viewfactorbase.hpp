28c679c7103e4ee9b76796ded6ee801014d0fe24
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-07-29 13:00:33 +0200

    up Compute ray tracing view factor for RHT #1892
    - work on add unobstructed view factor computation #1910
    - work on Add design of view factor classes #1915
    - add tests (still not working but compiling)

--- feelpp/feel/feelviewfactor/viewfactorbase.hpp
+++ feelpp/feel/feelviewfactor/viewfactorbase.hpp
@@ -20,23 +20,30 @@ namespace Feel
  * 
  * 
  */
+template <typename MeshType>
 class ViewFactorBase
 {
 public:
     using value_type = double;
+    using mesh_t = MeshType;
+    using mesh_ptrtype = std::shared_ptr<mesh_t>;
+    using mesh_ptr_t = mesh_ptrtype;
+    using mesh_const_ptrtype = std::shared_ptr<mesh_t const>;
+
     ViewFactorBase() = default;
-    ViewFactorBase( std::vector<std::string> const& list_of_bdys ) 
-        : 
-        list_of_bdys_( list_of_bdys ),
-        vf_( list_of_bdys.size(), list_of_bdys.size() ),
-        areas_( list_of_bdys.size() )
-        {}
+    ViewFactorBase( mesh_ptr_t const& mesh, nl::json const& specs )
+        : mesh_( mesh ),
+          j_( specs ),
+          list_of_bdys_( specs["markers"].get<std::vector<std::string>>() ),
+          vf_( list_of_bdys_.size(), list_of_bdys_.size() ),
+          areas_( list_of_bdys_.size() )
+    {}
     ViewFactorBase( const ViewFactorBase& ) = default;
     ViewFactorBase( ViewFactorBase&& ) = default;
     ViewFactorBase& operator=( const ViewFactorBase& ) = default;
     ViewFactorBase& operator=( ViewFactorBase&& ) = default;
     virtual ~ViewFactorBase() = default;
-    virtual void init( std::vector<std::string> const& list_of_bdys ) { list_of_bdys_ = list_of_bdys; }
+//    virtual void init( std::vector<std::string> const& list_of_bdys ) { list_of_bdys_ = list_of_bdys; }
 
     /// this function computes the deviation from reciprocity defined as Fij - Aj/Ai * Fji
     value_type devReciprocity( unsigned int i, unsigned int j ) const;
@@ -44,7 +51,31 @@ public:
     /// this function computes the maximum absolute value of the deviation from reciprocity
     Real maxDevReciprocity() const;
 
-  private:
+    /**
+     * @brief compute the view factor matrix
+     * 
+     */
+    virtual void compute() = 0;
+
+    /**
+     * @brief get the view factor matrix
+     * 
+     */
+    eigen_matrix_xx_type<value_type> const& viewFactors() const { return vf_; }
+
+    /**
+     * @brief get the area matrix
+     * 
+     */
+    eigen_vector_x_col_type<value_type> const& areas() const { return areas_; }
+    
+  protected:
+
+    /// the mesh
+    mesh_ptr_t mesh_;
+
+    /// the json object
+    nl::json j_;
 
     //! list of boundary markers where the view factor is computed
     std::vector<std::string> list_of_bdys_;
@@ -61,6 +92,20 @@ public:
     //! normalize view factors so that vf_.array().rowwise().sum() == 1
     bool vf_normalize_ = true;
 
+    inline static constexpr value_type exponent_ = (mesh_t::nDim==2)?1:2;
+    inline static constexpr value_type divisor_ = (mesh_t::nDim==2)?2:M_PI;
 };
 
+template <typename MeshType>
+typename ViewFactorBase<MeshType>::value_type
+ViewFactorBase<MeshType>::devReciprocity( unsigned int i, unsigned int j ) const
+{
+    return vf_( i, j ) - areas_( j ) / areas_( i ) * vf_( j, i );
+}
+template <typename MeshType>
+typename ViewFactorBase<MeshType>::value_type
+ViewFactorBase<MeshType>::maxDevReciprocity() const
+{
+    return 1.;//vf_.array().rowwise()
+}
 } // namespace Feel
