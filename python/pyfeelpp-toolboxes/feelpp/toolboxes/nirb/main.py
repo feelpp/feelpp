6265568d037ab899b757db0feb16ea16883738d2
Thomas Saigre <saigre@math.unistra.fr>
2022-09-02 11:20:15 +0200

    fix nirb execution and add toolbox fluid #1912 /cc @alielarif [ci skip]

--- python/pyfeelpp-toolboxes/feelpp/toolboxes/nirb/main.py
+++ python/pyfeelpp-toolboxes/feelpp/toolboxes/nirb/main.py
@@ -1,31 +1,42 @@
 import sys, os
-import feelpp.toolboxes.core as core
 from nirb import *
+import feelpp.toolboxes.core as core
 
 
 if __name__ == "__main__":
+
+    argc = len(sys.argv)
+    if argc > 1:
+        type_tb = sys.argv[1]
+    else:
+        type_tb = "heat"
 
     PWD = os.getcwd()
 
     # set the feelpp environment
     config = feelpp.globalRepository("nirb")
-    e = feelpp.Environment(sys.argv, opts = core.toolboxes_options("heat"), config=config)
+    e = feelpp.Environment(sys.argv, opts = core.toolboxes_options(type_tb), config=config)
 
     # fineness of two grids
     H = 0.1
     h = H**2
 
     # load the model
-    cfg_path = os.path.join(PWD, "nirb/square/square.cfg")
-    geo_path = os.path.join(PWD, "nirb/square/square.geo")
-    model_path = os.path.join(PWD,"nirb/square/square.json")
+    if type_tb == "heat":
+        cfg_path = os.path.join(PWD, "nirb/square/square.cfg")
+        geo_path = os.path.join(PWD, "nirb/square/square.geo")
+        model_path = os.path.join(PWD,"nirb/square/square.json")
+    else:
+        cfg_path = os.path.join(PWD, "/home/saigre/Documents/code/feelpp/toolboxes/fluid/cases/lid-driven-cavity/cfd2d.cfg")
+        geo_path = os.path.join(PWD, "/home/saigre/Documents/code/feelpp/toolboxes/fluid/cases/lid-driven-cavity/cfd2d.geo")
+        model_path = os.path.join(PWD,"/home/saigre/Documents/code/feelpp/toolboxes/fluid/cases/lid-driven-cavity/cfd2d.json")
 
     e.setConfigFile(cfg_path)
 
     model = loadModel(model_path)
 
-    tbCoarse = setToolbox(H, geo_path, model)
-    tbFine = setToolbox(h, geo_path, model)
+    tbCoarse = setToolbox(H, geo_path, model, type_tb=type_tb)
+    tbFine = setToolbox(h, geo_path, model, type_tb=type_tb)
 
     Dmu = loadParameterSpace(model_path)
 
@@ -39,24 +50,24 @@ if __name__ == "__main__":
     tbFine.solve()
     tbCoarse.solve()
 
-    temperatureFine = tbFine.fieldTemperature()
-    temperatureCorase = tbCoarse.fieldTemperature()
+    fieldFine = getField(tbFine, type_tb)
+    fieldCoarse = getField(tbCoarse, type_tb)
 
-    print("Size of coarse space :", temperatureCorase.size())
-    print("Size of fine space   :", temperatureFine.size())
+    print("Size of coarse space :", fieldCoarse.size())
+    print("Size of fine space   :", fieldFine.size())
 
-    I1 = createInterpolator(tbFine, tbCoarse)
-    temperatureInterpolate = I1.interpolate(temperatureFine)
+    I1 = createInterpolator(tbFine, tbCoarse, type_tb=type_tb)
+    temperatureInterpolate = I1.interpolate(fieldFine)
 
     print("Interpolation Fine -> Coarse")
     print("  Size of interpolated :", temperatureInterpolate.size())
-    assert temperatureInterpolate.size() == temperatureCorase.size()
-    print("  Norm :", (temperatureInterpolate - temperatureCorase).to_petsc().vec().norm() )
+    assert temperatureInterpolate.size() == fieldCoarse.size()
+    print("  Norm :", (temperatureInterpolate - fieldCoarse).to_petsc().vec().norm() )
 
-    I2 = createInterpolator(tbCoarse, tbFine)
-    temperatureCoraseInterpolate = I2.interpolate(temperatureCorase)
+    I2 = createInterpolator(tbCoarse, tbFine, type_tb=type_tb)
+    fieldCoarseInterpolate = I2.interpolate(fieldCoarse)
 
     print("Interpolation Coarse -> Fine")
-    print("  Size of interpolated :", temperatureCoraseInterpolate.size())
-    assert temperatureCoraseInterpolate.size() == temperatureFine.size()
-    print('  Norm :', (temperatureCoraseInterpolate - temperatureFine).to_petsc().vec().norm())
+    print("  Size of interpolated :", fieldCoarseInterpolate.size())
+    assert fieldCoarseInterpolate.size() == fieldFine.size()
+    print('  Norm :', (fieldCoarseInterpolate - fieldFine).to_petsc().vec().norm())
