3305b3631e0678870eb3bfc6b9cd80a132c350ca
Ali Elarif <elarif@gaya.math.unistra.fr>
2022-09-14 01:17:18 +0200

    remove dependency of nirb library on mordicus and up nirb online

--- python/pyfeelpp-mor/feelpp/mor/nirb/main.py
+++ python/pyfeelpp-mor/feelpp/mor/nirb/main.py
@@ -15,11 +15,28 @@ if __name__ == "__main__":
     geo_path = f"{modelsFolder}{modelfile[toolboxesOptions]}.geo"
     model_path = f"{modelsFolder}{modelfile[toolboxesOptions]}.json"
 
-    nirb_model = NIRB(dim, H, h, toolboxesOptions, cfg_path, model_path, geo_path, doRectification=False)
+    nirb_model = NIRB(dim, H, h, toolboxesOptions, cfg_path, model_path, geo_path, doRectification=True)
 
-    nirb_model.initProblem(10)
+    nirb_model.initProblem(10,computeCoarse=True)
     nirb_model.generateOperators()
     nirb_model.generateReducedBasis()
 
-    # nirb_model.saveData()
-    nirb_model.checkOrthonormalized()
+    nirb_model.orthonormalizeL2()
+    nirb_model.orthonormalizeH1()
+    nirb_model.saveData()
+    nirb_model.checkL2Orthonormalized()
+    nirb_model.checkH1Orthonormalized()
+
+    print(f"[NIRB] Offline part Done !")
+
+    nirb_model.loadData()
+    nirb_model.getInterpSol()
+    nirb_model.getCompressedSol()
+    nirb_model.getOnlineSol() 
+    error = nirb_model.computeErrors() 
+    print("----------- Errors ----------------------")
+    print(f"Nb mode = {error[0]}")
+    print(f"L2 norm= {error[1]}")
+    print(f"Inf norm = {error[2]}")
+
+    print(f"[NIRB] Online part Done !!")
