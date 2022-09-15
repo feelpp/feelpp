89a4bc1e4f46bff921a6aa2453e29ecad5017011
Ali Elarif <elarif@gaya.math.unistra.fr>
2022-09-15 18:05:45 +0200

    Split the nirb library in two libs (online and offline)

---
+++ python/pyfeelpp-mor/feelpp/mor/nirb/nirbOnline.py
@@ -0,0 +1,58 @@
+from timeit import timeit
+from nirb import *
+from utils import WriteVecAppend 
+
+if __name__ == "__main__":
+
+    # fineness of two grids
+    H = 0.1  # CoarseMeshSize
+    h = H**2 # fine mesh size
+    dim = 2
+
+    PWD = os.getcwd()
+    toolboxesOptions='heat'
+    modelfile={'heat':'square/square', 'fluid':'lid-driven-cavity/cfd2d'}
+    modelsFolder = f"{PWD}/model/"
+    cfg_path = f"{modelsFolder}{modelfile[toolboxesOptions]}.cfg"
+    geo_path = f"{modelsFolder}{modelfile[toolboxesOptions]}.geo"
+    model_path = f"{modelsFolder}{modelfile[toolboxesOptions]}.json"
+    
+    doRectification=False 
+
+    start=timeit()
+    nirb_on = nirbOnline(dim, H, h, toolboxesOptions, cfg_path, model_path, geo_path, doRectification=doRectification)
+
+    nirb_on.loadData()
+    nirb_on.getInterpSol()
+    nirb_on.getCompressedSol()
+    nirb_on.getOnlineSol() 
+    online1 = nirb_on.onlineSol.to_petsc().vec()[:] # en commentant cette ligne ça produite des nan à la solution onlineSol après computeErrors 
+
+    error = nirb_on.computeErrors()
+
+    online2 = nirb_on.onlineSol.to_petsc().vec()[:]
+
+    print("[nirb main] diff online sol befor/after error", np.max(np.abs(online1-online2)))
+
+    print("----------- Errors ----------------------")
+    print(f"Nb mode = {error[0]}")
+    print(f"L2 norm= {error[1]}")
+    print(f"Inf norm = {error[2]}")
+
+
+    finish = timeit()
+    perf = []
+    perf.append(nirb_on.N)
+    perf.append(finish-start)
+
+    file='online_time_exec.txt'
+    WriteVecAppend(file,perf)
+
+    if doRectification:
+        file='online_error_rectif.txt'
+    else :
+        file='online_error.txt'
+    WriteVecAppend(file,error)
+
+    print(f"[NIRB] Online Elapsed time =", finish-start)
+    print(f"[NIRB] Online part Done !!")
