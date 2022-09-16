f06f8851b5fef4e2cf5656404737a81f4f9090b8
Thomas Saigre <saigre@math.unistra.fr>
2022-09-16 11:31:28 +0200

    refactor : - move function to claa ToolboxModel, - rename toolboxOption to toolboxType - add argument to save [load] in [from) another directory

--- python/pyfeelpp-mor/feelpp/mor/nirb/utils.py
+++ python/pyfeelpp-mor/feelpp/mor/nirb/utils.py
@@ -36,40 +36,6 @@ def loadModel(model_path):
     return model
 
 
-def setToolbox(h, geo_path, model, dim=2, order=2, type_tb="heat"):
-    """Set up the toolbox object for the given model and mesh
-
-    Args:
-        h (float): size of the mesh
-        geo_path (str): path to the goemetries file
-        model (str): path to the model file
-        dim (int): dimension of the mesh
-        order (int): order of the finite elements
-        type_tb (str): name of the toolbox {"heat"|"fluid"}
-
-    Returns:
-        Toolbox: toolbox object
-    """
-
-    # load meshes
-    mesh_ = feelpp.mesh(dim=2, realdim=2)
-    mesh = feelpp.load(mesh_, geo_path, h)
-
-    # set mesh and model properties
-    if type_tb == "heat":
-        tb = heat.heat(dim=dim, order=order)
-    elif type_tb == "fluid":
-        tb = fluid.fluid(dim=dim)
-    else:
-        raise ValueError("Unknown toolbox")
-
-    tb.setMesh(mesh)
-    tb.setModelProperties(model)
-
-    tb.init()
-
-    return tb
-
 def loadParameterSpace(model_path):
     """Load parameter space from given model path
 
@@ -102,61 +68,7 @@ def assembleToolbox(tb, mu):
 
     tb.updateParameterValues()
 
-def getField(toolbox, type_tb):
-    """Get field of interest from the toolbox
 
-    Args:
-        toolbox (Toolbox): Tolbox object
-        type_tb (str): name of the toolbox {"heat"|"fluid"}
-
-    Raises:
-        ValueError: Unknow toolbox
-
-    Returns:
-        feelpp_.discr.Element_*: field of the solution
-    """
-    if type_tb == "heat":
-        return toolbox.fieldTemperature()
-    elif type_tb == "fluid":
-        return toolbox.fieldVelocity()
-    else:
-        raise ValueError("Unknown toolbox")
-
-
-def getSolution(tb, mu, type_tb):
-    """Get the solution of the toolbox tb for the parameter mu
-
-    Args:
-        tb (Toolbox): Toolbox object
-        mu (parameterSpaceElement): parameter
-        type_tb (str): name of the toolbox {"heat"|"fluid"}
-
-    Returns:
-        feelpp_.discr.Element_*: field of the solution
-    """
-    assembleToolbox(tb, mu)
-    tb.solve()
-    return getField(tb, type_tb)
-
-
-def createInterpolator(domain_tb, image_tb, type_tb):
-    """Create an interpolator between two toolboxes
-
-    Args:
-        domain_tb (Toolbox): coarse toolbox
-        image_tb  (Toolbox): fine toolbox
-
-    Returns:
-        OperatorInterpolation: interpolator object
-    """
-    if type_tb == "heat":
-        Vh_image = image_tb.spaceTemperature()
-        Vh_domain = domain_tb.spaceTemperature()
-    elif type_tb == "fluid":
-        Vh_image = image_tb.spaceVelocity()
-        Vh_domain = domain_tb.spaceVelocity()
-    interpolator = fi.interpolator(domain = Vh_domain, image = Vh_image, range = image_tb.rangeMeshElements())
-    return interpolator
 
 
 ############################################################################################################
