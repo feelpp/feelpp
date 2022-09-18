74363b1b584f3d4e20d63481f059e0d72909c723
Ali Elarif <elarif@gaya.math.unistra.fr>
2022-09-19 00:27:05 +0200

    compute mean error of a sampling of parameters

--- python/pyfeelpp-mor/feelpp/mor/nirb/nirbOnline.py
+++ python/pyfeelpp-mor/feelpp/mor/nirb/nirbOnline.py
@@ -20,38 +20,70 @@ if __name__ == "__main__":
     model_path = f"{modelsFolder}{modelfile[toolboxType]}.json"
 
     e = init_feelpp_environment(toolboxType, cfg_path)
-    # start = timeit()
+    
+    start = time()
+
     doRectification=False
 
     nirb_on = nirbOnline(dim, H, h, toolboxType, cfg_path, model_path, geo_path, doRectification=doRectification)
 
     nirb_on.loadData()
-    nirb_on.getInterpSol()
-    nirb_on.getCompressedSol()
-    nirb_on.getOnlineSol()
 
-    online1 = nirb_on.onlineSol.to_petsc().vec()[:] # en commentant cette ligne ça produite des nan à la solution onlineSol après computeErrors
+    ## Compute NIRB error with one parameter 
+    """ 
+    # nirb_on.getInterpSol()
+    # nirb_on.getCompressedSol()
+    # nirb_on.getOnlineSol()
+
+    # online1 = nirb_on.onlineSol.to_petsc().vec()[:] # en commentant cette ligne ça produite des nan à la solution onlineSol après computeErrors
   
-    error = nirb_on.computeErrors()
+    # error = nirb_on.computeErrors()
     
-    online2 = nirb_on.onlineSol.to_petsc().vec()[:]
+    # online2 = nirb_on.onlineSol.to_petsc().vec()[:]
 
-    print("[nirb main] diff online sol befor/after error", np.max(np.abs(online1-online2)))
+    # print("[nirb main] diff online sol befor/after error", np.max(np.abs(online1-online2)))
 
-    print("----------- Errors ----------------------")
+    # print("----------- Errors ----------------------")
     # print(f"Nb mode = {error[0]}")
     # print(f"L2 norm= {error[1]}")
     # print(f"Inf norm = {error[2]}")
-    print('error =', error)
+    # print('error =', error)
+    """
 
 
     # finish = timeit()
     Dmu = nirb_on.Dmu
-    Nsample = 50
+    Nsample = 10
     s = Dmu.sampling()
     s.sampling(Nsample, 'log-random')
     mus = s.getVector()
 
+    ## Compute mean error of a sampling of parameters 
+    err = np.zeros((Nsample,4))
+    for i,mu in enumerate(mus):
+        uH = nirb_on.getInterpSol(mu)
+        nirb_on.getCompressedSol()
+        uHh = nirb_on.getOnlineSol()
+        print(i, 'ok')
+        online1 = nirb_on.onlineSol.to_petsc().vec()[:]
+        uh = nirb_on.getSolution(nirb_on.tbFine, mu)
+        err[i,0] = (uHh - uh).l2Norm()
+        err[i,1] = (uHh - uh).linftyNorm()
+        err[i,2] = (uH - uh).l2Norm()
+        err[i,3] = (uH - uh).linftyNorm()
+        print(i, 'okk')
+    err_mean = err.mean(axis=0)
+    error = [nirb_on.N]
+    error.append(err_mean[0])
+    error.append(err_mean[1])
+    error.append(err_mean[2])
+    error.append(err_mean[3])
+    
+    print("[NIRB online] error = ", error)
+
+        
+        
+    """
     err = np.zeros(Nsample)
     start = time()
     for i,mu in enumerate(mus):
@@ -76,8 +108,9 @@ if __name__ == "__main__":
     time_nirb_elapsed = (time_nirb_finish - time_nirb_start) / Nsample
 
     WriteVecAppend('nirbOnline_time_exec.dat', [nirb_on.N, time_toolbox_elapsed, time_nirb_elapsed])
-
     """
+   
+    """ 
     # errors = np.zeros((Nsample, 3))
     errors = []
     for i, mu in enumerate(mus):
@@ -87,12 +120,15 @@ if __name__ == "__main__":
         online1 = nirb_on.onlineSol.to_petsc().vec()[:] # en commentant cette ligne ça produite des nan à la solution onlineSol après computeErrors
 
         err = nirb_on.computeErrors()
+        errors.append(err[:5])
+
         print(err)
-        if not(err[1] > 1e-10 or err[2] > 1e-10 or err[1] == nan or err[2] == nan):
-            errors.append(err)
+        if not(err[1] > 1e-10 or err[2] > 1e-10 ):
+            errors.append(err[:5])
         # errors[i,:] = nirb_on.computeErrors()
         
-        online2 = nirb_on.onlineSol.to_petsc().vec()[:]
+        # online2 = nirb_on.onlineSol.to_petsc().vec()[:]
+    
     errors = np.array(errors)
     print(errors)
     print("\n")
@@ -103,6 +139,7 @@ if __name__ == "__main__":
     error = np.concatenate((error_min, error_mean[1:], error_max[1:]))
     """
 
+    finish = time() 
 
     if doRectification:
         file='nirb_error_rectif.dat'
