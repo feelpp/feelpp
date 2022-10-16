14777148f52f4ddb2c06cf15d3a2418cbda876f6
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-10-16 10:54:02 +0200

    improve simulate to retrieve the postprocessing values #1974
    improve doc
    get postprocessing data

--- python/pyfeelpp-toolboxes/feelpp/toolboxes/core/__init__.py
+++ python/pyfeelpp-toolboxes/feelpp/toolboxes/core/__init__.py
@@ -11,17 +11,25 @@ def simulate(toolbox, export=True, buildModelAlgebraicFactory=True,data=None):
     Parameters:
         toolbox -- a toolbox which has been configured
 
+    Returns:
+        a tuple (status,results) where status is a boolean(True if success, False otherwise) and results is a dictionary
+
     Example::
         feelpp.Environment.setConfigFile('toolboxs/laplace/l-shape/l-shape.cfg')
         toolbox = toolboxs.toolboxs(dim=2)
-        simulate(toolbox)        
+        [success,measures]=simulate(toolbox)        
+        import pandas as pd
+        df = pd.DataFrame(measures)
+        print(df.head())
     """
     toolbox.init(buildModelAlgebraicFactory)
     #toolbox.printAndSaveInfo()
+    meas=[]
     if toolbox.isStationary():
         toolbox.solve()
         if export:
             toolbox.exportResults()
+        meas.append(toolbox.postProcessMeasures().values())
     else:
         if not toolbox.doRestart():
             toolbox.exportResults(toolbox.timeInitial())
@@ -32,7 +40,8 @@ def simulate(toolbox, export=True, buildModelAlgebraicFactory=True,data=None):
                 print("time simulation: {}s/{}s with step: {}".format(toolbox.time(),toolbox.timeFinal(),toolbox.timeStep()))
                 print("============================================================\n")
             toolbox.solve()
+            meas.append(toolbox.postProcessMeasures().values())
             if export:
                 toolbox.exportResults()
             toolbox.updateTimeStep()
-    return not toolbox.checkResults()
+    return [toolbox.checkResults(),meas]
