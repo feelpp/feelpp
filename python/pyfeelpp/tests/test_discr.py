d7731d641e0d2bea8fcc728c167003cd2462be0a
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-10-16 14:21:09 +0200

    add tests for timings
    add tic/toc support in python #1976

--- python/pyfeelpp/tests/test_discr.py
+++ python/pyfeelpp/tests/test_discr.py
@@ -1,12 +1,16 @@
 import feelpp
 import sys
 import pytest
-
+from feelpp.timing import tic,toc
 
 
 def run(m, geo):
+    tic()
     m2d = feelpp.load(m, geo, 0.1)
+    toc("Mesh")
+    tic()
     Xh = feelpp.functionSpace(mesh=m2d)
+    toc("functionSpace")
 
     if feelpp.Environment.isMasterRank():
         print("Xh basisname: ", Xh.basisName())
@@ -19,11 +23,16 @@ def run(m, geo):
 
     assert m3 == m2d
 
+    tic()
     u = Xh.element()
-
+    toc("FunctionSpace::Element")
+    tic()
     u.on(range=feelpp.elements(m2d), expr=feelpp.expr("x:x"))
+    toc("FunctionSpace::Element::on")
+    tic()
     w = (2*u-u)-u
     l2_w = feelpp.normL2(range=feelpp.elements(m2d), expr=w)
+    toc("FunctionSpace::Element::normL2")
     print("norm(w)", l2_w)
     assert abs(l2_w) < 1e-12
     assert u.functionSpace() == Xh
@@ -38,8 +47,12 @@ def test_discr(init_feelpp):
     }
     feelpp.Environment.changeRepository(
         directory="pyfeelpp-tests/discr/test_2d")
+    tic()
     run( feelpp.mesh(dim=2), geo['2'] )
+    toc("run 2D")
     feelpp.Environment.changeRepository(
         directory="pyfeelpp-tests/discr/test_3d")
+    tic()
     run( feelpp.mesh(dim=3,realdim=3), geo['3'] )
+    toc("run 3D")
     
