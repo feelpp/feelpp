6cfef6679314dcd682270ff6a7b87c47a82b6f6a
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-10-29 10:37:16 +0200

    detect linux flavor closes #1981
    /cc @vincentchabannes

--- feelpp/cmake/modules/feelpp.dependencies.cmake
+++ feelpp/cmake/modules/feelpp.dependencies.cmake
@@ -141,6 +141,26 @@ else()
   set( FEELPP_ENABLE_PACKAGE_DEFAULT_OPTION ON)
 endif()
 
+function(get_linux_lsb_release_information)
+    find_program(LSB_RELEASE_EXEC lsb_release)
+    if(NOT LSB_RELEASE_EXEC)
+        message(WARNING "Could not detect lsb_release executable, can not gather required information")
+    else()
+      execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --id OUTPUT_VARIABLE LSB_RELEASE_ID_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
+      execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --release OUTPUT_VARIABLE LSB_RELEASE_VERSION_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
+      execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --codename OUTPUT_VARIABLE LSB_RELEASE_CODENAME_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
+  
+      set(LSB_RELEASE_ID_SHORT "${LSB_RELEASE_ID_SHORT}" PARENT_SCOPE)
+      set(LSB_RELEASE_VERSION_SHORT "${LSB_RELEASE_VERSION_SHORT}" PARENT_SCOPE)
+      set(LSB_RELEASE_CODENAME_SHORT "${LSB_RELEASE_CODENAME_SHORT}" PARENT_SCOPE)
+    endif()
+endfunction()
+
+if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+  get_linux_lsb_release_information()
+  message(STATUS "Linux ${LSB_RELEASE_ID_SHORT} ${LSB_RELEASE_VERSION_SHORT} ${LSB_RELEASE_CODENAME_SHORT}")
+endif()
+
 find_package(PkgConfig REQUIRED)
 
 # enable mpi mode
