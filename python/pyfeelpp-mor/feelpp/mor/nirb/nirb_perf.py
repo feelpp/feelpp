6c8a308f8d238f15f3cda937b29e57292554689f
Thomas Saigre <saigre@math.unistra.fr>
2022-11-09 11:47:00 +0100

    doc: test new type of doctstring seems more adaptated for vscode

--- python/pyfeelpp-mor/feelpp/mor/nirb/nirb_perf.py
+++ python/pyfeelpp-mor/feelpp/mor/nirb/nirb_perf.py
@@ -3,29 +3,32 @@ from petsc4py import PETSc
 
 
 def ComputeErrors(nirb_on, mu):
-    """Compute the error between nirb solution and FE one for 
-        a given parameter 
-    Args:
-        nirb_on (class): initialized nirbOnline class 
-        mu (ParameterSpaceElement) : parameter 
+    """Compute the error between nirb solution and FE one for
+        a given parameter
 
-    return : 
+    Args:
+    -----
+        nirb_on (class): initialized nirbOnline class
+        mu (ParameterSpaceElement) : parameter
+
+    Return :
+    --------
     list: containing
         - the number reduced basis N
-        - the errors L2 and Linf between nirb solution and FE fine solution
-        - the errors L2 and Linf between inteprolated FE coarse solution and FE fine solution
-    list : 
+        - the errors L2 and H1 between nirb solution and FE fine solution
+        - the errors L2 and H1 between inteprolated FE coarse solution and FE fine solution
+
     """
     l2Mat, h1Mat = nirb_on.generateOperators(h1=True)
 
     uHh= nirb_on.getOnlineSol(mu)
     uH = nirb_on.getInterpSol(mu)
     uh = nirb_on.getToolboxSolution(nirb_on.tbFine, mu)
-    
+
     error = [nirb_on.N]
 
-    error.append(nirb_on.normMat(l2Mat, uHh-uh)) # L2 norm 
-    error.append(nirb_on.normMat(h1Mat, uHh-uh)) # H1 norm 
+    error.append(nirb_on.normMat(l2Mat, uHh-uh)) # L2 norm
+    error.append(nirb_on.normMat(h1Mat, uHh-uh)) # H1 norm
     error.append(nirb_on.normMat(l2Mat, uH-uh))
     error.append(nirb_on.normMat(h1Mat, uH-uh))
 
@@ -34,28 +37,36 @@ def ComputeErrors(nirb_on, mu):
     # error.append((uH - uh).l2Norm())
     # error.append((uH - uh).to_petsc().vec().norm(PETSc.NormType.NORM_INFINITY))
 
-    return error 
+    return error
 
 def ComputeErrorsH(nirb_on, tbRef, mu, path=None, name=None):
-    """Compute the error between nirb solution and a reference one given from 
-           a very fine mesh in respect to mesh size 
+    """Compute the error between nirb solution and a reference one given from
+           a very fine mesh in respect to mesh size
 
     Args:
-        nirb_on (class nirbOnline, initialized): nirb online instance 
-        tbRef (class ToolBoxModel, initialize): reference toolbox 
-        mu (ParameterSpaceElement): parameter 
+    -----
+        nirb_on (class nirbOnline, initialized): nirb online instance
+        tbRef (class ToolBoxModel, initialize): reference toolbox
+        mu (ParameterSpaceElement): parameter
         path (str) : a path to load the reference solution
-        name (str) : the name of the reference solution 
+        name (str) : the name of the reference solution
+    
+    Return :
+    --------
+    list: containing
+        - the number reduced basis N
+        - the errors L2 and Linf between nirb solution and FE fine solution
+        - the errors L2 and Linf between inteprolated FE coarse solution and FE fine solution
     """
 
-    uHh= nirb_on.getOnlineSol(mu) # nirb solution 
-    uh = nirb_on.getToolboxSolution(nirb_on.tbFine, mu) # FE solution 
+    uHh= nirb_on.getOnlineSol(mu) # nirb solution
+    uh = nirb_on.getToolboxSolution(nirb_on.tbFine, mu) # FE solution
 
-    if (path != None) and (name != None): 
+    if (path != None) and (name != None):
         uref = tbRef.Xh.element()
         uref.load(path,name)
     else :
-        uref = tbRef.getToolboxSolution(tbRef.tbFine, mu) # get reference sol 
+        uref = tbRef.getToolboxSolution(tbRef.tbFine, mu) # get reference sol
 
     Op = tbRef.createInterpolator(tbRef.tbFine, nirb_on.tbFine)
     Uref_int = Op.interpolate(uref)
@@ -69,14 +80,17 @@ def ComputeErrorsH(nirb_on, tbRef, mu, path=None, name=None):
     return error
 
 def ComputeErrorSampling(nirb_on, Nsample=1, samplingType='log-random', Xi_test=None):
-    """Compute the errors between nirb solution and FE one for 
-        a given number of parameters 
+    """Compute the errors between nirb solution and FE one for
+        a given number of parameters
+
     Args:
-        nirb_on (class): initialized nirbOnline class 
-        Nsample (int): number of parameter. Defaults to 1.  
+    -----
+        nirb_on (class): initialized nirbOnline class
+        Nsample (int): number of parameter. Defaults to 1.
         samplingType (str, optional): type of sampling distribution. Defaults to 'log-random'.
 
-    return : 
+    Return :
+    --------
     dict: containing
         - dict['parameter'] : the index of the parameter from 0... to Nsample
         - dict['l2(u-uHn)'] : the l2 norm between FE solution (u) and the nirb solution (uHn)
@@ -109,7 +123,7 @@ def ComputeErrorSampling(nirb_on, Nsample=1, samplingType='log-random', Xi_test=
         err[i,3] = nirb_on.normMat(h1Mat, uH-uh)
 
     errors = {}
-    errors['parameter'] = [*range(Nsample)] 
+    errors['parameter'] = [*range(Nsample)]
     errors['l2(u-uHn)'] = err[:,0]
     errors['h1(u-uHn)'] = err[:,1]
     errors['l2(u-uH)'] = err[:,2]
