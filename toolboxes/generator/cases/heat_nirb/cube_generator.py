15cc74c270654b71ddcb903da6cbeeaf85a49dcd
Thomas Saigre <saigre@math.unistra.fr>
2023-01-17 13:31:44 +0100

    fix 2d template for cube case generator #2014

--- toolboxes/generator/cases/heat_nirb/cube_generator.py
+++ toolboxes/generator/cases/heat_nirb/cube_generator.py
@@ -7,10 +7,11 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--Nv", help="number of fins in vertical direction [default=3]", type=str, default="3")
 parser.add_argument("--Nh", help="number of fins in horizontal direction [default=3]", type=str, default="3")
-parser.add_argument("--L", help="width of a fin [default=1]", type=str, default="1")
-parser.add_argument("--t", help="thickness of a fin [default=1]", type=str, default="1")
+parser.add_argument("--Nd", help="number of fins in depth direction [default=3]", type=str, default="3")
+parser.add_argument("--L", help="width of a cube [default=1]", type=str, default="1")
+parser.add_argument("--h", help="height of a cube [default=1]", type=str, default="1")
+parser.add_argument("--d", help="depth of a cube (for 3D only)", type=str, default="1")
 parser.add_argument("--dim", help="dimension of the case (2 or 3) [default=2]", type=str, default="2")
-parser.add_argument("--cylinder", help="shape of fin and post (0=boxes, 1=box/cylinders, 2=cylinders) [default=0]", type=int, default=0)
 parser.add_argument("--odir", help="output directory", type=str, default=".")
 
 
@@ -27,10 +28,6 @@ if not os.path.isdir(args.odir):
 if args.dim not in ["2","3"]:
     raise ValueError("dimension must be 2 or 3")
 
-if args.dim == "3" and args.cylinder not in [0, 1, 2]:
-    raise ValueError("cylinder must be 0, 1 or 2")
-
-print(__file__)
 
 DIRPATH = os.path.dirname(__file__)
 if len(DIRPATH) != 0:
@@ -53,7 +50,7 @@ renderCfg = ""
 
 if args.dim == "2":
     ElementShape = "Rectangle"
-    ElementArgs = "{ -L + (s-1)*L, -t + (r-1)*t, 0, L, t, 0}"
+    ElementArgs = "{ s*L, r*h, 0, L, h, 0}"
     eltDim = "Surface"
     eltDimM1 = "Curve"
     step = 1
@@ -71,7 +68,7 @@ renderGeo = templateGeo.render(
     Nv = args.Nv,
     Nh = args.Nh,
     L = args.L,
-    t = args.t,
+    h = args.h,
     ElementShape = ElementShape,
     ElementArgs = ElementArgs, 
     step = step,
@@ -84,13 +81,13 @@ renderCfg = templateCfg.render(
     dim = args.dim
 )
 
-fins = list(range(1,int(args.Nv)*int(args.Nh)+1))
+fins = list(range(2,int(args.Nv)*int(args.Nh)+1))
 
 renderJson = templateJson.render(
     fins = fins,
-    dim = args.dim
-    geofilename = geofilename
-    modelfilename = modelfilename
+    dim = args.dim,
+    geofilename = meshfile,
+    modelfilename = modelfile
 )
 
 
