32d369f4f3c2e8797693fabd9ae9dc41801a2d06
Céline Van Landeghem <celine@vanlandeghem.lu>
2023-01-24 11:30:32 +0100

    Add collision testcases #1836  #1837 #1839 skip CI

---
+++ python/pyfeelpp-toolboxes/tests/test_collision.py
@@ -0,0 +1,95 @@
+import feelpp as feelpp
+import numpy as np
+import pytest
+from feelpp.toolboxes.fluid import *
+
+
+collision_cases = [
+    ("circle",'fluid/moving_body/gravity/collisions/circle.cfg',["Circle"],["Cir"]),
+    ("ellipse",'fluid/moving_body/gravity/collisions/ellipse.cfg',["Ellipse"],["Ell"])
+]
+
+def remesh(f,required_facets,required_elts):
+    new_mesh, cpt = feelpp.remesh(mesh=f.mesh(), metric="gradedls({},{})".format(0.01, 0.2), 
+    required_elts=required_elts, required_facets=required_facets, params='{"remesh":{ "verbose":-1}}')    
+    f.applyRemesh(f.mesh(),new_mesh)
+
+
+@pytest.mark.parametrize("case,casefile,required_facets,required_elts", collision_cases)
+def test_collision(case,casefile,required_facets,required_elts):
+    
+    feelpp.Environment.setConfigFile(casefile)
+    f = fluid(dim=2, orderVelocity=2, orderPressure=1)
+    f.init()
+    
+    remesh(f,required_facets,required_elts)
+
+    radius = 0.125
+    forceRange = 0.03
+    deltaT = 0.001
+
+    pos_array = []
+
+    f.addContactForceModel()
+    f.addContactForceResModel()
+    f.startTimeStep()
+    while not f.timeStepBase().isFinished():
+        
+        if (case == "circle"):
+            if (f.timeStepBase().iteration() == 20 or f.timeStepBase().iteration() == 40):
+                remesh(f,required_facets,required_elts)
+                f.addContactForceModel()
+                f.addContactForceResModel()
+
+        if (case == "ellipse"):
+            if (f.timeStepBase().iteration() == 20 or f.timeStepBase().iteration() == 40 or f.timeStepBase().iteration() == 60):
+                remesh(f,required_facets,required_elts)
+                f.addContactForceModel()
+                f.addContactForceResModel()
+
+        
+        f.solve()
+        f.exportResults()
+
+        if (f.timeStepBase().iteration() == 1):
+
+            if (case == "circle"):
+                pos = f.postProcessMeasures().values().get('Quantities_body_Circle.mass_center_1')
+                assert(pos < forceRange + radius)
+                assert(pos > radius)
+                pos_array.append(pos)
+            
+            if (case == "ellipse"):
+                pos_array.append(f.postProcessMeasures().values().get('Quantities_body_Ellipse.mass_center_1'))
+            
+        else :
+
+            if (case == "circle"):
+                pos = f.postProcessMeasures().values().get('Quantities_body_Circle.mass_center_1')
+                assert(pos < pos_array[-1])
+                assert(pos < forceRange + radius)
+                assert(pos > radius)
+                pos_array.append(pos)
+
+            if (case == "ellipse"):
+                pos = f.postProcessMeasures().values().get('Quantities_body_Ellipse.mass_center_1')
+                assert(pos < pos_array[-1])
+                pos_array.append(pos)
+
+                    
+        f.updateTimeStep()
+    
+    if (case == "circle"):
+        Uy_final = (pos_array[-1] - pos_array[-2])/deltaT
+        assert(np.abs(Uy_final) < 0.03)
+    
+    if (case == "ellipse"):
+        assert(np.abs(f.postProcessMeasures().values().get('Quantities_body_Ellipse.rigid_rotation_angles')) < 0.56)
+        Uy_final = (pos_array[-1] - pos_array[-2])/deltaT
+        assert(np.abs(Uy_final) < 0.4)
+    
+
+#test_collision("circle",'fluid/moving_body/gravity/collisions/circle.cfg',["Circle"],["Cir"])
+#test_collision("ellipse",'fluid/moving_body/gravity/collisions/ellipse.cfg',["Ellipse"],["Ell"])
+
+
