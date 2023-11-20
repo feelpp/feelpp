6461e8826ad1e7b7681a5820c84e3007f380f2a2
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2023-11-20 10:26:09 +0100

    add starting point for testing specx
    add tests for specx #2186

---
+++ testsuite/feeltasks/test_specx.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MODULE test_specx
+#include <feel/feelcore/testsuite.hpp>
+
+#include <feel/feeldiscr/pch.hpp>
+#include <feel/feelfilters/exporter.hpp>
+#include <feel/feelfilters/loadmesh.hpp>
+#include <feel/feelvf/vf.hpp>
+
+FEELPP_ENVIRONMENT_NO_OPTIONS
+
+BOOST_AUTO_TEST_SUITE( testspecx_suite )
+
+BOOST_AUTO_TEST_CASE( test_specx_1 )
+{
+    // TODO
+}
+BOOST_AUTO_TEST_SUITE_END()
