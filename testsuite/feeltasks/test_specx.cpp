af3797da16aafe68daa8b2255ac0058e37631afd
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2023-11-20 10:49:09 +0100

    add hellow world
    add tests for specx #2186

--- testsuite/feeltasks/test_specx.cpp
+++ testsuite/feeltasks/test_specx.cpp
@@ -6,12 +6,27 @@
 #include <feel/feelfilters/loadmesh.hpp>
 #include <feel/feelvf/vf.hpp>
 
+#include <specx/Data/SpDataAccessMode.hpp>
+#include <specx/Legacy/SpRuntime.hpp>
+#include <specx/Task/SpPriority.hpp>
+#include <specx/Utils/SpArrayView.hpp>
+
 FEELPP_ENVIRONMENT_NO_OPTIONS
 
 BOOST_AUTO_TEST_SUITE( testspecx_suite )
 
 BOOST_AUTO_TEST_CASE( test_specx_1 )
 {
-    // TODO
+    SpRuntime<> runtime( 2 );
+    runtime.task([]()
+                    {
+                        std::cout << "Hello World!\n";
+                    }).setTaskName("hello");
+    runtime.task([]()
+                    {
+                        std::cout << "Howdi!\n";
+                    }).setTaskName("howdi");
+    runtime.waitAllTasks();
+    runtime.stopAllThreads();
 }
 BOOST_AUTO_TEST_SUITE_END()
