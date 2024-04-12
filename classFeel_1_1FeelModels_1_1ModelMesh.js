e00fb6c77ecb5c6f865638a3ad9eccf4b97b0223
vincentchabannes <vincent.chabannes@cemosis.fr>
2024-04-12 11:46:46 +0200

    Deploying to gh-pages from @ feelpp/feelpp@03c6664a27581406edeb06c43474c58ca3eee19a 🚀

--- classFeel_1_1FeelModels_1_1ModelMesh.js
+++ classFeel_1_1FeelModels_1_1ModelMesh.js
@@ -3,6 +3,7 @@ var classFeel_1_1FeelModels_1_1ModelMesh =
     [ "MeshAdaptation", "structFeel_1_1FeelModels_1_1ModelMesh_1_1MeshAdaptation.html", "structFeel_1_1FeelModels_1_1ModelMesh_1_1MeshAdaptation" ],
     [ "basisFieldTypeSupported", "classFeel_1_1FeelModels_1_1ModelMesh.html#a85080781487c19e14685df638e713c22", null ],
     [ "metadata", "classFeel_1_1FeelModels_1_1ModelMesh.html#a0053d713dea2b785249a02da45612a92", null ],
+    [ "modelFields", "classFeel_1_1FeelModels_1_1ModelMesh.html#ae5b61ff8de66fbde1d976b0fcf190f43", null ],
     [ "removeField", "classFeel_1_1FeelModels_1_1ModelMesh.html#ae27e714bc0d477a30084415ba9c4965f", null ],
     [ "updateField", "classFeel_1_1FeelModels_1_1ModelMesh.html#a1595ba1be71c63084085577ed9b0ec77", null ],
     [ "updateField", "classFeel_1_1FeelModels_1_1ModelMesh.html#ac13418379d03d501e13df441e122395d", null ],
--- linearform_8hpp.js
+++ linearform_8hpp.js
@@ -3,5 +3,12 @@ var linearform_8hpp =
     [ "Feel::meta::LinearForm< SpaceType, VectorType, ElemContType >", "structFeel_1_1meta_1_1LinearForm.html", null ],
     [ "form1_space_t", "linearform_8hpp.html#a6e6d5b52fe05c721d3fc64af64bc818c", null ],
     [ "form1_t", "linearform_8hpp.html#ad4a59195d00415d9e309cfe6bd139267", null ],
-    [ "form1_type", "linearform_8hpp.html#ad37d9e382554de2b0edff9411b3c0b51", null ]
+    [ "form1_type", "linearform_8hpp.html#ad37d9e382554de2b0edff9411b3c0b51", null ],
+    [ "operator*", "linearform_8hpp.html#ace05c15f6b84bdc04894799aaee36f73", null ],
+    [ "operator*", "linearform_8hpp.html#a2733a0d20df66af2d212d6eddd833f1d", null ],
+    [ "operator+", "linearform_8hpp.html#ad8680c8a60a14624161567f4ba6aee55", null ],
+    [ "operator-", "linearform_8hpp.html#afb4da6044fba5f2d708f4f25d16e434f", null ],
+    [ "operator-", "linearform_8hpp.html#a2108a7680259e2e40dbdd4d00ca4311a", null ],
+    [ "operator/", "linearform_8hpp.html#a7974639d0c6dea61d387835df04b530d", null ],
+    [ "sum", "linearform_8hpp.html#a9ce3e1a3e1074b58af342779537bd193", null ]
 ];
