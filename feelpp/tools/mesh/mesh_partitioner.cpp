cd04cf91c17fc737b9fbb403986afc56c526a64f
Vincent Chabannes <vincent.chabannes@feelpp.org>
2024-06-26 12:22:15 +0200

    work on #2248

--- feelpp/tools/mesh/mesh_partitioner.cpp
+++ feelpp/tools/mesh/mesh_partitioner.cpp
@@ -27,15 +27,15 @@
 
 namespace Feel {
 
-extern template void partition<Simplex<1>>( std::vector<int> const& nParts, nl::json const& j );
-extern template void partition<Simplex<2>>( std::vector<int> const& nParts, nl::json const& j );
-extern template void partition<Simplex<2,2>>( std::vector<int> const& nParts, nl::json const& j );
-extern template void partition<Simplex<3>>( std::vector<int> const& nParts, nl::json const& j );
-extern template void partition<Simplex<3,2>>( std::vector<int> const& nParts, nl::json const& j );
-extern template void partition<Hypercube<2>>( std::vector<int> const& nParts, nl::json const& j );
-extern template void partition<Hypercube<2,2>>( std::vector<int> const& nParts, nl::json const& j );
-extern template void partition<Hypercube<3>>( std::vector<int> const& nParts, nl::json const& j );
-extern template void partition<Hypercube<3,3>>( std::vector<int> const& nParts, nl::json const& j );
+extern template void partition<Simplex<1>>( nl::json const& j );
+extern template void partition<Simplex<2>>( nl::json const& j );
+extern template void partition<Simplex<2,2>>( nl::json const& j );
+extern template void partition<Simplex<3>>( nl::json const& j );
+extern template void partition<Simplex<3,2>>( nl::json const& j );
+extern template void partition<Hypercube<2>>( nl::json const& j );
+extern template void partition<Hypercube<2,2>>( nl::json const& j );
+extern template void partition<Hypercube<3>>( nl::json const& j );
+extern template void partition<Hypercube<3,3>>( nl::json const& j );
 
 }
 
@@ -44,8 +44,8 @@ int main( int argc, char** argv )
     using namespace Feel;
     using Feel::cout;
 
-	po::options_description meshpartoptions( "Mesh Partitioner options" );
-	meshpartoptions.add_options()
+    po::options_description meshpartoptions( "Mesh Partitioner options" );
+    meshpartoptions.add_options()
         ( "dim", po::value<int>()->default_value( 3 ), "mesh dimension" )
         ( "realdim", po::value<int>(), "real dimension of mesh nodes" )
         ( "shape", po::value<std::string>()->default_value( "simplex" ), "mesh basic unit" )
@@ -56,14 +56,16 @@ int main( int argc, char** argv )
         ( "json", po::value<std::string>(), "json configuration file" )
         ( "ifile", po::value<std::string>(), "input mesh filename" )
         ( "odir", po::value<std::string>(), "output directory [optional]" )
-        ( "ofile", po::value<std::string>(), "output mesh filename [optional]" )
+        //( "ofile", po::value<std::string>(), "output mesh filename [optional]" )
         ( "remesh", po::value<bool>()->default_value( 0 ), "remesh " )
         ( "remesh.metric", po::value<std::string>()->default_value( "" ), "remesh metric expression" )
         ( "remesh.h", po::value<std::string>()->default_value( "hmax" ), "remesh h size" )
         ( "scalar_expr", po::value<std::vector<std::string>>()->default_value( {"g|sin(x):x|nodal|element"} ), "list of scalar expressions with name and representations" )
         ( "vectorial_expr", po::value<std::vector<std::string>>()->default_value( {"gv|{sin(2*pi*x),sin(2*pi*x),sin(2*pi*x)}:x|nodal|element"} ), "list of vectorial  expressions with name and representations" )
-        ( "export_pid", po::value<bool>()->default_value( false ), "export partition id" )
-		;
+        ( "export-visualization", po::value<bool>(), "export mesh with partitioning(s)" )
+        ;
+
+    auto initialCurrentPath = fs::current_path();
 
     Environment env( _argc=argc, _argv=argv,
                      _desc=meshpartoptions,
@@ -72,9 +74,23 @@ int main( int argc, char** argv )
                                    _email="feelpp-devel@feelpp.org" ),
                      _directory=".",_subdir=false );
 
+    nl::json partconfig;
+    if ( Environment::vm().count("json") )
+    {
+        fs::path jsonFilename = Environment::expand( soption(_name="json") );
+        if ( jsonFilename.is_relative() )
+            jsonFilename = initialCurrentPath/jsonFilename;
+        if ( fs::exists( jsonFilename ) )
+        {
+            Feel::cout << "reading partitioner configuration: " << jsonFilename << std::endl;
+            std::ifstream ifs( jsonFilename );
+            partconfig = nl::json::parse( ifs, nullptr, true, true );
+        }
+    }
+
     int dim = ioption(_name="dim");
     int realdim;
-    if ( Environment::vm().count("realdim"))
+    if ( Environment::vm().count("realdim") )
     {
         realdim = ioption(_name="realdim");
         CHECK( realdim >= dim && realdim <=3 ) << "Realdim must be larger thank dim and at most 3";
@@ -86,43 +102,58 @@ int main( int argc, char** argv )
     std::string shape = soption(_name="shape");
     int order = ioption(_name="order");
 
-    std::vector<int> nParts;
-    if ( Environment::vm().count("part"))
-        nParts = Environment::vm()["part"].as<std::vector<int> >();
 
-    if ( nParts.empty() )
+    if ( Environment::vm().count("part") )
     {
-        if ( Environment::isMasterRank() )
-            std::cout << "do nothing because --part is missing\n";
-        return 0;
+        std::vector<int> nParts = Environment::vm()["part"].as<std::vector<int> >();
+        auto & jPartitioner = partconfig["partitioner"];
+        jPartitioner[ "number-of-partition" ] = nParts;
     }
 
-    if ( !Environment::vm().count("ifile") )
+    if ( Environment::vm().count("ifile") )
     {
-        if ( Environment::isMasterRank() )
-            std::cout << "do nothing because --ifile is missing\n";
-        return 0;
+        fs::path ifileOption = Environment::expand( soption("ifile") );
+        if ( ifileOption.is_relative() )
+            ifileOption = initialCurrentPath/ifileOption;
+        auto & jInput = partconfig["input"];
+        jInput[ "filename" ] = fs::canonical( ifileOption ).string();
     }
-    nl::json partconfig;
-    if ( Environment::vm().count("json") && fs::exists( fs::current_path() / soption("json") ) )
+
+    if ( Environment::vm().count("odir") )
     {
-        Feel::cout << "reading partitioner configuration: " << ( fs::current_path() / soption("json") ) << std::endl;
-        std::ifstream ifs( ( fs::current_path() / soption("json") ).string() );
-        partconfig = nl::json::parse( ifs );
+        fs::path odirOption = Environment::expand( soption("odir") );
+        if ( odirOption.is_relative() )
+            odirOption = initialCurrentPath/odirOption;
+        auto & jOutput = partconfig["output"];
+        jOutput[ "directory" ] = odirOption.string();
     }
+
+    if ( Environment::vm().count("export-visualization") )
+    {
+        auto & jVisuExporter = partconfig["visualization-exporter"];
+        jVisuExporter[ "enabled" ] = boption("export-visualization");
+    }
+
     Feel::cout << "json config: " << partconfig.dump(1) << std::endl;
 
-    fs::path pathInputMesh = fs::canonical( soption("ifile") );
-    if ( !fs::exists( pathInputMesh ) )
+    if ( !partconfig.contains("input") )
     {
         if ( Environment::isMasterRank() )
-            std::cout << "do nothing because --ifile : " << pathInputMesh.string() << " does not exist\n";
+            std::cout << "do nothing because no input file" << std::endl;
         return 0;
     }
+    if ( !partconfig.contains("partitioner") )
+    {
+        if ( Environment::isMasterRank() )
+            std::cout << "do nothing because partition setup is missing" << std::endl;
+        return 0;
+    }
+
+
 
     if ( dim == 1 )
     {
-        partition<Simplex<1>>( nParts, partconfig );
+        partition<Simplex<1>>( partconfig );
     }
     else
     {
@@ -132,19 +163,19 @@ int main( int argc, char** argv )
             {
             case 2 :
                 if ( order == 1 && realdim==2)
-                    partition<Simplex<2>>( nParts, partconfig );
+                    partition<Simplex<2>>( partconfig );
                 else if ( order == 1 && realdim==3)
-                    partition<Simplex<2,1,3>>( nParts, partconfig );
+                    partition<Simplex<2,1,3>>( partconfig );
                 else if ( order==2 && realdim==2)
-                    partition<Simplex<2,2>>( nParts, partconfig );
+                    partition<Simplex<2,2>>( partconfig );
                 else if ( order==2 && realdim==3)
-                    partition<Simplex<2,2,3>>( nParts, partconfig );
+                    partition<Simplex<2,2,3>>( partconfig );
                 break;
             case 3 :
                 if ( order == 1 )
-                    partition<Simplex<3>>( nParts, partconfig );
+                    partition<Simplex<3>>( partconfig );
                 else if ( order==2 )
-                    partition<Simplex<3,2>>( nParts, partconfig );
+                    partition<Simplex<3,2>>( partconfig );
                 break;
             }
         }
@@ -154,19 +185,20 @@ int main( int argc, char** argv )
             {
             case 2 :
                 if ( order == 1 )
-                    partition<Hypercube<2>>( nParts, partconfig );
+                    partition<Hypercube<2>>( partconfig );
                 else if ( order==2 )
-                    partition<Hypercube<2,2>>( nParts, partconfig );
+                    partition<Hypercube<2,2>>( partconfig );
                 break;
             case 3 :
                 if ( order == 1 )
-                    partition<Hypercube<3>>( nParts, partconfig );
+                    partition<Hypercube<3>>( partconfig );
                 else if ( order==2 )
-                    partition<Hypercube<3,2>>( nParts, partconfig );
+                    partition<Hypercube<3,2>>( partconfig );
                 break;
             }
         }
     }
+
     return 0;
 
 }
