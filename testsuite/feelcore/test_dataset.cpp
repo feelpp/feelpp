19c4470c3050f08630fe7fa7774510cbf3eff2f8
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2024-07-18 12:14:30 +0200

    added more data
    [ci skip]

--- testsuite/feelcore/test_dataset.cpp
+++ testsuite/feelcore/test_dataset.cpp
@@ -7,12 +7,16 @@ using namespace Feel;
 
 void runSimulation( const Dataset& dataset, const std::string& configDir )
 {
+    std::cout << fmt::format( "Running dataset: {}", dataset.name ) << std::endl;
+    std::cout << fmt::format( "Description: {}", dataset.description ) << std::endl;
+    std::cout << fmt::format( "Default configuration: {}", dataset.default_configuration ) << std::endl;
+    
     for ( const auto& config : dataset.configurations )
     {
         std::string configPath = configDir + "/" + config.filename;
         if ( fs::exists( configPath ) )
         {
-            std::cout << fmt::format( "Running simulation with config file: {}", configPath ) << std::endl;
+            std::cout << fmt::format( "Running simulation {} with config file: {}", config.name, configPath ) << std::endl;
             //Feel::Environment::setConfigFile( configPath );
             //// Call Feel++ simulation functions here
             //BOOST_TEST_MESSAGE( fmt::format( "Running simulation with config file: {}", configPath ) );
@@ -20,7 +24,7 @@ void runSimulation( const Dataset& dataset, const std::string& configDir )
         else
         {
             //BOOST_TEST_MESSAGE( fmt::format( "Config file {} does not exist", configPath ) );
-            std::cerr << fmt::format( "Config file {} does not exist", configPath ) << std::endl;
+            std::cerr << fmt::format( "Config {} file {} does not exist", config.name, configPath ) << std::endl;
         }
     }
 }
