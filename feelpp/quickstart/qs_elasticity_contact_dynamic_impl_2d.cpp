c0f548e0c8b33365dd4cf26645efe32f4c4cfc08
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2024-07-19 10:30:29 +0200

    split files
    skip tests skip toolboxes skip mor skip python

--- feelpp/quickstart/qs_elasticity_contact_dynamic_impl_2d.cpp
+++ feelpp/quickstart/qs_elasticity_contact_dynamic_impl_2d.cpp
@@ -5,7 +5,4 @@ namespace Feel
 {
     template class ContactDynamic<2,1,1>;
     template class ContactDynamic<2,2,2>;
-    
-    template class ContactDynamic<3,1,1>;
-    template class ContactDynamic<3,2,2>;
 }
