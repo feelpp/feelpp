adcdda2f53046751271c171dc0047d79469b9e48
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2024-11-29 08:34:04 +0100

    fix pasphrase
    [ci skip]

--- feelpp/tools/scripts/pkg/feelpp_pkg_publish.sh
+++ feelpp/tools/scripts/pkg/feelpp_pkg_publish.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 #set -x
 source $(dirname $0)/feelpp_pkg_common.sh
 
-aptly publish update -batch -passphrase=${GPG_PASSPHRASE} -force-overwrite  -gpg-key=${GPG_KEY} ${DIST} s3:apt.feelpp.org:${FLAVOR}/${DIST}
+aptly publish update -batch -passphrase="${GPG_PASSPHRASE}" -force-overwrite  -gpg-key=${GPG_KEY} ${DIST} s3:apt.feelpp.org:${FLAVOR}/${DIST}
