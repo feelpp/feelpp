5fcbd1285a7d68ea1b5a04b4c2acbb58058e3040
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2025-02-12 13:28:12 +0000

    Update pkg-dist.yml to use GPG keys for publishing and enable debug mode in feelpp_pkg_publish.sh
    [ci skip]

--- feelpp/tools/scripts/pkg/feelpp_pkg_publish.sh
+++ feelpp/tools/scripts/pkg/feelpp_pkg_publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
-#set -x
+set -x
 source $(dirname $0)/feelpp_pkg_common.sh
 
 aptly publish update -batch -passphrase="${GPG_PASSPHRASE}" -force-overwrite  -gpg-key=${GPG_KEY} ${DIST} s3:apt.feelpp.org:${FLAVOR}/${DIST}
