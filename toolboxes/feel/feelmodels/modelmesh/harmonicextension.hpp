63c95efdab2eefca1a39ac828f2091c0fdd79988
Vincent Chabannes <vincent.chabannes@feelpp.org>
2025-03-10 22:47:59 +0100

    fix generateALEMap

--- toolboxes/feel/feelmodels/modelmesh/harmonicextension.hpp
+++ toolboxes/feel/feelmodels/modelmesh/harmonicextension.hpp
@@ -101,18 +101,10 @@ public :
     void
     generateALEMap( elem_type const & dispOnBoundary )
     {
-#if 0
-        bool useGhostEltFromExtendedStencil = this->functionSpace()->dof()->buildDofTableMPIExtended() && this->mesh()->worldComm().localSize()>1;
-        EntityProcessType entityProcess = (useGhostEltFromExtendedStencil)? EntityProcessType::ALL : EntityProcessType::LOCAL_ONLY;
-        *M_dispImposedOnBoundary = vf::project(_space=this->functionSpace(),
-                                               _range=elements(this->mesh(),entityProcess),
-                                               _expr=vf::idv(dispOnBoundary) );
-#else
-        CHECK( !this->functionSpace()->dof()->buildDofTableMPIExtended() ) << "not implemented";
-        *M_dispImposedOnBoundary = vf::project(_space=this->functionSpace(),
-                                               _range=elements(support(this->functionSpace())),
-                                               _expr=vf::idv(dispOnBoundary) );
-#endif
+        // TODO range should be only on moving boundary
+        M_dispImposedOnBoundary->on(_range=elements(support(this->functionSpace())),
+                                    _expr=vf::idv(dispOnBoundary),
+                                    _close=true );
         this->solve();
     }
 
