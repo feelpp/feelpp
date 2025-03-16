62b3ad46595070ba6400f5eed841a3f78c65f20c
Vincent Chabannes <vincent.chabannes@feelpp.org>
2025-03-16 11:39:47 +0100

    fix python tests : not use gmsh file partitioning (ci skip feelpp skip tests skip toolboxes skip mor )

--- python/pyfeelpp/feelpp/mesh.hpp
+++ python/pyfeelpp/feelpp/mesh.hpp
@@ -184,7 +184,7 @@ void defMesh(py::module &m)
         "load",
         []( mesh_ptr_t m, std::string const& n, double h, int verbose, std::string const& prefix )
         {
-            return loadMesh( _mesh=new mesh_t, _filename=n, _h=h, _verbose=verbose, _prefix=prefix );
+            return loadMesh( _mesh=new mesh_t, _filename=n, _h=h, _verbose=verbose, _prefix=prefix, _rebuild_partitions=true );
         },
         "load a mesh from a file", py::arg("mesh"), py::arg("name"), py::arg("h")=0.1, py::arg("verbose") = 1 , py::arg("prefix") = "" );
 
