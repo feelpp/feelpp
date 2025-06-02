add1a2b969b083921e6003b50aa71ffdc84c4df4
Luplz <lucaspalazzolo.pro@gmail.com>
2025-06-02 11:36:50 +0200

    Update innerstrainrates.hpp

--- python/pyfeelpp-toolboxes/feelpp/toolboxes/fluid/innerstrainrates.hpp
+++ python/pyfeelpp-toolboxes/feelpp/toolboxes/fluid/innerstrainrates.hpp
@@ -23,12 +23,16 @@ innerStrainRates(FluidMechanics &t, Pchv &u1, Pchv &u2)
     auto Xh = Pch<2>(u1.functionSpace()->mesh());;
     auto r = Xh->element();
 
+    std::cout << "Computing strain rates" << std::endl;
     auto e1 = gradv(u1)+trans(gradv(u1))/2;
     auto e2 = gradv(u2)+trans(gradv(u2))/2;
 
+    std::cout << "Computing inner product of strain rates" << std::endl;
     auto e1_inner_e2 = inner(e1,e2);
 
+    std::cout << "Evaluating inner product of strain rates" << std::endl;
     r.on( _range=elements(u1.functionSpace()->mesh()), _expr=e1_inner_e2 );
+    std::cout << "Inner product of strain rates evaluated" << std::endl;
     return r;
 
 }
@@ -40,4 +44,3 @@ saveinnerStrainRates(FluidMechanics &t, Pch &e1_inner_e2, const std::string& pat
 {
     e1_inner_e2.saveHDF5( path );
 }
-
