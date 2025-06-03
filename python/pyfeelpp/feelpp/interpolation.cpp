e6a8234d8a178fc4baa22d66931529980d238173
Luplz <lucaspalazzolo.pro@gmail.com>
2025-06-03 13:38:03 +0200

    Add Pdh_type interpolation definitions for 2D and 3D meshes

--- python/pyfeelpp/feelpp/interpolation.cpp
+++ python/pyfeelpp/feelpp/interpolation.cpp
@@ -119,10 +119,12 @@ PYBIND11_MODULE( _interpolation, m )
         defInterpolate<Pch_type<Mesh<Simplex<2>>, _order>>( m );
         //std::cout << fmt::format("-- Pchv 2D P{}", _order ) << std::endl;
         defInterpolate<Pchv_type<Mesh<Simplex<2>>, _order>>( m );
+        defInterpolate<Pdh_type<Mesh<Simplex<2>>, _order>>( m );
         // 3D
         //std::cout << fmt::format("-- Pch 3D P{}", _order ) << std::endl;
         defInterpolate<Pch_type<Mesh<Simplex<3>>, _order>>( m );
         defInterpolate<Pchv_type<Mesh<Simplex<3>>, _order>>( m );
+        defInterpolate<Pdh_type<Mesh<Simplex<3>>, _order>>( m );
     });
     
 }
