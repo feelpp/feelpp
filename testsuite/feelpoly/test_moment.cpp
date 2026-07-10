ea75a0f867924bcaddf6db8ad3558699e976397f
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2026-07-10 09:35:49 +0200

    Refactor tests for product spaces and moments in Feel++ library
    - Added new test cases for product spaces in `testsuite/feeldiscr/test_productspaces.cpp` to verify degree of freedom counts, block form compatibility, and rectangular core allocation.
    - Implemented checks for Crouzeix-Raviart finite element properties in `testsuite/feelpoly/test_moment.cpp`, ensuring correct evaluation at midpoints and barycenters for 2D and 3D elements.
    - Updated licensing information in both test files to include SPDX identifiers and copyright details.
    
    Generalize blockform2 to support distinct test and trial product spaces
    Fixes #2498

--- testsuite/feelpoly/test_moment.cpp
+++ testsuite/feelpoly/test_moment.cpp
@@ -1,25 +1,11 @@
-/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*-
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=cpp:et:sw=4:ts=4:sts=4
 
-  This file is part of the Feel library
+    SPDX-FileContributor: Christophe Prud'homme <christophe.prudhomme@feelpp.org>
 
-  Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
-       Date: 2011-12-11
+    SPDX-FileCopyrightText: 2011 Université Joseph Fourier (Grenoble I)
+    SPDX-FileCopyrightText: 2026 University of Strasbourg
 
-  Copyright (C) 2011 Université Joseph Fourier (Grenoble I)
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+    SPDX-License-Identifier: LGPL-3.0-or-later
 */
 /**
    \file test_moment.cpp
@@ -45,6 +31,87 @@ FEELPP_ENVIRONMENT_NO_OPTIONS
 BOOST_AUTO_TEST_SUITE( moment )
 
 typedef boost::mpl::list<boost::mpl::int_<2> > test_types;
+
+template<typename FE>
+void
+checkCrouzeixRaviartSimplexElement( typename FE::value_type tol )
+{
+    using namespace Feel;
+
+    FE fe;
+    typename FE::reference_convex_type ref;
+
+    static_assert( FE::nDim == 2 || FE::nDim == 3 );
+    BOOST_CHECK_EQUAL( FE::nOrder, 1 );
+    BOOST_CHECK_EQUAL( FE::nLocalDof, FE::nDim + 1 );
+    BOOST_CHECK_EQUAL( fe.nbPoints(), FE::nDim + 1 );
+    BOOST_CHECK_EQUAL( FE::nDofPerVertex, 0 );
+    BOOST_CHECK_EQUAL( FE::nDofPerVolume, 0 );
+
+    if constexpr ( FE::nDim == 2 )
+    {
+        BOOST_CHECK_EQUAL( FE::nDofPerEdge, 1 );
+        BOOST_CHECK_EQUAL( FE::nDofPerFace, 0 );
+    }
+    else
+    {
+        BOOST_CHECK_EQUAL( FE::nDofPerEdge, 0 );
+        BOOST_CHECK_EQUAL( FE::nDofPerFace, 1 );
+    }
+
+    for ( uint16_type f = 0; f < FE::nDim + 1; ++f )
+    {
+        auto const& ptsOnFace = fe.points( f );
+        BOOST_REQUIRE_EQUAL( ptsOnFace.size2(), 1 );
+        for ( uint16_type c = 0; c < FE::nDim; ++c )
+        {
+            BOOST_CHECK_SMALL( std::abs( ptsOnFace( c, 0 ) - ref.faceBarycenter( f )( c ) ), tol );
+            BOOST_CHECK_SMALL( std::abs( fe.points()( c, f ) - ref.faceBarycenter( f )( c ) ), tol );
+        }
+    }
+
+    auto evalAtDofs = FE::polyset_type::toMatrix( fe.evaluate( fe.points() ) );
+    auto dofIdentity = ublas::identity_matrix<typename FE::value_type>( evalAtDofs.size1() );
+    BOOST_CHECK_EQUAL( evalAtDofs.size1(), FE::nDim + 1 );
+    BOOST_CHECK_EQUAL( evalAtDofs.size2(), FE::nDim + 1 );
+    typename FE::value_type dofIdentityError = ublas::norm_frobenius( evalAtDofs - dofIdentity );
+    BOOST_CHECK_SMALL( dofIdentityError, tol );
+
+    typename FE::points_type samplePts( FE::nDim, FE::nDim + 2 );
+    ublas::subrange( samplePts, 0, FE::nDim, 0, FE::nDim + 1 ) = ref.vertices();
+    ublas::column( samplePts, FE::nDim + 1 ) = ref.barycenter();
+
+    auto const linearFunction = []( auto const& pts, uint16_type q )
+        {
+            typename FE::value_type value = 1.25;
+            for ( uint16_type c = 0; c < FE::nDim; ++c )
+                value += ( ( c % 2 ) ? -1.0 : 1.0 ) * ( c + 2.0 ) * pts( c, q );
+            return value;
+        };
+
+    std::vector<typename FE::value_type> dofValues( FE::nLocalDof );
+    for ( uint16_type i = 0; i < FE::nLocalDof; ++i )
+        dofValues[i] = linearFunction( fe.points(), i );
+
+    auto basisAtSamples = FE::polyset_type::toMatrix( fe.evaluate( samplePts ) );
+    BOOST_CHECK_EQUAL( basisAtSamples.size1(), FE::nLocalDof );
+    BOOST_CHECK_EQUAL( basisAtSamples.size2(), FE::nDim + 2 );
+
+    for ( uint16_type q = 0; q < samplePts.size2(); ++q )
+    {
+        typename FE::value_type interpolated = 0;
+        for ( uint16_type i = 0; i < FE::nLocalDof; ++i )
+            interpolated += dofValues[i] * basisAtSamples( i, q );
+        BOOST_CHECK_SMALL( std::abs( interpolated - linearFunction( samplePts, q ) ), tol );
+    }
+
+    ublas::vector<typename FE::matrix_type> der = fe.derivate( fe.points() );
+    for ( uint16_type c = 0; c < FE::nDim; ++c )
+    {
+        typename FE::value_type derivativeError = ublas::norm_frobenius( der[c] - fe.derivate( c ).evaluate( fe.points() ) );
+        BOOST_CHECK_SMALL( derivativeError, tol );
+    }
+}
 
 BOOST_AUTO_TEST_CASE_TEMPLATE( test_QK, T, test_types )
 {
@@ -99,15 +166,29 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( test_QK, T, test_types )
     std::cout << "d rqv/dy :" << RQv.derivate( 1, m.points() ) << "\n";
 #endif
     fem::CrouzeixRaviart<2,2,Scalar,double,Hypercube> cr;
-    std::cout << "cr :" << cr.evaluate( m.points() ) << "\n";
-    std::cout << "d cr/dx :" << cr.derivate( 0, m.points() ) << "\n";
-    std::cout << "d cr/dy :" << cr.derivate( 1, m.points() ) << "\n";
+    auto crEval = cr.evaluate( m.points() );
+    BOOST_CHECK_EQUAL( crEval.size1(), cr.nLocalDof );
+    BOOST_CHECK_EQUAL( crEval.size2(), m.points().size2() );
 
     fem::CrouzeixRaviart<2,2,Vectorial,double,Hypercube> crv;
-    std::cout << "crv :" << crv.evaluate( m.points() ) << "\n";
-    std::cout << "d crv/dx :" << crv.derivate( 0, m.points() ) << "\n";
-    std::cout << "d crv/dy :" << crv.derivate( 1, m.points() ) << "\n";
+    auto crvEval = crv.evaluate( m.points() );
+    BOOST_CHECK( crvEval.size1() > 0 );
+    BOOST_CHECK_EQUAL( crvEval.size2(), m.points().size2() );
 
+}
+
+BOOST_AUTO_TEST_CASE( crouzeix_raviart_simplex_2d_dofs_are_edge_midpoints )
+{
+    using namespace Feel;
+    using fe_type = fem::CrouzeixRaviart<2, 2, Scalar, double, Simplex>;
+    checkCrouzeixRaviartSimplexElement<fe_type>( 1e-12 );
+}
+
+BOOST_AUTO_TEST_CASE( crouzeix_raviart_simplex_3d_dofs_are_face_barycenters )
+{
+    using namespace Feel;
+    using fe_type = fem::CrouzeixRaviart<3, 3, Scalar, double, Simplex>;
+    checkCrouzeixRaviartSimplexElement<fe_type>( 1e-12 );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
